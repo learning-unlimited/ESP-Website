aa5b321da6347b7c8432534893beec4bbb6e0cf4
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-06-16 22:39:13 +0000

    - Deleted old code from the "users" app - Wrote initial auth code, though it's completely untested
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@22 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/users/models.py
+++ esp/users/models.py
@@ -1,17 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from esp.workflow.models import Controller
 
 # Create your models here.
 
-# aseering 6/15/2006: Subclassing is broken in Django, so we aren't likely to use this code as-is.  It's kept for historical reasons, until I feel like either rewriting it or deleting it, or until it works with a newer Django version.
-
-class WebUsers(User):
-    class Admin:
-        pass
-
-class GodUsers(User):
-    class Admin:
-        pass
-
-
+class UserBit(models.Model):
+    """ Grant a user bits to a controller """
+    user = models.ForeignKey(User) # User to give this permission
+    permission = models.ForeignKey(Controller) # Controller to grant access to
 
--- esp/workflow/models.py
+++ esp/workflow/models.py
@@ -1,13 +1,47 @@
 from django.db import models
+from esp.watchlists.models import Datatree, DatatreeNodeData
+from esp.users.models import UserBit
 
 # Create your models here.
 
 # Argh: No virtual classes in Python?
 class Controller:
+    def sync_to_db(self):
+        """ Record our name to the database table of Controllers.  Don't record a duplicate. """
+        if Controller.objects.filter(controller=self.__name__).count() == 0:
+            c = Controller()
+            c.controller = self.__name__
+            c.save()
+
     """ A generic virtual superclass, that all Controllers inherit from """
-    def run(self, data):
+    def run(self, data, user):
         """ Do some action based on a given chunk of data """
         pass
 
 
-        
+class WorkflowDescriptor(models.Model):
+        """ Provides an identifier for a workflow within the database;
+        workflows don't have an inherent database presence.
+
+        Links each workflow to a node in the permissions tree. """
+
+        tree = models.ForeignKey(DatatreeNodeData) # Tree node that this workflow belongs to
+        controller = models.ForeignKey(Controller)
+
+        class Admin:
+            pass
+
+class Controller(models.Model):
+    """ A registry of the names of all known subclasses of Controller.  Used by the auth system as unique identifiers of these controllers. """
+        controller = models.TextField(blank=False) # Name of the relevant controller class (value of its __name__ field).  Note that this forces Controller classes to have universally unique names.
+
+def enforce_bits(controller_class, user):
+    def call(proc, *args):
+        """ Accepts a 'run' function, its associated Controller class (is there a way to getthat information automatically, from the function?), and a user; returns a function that runs the 'run' function and returns 'true' if the user can access this Controller class, and returns 'false' otherwise. """
+        proc(args)
+        return True
+
+    if UserBit.objects.filter(permission__controller=controller_class.__name__).filter(user_pk=user.id).count() != 0:
+        return decorator(call)
+    else:
+        return lambda : False
