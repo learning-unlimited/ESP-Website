3c8be66a4351826aac2d4fa07f67b26b2e0edfde
jalonso <jalonso@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-07-01 22:50:08 +0000

    Added code to test template
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@71 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/web/views.py
+++ esp/web/views.py
@@ -1,7 +1,75 @@
 from django.shortcuts import render_to_response
 from esp.calendar.models import Event
-#from mysite.polls.models import Poll
 
-def index(request):
-	latest_event_list = Event.objects.filter().order_by('-start')
-	return render_to_response('index.html')
+class navbar:
+	navbar_data = [
+		{ 'link': '/teach/what-to-teach.html',
+		  'text': 'What You Can Teach',
+		'indent': False },
+		{ 'link': '/teach/prev-classes.html',
+		  'text': 'Previous Classes',
+		  'indent': False },
+		{ 'link': '/teach/teaching-time.html',
+		  'text': 'Time Commitments',
+		  'indent': False },
+		{ 'link': '/teach/training.html',
+		  'text': 'Teacher Training',
+		  'indent': False },
+		{ 'link': '/teach/ta.html',
+		  'text': 'Become a TA',
+		  'indent': False },
+		{ 'link': '/teach/coteach.html',
+		  'text': 'Co-Teach',
+		  'indent': False },
+		{ 'link': '/teach/reimburse.html',
+		  'text': 'Reimbursements',
+		  'indent': False },
+		{ 'link': '/programs/hssp/teach.html',
+		  'text': 'HSSP',
+		  'indent': False },
+		{ 'link': '/teach/teacherinformation.html',
+		  'text': 'Teacher Information',
+		  'indent': True },
+		{ 'link': '/programs/hssp/classreg.html',
+		  'text': 'Class Registration',
+		  'indent': True },
+		{ 'link': '/programs/hssp/summerhssp.html',
+		  'text': 'Summer HSSP',
+		  'indent': False },
+		{ 'link': '/programs/hssp/classreg.html',
+		  'text': 'Class Registration',
+		  'indent': True },
+		{ 'link': '/programs/firehose/teach.html',
+		  'text': 'Firehose',
+		  'indent': False },
+		{ 'link': '/programs/junction/teach.html',
+		  'text': 'JUNCTION',
+		  'indent': False },
+		{ 'link': '/programs/junction/classreg.html',
+		  'text': 'Class Registration',
+		  'indent': True },
+		{ 'link': '/programs/delve/teach.html',
+		  'text': 'Delve (AP Program)',
+		  'indent': False }
+	]
+
+class preloader:
+	preload_images = [
+		'/media/images/level3/nav/home_ro.gif',
+		'/media/images/level3/nav/discoveresp_ro.gif',
+		'/media/images/level3/nav/takeaclass_ro.gif',
+		'/media/images/level3/nav/volunteertoteach_ro.gif',
+		'/media/images/level3/nav/getinvolved_ro.gif',
+		'/media/images/level3/nav/archivesresources_ro.gif',
+		'/media/images/level3/nav/myesp_ro.gif',
+		'/media/images/level3/nav/contactinfo_ro.gif'
+	]
+	  
+class home(navbar,preloader):
+	@staticmethod
+	def index(request):
+		latest_event_list = Event.objects.filter().order_by('-start')
+		return render_to_response('index.html', {
+				'navbar_list': navbar_data,
+				'preload_images', preload_images
+			})
