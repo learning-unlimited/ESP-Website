db11ef97a597aebe538738fdc53e2957bfc541aa
jalonso <jalonso@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-07-02 23:42:03 +0000

    Created a model for QSD
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@95 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/web/models.py
+++ esp/web/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from esp.watchlists.models import Datatree
+from esp.lib.markdown import markdown
 
 # Create your models here.
+
+class QuasiStaticData(models.Model):
+	""" A Markdown-encoded web page """
+	path = models.ForeignKey(Datatree)
+	content = models.TextField()
+	name = models.SlugField()
+
+	class Admin:
+		pass
+
+	def html();
+		return markdown.markdown(content)
+
+	@staticmethod
+	def find_by_url_parts(parts):
+		""" Fetch a QSD record by its url parts """
+		# Get the Q_Web root
+		Q_Web = Datatree.GetNode('Q/Web')
+
+		# Extract the last part
+		filename = parts.pop()
+
+		# Find the branch
+		branch = Q_Web.tree_decode( filename )
+
+		# Find the record
+		qsd = QuasiStaticData.objects.filter( path = branch, name = filename )
+
+		# Operation Complete!
+		return qsd
