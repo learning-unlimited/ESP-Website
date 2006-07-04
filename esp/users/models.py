0f2c309e20b468596ee747e7c631fa3345e021aa
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-07-04 07:19:30 +0000

    Ahhh
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@169 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/users/models.py
+++ esp/users/models.py
@@ -4,6 +4,7 @@ from esp.datatree.models import DataTree, PermToString
 #from peak.api import security, binding
 from esp.workflow.models import Controller
 from datetime import datetime
+django.db.models.Q
 
 # Create your models here.
 
