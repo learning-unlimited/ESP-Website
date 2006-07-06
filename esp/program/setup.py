fb18ffdeca6bbc8da3743573a4cd9868736d7e05
jalonso <jalonso@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-07-06 13:57:52 +0000

    Eliminated ProgramType pre-population
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@210 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/program/setup.py
+++ esp/program/setup.py
@@ -1,7 +1,6 @@
 from esp.datatree.models import GetNode
 from esp.program.Lists_ClassCategories import populate as populate_LCC
 from esp.program.Lists_EquipmentTypes import populate as populate_LET
-from esp.program.Lists_ProgramTypes import populate as populate_LPT
 
 def PopulateProgram(program_node):
 	for sub_node in ProgramTemplate:
@@ -10,9 +9,9 @@ def PopulateProgram(program_node):
 def populate():
 	populate_LCC()
 	populate_LET()
-	populate_LPT()
 
 ProgramTemplate = (
+    '/Critical',
     '/Prospectives',
     '/Prospectives/Teachers',
     '/Prospectives/Students',
