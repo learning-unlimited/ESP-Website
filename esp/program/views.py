8356c70b4c9f36d415147c7502d6e72f7f005aaa
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-07-06 14:07:38 +0000

    merp
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@211 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/program/views.py
+++ esp/program/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from esp.calendar.models import Event
+from esp.web.models import QuasiStaticData
+from esp.datatree.models import GetNode
+from esp.miniblog.models import Entry
+from django.http import HttpResponse, Http404
+
+from django.contrib.auth.models import User
+from esp.web.models import NavBarEntry
+
+def courseCatalogue(request, one, two):
+    treeItem = "Q/Programs/" + one + "/" + two 
+    prog = GetNode(treeItem)
+    classes = prog.program_set.all()
+    
+    assert False
