21e605c9298962f8627435622e2722538b0d4508
jalonso <jalonso@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-07-10 04:35:38 +0000

    Began improving PopulateProgram to add initial authorizations
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@214 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/program/setup.py
+++ esp/program/setup.py
@@ -1,14 +1,58 @@
-from esp.datatree.models import GetNode
+from esp.datatree.models import GetNode, DataTree
+from esp.users.models import UserBit
 from esp.program.Lists_ClassCategories import populate as populate_LCC
 from esp.program.Lists_EquipmentTypes import populate as populate_LET
 
-def PopulateProgram(program_node):
+def PopulateProgram(program_node,
+		program_term,
+		program_admins,
+		teacher_reg_range,
+		student_reg_range):
+	""" PopulateProgram initializes a program, establishing its Data Tree branch and
+	creating its registration deadline and administration permissions
+	"""
+
+	# Fetch/create the program node
+	anchor = GetNode( program_node )
+
+	# Set the friendly name for the program's term (e.g. 'Summer 2006')
+	anchor.friendly_name = program_term
+	anchor.update()
+
+	# Create the DataTree branches
 	for sub_node in ProgramTemplate:
 		GetNode(program_node + sub_node)
+
+	# Create the initial deadline authorizations
+	if student_reg_range is not null:
+		deadline_student = UserBit()
+		deadline_student.user = None
+		deadline_student.qsc = anchor
+		deadline_student.verb = GetNode( 'V/Deadline/StudentRegistration' )
+		deadline_student.startdate = student_reg_range[0]
+		deadline_student.enddate = student_reg_range[1]
+	if teacher_reg_range is not null:
+		deadline_teacher = UserBit()
+		deadline_teacher.user = None
+		deadline_teacher.qsc = anchor
+		deadline_teacher.verb = GetNode( 'V/Deadline/TeacherRegistration' )
+		deadline_teacher.startdate = teacher_reg_range[0]
+		deadline_teacher.enddate = teacher_reg_range[1]
+
+	# Create the administration authorizations
+	admin_verb = GetNode( 'V/Administer/Program' )
+	for director in program_admins:
+		admin_perm = UserBit()
+		admin_perm.user = director
+		admin_perm.qsc = anchor
+		admin_perm.verb = admin_verb
+		
 
 def populate():
 	populate_LCC()
 	populate_LET()
+	for v_node in VerbNodes:
+		GetNode( v_node )
 
 ProgramTemplate = (
     '/Critical',
@@ -19,3 +63,14 @@ ProgramTemplate = (
     '/Classes',
     '/Subprograms'
     )
+
+VerbNodes = (
+		'V/Publish',
+		'V/Deadline/StudentRegistration',
+		'V/Deadline/TeacherRegistration',
+		'V/Administer/Program',
+		'V/Administer/Program/Class'
+	    )
+# NOTE: V/Publish grants authorization to view a Q branch.
+# V/Deadline/StudentRegistration enforces the student registration deadline for a program.
+# V/Deadline/TeacherRegistration enforces the teacher registration deadline for a program.
