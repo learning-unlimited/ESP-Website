6d5cc8478c48096ffb1a9fb45a2b8b4667409890
jalonso <jalonso@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-07-12 02:59:54 +0000

    Fixed another broken call to tree_decode
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@256 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/program/models.py
+++ esp/program/models.py
@@ -132,7 +132,7 @@ class RegistrationProfile(models.Model):
 	
 	def preregistered_classes(self):
 		v = GetNode( 'V/Preregister' )
-		return UserBit.find_by_anchor_perms(Class, self.user, v, self.program.anchor.tree_decode('Classes'))
+		return UserBit.find_by_anchor_perms(Class, self.user, v, self.program.anchor.tree_decode(['Classes']))
 	def registered_classes(self):
 		v = GetNode( 'V/Subscribe' )
 		return UserBit.find_by_anchor_perms(Class, self.user, v, self.program.anchor.tree_decode(['Classes']))
