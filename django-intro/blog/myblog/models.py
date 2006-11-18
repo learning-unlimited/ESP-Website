363369ee8d5f3aeb80eecfe15f693a8bba3d8751
jalonso <jalonso@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-11-18 02:28:48 +0000

    Implemented basic BlogEntry model
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@441 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- django-intro/blog/myblog/models.py
+++ django-intro/blog/myblog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class BlogEntry(models.Model):
+    author = models.ForeignKey(User)
+    title = models.TextField()
     text = models.TextField()
-    timestamp = models.DateTimeField()
-    
+    timestamp = models.DateTimeField(default=datetime.now())
+    class Admin:
+        pass
