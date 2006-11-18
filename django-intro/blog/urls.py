89c50d96b94576eaafaa1ad7933b631a335c8bf1
jalonso <jalonso@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2006-11-18 03:16:03 +0000

    Added media folder, served statically, and friendly comments in urls.py
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@449 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- django-intro/blog/urls.py
+++ django-intro/blog/urls.py
@@ -4,7 +4,13 @@ urlpatterns = patterns('',
     # Example:
     # (r'^blog/', include('blog.apps.foo.urls.foo')),
 
-    # Uncomment this for admin:
+    # This gives us our admin interface at http://localhost:8000/admin/
      (r'^admin/', include('django.contrib.admin.urls')),
+
+    # This gives us our list of blog entries at http://localhost:8000/
      (r'^$', 'blog.myblog.views.list_blogs'),
+
+    # This gives us our images from our media/ folder at http://localhost:8000/media/
+    # NOTE: On a real site, you'd actually let the web server handle this.
+     (r'^media/(.*)$', 'django.views.static.serve', {'document_root': 'media'}),
 )
