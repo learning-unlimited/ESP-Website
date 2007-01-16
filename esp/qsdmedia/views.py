1aee1edf0c6807fe8d7c04e0edc936f79ab23e21
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-01-16 19:53:53 +0000

    Fixed qsdmedia in the case where a file is uploaded more than oncey
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@620 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/qsdmedia/views.py
+++ esp/qsdmedia/views.py
@@ -15,6 +15,9 @@ def qsdmedia(request, branch, section, url_name, url_verb, base_url):
         media_rec = Media.objects.get(anchor=branch, friendly_name=filename)
     except Media.DoesNotExist:
         raise Http404
+    except AssertionError: # We get an AssertionError if 'get' fails because of multiple Media entries.  If there exist multiple Media entries, we want the first one
+        media_rec = Media.objects.filter(anchor=branch, friendly_name=filename).order_by('-id')[0]
+
     
     # aseering 8-7-2006: Add permissions enforcement; Only show the page if the current user has V/Flags/Public on this node
     have_view = UserBit.UserHasPerms( request.user, media_rec.anchor, GetNode('V/Flags/Public') )
