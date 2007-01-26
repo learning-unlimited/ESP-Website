6078b65ade7fe769fc4f587a4b9913a40befb988
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-01-26 01:30:40 +0000

    Added
    m164-teachers@esp.mit.edu
    and 
    m164-students@esp.mit.edu
    
    
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@664 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/mailgates/dbmail_classes.py
+++ esp/mailgates/dbmail_classes.py
@@ -8,7 +8,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'esp.settings'
 
 from esp.program.models import Class
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     sys.exit()
 
 classes = Class.objects.filter(id = sys.argv[1])
@@ -17,8 +17,13 @@ if len(classes) != 1:
 
 clsobj = classes[0]
 
-users = list(clsobj.teachers()) + clsobj.students()
-
+request = sys.argv[2]
+if request == 'class':
+    users = list(clsobj.teachers()) + clsobj.students()
+elif request == 'students':
+    users = clsobj.students()
+elif request == 'teachers':
+    users = list(clsobj.teachers())
 
 for user in users:
     print '%s\t\t\t%s %s' % \
