3581221ac76ea5193f699e9505af1a460324d7aa
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-02-01 15:39:42 +0000

    made modifications to template
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@681 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/web/data.py
+++ esp/web/data.py
@@ -12,7 +12,11 @@ def render_to_response(template, requestOrContext, prog = None, context = None):
     
     if context is not None:
         request = requestOrContext
+
+        context['page_setup'] = get_page_setup(request)
+        
         section = ''
+
 
         if type(prog) == tuple:
             section = prog[1]
@@ -43,6 +47,76 @@ def render_to_response(template, requestOrContext, prog = None, context = None):
         return django.shortcuts.render_to_response(template, context)
         
     assert False, 'render_to_response expects 2 or 4 arguments.'
+
+
+def get_page_setup(request):
+    path = request.path.strip('/').split('/')
+    if path[0] not in known_navlinks:
+        return False
+
+    page_setup = {}
+
+    is_admin = ESPUser(request.user).isAdmin()
+    
+    
+    if len(path) == 2 and request.path.lower() in [ x[2] for x in section_convert.values() ]:
+        page_setup['navlinks'] = []
+        page_setup['buttonlocation'] = 'lev2'
+        page_setup['stylesheet']     = [ x for x in basic_navlinks if section_convert[x][0] == path[0]][0]+'2'
+        for section in basic_navlinks:
+            page_setup['navlinks'].append({'id'       : section,
+                                           'alt'      : section_convert[section][1],
+                                           'highlight': request.path.lower() == section_convert[section][2],
+                                           'href'     : section_convert[section][2]})
+            if request.path.lower() == section_convert[section][2]:
+                page_setup['section'] = {'id': section+'/lev2', 'alt': section_convert[section][1]}
+        if is_admin:
+            section = 'admin'
+            page_setup['navlinks'].append({'id'       : section,
+                                           'alt'      : section_convert[section][1],
+                                           'highlight': request.path.lower() == section_convert[section][2],
+                                           'href'     : section_convert[section][2]})
+        return page_setup
+    
+    elif path[0] in known_navlinks:
+        page_setup['navlinks'] = []
+        page_setup['buttonlocation'] = 'lev3'
+        page_setup['stylesheet'] = [ x for x in basic_navlinks if section_convert[x][0] == path[0]][0]+'3'
+        for section in basic_navlinks:
+            if path[0] == section_convert[section][0]:
+                page_setup['section'] = {'id': section+'/lev3',
+                                         'alt': section_convert[section][1],
+                                         'cursection': section}
+                page_setup['buttonlocation'] = section+'/lev3'
+                
+        for section in basic_navlinks:
+            page_setup['navlinks'].append({'id'       : section,
+                                           'alt'      : section_convert[section][1],
+                                           'highlight': path[0] == section_convert[section][0],
+                                           'href'     : section_convert[section][2]})
+
+
+        if is_admin:
+            section = 'admin'
+            page_setup['navlinks'].append({'id'       : section,
+                                           'alt'      : section_convert[section][1],
+                                           'highlight': path[0] == section_convert[section][0],
+                                           'href'     : section_convert[section][2]})
+        return page_setup
+
+    return False
+
+known_navlinks = ['about','learn','teach','getinvolved','archives','myesp','contactinfo']
+basic_navlinks = ['discoveresp','takeaclass','volunteertoteach','getinvolved','archivesresources','myesp','contactinfo']
+
+section_convert = {'volunteertoteach' : ('teach', 'Volunteer to Teach!', '/teach/index.html'),
+                   'takeaclass'       : ('learn', 'Take a Class!', '/learn/index.html'),
+                   'discoveresp'      : ('about', 'Discover ESP', '/about/index.html'),
+                   'myesp'            : ('myesp', 'myESP', '/myesp/home/'),
+                   'getinvolved'      : ('getinvolved', 'Get Involved!','/getinvolved/index.html'),
+                   'archivesresources': ('archives', 'ESP Archives','/archives/index.html'),
+                   'contactinfo'      : ('about', 'Contact Us','/about/contact.html'),
+                   'admin'            : ('admin', 'Admin Section', '/myesp/admin/')}
 
 
 navbar_data = [
