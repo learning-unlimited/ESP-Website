aee1d454a885b6263e118ea6a7b9fb31c26de706
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-02-02 20:32:07 +0000

    Forgot one file...
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@686 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

---
+++ esp/program/modules/handlers/commpanel.py
@@ -0,0 +1,49 @@
+from esp.program.modules.base import ProgramModuleObj, needs_teacher, needs_student, needs_admin, usercheck_usetl
+from esp.program.modules import module_ext
+from esp.web.data        import render_to_response
+from esp.program.manipulators import SATPrepInfoManipulator
+from django import forms
+from esp.program.models import SATPrepRegInfo
+from esp.users.models   import ESPUser
+from django.db.models import Q
+
+
+
+class CommModule(ProgramModuleObj):
+
+    def students(self,QObject = False):
+        if QObject:
+            return {'satprepinfo': Q(satprepreginfo__program = self.program)}
+        students = ESPUser.objects.filter(satprepreginfo__program = self.program).distinct()
+        return {'satprepinfo': students }
+
+    def isCompleted(self):
+        
+	satPrep = SATPrepRegInfo.getLastForProgram(self.user, self.program)
+	return satPrep.id is not None
+
+
+    @needs_student
+    def satprepinfo(self, request, tl, one, two, module, extra, prog):
+	manipulator = SATPrepInfoManipulator()
+	new_data = {}
+	if request.method == 'POST':
+		new_data = request.POST.copy()
+		
+		errors = manipulator.get_validation_errors(new_data)
+		
+		if not errors:
+			manipulator.do_html2python(new_data)
+			new_reginfo = SATPrepRegInfo.getLastForProgram(request.user, prog)
+			new_reginfo.addOrUpdate(new_data, request.user, prog)
+
+                        return self.goToCore(tl)
+	else:
+		satPrep = SATPrepRegInfo.getLastForProgram(request.user, prog)
+		
+		new_data = satPrep.updateForm(new_data)
+		errors = {}
+
+	form = forms.FormWrapper(manipulator, new_data, errors)
+	return render_to_response('program/modules/satprep_stureg.html', request, (prog, tl), {'form':form})
+
