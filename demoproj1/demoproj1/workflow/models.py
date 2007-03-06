501173a84a579f39813c96171a19b6c17589be21
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-03-06 19:32:13 +0000

    some spring cleaning these projects are no longer in development for esp
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@826 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- demoproj1/demoproj1/workflow/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Controller:
-    pass
--- django-intro/blog/myblog/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-
-from blog.myblog.models import BlogEntry
-from django.shortcuts import render_to_response
-from django.http import Http404
-
-def list_blogs(request):
-	entries = BlogEntry.objects.all()
-	return render_to_response('blog_list.html', { 'entries': entries })
