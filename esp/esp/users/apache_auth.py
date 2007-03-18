8610ed7fd182b5ff7cc7832660a6d87d12c2004a
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-03-18 01:54:30 +0000

    Added authenhandler
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@854 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/users/apache_auth.py
+++ esp/esp/users/apache_auth.py
@@ -45,3 +45,56 @@ def accesshandler(req):
             return apache.OK
 
     return apache.HTTP_UNAUTHORIZED
+
+
+def authenhandler(req, **kwargs):
+    """
+    Authentication handler that checks against Django's auth database.
+    """
+
+    # mod_python fakes the environ, and thus doesn't process SetEnv.  This fixes
+    # that so that the following import works
+    os.environ.update(req.subprocess_env)
+    uri = req.subprocess_env['REQUEST_URI']
+
+    # check for PythonOptions
+    _str_to_bool = lambda s: s.lower() in ('1', 'true', 'on', 'yes')
+
+    options = req.get_options()
+    permission_verb_name = options.get('ESPPermissionName', 'V/Flags/Public')
+    settings_module = options.get('DJANGO_SETTINGS_MODULE', None)
+    
+    if settings_module:
+        os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
+            
+    from esp.users.models import UserBit
+    from esp.datatree.models import get_lowest_parent
+
+
+
+    from django.contrib.auth.models import User
+    from django import db
+    db.reset_queries()
+
+    # check that the username is valid
+    kwargs = {'username': req.user, 'is_active': True}
+
+    try:
+        try:
+            user = User.objects.get(**kwargs)
+        except User.DoesNotExist:
+            return apache.HTTP_UNAUTHORIZED
+    
+        # check the password and any permission given
+        if user.check_password(req.get_basic_auth_pw()):
+            if request.user.is_authenticated():
+                qsc  = get_lowest_parent('Q/Static/' + uri.strip('/'))
+                verb = get_lowest_parent(permission_verb_name)
+                if UserBit.UserHasPerms(request.user, qsc, verb):
+                    return apache.OK
+                else:
+                    return apache.HTTP_UNAUTHORIZED
+        else:
+            return apache.HTTP_UNAUTHORIZED
+    finally:
+        db.connection.close()
