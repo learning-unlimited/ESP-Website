3966e16748d602891d8221448a5b2a372403e292
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-05-23 07:45:00 +0000

    disabled blog urls
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@958 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/miniblog/urls.py
+++ esp/esp/miniblog/urls.py
@@ -5,7 +5,8 @@ urlpatterns = patterns('esp.miniblog.views',
                      # Mini-Blog pages
                      # These are broken...must fix (axiak)
                      #(r'^(?P<subsection>teach|learn|help)/(?P<url>.*)/blog/$', 'esp.miniblog.views.show_miniblog', {'section_redirect_keys': section_redirect_keys}),
-                     (r'^(?P<url>.*)/post/$', 'post_miniblog'),
-                     (r'^(?P<url>.*)/$', 'show_miniblog_entry'),
-                     (r'^$', 'show_miniblog', {'url': '', 'section_redirect_keys': section_redirect_keys}),
+                     (r'^asfajwaj389f8#@$F*javn  =324124$', 'post_miniblog'),
+#                     (r'^(?P<url>.*)/post/$', 'post_miniblog'),
+#                     (r'^(?P<url>.*)/$', 'show_miniblog_entry'),
+#                     (r'^$', 'show_miniblog', {'url': '', 'section_redirect_keys': section_redirect_keys}),
 )
