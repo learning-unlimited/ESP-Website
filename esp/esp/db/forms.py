7df0311dcb27aecf87be679af8d37e11c875b2a1
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-05-29 00:43:15 +0000

    Made ajaxforeignkey not destroy everything
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@1001 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/db/forms.py
+++ esp/esp/db/forms.py
@@ -7,10 +7,10 @@ import re
 get_id_re = re.compile('.*\D(\d+)\D')
 
 class AjaxForeignKeyFormField(oldforms.FormField):
-    def __init__(self, field_name, field,
+    def __init__(self, field_name, field,queryset=None
                  is_required=False, validator_list=None,
                  member_name=None, ajax_func = None, width=None):
-
+        self.queryset    = queryset
         self.field_name  = field_name
         self.field       = field
         self.is_required = is_required
