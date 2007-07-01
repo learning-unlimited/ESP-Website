eb06305c3f0701a90ded05f0f77698dc7dae0324
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-07-01 01:37:57 +0000

    Part of the projector catalog display.  Handing off to Mike Price.
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@1053 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/program/templatetags/class_render.py
+++ esp/esp/program/templatetags/class_render.py
@@ -24,4 +24,20 @@ def render_class(cls, user=None, prereg_url=None, filter=False, request=None):
             'prereg_url': prereg_url,
             'errormsg':   errormsg,
             'show_class': show_class}
+
+@cache_inclusion_tag(register, 'inclusion/program/class_catalog_minimal.html', cache_key_func=cache_key_func)
+def render_class_minimal(cls, user=None, prereg_url=None, filter=False, request=None):
+    errormsg = None
+
+    if user and prereg_url:
+        errormsg = cls.cannotAdd(user, True, request=request)
+
+    show_class =  (not filter) or (not errormsg)
+    
+    
+    return {'class':      cls,
+            'user':       user,
+            'prereg_url': prereg_url,
+            'errormsg':   errormsg,
+            'show_class': show_class}
            
