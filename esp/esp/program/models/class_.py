6708973a3b90d6b70d9e19690976455c31b5b32f
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-08-31 05:58:47 +0000

    Fixed a very silly typo that made the site very sad
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@1094 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/program/models/class_.py
+++ esp/esp/program/models/class_.py
@@ -254,7 +254,7 @@ class Class(models.Model):
             return 'This class is not accepted.'
 
         if checkFull and self.parent_program.isFull():
-            return 'This programm cannot accept any more students!  Please try again in its next session."
+            return 'This programm cannot accept any more students!  Please try again in its next session.'
 
         if checkFull and self.isFull():
             return 'Class is full!'
