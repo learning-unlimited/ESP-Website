799bc062d861b0ff678bc1681544c9709f0b911f
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-11-01 06:55:21 +0000

    new queue manager; hopefully more robust
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@1186 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/queue/middleware.py
+++ esp/esp/queue/middleware.py
@@ -3,50 +3,84 @@
 import time
 from esp.queue.models import *
 from django.http import HttpResponse
+from django.core.cache import cache
+from datetime import datetime, timedelta
+import os
 import re
+import random
 
 no_queue_list = [ 
-    r'^18.187.*',
-    r'^18.208.*',
-    r'^127\.0\.0\.1$',
     ]
 
 no_queue_list = [ re.compile(x) for x in no_queue_list ]
 
+USERS_IN_SITE = 1
+SITE_TIMEOUT = timedelta(minutes=5)
+SITE_REFRESH_TIME = 240
+CACHE_KEY_USERS = "CACHE_KEY_USERS_ON_SITE"
+CACHE_KEY_IN = "CACHE_KEY_USERS_IN_SITE"
+
 class QueueMiddleware(object):
 
     def process_request(self, request):
-        if request.META.has_key('REMOTE_ADDR'):
-            ip = request.META['REMOTE_ADDR']
-            for i in no_queue_list:
-                if re.search(i, ip):
-                    return None
+#        if os.getloadavg()[1] < 10.0: # We don't care for less-than-crazy loads
+#            return None
 
-        q = QueueItem(request)
-        
-        try:
-            q.update_queue()
-        except WaitInQueue, wait:
-            pass
-        else:
+        ip = request.META['REMOTE_ADDR']
+        for i in no_queue_list:
+            if re.search(i, ip):
+                return None
+
+        do_clean = random.randint(0, USERS_IN_SITE)
+        if do_clean == 0:
+            UserQueue.objects.filter(time_since_last_refresh__lte = datetime.now()-SITE_TIMEOUT).delete()
+
+        browser_key = ip + ':' + request.META.get('HTTP_USER_AGENT','')
+        browser_key = browser_key[:60]
+
+        queueentry, created = UserQueue.objects.get_or_create(browser = browser_key)
+
+        if queueentry.in_site:
             return None
+
+        in_site_users_count = cache.get(CACHE_KEY_IN)
+        if in_site_users_count == None:
+            in_site_users_count = UserQueue.objects.filter(in_site=True).count()
+            cache.set(CACHE_KEY_IN, in_site_users_count, 2)
+
+        if in_site_users_count < USERS_IN_SITE:
+            cache.set(CACHE_KEY_IN, in_site_users_count + 1)
+            queueentry.in_site = True
+            queueentry.save()
+            return None
+
+        if not created:
+            queueentry.save()
+
+        users_on_site = cache.get(CACHE_KEY_USERS)
+        if users_on_site == None:
+            users_on_site = UserQueue.objects.filter(in_site=False).count()
+            cache.set(CACHE_KEY_USERS, users_on_site, SITE_REFRESH_TIME)
+        elif created:
+            users_on_site = users_on_site + 1
+            cache.set(CACHE_KEY_USERS, users_on_site, SITE_REFRESH_TIME)
 
 
         message = """
 <h1>Please wait...</h1>
 
 <p>Our server is under extreme load. To satisfy everyone's need, you
-have been placed in a queue. You are #%s in a line of %s people.
+have been placed in a queue. You are in a line of %s people.
 <br /><br />
 <!-- Your estimated time is: <strong>TODO</strong> //-->
 
 <strong>Note: Please do not refresh and/or close this page! This
 page will automatically reload and update for you.</strong>
-""" % (wait.num_in_queue, wait.queue_size)
+""" % (users_on_site)
 
         page = open(QUEUE_FILE, 'r').read(50000) % message
 
         response = HttpResponse(page)
-        response['Refresh'] = str(wait.refresh_time)
+        response['Refresh'] = str(SITE_REFRESH_TIME)
 
         return response
--- esp/esp/queue/models.py
+++ esp/esp/queue/models.py
@@ -1,150 +1,21 @@
 """ Models for the Queue. """
 from django.conf import settings
-from esp.utils.memdb import mem_db
-import md5, time
-
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
+from django.db import models
 
 QUEUE_FILE = settings.TEMPLATE_DIRS[0] + '/errors/queue.html'
 WELCOME_SCREEN = settings.TEMPLATE_DIRS[0] + '/queue_welcome.html'
 
-__all__ = ['QUEUE_FILE','QueueItem','WaitInQueue']
+class UserQueue(models.Model):
+    browser = models.CharField(maxlength=64)
+    time_in_queue = models.DateTimeField(auto_now_add=True)
+    time_since_last_refresh = models.DateTimeField(auto_now=True)
+    in_site = models.BooleanField(default=False)
 
-##########
-# DEFAULTS
-# Note: These are adjustable in the memory database.
-##########
-DEFAULT_TIMEOUT = 600#: Ten minute timeout.
-MAX_SIZE = 120
-REFRESH_TIME = 30
+    def __str__(self):
+        if self.in_site:
+            return "In:     (%s) (%s) %s" % (self.time_since_last_refresh, self.time_in_queue, self.browser)
+        else:
+            return "Not In: (%s) (%s) %s" % (self.time_since_last_refresh, self.time_in_queue, self.browser)
 
-class WaitInQueue(Exception):
-    """ The user is waiting in a queue. """
-    def __init__(self, num_in_queue=0, queue_size=0, expected_time=0, refresh_time=0):
-        self.num_in_queue = num_in_queue
-        self.expected_time = expected_time
-        self.queue_size = queue_size
-        self.refresh_time = refresh_time
-
-class QueueProperty(object):
-    """ A global property for a queue object. These are written and
-    read to/from the mem_db.
-    """
-    def __init__(self, name, default=None):
-        self.name = name
-        self.default = default
-
-    def __set__(self, instance, value):
-        if not hasattr(instance, '_properties'):
-            instance._properties = {}
-        instance._properties[self.name] = value
-        mem_db.set(self.name, pickle.dumps(value))
-
-    def __get__(self, instance, class_):
-        if not hasattr(instance, '_properties'):
-            instance._properties = {}
-
-        if self.name not in instance._properties:
-            value = mem_db.get(self.name) or self.default
-            try:
-                value = pickle.loads(value)
-            except:
-                pass
-
-            instance._properties[self.name] = value
-
-        return instance._properties[self.name]
-
-
-class QueueItem(object):
-    max_inside = QueueProperty('QUEUE_MAX', MAX_SIZE)#: The number of people inside
-    timeout = QueueProperty('QUEUE_TIMEOUT', DEFAULT_TIMEOUT)#: How long before the person times out
-    inside_keys = QueueProperty('QUEUE_KEYS', [])#: Keys inside the web site
-    queue_keys = QueueProperty('QUEUE_LIST', [])#: Keys waiting on the doorstep
-    refresh_time = QueueProperty('QUEUE_REFRESH_TIME', REFRESH_TIME)#: How long to refresh
-    show_welcome = QueueProperty("QUEUE_SHOW_WELCOME", False)
-
-    def __init__(self, request=None):
-        if request is not None:
-            self.ip_addr = request.META['REMOTE_ADDR']
-            self.user_agent = request.META.get('HTTP_USER_AGENT','')
-            self.request = request
-            self.cache_key = 'QUEUE__' + md5.new(self.ip_addr + '::' + self.user_agent).hexdigest()
-            # If the user signs out, remove them
-            if 'signout' in request.path:
-                mem_db.delete(self.cache_key)
-
-    def clean_keys(self):
-        inside_old_keys = self.inside_keys
-        queue_old_keys = self.queue_keys
-        queue_new_keys, inside_new_keys = [],[]
-
-        for key in inside_old_keys:
-            last_time = mem_db.get(key)
-            if last_time is not None:
-                inside_new_keys.append(key)
-
-        for key in queue_old_keys:
-            last_time = QueueItem.get_time_from_value(mem_db.get(key))
-            if key != self.cache_key or \
-               abs(time.time() - last_time - self.refresh_time) < (.2 * self.refresh_time):
-                # A reload has to be within 20% of when it was supposed to be refreshed.
-                queue_new_keys.append(key)
-
-        if set(inside_new_keys) != set(inside_old_keys):
-            self.inside_keys = inside_new_keys
-
-        if set(queue_new_keys) != set(queue_old_keys):
-            self.queue_keys = queue_new_keys
-
-    @staticmethod
-    def get_time_from_value(value):
-        if value is None:
-            return 0
-        try:
-            return float(value.split(',', 1)[0])
-        except:
-            return 0
-
-    @property
-    def cache_value(self):
-        return ','.join((str(time.time()), self.ip_addr, self.user_agent))
-
-    def update_queue(self):
-        self.clean_keys()
-
-        if self.cache_key not in self.inside_keys:
-            if self.cache_key not in self.queue_keys:
-                # We are currently not in the queue.
-                queue_keys = self.queue_keys
-                queue_keys.append(self.cache_key)
-                self.queue_keys = queue_keys
-            num_avail = self.max_inside - len(self.inside_keys)
-
-            if self.cache_key in self.queue_keys[:num_avail]:
-                queue_keys = self.queue_keys
-                queue_keys.remove(self.cache_key)
-                
-                inside_keys = self.inside_keys
-                inside_keys.append(self.cache_key)
-
-                self.queue_keys = queue_keys
-                self.inside_keys = inside_keys
-            else:
-                mem_db.set(self.cache_key, self.cache_value, self.timeout)
-                raise WaitInQueue(self.queue_keys.index(self.cache_key) + 1,
-                                 len(self.queue_keys),
-                                 0,
-                                 self.refresh_time)
-
-        print """=============
-%s
-==========
-%s
-==========""" % (self.queue_keys, self.inside_keys)
-        mem_db.set(self.cache_key, self.cache_value, self.timeout)
-
-        return True
+    class Admin:
+        pass
