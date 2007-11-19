b954ed9ff354ea381464754136b0e836f27a5299
axiak <axiak@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2007-11-19 03:38:26 +0000

    Added survey app
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@1238 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

---
+++ esp/esp/survey/models.py
@@ -0,0 +1,47 @@
+" Survey models for Educational Studies Program. "
+
+__author__    = "$LastChangedBy$"
+__date__      = "$LastChangedDate$"
+__rev__       = "$LastChangedRevision$"
+__headurl__   = "$HeadURL$"
+__license__   = "GPL v2"
+__copyright__ = """
+This file is part of the ESP Web Site
+Copyright (c) 2007 MIT ESP
+
+The ESP Web Site is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Contact Us:
+ESP Web Group
+MIT Educational Studies Program,
+84 Massachusetts Ave W20-467, Cambridge, MA 02139
+Phone: 617-253-4882
+Email: web@esp.mit.edu
+"""
+
+import datetime
+from django.db import models
+
+from esp.db.fields import AjaxForeignKey
+
+# Models to depend on.
+from esp.datatree.models import DataTre
+
+class SurveyUnit(models.Model):
+    time_filled = models.DateTimeField(default=datetime.datetime.now)
+    anchor = models.AjaxForeignKey(DataTree)
+
+class QuestionType(models.Model):
+    
