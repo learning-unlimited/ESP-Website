dcc6d1e3711ab2da5ec23936026ca5a5599faa55
aseering <aseering@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2008-02-13 16:43:33 +0000

    Fixed the 'Everything POSTs back to Splash 2007' bug
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@1345 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/program/views.py
+++ esp/esp/program/views.py
@@ -191,7 +191,10 @@ def managepage(request, page):
             #            except:
             #                raise ESPError(), "A server error occurred while logging your credit card transaction.  The transaction has not been lost; this just means that the green Credit Card checkbox may not be properly checked off, allowing you to finish registering for this program.  Please <a href=\"mailto:esp-webmasters@mit.edu\">e-mail us</a> and ask us to correct this manually.  We apologize for the inconvenience."
 
-            return HttpResponseRedirect("http://%s/learn/Splash/2007/confirmreg" % request.META['HTTP_HOST'])
+            one = document.anchor.parent.name
+            two = document.anchor.name
+
+            return HttpResponseRedirect("http://%s/learn/%s/%s/confirmreg" % (request.POST['HTTP_HOST'], one, two))
             
         return render_to_response( 'accounting_docs/credit_rejected.html', request, GetNode('Q/Accounting/'), {} )
 
