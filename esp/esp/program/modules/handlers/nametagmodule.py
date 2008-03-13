1f8c8723413be22a9295832d667899b5ee24fa73
rye <rye@9f9a36c0-4815-0410-8ed4-a58642b8ae3a>
2008-03-13 00:52:42 +0000

    Made teacher nametags generate the right list
    git-svn-id: http://esp.mit.edu/code/esp-project/trunk@1457 9f9a36c0-4815-0410-8ed4-a58642b8ae3a

--- esp/esp/program/modules/handlers/nametagmodule.py
+++ esp/esp/program/modules/handlers/nametagmodule.py
@@ -74,7 +74,8 @@ class NameTagModule(ProgramModuleObj):
         elif idtype == 'teacher':
             teachers = []
             teacher_dict = self.program.teachers(QObjects=True)
-            teachers = User.objects.filter(teacher_dict['teacher_profile'] | teacher_dict['class_rejected']).distinct()
+            teachers = User.objects.filter(teacher_dict['class_approved']).distinct()
+#            teachers = User.objects.filter(teacher_dict['teacher_profile'] | teacher_dict['class_rejected']).distinct()
 
 	    teachers = [ ESPUser(teacher) for teacher in teachers ]
             teachers = filter(lambda x: len(x.first_name+x.last_name), teachers)
