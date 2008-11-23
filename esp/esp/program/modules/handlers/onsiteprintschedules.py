4efc2a5b3d6d9e648b8bf94c3a9282c65d956317
Adam Seering <aseering@esp.mit.edu>
2008-11-23 10:01:08 -0500

    Tweak student schedules for Splash.  Upgrade to use new financial system, and display more information

--- esp/esp/program/modules/handlers/onsiteprintschedules.py
+++ esp/esp/program/modules/handlers/onsiteprintschedules.py
@@ -39,7 +39,7 @@ from esp.datatree.models import *
 from esp.users.models import UserBit
 from datetime         import datetime
 from django.db.models.query   import Q
-from esp.money.models import LineItemType, RegisterLineItem, LineItem
+from esp.accounting_docs.models import Document, MultipleDocumentError
 
 class OnsitePrintSchedules(ProgramModuleObj):
     @classmethod
@@ -90,14 +90,27 @@ class OnsitePrintSchedules(ProgramModuleObj):
             # now we sort them by time/title
             classes.sort()
                 
-            #   Payment information using new accounting system
-            ps = student.paymentStatus(self.program_anchor_cached())
-            student.itemizedcosts = ps[3]
-            student.itemizedcosttotal = ps[2]
-            student.has_paid = ps[0]  
+
+            # get payment information
+            li_types = prog.getLineItemTypes(student)
+            try:
+                invoice = Document.get_invoice(student, self.program_anchor_cached(parent=True), li_types, dont_duplicate=True, get_complete=True)
+            except MultipleDocumentError:
+                invoice = Document.get_invoice(student, self.program_anchor_cached(parent=True), li_types, dont_duplicate=True)
+            
+            # attach payment information to student
+            student.invoice_id = invoice.locator
+            student.itemizedcosts = invoice.get_items()
+            student.meals = student.itemizedcosts.filter(li_type__anchor__name='BuyOne')
+            student.itemizedcosttotal = invoice.cost()
+            student.has_financial_aid = student.hasFinancialAid(self.program_anchor_cached())
+            if student.has_financial_aid:
+                student.itemizedcosttotal = 0
+            student.has_paid = ( student.itemizedcosttotal == 0 )
+            
             student.payment_info = True
             student.classes = classes
-                
+
             students.append(student)
 
         if len(students) == 0:
