de0c1b28ce89b3d240479211463e005c3ffa454b
Andrew Geng <ageng@mit.edu>
2008-11-26 03:24:39 -0500

    Make some first name, last name, and e-mail form max lengths consistent with database limits. Should (mostly) take care of Trac #158.

--- esp/esp/program/modules/forms/onsite.py
+++ esp/esp/program/modules/forms/onsite.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class OnSiteRegForm(forms.Form):
-    first_name = forms.CharField(max_length=64, widget=forms.TextInput({'size':20, 'class':'required'}))
-    last_name = forms.CharField(max_length=64, widget=forms.TextInput({'size':30, 'class':'required'}))
-    email = forms.EmailField(max_length=64, widget=forms.TextInput({'size':20, 'class':'required'}))
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput({'size':20, 'class':'required'}))
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput({'size':30, 'class':'required'}))
+    email = forms.EmailField(max_length=75, widget=forms.TextInput({'size':20, 'class':'required'}))
 
     grade = forms.ChoiceField(choices = zip(range(7, 13), range(7, 13)), widget=forms.Select({'class':'required'}))
 
@@ -13,9 +13,9 @@ class OnSiteRegForm(forms.Form):
         
 class OnSiteSATPrepRegForm(forms.Form):
     # TODO: Would like to subclass OnSiteRegForm, but this one lacks the grade entry
-    first_name = forms.CharField(max_length=64, widget=forms.TextInput({'size':20, 'class':'required'}))
-    last_name = forms.CharField(max_length=64, widget=forms.TextInput({'size':30, 'class':'required'}))
-    email = forms.EmailField(max_length=64, widget=forms.TextInput({'size':20, 'class':'required'}))
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput({'size':20, 'class':'required'}))
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput({'size':30, 'class':'required'}))
+    email = forms.EmailField(max_length=75, widget=forms.TextInput({'size':20, 'class':'required'}))
 
     paid = forms.BooleanField(required = False)
     medical = forms.BooleanField(required = False)
