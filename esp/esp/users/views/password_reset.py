20dac9a4ff6c17075fd9daeede5197d6433ac755
David Benjamin <davidben@mit.edu>
2008-12-31 17:57:02 -0500

    Use PasswordRecoveryTicket in email_passwd_followup and reword relevant templates.

--- esp/esp/users/views/password_reset.py
+++ esp/esp/users/views/password_reset.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.template import loader
 from django.http import HttpResponseRedirect
 from django.db.models.query import Q
-from esp.users.models import PersistentQueryFilter, ESPUser
+from esp.users.models import PersistentQueryFilter, ESPUser, PasswordRecoveryTicket
 from esp.web.util.main import render_to_response
 from esp.users.decorators import anonymous_only
 from django.contrib.auth import authenticate, login
@@ -52,6 +52,7 @@ def initial_passwd_request(request, success=None):
 
 
 def email_passwd_followup(request,success=None):
+    """ Allow users to reset their password, given a recovery code. """
 
     if success:
         return render_to_response('users/recovery_finished.html',
@@ -61,22 +62,30 @@ def email_passwd_followup(request,success=None):
     except KeyError:
         code = request.POST.get('code','')
 
-    if len(User.objects.filter(password = code).values('id')[:1]) == 0:
+    ticket = PasswordRecoveryTicket.objects.filter(recover_key=code)[:1]
+    if len(ticket) == 0 or not ticket[0].is_valid():
         return render_to_response('users/recovery_invalid_code.html',
                                   request,request.get_node('Q/Web/myesp'),{})
+    ticket = ticket[0]
 
     if request.method == 'POST':
         form = NewPasswordSetForm(request.POST)
 
         if form.is_valid():
-            user = User.objects.get(username = form.cleaned_data['username'])
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            auth_user = authenticate(username = form.cleaned_data['username'],
-                                     password = form.cleaned_data['password'])
-            login(request, auth_user)
-            return HttpResponseRedirect('%ssuccess/' % request.path)
-                              
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            changed = ticket.change_password(username, password)
+
+            if changed:
+                auth_user = authenticate(username = form.cleaned_data['username'],
+                                         password = form.cleaned_data['password'])
+                login(request, auth_user)
+                return HttpResponseRedirect('%ssuccess/' % request.path)
+            else:
+                return render_to_response('users/recovery_invalid_code.html',
+                                  request,request.get_node('Q/Web/myesp'),{})
+
     else:
         form = NewPasswordSetForm(initial={'code':code})
 
