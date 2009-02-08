c61ebdfd94ea4a21a26e5d4d0dc5b09b7cd923d2
David Benjamin <davidben@mit.edu>
2009-02-08 18:39:28 -0500

    Twiddle tempfile.tempdir in settings.py
    Should fix multiple server instances trying to access the same file at once,
    notably /tmp/pspicture.ps.

--- esp/esp/settings.py
+++ esp/esp/settings.py
@@ -48,3 +48,13 @@ TEMPLATE_DIRS = (
 CACHE_BACKEND = "esp.utils.memcached_multikey://127.0.0.1:11211/?timeout=%d" % DEFAULT_CACHE_TIMEOUT
 
 MIDDLEWARE_CLASSES = [pair[1] for pair in sorted(MIDDLEWARE_GLOBAL + MIDDLEWARE_LOCAL)]
+
+# set tempdir so that we don't have to worry about collision
+import tempfile
+import os
+if not getattr(tempfile, 'alreadytwiddled', False): # Python appears to run this multiple times
+    tempdir = os.path.join(tempfile.gettempdir(), "esptmp__" + CACHE_PREFIX)
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
+    tempfile.tempdir = tempdir
+    tempfile.alreadytwiddled = True
