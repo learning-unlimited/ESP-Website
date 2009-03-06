6a9634ee9644b1afb8891a14e8efd09275812e4f
Yuri Lin <yuri@mist.(none)>
2009-03-05 20:50:14 -0500

    try 2: try again, this time to just print one schedule at a time

--- esp/esp/program/modules/handlers/onsiteprintschedules.py
+++ esp/esp/program/modules/handlers/onsiteprintschedules.py
@@ -70,7 +70,7 @@ class OnsitePrintSchedules(ProgramModuleObj):
         Q_qsc  = Q(qsc  = qsc.id)
         Q_verb = Q(verb__in = [ verb.id ] + list( verb.children() ) )
         
-        ubits = UserBit.valid_objects().filter(Q_qsc & Q_verb).order_by('startdate')[:5]
+        ubits = UserBit.valid_objects().filter(Q_qsc & Q_verb).order_by('startdate')[0]
         
         for ubit in ubits:
             ubit.enddate = datetime.now()
@@ -122,7 +122,7 @@ class OnsitePrintSchedules(ProgramModuleObj):
                             request, (prog, tl), {'students': students})
 
             # set the refresh rate
-            response['Refresh'] = '2'
+            response['Refresh'] = '0'
 
         return response
 
