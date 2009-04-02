fb7654c7612ae150638b98b1f2caa7c2ee8fbe1e
David Benjamin <davidben@mit.edu>
2009-04-02 17:59:45 -0400

    Add a middleware to detect serverloadedness
    This can be done better and might not actually be needed anymore
    (historical thing). Will get rid of it later.

--- esp/esp/django_settings.py
+++ esp/esp/django_settings.py
@@ -123,6 +123,7 @@ TEMPLATE_LOADERS = (
 
 # Set MIDDLEWARE_LOCAL in local_settings.py to configure this
 MIDDLEWARE_GLOBAL = [
+    (   0, 'esp.middleware.serverloaded.ServerLoadedMiddleware'),
     ( 100, 'django.middleware.http.SetRemoteAddrFromForwardedFor'),
    #( 200, 'esp.queue.middleware.QueueMiddleware'),
     ( 300, 'esp.middleware.FixIEMiddleware'),
