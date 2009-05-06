b947f12af51ccca696e1f0889fad7c75d414f182
David Benjamin <davidben@mit.edu>
2009-05-05 20:11:39 -0400

    display_time should default to False
    It currently enables a field that we don't use anymore and is still
    required, breaking program test cases.

--- esp/esp/program/modules/module_ext.py
+++ esp/esp/program/modules/module_ext.py
@@ -96,7 +96,7 @@ class ClassRegModuleInfo(models.Model):
     module               = models.ForeignKey(ProgramModuleObj)
     allow_coteach        = models.BooleanField(blank=True, default=True)
     set_prereqs          = models.BooleanField(blank=True, default=True)
-    display_times        = models.BooleanField(blank=True, default=True)
+    display_times        = models.BooleanField(blank=True, default=False)
     times_selectmultiple = models.BooleanField(blank=True, default=True)
     
     #   The maximum length of a class, in minutes.
