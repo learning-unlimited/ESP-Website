7ef6e3e23d2021098ca4714d9f3bf1ea67f2a795
David Benjamin <davidben@mit.edu>
2009-05-13 16:45:07 -0400

    Get rid of code copied from Django 1.0
    We're not even using this code anymore!

--- esp/esp/context_processors.py
+++ esp/esp/context_processors.py
@@ -33,47 +33,9 @@ def index_backgrounds(request):
                             "/media/images/home/pagebkg3.jpg"]}
     return {}
 
-def auth(request):
-
-    if request.path.startswith('/admin'):
-        return {
-            'user': request.user,
-            'messages': request.user.get_and_delete_messages(),
-            'perms': PermWrapper(request.user)
-            }          
-
-    return {
-        'user': request.user,
-        }
-
-
 def current_site(request):
 
     if hasattr(settings, 'SITE_INFO'):
         return {'current_site': Site(*settings.SITE_INFO) }
 
     return {'current_site': Site.objects.get_current()}
-
-    
-# PermWrapper and PermLookupDict proxy the permissions system into objects that
-# the template system can understand.
-
-class PermLookupDict(object):
-    def __init__(self, user, module_name):
-        self.user, self.module_name = user, module_name
-
-    def __repr__(self):
-        return str(self.user.get_all_permissions())
-
-    def __getitem__(self, perm_name):
-        return self.user.has_perm("%s.%s" % (self.module_name, perm_name))
-
-    def __nonzero__(self):
-        return self.user.has_module_perms(self.module_name)
-
-class PermWrapper(object):
-    def __init__(self, user):
-        self.user = user
-
-    def __getitem__(self, module_name):
-        return PermLookupDict(self.user, module_name)
