6f049ff6022c075b74a5dd4847c06eb99b7a0878
Adam Seering <aseering@esp.mit.edu>
2009-07-11 19:02:53 -0400

    Fix for some edge cases for usernames in the MediaWiki AuthPlugin

--- esp/cmdline_auth.py
+++ esp/cmdline_auth.py
@@ -10,41 +10,46 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'esp.settings'
 
 from esp import cache_loader
 import esp.manage
+from esp.users.models import ESPUser
 
 def return_(val):
     """ Return the specified return value in the appropriate format """
     if val == True:
-        print "true"
+        sys.stdout.write("true")
         sys.exit(0)
     elif val == False:
-        print "false"
+        sys.stdout.write("false")
         sys.exit(1)
     else:
-        print val
+        sys.stdout.write(val)
         sys.exit(255)
 
 
-args = sys.stdin.read().split('\n')
+args_raw = sys.stdin.read()
+args = args_raw.split('\n')
 
 assert len(args) >= 3, "Error: Must pass in action, username, and password"
 
 action, username, password = args[:3]
 
 if action == "USER_EXISTS":
-    from esp.users.models import ESPUser
-    if len(ESPUser.objects.filter(username=username)[:1]) > 0:
+    if len(ESPUser.objects.filter(username__iexact=username)[:1]) > 0:
         return_(True)
     else:
         return_(False)
 
 elif action == "AUTHENTICATE":
-    from django.contrib.auth import authenticate
-    user = authenticate(username=username, password=password)
-    if user is None:
+    # aseering 7/11/2009 -- MediaWiki enforces that the first letter of usernames be capitalized and that lookups are
+    # case-insensitive.  This isn't how Django works; so we can't use their "authenticate" builtin.
+    #from django.contrib.auth import authenticate
+    #user = authenticate(username=username, password=password)
+
+    user = ESPUser.objects.get(username__iexact=username)
+    if not user.check_password(password):
         return_(False)
 
     from esp.users.models import UserBit, GetNode
-    if UserBit.UserHasPerms(user, GetNode("Q/Wiki"), GetNode("V/Administer")):
+    if UserBit.UserHasPerms(user, GetNode("Q/Wiki"), GetNode("V/Administer"), recursive_required=True):
         return_(True)
     else:
         return_(False)
