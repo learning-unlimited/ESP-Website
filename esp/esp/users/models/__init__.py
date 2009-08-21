3dec17bafdbc08433fa811bb3bdb74e62bcff820
aseering <aseering@mit.edu>
2009-08-20 22:35:43 -0700

    Fixed the ZipCode search; turns out we don't actually want to be converting to a Decimal here

--- esp/esp/users/models/__init__.py
+++ esp/esp/users/models/__init__.py
@@ -1129,7 +1129,7 @@ class ZipCode(models.Model):
             distance from this zip code. """
         from decimal import Decimal
         try:
-            distance = Decimal(str(distance))
+            distance_decimal = Decimal(str(distance))
         except:
             raise ESPError(), '%s should be a valid decimal number!' % distance
 
@@ -1141,6 +1141,7 @@ class ZipCode(models.Model):
 
         if len(oldsearches) > 0:
             return oldsearches[0].zipcodes.split(',')
+
         all_zips = list(ZipCode.objects.exclude(id = self.id))
         winners  = [ self.zip_code ]
 
