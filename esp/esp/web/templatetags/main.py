c193eaba608a441adf332ea40132a65adec26904
Tony Valderrama <tvald@mit.edu>
2009-08-26 07:47:04 -0400

    Updated theme color selection to use filter.

--- esp/esp/web/templatetags/main.py
+++ esp/esp/web/templatetags/main.py
@@ -9,3 +9,21 @@ def split(str,splitter):
 def concat(str,text):
     return str + text
 
+theme = {
+    '': 'yellowgreen',
+    'Splash': 'blue',
+    'Spark': 'purple',
+    'HSSP': 'red',
+    'SATPrep': 'orange',
+    'Junction': 'yellow',
+    'Delve': 'lightgreen',
+    'ProveIt': 'darkgreen',
+
+    'grey': 'myesp',
+    'black': 'manage'
+}
+
+@register.filter
+def extract_theme(str):
+    str = (str + '//').split('/')[2]
+    return theme.get(str,'yellowgreen')
