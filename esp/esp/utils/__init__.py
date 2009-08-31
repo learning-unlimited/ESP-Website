77c83ea4a19cd3375a263af8920e91ee17fac898
Andrew Geng <ageng@mit.edu>
2009-08-31 00:18:37 -0400

    Add echo function; silence DataTree test by default.
    echo should be safe for non-UTF8 terminals where print isn't.
    DataTree test is now mostly silent.
        (It hasn't failed for serious reasons in a while.)
    
    Also, actually get esp.utils tests to run.
        (See http://code.djangoproject.com/ticket/7198 for more info)

---
+++ esp/esp/utils/__init__.py
@@ -0,0 +1,34 @@
+"""
+This app contains the little things that didn't really belong anywhere else.
+
+The modules in this directory are globally relevant
+    but too small to merit their own apps.
+
+The functions in this file are globally relevant, too annoying to inline,
+    and too small to merit their own modules.
+"""
+
+import sys
+
+def echo( *args, **kwargs ):
+    """
+    echo( *args, quiet=False )
+    
+    A print function that attempts to discard encoding errors.
+    Unrecognized characters become question marks.
+    Pass quiet=True to make it nop.
+    
+    >>> echo( 123 )
+    123
+    >>> echo( u'\xc3\x85ngstrom' )
+    ... ngstrom
+    >>> echo( '\xc3\x85ngstrom' )
+    Traceback (most recent call last):
+    UnicodeDecodeError: 'ascii' codec can't decode byte 0xc3 in position 0: ordinal not in range(128)
+    >>> echo( echo )
+    <function echo at ... >
+    """
+    if kwargs.has_key('quiet') and kwargs['quiet']:
+        return
+    for x in args:
+        print unicode(x).encode( sys.stdout.encoding, 'replace' )
