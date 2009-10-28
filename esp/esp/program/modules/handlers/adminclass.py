208223a9bfe9666695216b6da31fd6e7907f0d52
aseering <aseering@mit.edu>
2009-10-27 23:08:47 -0400

    Make ESPUser().getEditable() take an anchor to pass through to find_by_anchor_perms(), so that we don't get >1mb of pickled text to cache from that function just for the Dashboard

--- esp/esp/program/modules/handlers/adminclass.py
+++ esp/esp/program/modules/handlers/adminclass.py
@@ -104,7 +104,7 @@ class AdminClass(ProgramModuleObj):
         return clsTimeSlots
 
     def getClasses(self):
-        return self.user.getEditable(ClassSubject).filter(parent_program = self.program).order_by('anchor__name')
+        return self.user.getEditable(ClassSubject, qsc=self.program.anchor).order_by('anchor__name')
         
     def prepare(self, context=None):
         if context is None: context = {}
