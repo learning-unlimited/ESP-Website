b0fdfc1806dead1276a90e39d7ba696701c2e8b8
Adam Seering <aseering@esp.mit.edu>
2009-11-16 21:34:22 -0500

    Switch from dvipdf to dvipdfm in the Student Schedules PDF-ifier; it properly supports letter-size paper

--- esp/esp/web/util/latex.py
+++ esp/esp/web/util/latex.py
@@ -94,7 +94,7 @@ def gen_latex(texcode, type='pdf'):
     if type=='pdf':
         mime = 'application/pdf'
         os.system('cd %s; latex %s.tex' % (TEX_TEMP, file_base))
-        os.system('cd %s; dvipdf %s.dvi' % (TEX_TEMP, file_base))
+        os.system('cd %s; dvipdfm -p letter %s.dvi' % (TEX_TEMP, file_base))
         if remove_files:
             os.remove('%s.dvi' % file_base)
             
