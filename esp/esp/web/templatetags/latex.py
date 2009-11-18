learning-unlimited/ESP-Website

commit 41f0c81af948169baba10853be28d7443aade9a0

Escape carets and tildes in LaTeX.

esp/esp/web/templatetags/latex.py
@@ -76,6 +76,8 @@ def texescape(value):
     value = value.replace('\r\n', '\n')
     value = value.replace('\r',   '\n')
     value = value.replace('\n',   '\\\\\n')
+    value = value.replace('^',    '\\textasciicircum')
+    value = value.replace('~',    '$\sim$')
 
     value = value.encode('ascii', 'ignore')
 

commit 6747391d2474ec5b5daf84f3cff6f26dfc614b15

Oops. 41f0c81 broke superscripts. Fixing that...

esp/esp/web/templatetags/latex.py
@@ -59,6 +59,8 @@ def texescape(value):
         strings[i] = strings[i].replace('\\', special_backslash)
         for char in '&$%#_{}':
             strings[i] = strings[i].replace(char, '\\' + char)
+        strings[i] = strings[i].replace('^', '\\textasciicircum')
+        strings[i] = strings[i].replace('~', '$\sim$')
         strings[i] = strings[i].replace(special_backslash, '$\\backslash$')
     
 
@@ -81,8 +83,6 @@ def texescape(value):
     value = value.replace('\r\n', '\n')
     value = value.replace('\r',   '\n')
     value = value.replace('\n',   '\\\\\n')
-    value = value.replace('^',    '\\textasciicircum')
-    value = value.replace('~',    '$\sim$')
 
     value = value.encode('ascii', 'ignore')
 