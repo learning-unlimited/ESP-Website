fb989f2cf1bdcd9ecdc643cf932439b5398144d1
Adam Seering <aseering@esp.mit.edu>
2009-11-21 01:08:38 -0500

    Re-enable warning labels/dialogs for onsite

--- esp/esp/program/templatetags/class_render.py
+++ esp/esp/program/templatetags/class_render.py
@@ -86,7 +86,7 @@ def render_class(cls, user=None, prereg_url=None, filter=False, timeslot=None, r
         if user is not None and ESPUser(user).is_authenticated():
             prereg_url = cls.parent_program.get_learn_url() + 'ajax_addclass'
 
-    if False and user and prereg_url:
+    if user and prereg_url:
         error1 = cls.cannotAdd(user, True, request=request)
         # If we can't add the class at all, then we take that error message
         if error1:
