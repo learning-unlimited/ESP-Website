291bcf5b4906904152f1839770ccfac3662fdbe4
aseering <aseering@mit.edu>
2009-12-23 15:52:49 -0500

    Re-add the survey_results view to esp/shortterm/urls.py, since post-merge that's the urls.py file for the shortterm app

--- esp/esp/shortterm/urls.py
+++ esp/esp/shortterm/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('esp.shortterm.views',
                        (r'^school_response/?', 'school_response_form'),
                        (r'^volunteer/signup/?$', 'volunteer_signup',),
-                      )
+                       (r'^survey_results/$', 'excel_survey_responses')
+                       )
