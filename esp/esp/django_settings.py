0e6fb3ffabd01aa7f09b76d7541c5a2d8911e46a
aseering <aseering@mit.edu>
2009-12-29 19:42:37 -0500

    Comment out the SetRemoteAddrFromForwardedFor middleware, since it has been deprecated and removed already anyway so it's just displaying an error message for us

--- esp/esp/django_settings.py
+++ esp/esp/django_settings.py
@@ -124,7 +124,7 @@ TEMPLATE_LOADERS = (
 
 # Set MIDDLEWARE_LOCAL in local_settings.py to configure this
 MIDDLEWARE_GLOBAL = [
-    ( 100, 'django.middleware.http.SetRemoteAddrFromForwardedFor'),
+   #( 100, 'django.middleware.http.SetRemoteAddrFromForwardedFor'),
    #( 200, 'esp.queue.middleware.QueueMiddleware'),
     ( 300, 'esp.middleware.FixIEMiddleware'),
     ( 500, 'esp.middleware.ESPErrorMiddleware'),
