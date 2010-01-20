learning-unlimited/ESP-Website

commit 5ee165720b96a4107a561a37106e342cb7590b6b

Fix a correctness problem in DataTree.save() where it doesn't set URI's of itself or its children to be incorrect if the node's name changes.  Test case included.

esp/esp/datatree/models.py
@@ -146,6 +146,9 @@ class DataTree(models.Model):
             self.__dict__.update(obj.__dict__)
             return self
 
+        DataTree.objects.filter(QTree(below=self)).update(uri_correct=False)
+        self.uri_correct=False
+        
         old_node = DataTree.objects.get(id=self.id)
         if old_node.parent_id != self.parent_id:
             raise NotImplementedError("Have not yet written the parent moving code.")

esp/esp/datatree/tests.py
@@ -1,6 +1,55 @@
 from django.test import TestCase
 from esp.datatree.models import DataTree
+from esp.utils.custom_cache import custom_cache
 
 class DataTree__randwordtest(TestCase):
     def runTest(self):
         assert DataTree.randwordtest(limit=50)
+
+class DataTreeTest(TestCase, custom_cache):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    def test_get_by_uri(self):
+        """ Test DataTree.get_by_uri() for known corner cases """
+        path = "test/get/by/uri/node"
+
+        self.assertEqual(DataTree.objects.filter(uri=path).count(), 0)
+
+        # Test creating some nodes
+        node = DataTree.get_by_uri(path, create=True)
+        self.assertEqual(DataTree.objects.filter(uri=path).count(), 1)
+        self.assertEqual(node.uri, path)
+
+        # Try a child node
+        child_path = path + "/child"
+        child = DataTree.get_by_uri(child_path, create=True)
+        self.assertEqual(DataTree.objects.filter(uri__startswith=path).count(), 2)
+        self.assertEqual(child.get_uri(), child_path)
+
+        # Try getting the parent node
+        uri_path = "/".join(path.split('/')[:-1])
+        uri = DataTree.get_by_uri(uri_path, create=True)
+        self.assertEqual(DataTree.objects.filter(uri__startswith=uri_path).count(), 3)
+        self.assertEqual(uri.uri, uri_path)
+
+        # Try renaming a node:
+        uri.name = "uri2"
+        uri.save()
+
+        # Make sure the URI's of the child nodes are updated
+        self.assertEqual(DataTree.get_by_uri("test/get/by/uri2", create=True).get_uri(), "test/get/by/uri2")
+        self.assertEqual(DataTree.get_by_uri("test/get/by/uri2/node", create=True).get_uri(), "test/get/by/uri2/node")
+        self.assertEqual(DataTree.get_by_uri("test/get/by/uri2/node/child", create=True).get_uri(), "test/get/by/uri2/node/child")
+
+        # Try deleting a node; make sure it goes away
+        child.delete()
+        try:
+            node = DataTree.get_by_uri("test/get/by/uri2/node/child")
+        except DataTree.DoesNotExist:
+            node = None
+
+        self.assertEqual(node, None)

commit 1ca12208c385fa28fbf90d4269121dc91cdfb61e

DataTree fix: uri_correct was always False
Bug introduced in 5ee165720b96a4107a561a37106e342cb7590b6b.

esp/esp/datatree/models.py
@@ -145,13 +145,15 @@ class DataTree(models.Model):
             obj = DataTree.objects.create(name=self.name, friendly_name=self.name, parent=self.parent, start_size=start_size, uri=self.uri)
             self.__dict__.update(obj.__dict__)
             return self
-
-        DataTree.objects.filter(QTree(below=self)).update(uri_correct=False)
-        self.uri_correct=False
         
         old_node = DataTree.objects.get(id=self.id)
         if old_node.parent_id != self.parent_id:
             raise NotImplementedError("Have not yet written the parent moving code.")
+        
+        if not uri_fix:
+            if not self.parent.uri_correct or self.uri != '%s%s%s' % (self.parent.uri, DataTree.DELIMITER, self.name):
+                DataTree.objects.filter(QTree(below=self)).update(uri_correct=False)
+                self.uri_correct=False
 
         self.save_db(*self.SAFE_COLS)
 

esp/esp/datatree/tests.py
@@ -40,10 +40,16 @@ class DataTreeTest(TestCase, custom_cache):
         uri.name = "uri2"
         uri.save()
 
+        # Make sure the URI is marked dirty
+        self.assertEqual(uri.uri_correct, False)
+
         # Make sure the URI's of the child nodes are updated
         self.assertEqual(DataTree.get_by_uri("test/get/by/uri2", create=True).get_uri(), "test/get/by/uri2")
         self.assertEqual(DataTree.get_by_uri("test/get/by/uri2/node", create=True).get_uri(), "test/get/by/uri2/node")
         self.assertEqual(DataTree.get_by_uri("test/get/by/uri2/node/child", create=True).get_uri(), "test/get/by/uri2/node/child")
+
+        # Is it marked clean now that get_uri() has been called?
+        self.assertEqual(DataTree.objects.get(uri="test/get/by/uri2").uri_correct, True)
 
         # Try deleting a node; make sure it goes away
         child.delete()