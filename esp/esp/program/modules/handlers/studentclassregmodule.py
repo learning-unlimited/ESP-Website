d8ff563e2fe41ebbf9004729fc3622f997827728
Michael Price <price@price-desktop.(none)>
2010-01-24 13:46:28 -0500

    Add cached function for common query of all timeslots in a program

--- esp/esp/program/modules/handlers/studentclassregmodule.py
+++ esp/esp/program/modules/handlers/studentclassregmodule.py
@@ -157,7 +157,7 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
     @needs_student
     def prepare(self, context={}):
         regProf = RegistrationProfile.getLastForProgram(self.user, self.program)
-        timeslots = list(self.program.getTimeSlots(exclude_types=[]).order_by('start').select_related('event_type'))
+        timeslots = self.program.getTimeSlotList(exclude_compulsory=False)
         classList = ClassSection.prefetch_catalog_data(regProf.preregistered_classes())
         
         prevTimeSlot = None
