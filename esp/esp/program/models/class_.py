965f1671b6b5354702c3c8a8b147cbd84fd7ed82
Adam Seering <aseering@mit.edu>
2010-01-26 16:21:30 -0500

    Fixed a bug in Section.capacity's use of max_class_capacity (it was being completely ignored)

--- esp/esp/program/models/class_.py
+++ esp/esp/program/models/class_.py
@@ -328,7 +328,7 @@ class ClassSection(models.Model):
     @cache_function
     def _get_capacity(self, ignore_changes=False):
         if self.max_class_capacity is not None:
-            ans = self.max_class_capacity
+            return self.max_class_capacity
 
         rooms = self.initial_rooms()
         if len(rooms) == 0:
@@ -344,6 +344,7 @@ class ClassSection(models.Model):
             return int(ans)
 
     _get_capacity.depend_on_m2m(lambda:ClassSection, 'meeting_times', lambda sec, event: {'self': sec})
+    _get_capacity.depend_on_row(lambda:ClassSection, lambda r: {'self': r})
     _get_capacity.depend_on_model(lambda:ClassSubject)
     _get_capacity.depend_on_model(lambda: Resource)
     _get_capacity.depend_on_row(lambda:ResourceRequest, lambda r: {'self': r.target})
