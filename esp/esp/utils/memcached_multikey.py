df8c5963fd34dd4d2c7bc4393434216cc4f7af1c
Adam Seering <adam@fusion.mit.edu>
2010-02-21 17:01:26 -0500

    memcached may report concurrency errors if get_many/set_many invocations happen too close to each other.  Wrap these in a procedure that will retry up to (arbitrarily) 8 times before failing outright.
    Note for the future:  Almost all of our read-based uses of the cache are safe if they declare a cache miss when there wasn't one.  (That's what python-memcached did.)  We may want to make this be the default behavior after a few retries, so that we don't error at all.  (Though we should log these cases in case we hit a memcached bug and start having no cache misses.)

--- esp/esp/utils/memcached_multikey.py
+++ esp/esp/utils/memcached_multikey.py
@@ -2,6 +2,7 @@
 from django.core.cache.backends.base import BaseCache
 from django.core.cache.backends.memcached import CacheClass as MemcacheCacheClass
 from esp import settings
+from esp.utils.try_multi import try_multi
 import urllib
 
 try:
@@ -35,21 +36,26 @@ class CacheClass(BaseCache):
                 assert False, "Data size for key '%s' too large: %d bytes" % (key, data_size)
             elif data_size > CACHE_WARNING_SIZE:
                 print "Data size for key '%s' is dangerously large: %d bytes" % (key, data_size)
-    
+
+    @try_multi(8)
     def add(self, key, value, timeout=0):
         self._failfast_test(key, value)
         return self._wrapped_cache.add(self.make_key(key), value, timeout=timeout)
 
+    @try_multi(8)
     def get(self, key, default=None):
         return self._wrapped_cache.get(self.make_key(key), default=default)
 
+    @try_multi(8)
     def set(self, key, value, timeout=0):
         self._failfast_test(key, value)
         return self._wrapped_cache.set(self.make_key(key), value, timeout=timeout)
 
+    @try_multi(8)
     def delete(self, key):
         return self._wrapped_cache.delete(self.make_key(key))
 
+    @try_multi(8)
     def get_many(self, keys):
         wrapped_ans = self._wrapped_cache.get_many([self.make_key(key) for key in keys])
         ans = {}
@@ -58,10 +64,12 @@ class CacheClass(BaseCache):
         return ans
 
     # Django 1.1 feature
+    # Don't try_multi, that could be all kinds of bad...
     def incr(self, key, delta=1):
         return self._wrapped_cache.incr(self.make_key(key), delta)
 
     # Django 1.1 feature
+    # Don't try_multi, that could be all kinds of bad...
     def decr(self, key, delta=1):
         return self._wrapped_cache.decr(self.make_key(key), delta)
 
