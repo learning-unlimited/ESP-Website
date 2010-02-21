a8de2d037c945635aee5269deb2aaead30354b7e
Adam Seering <aseering@esp.mit.edu>
2010-02-21 11:12:21 -0500

    Add some more sanity checks, pertaining to classes being approved (or not) when they should be

--- esp/esp/utils/sanity_check_schedules.py
+++ esp/esp/utils/sanity_check_schedules.py
@@ -66,11 +66,42 @@ def sanity_check_resource_assignments(prog):
     return retVal
 
 
+def sanity_check_no_unapproved_classes(prog):
+    # If you're at the point of sanity checking a schedule,
+    # there shouldn't be any classes left whose status isn't some varity of
+    # "approved" or "rejected".
+    from esp.program.models import ClassSubject, ClassSection
+
+    retVal = True
+    for cls in ClassSubject.objects.filter(parent_program=prog, status=0):  # Just for future-proofing, don't trust prog.students(), etc; in case we decide to have those functions ignore rejected classes or the like
+        print "Error in class %s: ClassSubject has been neither approved nor rejected!" % (cls)
+        retVal = False
+
+    for sec in ClassSection.objects.filter(parent_class__parent_program=prog).select_related():
+        if sec.status != sec.parent_class.status:
+            if sec.status < 0 and sec.parent_class.status > 0:
+                # We probably approved the class but only want to schedule one of its sections
+                print "Warning in class %s: ClassSubject has status %s, but its child Section %s has status %s (this is unusual, but it's ok if you meant to schedule the class but punt one of its sections)" % (sec.parent_class, sec.parent_class.status, sec, sec.status)
+            else:
+                print "Error in class %s: ClassSubject has status %s, but its child Section %s has status %s (the two statuses should be the same)" % (sec.parent_class, sec.parent_class.status, sec, sec.status)
+                retVal = False
+
+    return retVal
+
+
+def sanity_check_approved_classes_are_scheduled(prog):
+    for sec in prog.sections().filter(status__gt=0):
+        if len(sec.classrooms()) < 1:
+            print "Error in class %s: Section is approved but not scheduled!" % (sec)
+    
+
 def sanity_check_schedules(prog):
     checkers = [ sanity_check_class_overlaps,
                  sanity_check_teacher_availabilities,
                  sanity_check_resource_assignments,
-                 #sanity_check_clobber_lunch_Spark2010,
+                 sanity_check_no_unapproved_classes,
+                 sanity_check_approved_classes_are_scheduled,
+                 sanity_check_clobber_lunch_Spark2010,
                  ]
 
 
