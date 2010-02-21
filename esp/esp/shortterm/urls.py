26d9c00a4d800483ccce571329f409a46ebda194
Andrew Geng <ageng@mit.edu>
2010-02-21 11:13:36 -0500

    Spark Survey code.  Putting in git because we'll be sad if it's not there if/when we set up Amazon replication...

--- esp/esp/shortterm/urls.py
+++ esp/esp/shortterm/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('esp.shortterm.views',
                        (r'^school_response/?', 'school_response_form'),
                        (r'^volunteer/signup/?$', 'volunteer_signup',),
-                       (r'^survey_results/$', 'excel_survey_responses')
+                       (r'^survey_results/$', 'excel_survey_responses'),
+					   (r'^logistics_quiz_start/?$', 'logistics_quiz_start'),
+					   (r'^logistics_quiz_check/?$', 'logistics_quiz_check'),
                        )
