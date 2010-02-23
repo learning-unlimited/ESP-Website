f07965416836d300b60232e3bffa62ea71cbc799
Michael Price <price@kilentra.net>
2010-02-22 20:39:16 -0500

    Fixes for small bugs discovered with auto-lunch script

--- esp/esp/program/models/class_.py
+++ esp/esp/program/models/class_.py
@@ -1223,7 +1223,7 @@ class ClassSubject(models.Model):
     meeting_times = models.ManyToManyField(Event, blank=True)
 
     def get_sections(self):
-        if not hasattr(self, "_sections"):
+        if not hasattr(self, "_sections") or self._sections is None:
             self._sections = self.sections.all()
 
         return self._sections
@@ -1344,6 +1344,8 @@ class ClassSubject(models.Model):
         new_section.status = status
         new_section.save()
         self.sections.add(new_section)
+        
+        self._sections = None
 
         return new_section
 
