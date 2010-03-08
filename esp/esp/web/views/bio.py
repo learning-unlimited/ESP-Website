a0a5350d4ec132b5a216a28be499d27115e2e4f8
Adam Seering <aseering@mit.edu>
2010-03-08 00:28:56 -0500

    Don't display ArchiveClass recorrds if the corresponding real class is still around

--- esp/esp/web/views/bio.py
+++ esp/esp/web/views/bio.py
@@ -159,8 +159,19 @@ def bio_user(request, founduser):
         teacherbio.bio     = 'Not Available.'
 
 
-    recent_classes = founduser.getTaughtClassesAll().filter(status__gte=10).filter(meeting_times__end__lte=datetime.now()).order_by('anchor__parent__parent')
-    classes = ArchiveClass.getForUser(founduser)
+    # Only show classes that were approved and that have already run
+    # If we show classes that are yet to run, it's possible that
+    # the corresponding course catalog isn't up yet, in which case
+    # the teacher-bio pages leak information.
+    # Also, sort by the order of the corresponding program's DataTree node.
+    # This should roughly order by program date; at the least, it will
+    # cluster listed classes by program.
+    recent_classes = founduser.getTaughtClassesAll().filter(status__gte=10).filter(meeting_times__end__lte=datetime.now()).order_by('-anchor__parent__parent__id')
+
+    # Ignore archived classes where we still have a log of the original class
+    # Archives lose information; so, display the original form if we still have it
+    cls_ids = [x.id for x in recent_classes]
+    classes = ArchiveClass.getForUser(founduser).exclude(original_id__in=cls_ids)
 
     return render_to_response('users/teacherbio.html', request, GetNode('Q/Web/Bio'),
                               {'biouser': founduser,
