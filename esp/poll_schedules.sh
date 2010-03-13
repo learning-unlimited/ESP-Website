cf0fcd40ebe773de5a9b1148381020368e9cd8c7
Yuri Lin <rye@mit.edu>
2010-03-12 20:00:22 -0500

    Fix up the autoprinter script for Spark

--- esp/poll_schedules.sh
+++ esp/poll_schedules.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PROGRAM="Spark"
+INSTANCE="2010"
+
 echo "What printer do you want to print to? (lpr printer name)"
 read PRINTERNAME
 
@@ -14,18 +17,18 @@ echo
 CURL_COOKIE_STORE="curl_cookies.txt"
 
 echo "Logging in..."
-curl -c ${CURL_COOKIE_STORE} "https://esp.mit.edu/admin/" >/dev/null 2>&1
-curl -b ${CURL_COOKIE_STORE} -c ${CURL_COOKIE_STORE} -d "username=${USERNAME}&password=${PASSWORD}&this_is_the_login_form=1" "https://esp.mit.edu/admin/" >/dev/null 2>&1
+curl -c "${CURL_COOKIE_STORE}" "https://esp.mit.edu/admin/" >/dev/null 2>&1
+curl -b "${CURL_COOKIE_STORE}" -c "${CURL_COOKIE_STORE}" -d "username=${USERNAME}&password=${PASSWORD}&this_is_the_login_form=1" "https://esp.mit.edu/admin/" >/dev/null 2>&1
 echo "Logged in!"
 
 while (true); do
 	TMPFILE="`mktemp -t esp_print_tmp`"	
-	curl -b "${CURL_COOKIE_STORE}" "https://esp.mit.edu/onsite/Splash/2009/printschedules?gen_img&img_format=ps" -o "${TMPFILE}" 2>/dev/null
+	curl -b "${CURL_COOKIE_STORE}" "https://esp.mit.edu/onsite/${PROGRAM}/${INSTANCE}/printschedules?gen_img&img_format=ps" -o "${TMPFILE}" 2>/dev/null
 
 	if [ -n "`cat "${TMPFILE}"`" ]; then
+		lpr -P${PRINTERNAME} < "${TMPFILE}"
 		echo -n "Printed! "
 		date
-		lpr -P${PRINTERNAME} < "${TMPFILE}"
 		rm "${TMPFILE}"
 	else
 		echo -n "."
