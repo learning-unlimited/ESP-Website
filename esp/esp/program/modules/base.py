2629105220c1f629dab4db1961162282a4aea153
Adam Seering <aseering@mit.edu>
2010-04-06 19:32:34 -0400

    Edit @needs_teacher decorator to respect the allowed_teacher_types Tag

--- esp/esp/program/modules/base.py
+++ esp/esp/program/modules/base.py
@@ -520,10 +520,10 @@ def usercheck_usetl(method):
 
 def needs_teacher(method):
     def _checkTeacher(moduleObj, request, *args, **kwargs):
-        
+        allowed_teacher_types = Tag.getTag("allowed_teacher_types", moduleObj.program, default='').split(",")
         if not moduleObj.user or not moduleObj.user.is_authenticated():
             return HttpResponseRedirect('%s?%s=%s' % (LOGIN_URL, REDIRECT_FIELD_NAME, quote(request.get_full_path())))
-        if not moduleObj.user.isTeacher() and not moduleObj.user.isAdmin(moduleObj.program):
+        if not moduleObj.user.isTeacher() and not moduleObj.user.isAdmin(moduleObj.program) and not (set(moduleObj.getUserTypes()) & set(allowed_teacher_types)):
             return render_to_response('errors/program/notateacher.html', request, (moduleObj.program, 'teach'), {})
         return method(moduleObj, request, *args, **kwargs)
 
