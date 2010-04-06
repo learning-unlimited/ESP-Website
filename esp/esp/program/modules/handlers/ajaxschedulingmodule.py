17a849e3ecc855ddabc5c2467504f5a98dcfdcb8
Adam Seering <aseering@mit.edu>
2010-04-06 19:33:18 -0400

    Fix some typos in ajaxschedulingmodule.py

--- esp/esp/program/modules/handlers/ajaxschedulingmodule.py
+++ esp/esp/program/modules/handlers/ajaxschedulingmodule.py
@@ -105,7 +105,7 @@ class AJAXSchedulingModule(ProgramModuleObj):
                 'class_id': s.parent_class_id,
                 'block_contents': render_to_string(self.baseDir() + 'section_block_label.html', {'sec': s}),
                 'emailcode': s.emailcode(),
-                'text': s.title,
+                'text': s.title(),
                 'category': s.category.category,
                 'length': float(s.duration),
                 'teachers': teacher_dict[s.parent_class.anchor_id],
@@ -171,7 +171,7 @@ class AJAXSchedulingModule(ProgramModuleObj):
         resources_for_user = defaultdict(list)
 
         for resource in resources:
-            resources_for_user[resource['user_id']].append(resource['event__id'])
+            resources_for_user[resource['user_id']].append(resource['event_id'])
         
         teacher_dicts = [
             {   'uid': t.id,
