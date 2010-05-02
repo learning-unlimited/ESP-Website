ee148d64f8a2347f9642b9ac3fc2eec681093638
Michael Price <price@kilentra.net>
2010-05-02 15:31:45 -0400

    Add missing import to force_show_required_modules

--- esp/esp/program/modules/handlers/programprintables.py
+++ esp/esp/program/modules/handlers/programprintables.py
@@ -28,7 +28,7 @@ MIT Educational Studies Program,
 Phone: 617-253-4882
 Email: web@esp.mit.edu
 """
-from esp.program.modules.base import ProgramModuleObj, needs_teacher, needs_student, needs_admin, usercheck_usetl, needs_onsite, main_call, aux_call
+from esp.program.modules.base import ProgramModuleObj, needs_teacher, needs_student, needs_admin, usercheck_usetl, needs_onsite, needs_onsite_no_switchback, main_call, aux_call
 from esp.program.modules import module_ext
 from esp.web.util        import render_to_response
 from django.contrib.auth.decorators import login_required
