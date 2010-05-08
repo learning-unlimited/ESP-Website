4d40def9e160daaaebcc309e204521052510aeab
Michael Price <price@kilentra.net>
2010-05-08 17:48:00 -0400

    Fix program modules migrations

--- esp/esp/program/modules/migrations/0004_modules_satprepteachermoduleinfo.py
+++ esp/esp/program/modules/migrations/0004_modules_satprepteachermoduleinfo.py
@@ -2,167 +2,21 @@
 from south.db import db
 from django.db import models
 from esp.program.modules.models import *
+from esp.db.fields import AjaxForeignKey
 
 class Migration:
     
     def forwards(self, orm):
+
+        # Creating unique_together for [user, program] on SATPrepTeacherModuleInfo.
+        db.create_unique('modules_satprepteachermoduleinfo', ['user_id', 'program_id'])
         
-        # Adding model 'StudentClassRegModuleInfo'
-        db.create_table('modules_studentclassregmoduleinfo', (
-            ('id', orm['modules.StudentClassRegModuleInfo:id']),
-            ('module', orm['modules.StudentClassRegModuleInfo:module']),
-            ('enforce_max', orm['modules.StudentClassRegModuleInfo:enforce_max']),
-            ('class_cap_multiplier', orm['modules.StudentClassRegModuleInfo:class_cap_multiplier']),
-            ('class_cap_offset', orm['modules.StudentClassRegModuleInfo:class_cap_offset']),
-            ('signup_verb', orm['modules.StudentClassRegModuleInfo:signup_verb']),
-            ('use_priority', orm['modules.StudentClassRegModuleInfo:use_priority']),
-            ('priority_limit', orm['modules.StudentClassRegModuleInfo:priority_limit']),
-            ('register_from_catalog', orm['modules.StudentClassRegModuleInfo:register_from_catalog']),
-            ('visible_enrollments', orm['modules.StudentClassRegModuleInfo:visible_enrollments']),
-            ('visible_meeting_times', orm['modules.StudentClassRegModuleInfo:visible_meeting_times']),
-            ('show_unscheduled_classes', orm['modules.StudentClassRegModuleInfo:show_unscheduled_classes']),
-            ('confirm_button_text', orm['modules.StudentClassRegModuleInfo:confirm_button_text']),
-            ('view_button_text', orm['modules.StudentClassRegModuleInfo:view_button_text']),
-            ('cancel_button_text', orm['modules.StudentClassRegModuleInfo:cancel_button_text']),
-            ('temporarily_full_text', orm['modules.StudentClassRegModuleInfo:temporarily_full_text']),
-            ('cancel_button_dereg', orm['modules.StudentClassRegModuleInfo:cancel_button_dereg']),
-            ('progress_mode', orm['modules.StudentClassRegModuleInfo:progress_mode']),
-            ('send_confirmation', orm['modules.StudentClassRegModuleInfo:send_confirmation']),
-            ('show_emailcodes', orm['modules.StudentClassRegModuleInfo:show_emailcodes']),
-        ))
-        db.send_create_signal('modules', ['StudentClassRegModuleInfo'])
-        
-        # Adding model 'RemoteProfile'
-        db.create_table('modules_remoteprofile', (
-            ('id', orm['modules.RemoteProfile:id']),
-            ('user', orm['modules.RemoteProfile:user']),
-            ('program', orm['modules.RemoteProfile:program']),
-            ('volunteer', orm['modules.RemoteProfile:volunteer']),
-            ('need_bus', orm['modules.RemoteProfile:need_bus']),
-        ))
-        db.send_create_signal('modules', ['RemoteProfile'])
-        
-        # Adding model 'ClassRegModuleInfo'
-        db.create_table('modules_classregmoduleinfo', (
-            ('id', orm['modules.ClassRegModuleInfo:id']),
-            ('module', orm['modules.ClassRegModuleInfo:module']),
-            ('allow_coteach', orm['modules.ClassRegModuleInfo:allow_coteach']),
-            ('set_prereqs', orm['modules.ClassRegModuleInfo:set_prereqs']),
-            ('display_times', orm['modules.ClassRegModuleInfo:display_times']),
-            ('times_selectmultiple', orm['modules.ClassRegModuleInfo:times_selectmultiple']),
-            ('class_max_duration', orm['modules.ClassRegModuleInfo:class_max_duration']),
-            ('class_max_size', orm['modules.ClassRegModuleInfo:class_max_size']),
-            ('class_size_step', orm['modules.ClassRegModuleInfo:class_size_step']),
-            ('director_email', orm['modules.ClassRegModuleInfo:director_email']),
-            ('class_durations', orm['modules.ClassRegModuleInfo:class_durations']),
-            ('teacher_class_noedit', orm['modules.ClassRegModuleInfo:teacher_class_noedit']),
-            ('allowed_sections', orm['modules.ClassRegModuleInfo:allowed_sections']),
-            ('session_counts', orm['modules.ClassRegModuleInfo:session_counts']),
-            ('num_teacher_questions', orm['modules.ClassRegModuleInfo:num_teacher_questions']),
-            ('num_class_choices', orm['modules.ClassRegModuleInfo:num_class_choices']),
-            ('color_code', orm['modules.ClassRegModuleInfo:color_code']),
-            ('allow_lateness', orm['modules.ClassRegModuleInfo:allow_lateness']),
-            ('ask_for_room', orm['modules.ClassRegModuleInfo:ask_for_room']),
-            ('progress_mode', orm['modules.ClassRegModuleInfo:progress_mode']),
-        ))
-        db.send_create_signal('modules', ['ClassRegModuleInfo'])
-        
-        # Adding model 'SATPrepTeacherModuleInfo'
-        db.create_table('modules_satprepteachermoduleinfo', (
-            ('id', orm['modules.SATPrepTeacherModuleInfo:id']),
-            ('sat_math', orm['modules.SATPrepTeacherModuleInfo:sat_math']),
-            ('sat_writ', orm['modules.SATPrepTeacherModuleInfo:sat_writ']),
-            ('sat_verb', orm['modules.SATPrepTeacherModuleInfo:sat_verb']),
-            ('mitid', orm['modules.SATPrepTeacherModuleInfo:mitid']),
-            ('subject', orm['modules.SATPrepTeacherModuleInfo:subject']),
-            ('user', orm['modules.SATPrepTeacherModuleInfo:user']),
-            ('program', orm['modules.SATPrepTeacherModuleInfo:program']),
-            ('section', orm['modules.SATPrepTeacherModuleInfo:section']),
-        ))
-        db.send_create_signal('modules', ['SATPrepTeacherModuleInfo'])
-        
-        # Adding model 'ProgramModuleObj'
-        db.create_table('modules_programmoduleobj', (
-            ('id', orm['modules.ProgramModuleObj:id']),
-            ('program', orm['modules.ProgramModuleObj:program']),
-            ('module', orm['modules.ProgramModuleObj:module']),
-            ('seq', orm['modules.ProgramModuleObj:seq']),
-            ('required', orm['modules.ProgramModuleObj:required']),
-        ))
-        db.send_create_signal('modules', ['ProgramModuleObj'])
-        
-        # Adding model 'CreditCardModuleInfo'
-        db.create_table('modules_creditcardmoduleinfo', (
-            ('id', orm['modules.CreditCardModuleInfo:id']),
-            ('module', orm['modules.CreditCardModuleInfo:module']),
-            ('base_cost', orm['modules.CreditCardModuleInfo:base_cost']),
-        ))
-        db.send_create_signal('modules', ['CreditCardModuleInfo'])
-        
-        # Adding model 'SATPrepAdminModuleInfo'
-        db.create_table('modules_satprepadminmoduleinfo', (
-            ('id', orm['modules.SATPrepAdminModuleInfo:id']),
-            ('module', orm['modules.SATPrepAdminModuleInfo:module']),
-            ('num_divisions', orm['modules.SATPrepAdminModuleInfo:num_divisions']),
-        ))
-        db.send_create_signal('modules', ['SATPrepAdminModuleInfo'])
-        
-        # Adding model 'DBReceipt'
-        db.create_table('modules_dbreceipt', (
-            ('id', orm['modules.DBReceipt:id']),
-            ('action', orm['modules.DBReceipt:action']),
-            ('program', orm['modules.DBReceipt:program']),
-            ('receipt', orm['modules.DBReceipt:receipt']),
-        ))
-        db.send_create_signal('modules', ['DBReceipt'])
-        
-        # Adding ManyToManyField 'RemoteProfile.volunteer_times'
-        db.create_table('modules_remoteprofile_volunteer_times', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('remoteprofile', models.ForeignKey(orm.RemoteProfile, null=False)),
-            ('event', models.ForeignKey(orm['cal.Event'], null=False))
-        ))
-        
-        # Adding ManyToManyField 'RemoteProfile.bus_runs'
-        db.create_table('modules_remoteprofile_bus_runs', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('remoteprofile', models.ForeignKey(orm.RemoteProfile, null=False)),
-            ('datatree', models.ForeignKey(orm['datatree.DataTree'], null=False))
-        ))
     
     
     def backwards(self, orm):
         
-        # Deleting model 'StudentClassRegModuleInfo'
-        db.delete_table('modules_studentclassregmoduleinfo')
-        
-        # Deleting model 'RemoteProfile'
-        db.delete_table('modules_remoteprofile')
-        
-        # Deleting model 'ClassRegModuleInfo'
-        db.delete_table('modules_classregmoduleinfo')
-        
-        # Deleting model 'SATPrepTeacherModuleInfo'
-        db.delete_table('modules_satprepteachermoduleinfo')
-        
-        # Deleting model 'ProgramModuleObj'
-        db.delete_table('modules_programmoduleobj')
-        
-        # Deleting model 'CreditCardModuleInfo'
-        db.delete_table('modules_creditcardmoduleinfo')
-        
-        # Deleting model 'SATPrepAdminModuleInfo'
-        db.delete_table('modules_satprepadminmoduleinfo')
-        
-        # Deleting model 'DBReceipt'
-        db.delete_table('modules_dbreceipt')
-        
-        # Dropping ManyToManyField 'RemoteProfile.volunteer_times'
-        db.delete_table('modules_remoteprofile_volunteer_times')
-        
-        # Dropping ManyToManyField 'RemoteProfile.bus_runs'
-        db.delete_table('modules_remoteprofile_bus_runs')
-        
+        # Deleting unique_together for [user, program] on SATPrepTeacherModuleInfo.
+        db.delete_unique('modules_satprepteachermoduleinfo', ['user_id', 'program_id'])
     
     
     models = {
@@ -194,7 +48,7 @@ class Migration:
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
         'cal.event': {
-            'anchor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['datatree.DataTree']"}),
+            'anchor': ('AjaxForeignKey', ["orm['datatree.DataTree']"], {}),
             'description': ('django.db.models.fields.TextField', [], {}),
             'end': ('django.db.models.fields.DateTimeField', [], {}),
             'event_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cal.EventType']"}),
@@ -220,7 +74,7 @@ class Migration:
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'lock_table': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'child_set'", 'null': 'True', 'to': "orm['datatree.DataTree']"}),
+            'parent': ('AjaxForeignKey', ["orm['datatree.DataTree']"], {'related_name': "'child_set'", 'null': 'True', 'blank': 'True'}),
             'range_correct': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
             'rangeend': ('django.db.models.fields.IntegerField', [], {}),
             'rangestart': ('django.db.models.fields.IntegerField', [], {}),
@@ -272,7 +126,7 @@ class Migration:
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'need_bus': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
             'program': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['program.Program']", 'null': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'user': ('AjaxForeignKey', ["orm['auth.User']"], {'null': 'True', 'blank': 'True'}),
             'volunteer': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
             'volunteer_times': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['cal.Event']", 'blank': 'True'})
         },
@@ -291,7 +145,7 @@ class Migration:
             'sat_writ': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'section': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
             'subject': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+            'user': ('AjaxForeignKey', ["orm['auth.User']"], {'null': 'True', 'blank': 'True'})
         },
         'modules.studentclassregmoduleinfo': {
             'cancel_button_dereg': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
@@ -308,7 +162,7 @@ class Migration:
             'send_confirmation': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
             'show_emailcodes': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
             'show_unscheduled_classes': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
-            'signup_verb': ('django.db.models.fields.related.ForeignKey', [], {'default': "orm['datatree.DataTree'].objects.get(pk=1269)", 'to': "orm['datatree.DataTree']"}),
+            'signup_verb': ('AjaxForeignKey', ["orm['datatree.DataTree']"], {'default': " lambda :GetNode(REG_VERB_BASE+'/Enrolled')"}),
             'temporarily_full_text': ('django.db.models.fields.CharField', [], {'default': "'Class temporarily full; please check back later'", 'max_length': '255'}),
             'use_priority': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
             'view_button_text': ('django.db.models.fields.CharField', [], {'default': "'View Receipt'", 'max_length': '80'}),
@@ -321,7 +175,7 @@ class Migration:
             'symbol': ('django.db.models.fields.CharField', [], {'default': "'?'", 'max_length': '1'})
         },
         'program.program': {
-            'anchor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['datatree.DataTree']"}),
+            'anchor': ('AjaxForeignKey', ["orm['datatree.DataTree']"], {}),
             'class_categories': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['program.ClassCategories']"}),
             'class_size_max': ('django.db.models.fields.IntegerField', [], {}),
             'class_size_min': ('django.db.models.fields.IntegerField', [], {}),
