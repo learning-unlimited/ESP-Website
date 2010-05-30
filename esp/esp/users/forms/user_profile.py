fe9cd37d1a49f7c5340cf1ed710ceed1f4a540d9
Michael Price <price@kilentra.net>
2010-05-30 00:26:28 -0400

    Add user profile forms contributed from Univ. of Chicago site

--- esp/esp/users/forms/user_profile.py
+++ esp/esp/users/forms/user_profile.py
@@ -4,6 +4,8 @@ from esp.utils.forms import SizedCharField, FormWithRequiredCss, FormUnrestricte
 from esp.db.forms import AjaxForeignKeyNewformField
 from esp.utils.widgets import SplitDateWidget
 from esp.users.models import K12School, StudentInfo
+from esp.utils.defaultclass import defaultclass
+from datetime import datetime
 import re
 
 # SRC: esp/program/manipulators.py
@@ -234,6 +236,7 @@ GuardianInfoForm.base_fields['num_kids'].widget.attrs['maxlength'] = 16
 
 class StudentProfileForm(UserContactForm, EmergContactForm, GuardContactForm, StudentInfoForm):
     """ Form for student profiles """
+StudentProfileForm = defaultclass(StudentProfileForm)
 
 class TeacherProfileForm(TeacherContactForm, TeacherInfoForm):
     """ Form for teacher profiles """
@@ -244,13 +247,51 @@ class GuardianProfileForm(UserContactForm, GuardianInfoForm):
 class EducatorProfileForm(UserContactForm, EducatorInfoForm):
     """ Form for educator profiles """
 
+class VisitingUserInfo(FormUnrestrictedOtherUser):
+    profession = SizedCharField(length=12, max_length=64, required=False)
+
 class MinimalUserInfo(FormUnrestrictedOtherUser):
     first_name = SizedCharField(length=25, max_length=64)
     last_name = SizedCharField(length=30, max_length=64)
     e_mail = forms.EmailField()
     address_street = SizedCharField(length=40, max_length=100)
     address_city = SizedCharField(length=20, max_length=50)
-    address_state = forms.ChoiceField(choices=zip(_states,_states), initial="IL")
+    address_state = forms.ChoiceField(choices=zip(_states,_states))
     address_zip = SizedCharField(length=5, max_length=5)
     address_postal = forms.CharField(required=False, widget=forms.HiddenInput())
 
+_grad_years = range(datetime.now().year, datetime.now().year + 6)
+
+class UofCProfileForm(MinimalUserInfo):
+    graduation_year = forms.ChoiceField(choices=zip(_grad_years, _grad_years))
+    major = SizedCharField(length=30, max_length=32, required=False)
+
+    def clean_graduation_year(self):
+        gy = self.cleaned_data['graduation_year'].strip()
+        try:
+            gy = str(abs(int(gy)))
+        except:
+            if gy != 'G':
+                gy = 'N/A'
+        return gy
+    
+class AlumProfileForm(MinimalUserInfo):
+    """ This is the visiting-teacher contact form as used by UChicago's Ripple program """
+    graduation_year = SizedCharField(length=4, max_length=4, required=False)
+    major = SizedCharField(length=30, max_length=32, required=False)
+
+    def clean_graduation_year(self):
+        gy = self.cleaned_data['graduation_year'].strip()
+        try:
+            gy = str(abs(int(gy)))
+        except:
+            if gy != 'G':
+                gy = 'N/A'
+        return gy
+
+class UofCProfForm(MinimalUserInfo):
+    major = SizedCharField(length=30, max_length=32, label="Department", required=False)
+
+class VisitingGenericUserProfileForm(MinimalUserInfo):
+    """ This is a form for a generic visitor user """
+    major = SizedCharField(length=30, max_length=32, label="Profession", required=False)
