a3309d0ecfb05f060bf7a9e2ac32a64b4a917855
Michael Price <price@kilentra.net>
2010-05-30 00:33:21 -0400

    Remove old battlescreen code from myesp views

--- esp/esp/web/views/myesp.py
+++ esp/esp/web/views/myesp.py
@@ -36,7 +36,6 @@ from esp.miniblog.models import AnnouncementLink, Entry
 from esp.miniblog.views import preview_miniblog
 from esp.program.models import Program, RegistrationProfile, ClassSubject
 from django.http import Http404, HttpResponseRedirect
-#from icalendar import Calendar, Event as CalEvent, UTC
 import datetime
 from esp.middleware import ESPError
 from esp.users.forms.password_reset import UserPasswdForm
@@ -78,142 +77,6 @@ def myesp_home(request, module):
 	annlinks = [x.html() for x in annlinks.filter(Q(highlight_expire__isnull=True) | Q(highlight_expire__gte=datetime.datetime.now()))]
 	ann = entries + annlinks
 	return render_to_response('display/battlescreen', request, GetNode('Q/Web/myesp'), {'announcements': ann})
-
-#	Format for battlescreens 			Michael P
-#	----------------------------------------------
-#	The battle screen template is good for drawing individual blocks of helpful stuff.
-#	So, it passes the template an array called blocks.
-
-#	Each variable in the blocks array has:
-#	-	title
-#	-	array of 'header' html strings 
-#		(in case you don't want to use separate sections)
-#	-	array of sections
-
-#	Each variable in the sections array has
-#	-	header text including links
-#	-	array of list items
-#	-	list of input items
-#	-	footer text including links / form items
-
-#	The list item is displayed with a wide cell on the left followed by a narrow cell on the right.
-#	The wide cell is like the name of some object to deal with,
-#	and the narrow cell is like the administrative actions that can be applied to the object.
-#	For example, the wide cell might have "Michael Price: Audio and Speakerbuilding" and the narrow cell
-#	might be "Approve / Reject". 
-
-#	An input item is a plain array of 4 containing
-#	-	label text
-#	-	text value (request.POST[value] = input_text)
-#	-	button label (value)
-#	-	form action (what URL the data is submitted to)
-
-#	A list item is a plain array of 3 to 5 containing
-#	-	left-hand (wide) cell text 
-#	-	left-hand (wide) cell url
-#	-	right-hand (narrow) cell html
-#	-	OPTIONAL: command string
-#	-	OPTIONAL: form post URL
-
-#	That hopefully completes the array structure needed for this thing.
-
-
-def myesp_battlescreen(request, module, admin_details = False, student_version = False):
-	"""This function is the main battlescreen for the teachers. It will go through and show the classes
-	that they can edit and display. """
-
-	# request.user just got 1-up'd
-	currentUser = ESPUser(request.user)
-
-	if request.POST:
-		#	If you clicked a button to approve or reject, first clear the "proposed" bit
-		#	by setting the end date to now.
-		if request.POST.has_key('command'):
-			try:
-				# not general yet...but started as 'edit','class','classid'...
-				command, objtype, objid = request.POST['command'].split('_')
-			except ValueError:
-				command = None
-
-			if command == 'edit' and objtype == 'class':
-				clsid = int(objid) # we have a class
-				clslist = list(ClassSubject.objects.filter(id = clsid))
-				if len(clslist) != 1:
-					assert False, 'Zero (or more than 1) classes match selected ID.'
-				clsobj = clslist[0]
-				prog = clsobj.parent_program
-				two  = prog.anchor.name
-				one  = prog.anchor.parent.name
-				return program_teacherreg2(request, 'teach', one, two, 'teacherreg', '', prog, clsobj)
-	
-	# I have no idea what structure this is, but at its simplest level,
-	# it's a dictionary
-	announcements = preview_miniblog(request)
-
-	usrPrograms = currentUser.getVisible(Program)
-
-	# get a list of editable classes
-	if student_version:
-		clslist = currentUser.getEnrolledClasses()
-	else:
-		clslist = currentUser.getEditable(ClassSubject)
-
-	fullclslist = {}
-	
-	# not the most direct way of doing it,
-	# but hey, it's O(n) in class #, which
-	# is nice.
-	for cls in clslist:
-		if fullclslist.has_key(cls.parent_program.id):
-			fullclslist[cls.parent_program.id].append(cls)
-		else:
-			fullclslist[cls.parent_program.id] = [cls]
-	
-	# I don't like adding this second structure...
-	# but django templates made me do it!
-	responseProgs = []
-
-	if admin_details:
-		admPrograms = currentUser.getEditable(Program).order_by('-id')
-	else:
-		admPrograms = []
-	
-	for prog in usrPrograms:
-		if not fullclslist.has_key(prog.id):
-			curclslist = []
-		else:
-			curclslist = fullclslist[prog.id]
-		responseProgs.append({'prog':        prog,
-				      'clslist':     curclslist,
-				      'shortened':   False,#len(curclslist) > 5,
-				      'totalclsnum': len(curclslist)})
-
-	return render_to_response('battlescreens/general.html', request, GetNode('Q/Web/myesp'), {'page_title':    'MyESP: Teacher Home Page',
-								 'progList':      responseProgs,
-								 'admin_details': admin_details,
-								 'admPrograms'  : admPrograms,
-								 'student_version': student_version,
-								 'announcements': {'announcementList': announcements[:5],
-										   'overflowed':       len(announcements) > 5,
-										   'total':            len(announcements)}})
-
-
-@login_required
-def myesp_battlescreen_admin(request, module):
-	curUser = ESPUser(request.user)
-	if curUser.isAdministrator():
-		return myesp_battlescreen(request, module, admin_details = True)
-	else:
-		raise Http404
-
-@login_required
-def myesp_battlescreen_student(request, module):
-	curUser = ESPUser(request.user)
-	if curUser.isStudent():
-		return myesp_battlescreen(request, module, student_version = True)
-	else:
-		raise Http404
-
 
 @login_required
 def myesp_switchback(request, module):
@@ -404,23 +267,11 @@ def myesp_onsite(request, module):
 		navnode = GetNode('Q/Web/myesp')
 		return render_to_response('program/pickonsite.html', request, navnode, {'progs': progs})
 
-@login_required
-def myesp_battlescreen_teacher(request, module):
-	qscs = UserBit.bits_get_qsc(user=request.user, verb=GetNode("V/Flags/UserRole/Teacher"))
-	if qscs.count() > 0:
-		return myesp_battlescreen(request, module)
-	else:
-		raise Http404	
-
-
 myesp_handlers = {
 		   'home': myesp_home,
 		   'switchback': myesp_switchback,
 		   'onsite': myesp_onsite,
 		   'passwd': myesp_passwd,
-		   'student': myesp_battlescreen_student,
-		   'teacher': myesp_battlescreen_teacher,
-		   'admin': myesp_battlescreen_admin,
 		   'profile': edit_profile
 		   }
 
