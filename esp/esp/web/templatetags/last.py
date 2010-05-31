c436c6097125f43201b1ae9f764528199603d051
Michael Price <price@kilentra.net>
2010-05-30 22:52:00 -0400

    Add startsblock, endsblock template tags

--- esp/esp/web/templatetags/last.py
+++ esp/esp/web/templatetags/last.py
@@ -11,3 +11,11 @@ def last(list):
         return list[-1]
     except:
         return ''
+        
+@register.filter
+def startsblock(value, arg):
+    return (value % int(arg)) == 0
+
+@register.filter
+def endsblock(value, arg):
+    return (value % int(arg)) == (int(arg)-1)
