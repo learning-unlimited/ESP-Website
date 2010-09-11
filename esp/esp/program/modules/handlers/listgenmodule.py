77e05ed04f4032913fadc4166f878b16cc1c87de
Michael Price <price@kilentra.net>
2010-09-11 16:31:44 -0400

    Allow selection of fields in selectList view; make HTML output sortable

--- esp/esp/program/modules/handlers/listgenmodule.py
+++ esp/esp/program/modules/handlers/listgenmodule.py
@@ -32,7 +32,125 @@ from esp.program.modules.base import ProgramModuleObj, needs_admin, main_call, a
 from esp.web.util        import render_to_response
 from esp.users.models   import ESPUser, User
 from django.db.models.query      import Q
+from django import forms
 
+"""
+
+   <th>#</th>
+   <th>ID#</th>
+   <th>Name</th>
+   <th>Address</th>
+   <th>Email</th>
+   <th>Account Date</th>
+   <th>Cell Phone</th>
+   <th>Text Msg?</th>
+   <th># of class hours</th>
+   <th>School</th>
+   <th>School (if no match)</th>
+   <th>Found out about Splash from...</th>
+
+   <td><strong>{{user.id}}</strong></td>
+   <td>{{ user.name }}</td>
+   <td>{% if user.getLastProfile.contact_user.address %}{{ user.getLastProfile.contact_user.address}}{% else %}N/A{% endif %}</td>
+   <td>{{ user.email }}</td>
+   <td>{{ user.date_joined|date:"m/d/Y" }}</td>
+   <td>{% if user.getLastProfile.contact_user.phone_cell %}{{ user.getLastProfile.contact_user.phone_cell}}{% else %}N/A{% endif %}</td>
+   <td>{% if user.getLastProfile.text_reminder %}<b>Yes</b>{% else %}No{% endif %}</td>
+   <td>{{ user.class_count }}</td>
+   <td>{% if user.getLastProfile.student_info.k12school %}{{ user.getLastProfile.student_info.k12school.name }}{% else %}N/A{% endif %}</td>
+   <td>{% if user.getLastProfile.student_info.school %}{{ user.getLastProfile.student_info.school }}{% else %}N/A{% endif %}</td>
+   <td>{% if user.getLastProfile.student_info.heardofesp %}{{ user.getLastProfile.student_info.heardofesp }}{% else %}N/A{% endif %}</td>
+   """
+
+class UserAttributeGetter(object):
+    @staticmethod
+    def getFunctions():
+        """ Enter labels for available fields here; they are sorted alphabetically by key """
+        labels = {  '01_id': 'ID',
+                    '02_address': 'Address',
+                    '03_fullname': 'Full Name',
+                    '04_lastname': 'Last Name',
+                    '05_firstname': 'First Name',
+                    '06_email': 'E-mail',
+                    '07_accountdate': 'Created Date',
+                    '08_cellphone': 'Cell Phone',
+                    '09_textmsg': 'Text Msg?',
+                    '10_classhours': 'Num Class Hrs',
+                    '11_school': 'School',
+                    '12_heard_about': 'Heard about Splash from',
+                 }
+        result = {}
+        for item in dir(UserAttributeGetter):
+            label_map = {}
+            for x in labels.keys():
+                label_map[x[3:]] = x
+            if item.startswith('get_') and item[4:] in label_map:
+                result[label_map[item[4:]]] = labels[label_map[item[4:]]]
+        return result
+    
+    def __init__(self, user, program):
+        self.user = ESPUser(user)
+        self.program = program
+        self.profile = self.user.getLastProfile()
+        
+    def get(self, attr):
+        attr = attr.lstrip('0123456789_')
+        result = getattr(self, 'get_' + attr)()
+        if result is None:
+            return 'N/A'
+        else:
+            return result
+        
+    def get_id(self):
+        return self.user.id
+        
+    def get_address(self):
+        if self.profile.contact_user:
+            return self.profile.contact_user.address()
+    
+    def get_fullname(self):
+        return self.user.name()
+        
+    def get_lastname(self):
+        return self.user.last_name
+        
+    def get_firstname(self):
+        return self.user.first_name
+        
+    def get_email(self):
+        return self.user.email
+        
+    def get_accountdate(self):
+        return self.user.date_joined.strftime("%m/%d/%Y")
+        
+    def get_cellphone(self):
+        if self.profile.contact_user:
+            return self.profile.contact_user.phone_cell
+            
+    def get_textmsg(self):
+        if self.profile.contact_user:
+            return self.profile.contact_user.receive_txt_message
+            
+    def get_classhours(self):
+        return sum([x.meeting_times.count() for x in self.user.getEnrolledSections(self.program)])
+        
+    def get_school(self):
+        if self.profile.student_info:
+            if self.profile.student_info.k12school:
+                return self.profile.student_info.k12school.name
+            else:
+                return self.profile.student_info.school
+                
+    def get_heard_about(self):
+        if self.profile.student_info:
+            return self.profile.student_info.heard_about
+            
+class ListGenForm(forms.Form):
+    attr_choices = choices=UserAttributeGetter.getFunctions().items()
+    attr_choices.sort(key=lambda x: x[0])
+
+    fields = forms.MultipleChoiceField(choices=attr_choices, initial=['id','fullname','address','cellphone','school'], widget=forms.CheckboxSelectMultiple)
+    output_type = forms.ChoiceField(choices=(('csv', 'CSV format'), ('html', 'HTML format')), initial='html')
 
 class ListGenModule(ProgramModuleObj):
     """ While far from complete, this will allow you to just generate a simple list of users matching a criteria (criteria very similar to the communications panel)."""
@@ -62,16 +180,24 @@ class ListGenModule(ProgramModuleObj):
         if not found:
             return filterObj
 
-        if not 'type' in request.GET:
-            return render_to_response(self.baseDir()+'options.html', request, (prog, tl), {'filterid': filterObj.id})
+        if request.method == 'POST' and 'fields' in request.POST:
+            form = ListGenForm(request.POST)
+            if form.is_valid():
+                labels_dict = UserAttributeGetter.getFunctions()
+                fields = [labels_dict[f] for f in form.cleaned_data['fields']]
+                output_type = form.cleaned_data['output_type']
+            
+                users = list(ESPUser.objects.filter(filterObj.get_Q()).filter(is_active=True).distinct())
+                users.sort()
+                for u in users:
+                    ua = UserAttributeGetter(u, self.program)
+                    user_fields = [ua.get(x) for x in form.cleaned_data['fields']]
+                    u.fields = user_fields
 
-        strtype = request.GET['type']
-
-        users = list(ESPUser.objects.filter(filterObj.get_Q()).filter(is_active=True).distinct())
-        users.sort()
-        for u in users:
-            u.class_count = sum([x.meeting_times.count() for x in u.getEnrolledSections(self.program)])
-        
-        return render_to_response(self.baseDir()+('list_%s.html'%strtype), request, (prog, tl), {'users': users, 'listdesc': filterObj.useful_name})
-                                                                                                 
+                return render_to_response(self.baseDir()+('list_%s.html' % output_type), request, (prog, tl), {'users': users, 'fields': fields, 'listdesc': filterObj.useful_name})
+            else:
+                return render_to_response(self.baseDir()+'options.html', request, (prog, tl), {'form': form, 'filterid': filterObj.id})
+        else:
+            form = ListGenForm()
+            return render_to_response(self.baseDir()+'options.html', request, (prog, tl), {'form': form, 'filterid': filterObj.id})
 
