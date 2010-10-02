6710e89b36446c26ff50541526fa313b61442c50
Michael Price <price@kilentra.net>
2010-10-01 23:32:50 -0400

    Re-fix bug on profile redirect for different user types

--- esp/esp/program/modules/handlers/regprofilemodule.py
+++ esp/esp/program/modules/handlers/regprofilemodule.py
@@ -92,9 +92,9 @@ class RegProfileModule(ProgramModuleObj):
 
         # Make sure we are editing the right type of profile
         if role == 'teacher' and not request.user.isTeacher():
-            return needs_teacher(self.profile)(self, request, tl, one, two, module, extra, prog)
+            return needs_teacher(self.profile)(self, request, tl, one, two, module, extra)
         if role == 'student' and not request.user.isStudent():
-            return needs_student(self.profile)(self, request, tl, one, two, module, extra, prog)
+            return needs_student(self.profile)(self, request, tl, one, two, module, extra)
 
         #   Reset e-mail address for program registrations.
         if prog is None:
