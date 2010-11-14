150bba7de64503475b658a3c97b0ec09a55e5014
Michael Price <price@kilentra.net>
2010-11-14 15:36:40 -0500

    Check for existing userbits when checking in a student

--- esp/esp/program/modules/handlers/onsitecheckinmodule.py
+++ esp/esp/program/modules/handlers/onsitecheckinmodule.py
@@ -180,7 +180,9 @@ class OnSiteCheckinModule(ProgramModuleObj):
             form = OnSiteRapidCheckinForm(request.POST)
             if form.is_valid():
                 student = ESPUser(form.cleaned_data['user'])
-                bit, created = UserBit.objects.get_or_create(user=student, qsc=prog.anchor, verb=GetNode('V/Flags/Registration/Attended'))
+                existing_bits = UserBit.valid_objects().filter(user=student, qsc=prog.anchor, verb=GetNode('V/Flags/Registration/Attended'))
+                if not existing_bits.exists():
+                    new_bit, created = UserBit.objects.get_or_create(user=student, qsc=prog.anchor, verb=GetNode('V/Flags/Registration/Attended'))
                 context['message'] = '%s %s marked as attended.' % (student.first_name, student.last_name)
                 if request.is_ajax():
                     return self.ajax_status(request, tl, one, two, module, extra, prog, context)
