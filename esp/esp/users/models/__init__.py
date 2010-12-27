78fcf8ced19c7da0afc8846732c064033cd3bf5e
Andrew Geng <ageng@mit.edu>
2010-12-26 22:01:47 -0500

    Fix a call to reg_verbs.
    'uris' argument removed in 5397c919fe0efa6f77b809d37494ba5da69a0d98,
    following the move away from using UserBits for registrations.
    
    This fixes the recent "whoops" messages upon visiting the student
    registration page of any program with StudentJunctionAppModule.

--- esp/esp/users/models/__init__.py
+++ esp/esp/users/models/__init__.py
@@ -544,7 +544,7 @@ class ESPUser(User, AnonymousUser):
         #   If priority registration is enabled, add in more verbs.
         if program:
             scrmi = program.getModuleExtension('StudentClassRegModuleInfo')
-            verb_list = scrmi.reg_verbs(uris=True)
+            verb_list = [v.name for v in scrmi.reg_verbs()]
         else:
             verb_list = ['Applied']
             
