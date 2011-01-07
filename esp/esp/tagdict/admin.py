4d3b516663aab16f24612307dbcac991a4f2f09b
Alex Dehnert <adehnert@mit.edu>
2011-01-07 03:48:42 -0500

    Make the tag admin more readable

--- esp/esp/tagdict/admin.py
+++ esp/esp/tagdict/admin.py
@@ -1,4 +1,7 @@
 from esp.tagdict.models import Tag
 from django.contrib import admin
 
-admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('key', 'value', 'target', )
+
+admin.site.register(Tag, TagAdmin)
