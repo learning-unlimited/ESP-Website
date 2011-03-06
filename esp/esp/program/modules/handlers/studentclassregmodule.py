learning-unlimited/ESP-Website

commit 1d0fdea1960cccc61908a3f2f8dbe20278e56861

Move student registrations from UserBits to new models...
This is a substantial change both in schema and in code that
accesses student registration data.  Many things are still
broken, and the bugs will be addressed in future commits.

Essential migration included:
  esp/esp/program/migrations/0015_studentreg_models.py

esp/esp/program/modules/handlers/studentclassregmodule.py
@@ -379,10 +379,10 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
 
         section = ClassSection.objects.get(id=sectionid)
         if not scrmi.use_priority:
-            error = section.cannotAdd(self.user,self.enforce_max,use_cache=False)
+            error = section.cannotAdd(self.user,self.enforce_max)
         if scrmi.use_priority or not error:
             cobj = ClassSubject.objects.get(id=classid)
-            error = cobj.cannotAdd(self.user,self.enforce_max,use_cache=False) or section.cannotAdd(self.user, self.enforce_max, use_cache=False)
+            error = cobj.cannotAdd(self.user,self.enforce_max) or section.cannotAdd(self.user, self.enforce_max)
 
         if scrmi.use_priority:
             priority = self.user.getRegistrationPriority(prog, section.meeting_times.all())
@@ -399,9 +399,9 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
                 for cls in ClassSubject.objects.filter(id__in=implication.member_id_ints):
                     #   Override size limits on subprogram classes (checkFull=False). -Michael P
                     sec = cls.default_section()
-                    if sec.cannotAdd(self.user, checkFull=False, use_cache=False):
+                    if sec.cannotAdd(self.user, checkFull=False):
                         blocked_class = cls
-                        cannotadd_error = sec.cannotAdd(self.user, checkFull=False, use_cache=False)
+                        cannotadd_error = sec.cannotAdd(self.user, checkFull=False)
                     else:
                         if sec.preregister_student(self.user, overridefull=True, automatic=True, priority=priority, prereg_verb = prereg_verb):
                             auto_classes.append(sec)
@@ -423,7 +423,7 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
             raise ESPError(False), error
         
         #   Desired priority level is 1 above current max
-        if section.preregister_student(self.user, self.user.onsite_local, False, priority, prereg_verb = prereg_verb):
+        if section.preregister_student(self.user, self.user.onsite_local, priority, prereg_verb = prereg_verb):
             bits = UserBit.objects.filter(user=self.user, verb=GetNode("V/Flags/Public"), qsc=GetNode("/".join(prog.anchor.tree_encode()) + "/Confirmation")).filter(enddate__gte=datetime.now())
             if bits.count() == 0 and Tag.getTag('confirm_on_addclass'):
                 bit = UserBit.objects.create(user=self.user, verb=GetNode("V/Flags/Public"), qsc=GetNode("/".join(prog.anchor.tree_encode()) + "/Confirmation"))
@@ -697,38 +697,16 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
     def clearslot_logic(self, request, tl, one, two, module, extra, prog):
         """ Clear the specified timeslot from a student registration and return True if there are no errors """
         
-        #   The registration verb can be anything under this.
-        v_registered_base = request.get_node('V/Flags/Registration')
-        
-        #   This query just gets worse and worse.   -Michael
-        #   Maybe a little better with QTree?   -Axiak
-        oldclasses = ClassSection.objects.filter(
-            QTree(anchor__userbit_qsc__verb__below = v_registered_base),
-            meeting_times=extra,
-            parent_class__parent_program = self.program,
-            anchor__userbit_qsc__user = self.user).distinct()
-                             
+        #   Get the sections that the student is registered for in the specified timeslot.
+        oldclasses = self.user.getSections(prog).filter(meeting_times=extra)
         #   Narrow this down to one class if we're using the priority system.
         if request.GET.has_key('sec_id'):
             oldclasses = oldclasses.filter(id=request.GET['sec_id'])
-                             
-        #classes = self.user.getEnrolledClasses()
-        class_ids = [c.parent_class.id for c in oldclasses]
-        for cls in oldclasses:
-            # Make sure deletion doesn't violate any class implications before proceeding
-            for implication in ClassImplication.objects.filter(cls__in=class_ids, enforce=True, parent__isnull=True):
-                if implication.fails_implication(self.user, without_classes=set([cls.id])):
-                    raise ESPError(False), 'This class is required for your %s class "%s"! To remove this class, please remove the one that requires it through <a href="%sstudentreg">%s Student Registration</a>.' % (implication.cls.parent_program.niceName(), implication.cls.title(), implication.cls.parent_program.get_learn_url(), implication.cls.parent_program.niceName())
-            cls.unpreregister_student(self.user)
-            
-            # Undo auto-registrations of sections
-            for implication in ClassImplication.objects.filter(cls=cls, enforce=True):
-                for auto_class in ClassSubject.objects.filter(id__in=implication.member_id_ints):
-                    auto_class.unpreregister_student(self.user)
-            
+        #   Take the student out
+        for sec in oldclasses:
+            sec.unpreregister_student(self.user)
         #   Return the ID of classes that were removed.
         return oldclasses.values_list('id', flat=True)
-
 
     @aux_call
     @needs_student
@@ -801,7 +779,7 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
         
         # Checking if we can register for the new class
         newclass = ClassSubject.objects.filter(id=classid)[0]
-        error = newclass.cannotAdd(self.user, self.enforce_max, use_cache=False)
+        error = newclass.cannotAdd(self.user, self.enforce_max)
         if error and not self.user.onsite_local:
             # Undo by re-registering the old class. Theoretically "overridefull" is okay, since they were already registered for oldclass anyway.
             oldclass.preregister_student(self.user, overridefull=True, automatic=automatic)

esp/esp/program/admin.py
@@ -38,6 +38,8 @@ from esp.program.models import VolunteerRequest, VolunteerOffer
 
 from esp.program.models import BooleanToken, BooleanExpression, ScheduleConstraint, ScheduleTestOccupied, ScheduleTestCategory, ScheduleTestSectionList
 
+from esp.program.models import RegistrationType, StudentRegistration
+
 from esp.program.models import ProgramCheckItem, ClassSection, ClassSubject, ClassCategories, ClassSizeRange
 from esp.program.models import StudentApplication, StudentAppQuestion, StudentAppResponse, StudentAppReview
 
@@ -128,6 +130,9 @@ admin.site.register(VolunteerOffer)
 class ProgramCheckItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'program')
 admin.site.register(ProgramCheckItem, ProgramCheckItemAdmin)
+
+admin.site.register(RegistrationType)
+admin.site.register(StudentRegistration)
 
 class SectionAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'friendly_times', 'status', 'duration')

esp/esp/program/models/__init__.py
@@ -1757,6 +1757,65 @@ class VolunteerOffer(models.Model):
     phone = PhoneNumberField(blank=True, null=True)
     
     
+
+""" This class provides the information that was provided by the DataTree
+    anchor of each Userbit.  For example:
+        URI V/Flags/Registration/Enrolled (name = 'Enrolled') -> 'name'
+        Friendly name 'Student is enrolled in the class' -> 'description'
+    In general, intermediate models for many-to-many relationships can 
+    include a foreign key to this model unless it the relationships are
+    inherently unambiguous.  There are too many different ways
+    for students to be associated with a class for there to be a
+    separate relationship for each (i.e. 'enrolled_students' field,
+    'applied_students', etc.)
+    
+    Note: These models fit better in class_.py but cause validation errors
+    due to Django's import scheme if they are placed there.
+"""
+class RegistrationType(models.Model):
+    #   The 'key' (not really the primary key since we may want duplicate names)
+    name = models.CharField(max_length=32)
+    description = models.TextField(blank=True, null=True)
+    #   Purely for bookkeeping on the part of administrators 
+    #   without reading the whole description
+    category = models.CharField(max_length=32)
+
+    @cache_function
+    def get_map(include=None, category=None):
+        #   If 'include' is specified, make sure we have keys named in that list
+        if include:
+            if not isinstance(category, str):
+                raise ESPError(True), 'Need to supply category to RegistrationType.get_map() when passing include arguments'
+            for name in include:
+                type, created = RegistrationType.objects.get_or_create(name=name, category=category)
+        
+        #   Build a dictionary where names point to RegistrationType objects
+        result = {}
+        for item in RegistrationType.objects.all():
+            result[item.name] = item
+        return result
+    get_map.depend_on_model(lambda: RegistrationType)
+    get_map = staticmethod(get_map)
+
+    def __unicode__(self):
+        return self.name
+
+class StudentRegistration(models.Model):
+    section = models.ForeignKey('ClassSection')
+    #   section = models.ForeignKey(get_model('program', 'ClassSection'))
+    user = AjaxForeignKey(ESPUser)
+    
+    relationship = models.ForeignKey(RegistrationType)   #   Same as userbit verb after V/Flags/Registration/
+    start_date = models.DateTimeField(default=datetime.now)
+    end_date = models.DateTimeField(default=datetime(9999,1,1))    
+    
+    def expire(self):
+        self.end_date = datetime.now()
+        self.save()
+    
+    def __unicode__(self):
+        return u'%s %s in %s' % (self.user, self.relationship, self.section)
+    
 from esp.program.models.class_ import *
 from esp.program.models.app_ import *
 

esp/esp/program/modules/handlers/teacherclassregmodule.py
@@ -32,7 +32,7 @@ from esp.program.modules.base    import ProgramModuleObj, needs_teacher, meets_d
 from esp.program.modules.module_ext     import ClassRegModuleInfo
 from esp.program.modules         import module_ext
 from esp.program.modules.forms.teacherreg   import TeacherClassRegForm, TeacherOpenClassRegForm
-from esp.program.models          import ClassSubject, ClassSection, ClassCategories, ClassImplication, Program, StudentAppQuestion, ProgramModule
+from esp.program.models          import ClassSubject, ClassSection, ClassCategories, ClassImplication, Program, StudentAppQuestion, ProgramModule, StudentRegistration, RegistrationType
 from esp.program.controllers.classreg import ClassCreationController, ClassCreationValidationError
 from esp.datatree.models import *
 from esp.tagdict.models          import Tag
@@ -208,34 +208,30 @@ class TeacherClassRegModule(ProgramModuleObj, module_ext.ClassRegModuleInfo):
                         ignore = True
                         
                     if not ignore:
-                        verb = DataTree.get_by_uri('V/Flags/Registration/' + verb_name, create=True)
-                        other_bits = sec.getRegBits(student).filter(verb__name__in=['Enrolled', 'Rejected'])
+                        rel = RegistrationType.get_map(include=['Enrolled', 'Rejected'], category='student')[verb_name]
+                        other_regs = sec.getRegistrations(student).filter(relationship__name__in=['Enrolled', 'Rejected'])
                         found = False
-                        for bit in other_bits:
-                            if not found and bit.verb == verb:
+                        for reg in other_regs:
+                            if not found and reg.relationship == rel:
                                 found = True
                             else:
-                                bit.expire()
+                                reg.expire()
                             #   result_strs.append('Expired: %s' % bit)
                         if not found:
-                            new_bit = UserBit(user=student, verb=verb, qsc=sec.anchor)
-                            new_bit.save()
-                        #   result_strs.append('Created: %s' % new_bit)
+                            new_reg = StudentRegistration(user=student, relationship=rel, section=sec)
+                            new_reg.save()
                         
         #   Jazz up this information a little
         for student in students_list:
-            uri_base = 'V/Flags/Registration/'
-            uri_start = len(uri_base)
-            bits = sec.getRegBits(student)
-            student.bits = [bit.verb.get_uri()[uri_start:] for bit in bits]
+            student.bits = sec.getRegVerbs(student)
             student.app = student.getApplication(self.program, False)
-            student.other_classes = [(sec2, [bit.verb.get_uri()[uri_start:] for bit in sec2.getRegBits(student)]) for sec2 in student.getSections(self.program).exclude(id=sec.id)]
-            prereg_bits = bits.exclude(verb__name__in=['Enrolled', 'Rejected'])
-            if prereg_bits.count() != 0:
-               student.added_class = prereg_bits[0].startdate
-            if bits.filter(verb__name='Enrolled').count() > 0:
+            student.other_classes = [(sec2, sec2.getRegVerbs(student)) for sec2 in student.getSections(self.program).exclude(id=sec.id)]
+            preregs = sec.getRegistrations(student).exclude(relationship__name__in=['Enrolled', 'Rejected'])
+            if preregs.count() != 0:
+               student.added_class = preregs[0].start_date
+            if 'Enrolled' in student.bits:
                 student.enrolled = True
-            elif bits.filter(verb__name='Rejected').count() > 0:
+            elif 'Rejected' in student.bits:
                 student.rejected = True
 
         #   Detect if there is an application module

esp/esp/program/templatetags/class_render.py
@@ -72,7 +72,7 @@ def render_class_core(cls):
     if cls.studentappquestion_set.count():
         cls._sections = list(cls.get_sections())
         for sec in cls._sections:
-            sec.num_apps = sec.num_students(verbs=['/Applied'])
+            sec.num_apps = sec.num_students(verbs=['Applied'])
 
     return {'class': cls,
             'colorstring': colorstring,
@@ -99,13 +99,13 @@ def render_class(cls, user=None, prereg_url=None, filter=False, timeslot=None, r
     prereg_url = cls.parent_program.get_learn_url() + 'addclass'
 
     if user and prereg_url and timeslot:
-        error1 = cls.cannotAdd(user, True, request=request)
+        error1 = cls.cannotAdd(user, request=request)
         # If we can't add the class at all, then we take that error message
         if error1:
             errormsg = error1
         else:  # there's some section for which we can add this class; does that hold for this one?
             if section:
-                errormsg = section.cannotAdd(user, True, request=request)
+                errormsg = section.cannotAdd(user, request=request)
         
     show_class =  (not filter) or (not errormsg)
     

esp/esp/users/models/__init__.py
@@ -119,6 +119,10 @@ class UserAvailability(models.Model):
 class ESPUserManager(ProcedureManager):
     pass
 
+def get_studentreg_model():
+    from esp.program.models import StudentRegistration
+    return StudentRegistration
+
 class ESPUser(User, AnonymousUser):
     """ Create a user of the ESP Website
     This user extends the auth.User of django"""
@@ -538,7 +542,7 @@ class ESPUser(User, AnonymousUser):
             scrmi = program.getModuleExtension('StudentClassRegModuleInfo')
             verb_list = scrmi.reg_verbs(uris=True)
         else:
-            verb_list = ['/Applied']
+            verb_list = ['Applied']
             
         return self.getClasses(program, verbs=verb_list)
 
@@ -550,45 +554,30 @@ class ESPUser(User, AnonymousUser):
 
     @cache_function
     def getEnrolledClassesFromProgram(self, program):
-        return self.getClasses(program, verbs=['/Enrolled'])
-    getEnrolledClassesFromProgram.depend_on_row(lambda:UserBit, lambda bit: {'self': bit.user, 'program': Program.objects.get(anchor=bit.qsc.parent.parent.parent)},
-                                                 lambda bit: bit.verb_id == GetNode('V/Flags/Registration/Enrolled').id)
+        return self.getClasses(program, verbs=['Enrolled'])
+    getEnrolledClassesFromProgram.depend_on_row(get_studentreg_model, lambda reg: {'self': reg.user, 'program': reg.section.parent_program})
 
     @cache_function
     def getEnrolledClassesAll(self):
-        return self.getClasses(None, verbs=['/Enrolled'])
-    getEnrolledClassesAll.depend_on_row(lambda:UserBit, lambda bit: {'self': bit.user}, 
-                                         lambda bit: bit.verb_id == GetNode('V/Flags/Registration/Enrolled').id)
-
+        return self.getClasses(None, verbs=['Enrolled'])
+    getEnrolledClassesAll.depend_on_row(get_studentreg_model, lambda reg: {'self': reg.user})
 
     def getSections(self, program=None, verbs=None):
         """ Since enrollment is not the only way to tie a student to a ClassSection,
         here's a slightly more general function for finding who belongs where. """
-        from esp.program.models import ClassSection
+        from esp.program.models import ClassSection, RegistrationType
         
-        if program:
-            qsc_base = program.anchor
+        now = datetime.now()
+        
+        if verbs:
+            rts = RegistrationType.objects.filter(name__in=verbs)
         else:
-            qsc_base = GetNode('Q')
-                
-        if not verbs:
-            verb_base = GetNode('V/Flags/Registration')
-                
-            csl = ClassSection.objects.filter(QTree(anchor__below=qsc_base,
-                                                    anchor__userbit_qsc__verb__below=verb_base)
-                                              & Q( anchor__userbit_qsc__user=self,
-                                                   anchor__userbit_qsc__enddate__gte=datetime.now())).distinct()
+            rts = RegistrationType.objects.all()
 
-        else:            
-            verb_uris = ('V/Flags/Registration' + verb_str for verb_str in verbs)
-            
-            csl = ClassSection.objects.filter(QTree(anchor__below=qsc_base)
-                                              & Q(anchor__userbit_qsc__enddate__gte=datetime.now()),
-                                              Q(anchor__userbit_qsc__user=self,
-                                                anchor__userbit_qsc__verb__uri__in=verb_uris)
-                                              ).distinct()
-            
-        return csl
+        if program:
+            return ClassSection.objects.filter(id__in=self.studentregistration_set.filter(relationship__in=rts, start_date__lte=now, end_date__gte=now).values_list('section', flat=True)).filter(parent_class__parent_program=program)
+        else:
+            return ClassSection.objects.filter(id__in=self.studentregistration_set.filter(relationship__in=rts, start_date__lte=now, end_date__gte=now).values_list('section', flat=True))
 
     @cache_function
     def getSectionsFromProgram(self, program):
@@ -596,7 +585,7 @@ class ESPUser(User, AnonymousUser):
     #   Invalidate cache if bits are changed on either classes or sections.
     #   This should be less conservative but there's no easy way to filter the bits as they are saved
     #   (since we would need to check for all verbs under 'V/Flags/Registration')
-    getSectionsFromProgram.depend_on_row(lambda:UserBit, lambda bit: {'self': bit.user})
+    getSectionsFromProgram.depend_on_row(get_studentreg_model, lambda reg: {'self': reg.user})
 
     def getEnrolledSections(self, program=None):
         if program is None:
@@ -606,13 +595,13 @@ class ESPUser(User, AnonymousUser):
 
     @cache_function
     def getEnrolledSectionsFromProgram(self, program):
-        return self.getSections(program, verbs=['/Enrolled'])
+        return self.getSections(program, verbs=['Enrolled'])
     getEnrolledSectionsFromProgram.depend_on_row(lambda:UserBit, lambda bit: {'self': bit.user, 'program': Program.objects.get(anchor=bit.qsc.parent.parent.parent)},
                                                  lambda bit: bit.verb_id == GetNode('V/Flags/Registration/Enrolled').id)
 
     @cache_function
     def getEnrolledSectionsAll(self):
-        return self.getSections(None, verbs=['/Enrolled'])
+        return self.getSections(None, verbs=['Enrolled'])
     getEnrolledSectionsAll.depend_on_row(lambda:UserBit, lambda bit: {'self': bit.user}, 
                                          lambda bit: bit.verb_id == GetNode('V/Flags/Registration/Enrolled').id)
 
@@ -648,9 +637,9 @@ class ESPUser(User, AnonymousUser):
             for t in smt:
                 if t.id in priority_dict:
                     for v in cv:
-                        if v.parent.name == 'Priority':
-                            priority_dict[t.id].append(int(v.name))
-                        elif v.name == 'Enrolled':
+                        if v.startswith('Priority'):
+                            priority_dict[t.id].append(int(v[9:]))
+                        elif v == 'Enrolled':
                             return 0
         #   Now priority_dict is a dictionary where the keys are timeslot IDs and the values
         #   are lists of taken priority levels.  Merge those and find the lowest positive
@@ -679,17 +668,11 @@ class ESPUser(User, AnonymousUser):
             result[id_order[i]] = self.getRegistrationPriority(prog, [events[i]])
         return result
         
-    #   Invalidate on any user bit change (due to difficulty of screening registration bits)
-    getRegistrationPriorities.depend_on_row(lambda: UserBit, lambda bit: {'self': bit.user})
+    #   Invalidate on any registration change (due to difficulty of screening registration bits)
+    getRegistrationPriorities.depend_on_row(get_studentreg_model, lambda reg: {'self': reg.user})
 
     def isEnrolledInClass(self, clsObj, request=None):
-        verb_str = 'V/Flags/Registration/Enrolled'
-        if request:
-            verb = request.get_node(verb_str)
-        else:
-            verb = GetNode(verb_str)
-
-        return UserBit.UserHasPerms(self, clsObj.anchor, verb)
+        return clsObj.students().filter(id=self.id).exists()
 
     def canAdminister(self, nodeObj):
         return UserBit.UserHasPerms(self, nodeObj.anchor, GetNode('V/Administer'))

commit d79c3fd10559dce06ee96d6aa3aa94ada746598e

Don't refer to .name on a Unicode object
AFAICT, this should have been broken since 1d0fdea1960cccc61908a3f2f8dbe202
in October 2010. It would only have been visible when the verb
V/Deadline/Registration/Student/Classes wasn't available. It seems
just barely believable that that verb hasn't been unset since October 2010
while enough of reg was open to not break things horribly.

esp/esp/program/modules/handlers/studentclassregmodule.py
@@ -356,7 +356,7 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
                 reg_verbs = cls.getRegVerbs(request.user)
                 is_auto = 0
                 for r in reg_verbs:
-                    if r.name == 'Automatic':
+                    if r == 'Automatic':
                         is_auto = 1
                 if not is_auto:
                     classes_registered += 1