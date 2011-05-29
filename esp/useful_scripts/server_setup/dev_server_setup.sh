f301e0ced2944c4b25ce76c4700e7c032d065ac6
Michael Price <price@kilentra.net>
2011-05-29 17:31:58 -0400

    Drop DB user in dev server setup script, preventing an error if it is re-run

--- esp/useful_scripts/server_setup/dev_server_setup.sh
+++ esp/useful_scripts/server_setup/dev_server_setup.sh
@@ -580,6 +580,7 @@ fi
 if [[ "$MODE_DB" || "$MODE_ALL" ]]
 then
     sudo -u postgres psql template1 -c "CREATE LANGUAGE plpgsql;"
+    sudo -u postgres psql -c "DROP ROLE IF EXISTS $DBUSER;"
 	sudo -u postgres psql -c "CREATE USER $DBUSER CREATEDB;"
 	sudo -u postgres psql -c "ALTER ROLE $DBUSER WITH PASSWORD '$DBPASS';"
 	sudo -u postgres psql -c "DROP DATABASE $DBNAME;"
