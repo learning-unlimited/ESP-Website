24ded8a2944b0729a0514b8e678e82d1f935c942
Jordan Moldow <jmoldow@mit.edu>
2011-06-17 11:32:59 -0400

    Second version of test function for finding relationships between models.
    This function takes a model and an arbitrarily long list of instances of models, and returns a filter of the model based on those conditions.
    
    Uses the first version to construct the relationships between the model and each of the conditions.

--- esp/esp/dataviews/__init__.py
+++ esp/esp/dataviews/__init__.py
@@ -4,10 +4,11 @@ from django.db.models import Model
 from django.db.models.related import RelatedObject
 from django.db.models.fields.related import RelatedField, ManyToManyField
 from inspect import isclass
+import string
 
 def path_v1(*args):
     '''
-Finds a path from args[0] to args[1], or returns None if they aren't related in the forward direction.
+Finds a path from args[0] to args[1], or returns [] if they aren't related in the forward direction.
 
 If they are both models, returns a list of strings, which represent valid field lookup paths that can filter the first based on the second. For example, path_v1(StudentRegistration, User) would return ['user'], and path_v1(StudentRegistration, ClassSubject) would return ['section__parent_class'].
 
@@ -15,22 +16,20 @@ If the first is a model and the second is an instance, a filter is applied to th
 
 If the first is an instance and the second is a model, the function returns the instance of the model that is linked to the given instance. For example, path_v1(sr, User) would return sr.user, and path_v1(sr, ClassSubject) would return sr.section.parent_class (where sr is an instance of StudentRegistration).
     '''
-    if not 2 <= len(args) <= 4:
-        raise TypeError("path expected between 2 and 4 arguments, got "+str(len(args)))
-    if not 2 == len(args):
-        raise NotImplementedError("path is not yet implemented for more than 2 arguments, got "+str(len(args)))
-    instances = [None for i in range(len(args))]
-    classes = [None for i in range(len(args))]
-    for i in range(len(args)):
+    if 2 != len(args):
+        raise TypeError("path_v1 expected 2 arguments, got "+str(len(args)))
+    instances = [None for i in range(2)]
+    classes = [None for i in range(2)]
+    for i in range(2):
         if (not (isclass(args[i]) and issubclass(args[i], Model))) and (not isinstance(args[i], Model)): 
-            raise TypeError("path argument "+str(i+1)+" must be a model or an instance of a model")
+            raise TypeError("path_v1 argument "+str(i+1)+" must be a model or an instance of a model")
         if isclass(args[i]):
             classes[i] = args[i]
         else:
             instances[i] = args[i]
             classes[i] = type(args[i])
     if instances[0] and instances[1]:
-        raise TypeError("at least one of the path arguments must be a model")
+        raise TypeError("at least one of the path_v1 arguments must be a model")
     stack = [((classes[0],),())]
     paths = []
     while stack:
@@ -59,6 +58,46 @@ If the first is an instance and the second is a model, the function returns the 
     else:
         results = ['__'.join(path) for path in paths]
     if not results or not results[0]:
-        return None
+        return []
     return results
 
+def path_v2(model, *conditions):
+    '''
+Returns a filter of all objects of type model, subject to the list of conditions. 
+
+The first argument, model, must be a model. It cannot be an instance of a model, or any other object.
+
+The list of conditions can be arbitrarily long, but must consist entirely of instances of models.
+
+The function uses path_v1() to find the forward path from model to conditions[i] (for all i), and then applies the appropriate filter.
+
+If there is more than one path from model to conditions[i], the functions asks the user (via the raw_input() function) which path(s) to filter on.
+    '''
+    if not (isclass(model) and issubclass(model, Model)):
+        raise TypeError("path_v2 argument 1 must be a model")
+    if not len(conditions):
+        return model.objects.all()
+    models = [None for i in range(len(conditions))]
+    for i in range(len(conditions)):
+        if not isinstance(conditions[i], Model): 
+            raise TypeError("path_v2 argument 2 must be a list of instances of models")
+        models[i] = type(conditions[i])
+    kwargs = {}
+    paths = None
+    repeat = None
+    use = None
+    for i in range(len(conditions)):
+        paths = path_v1(model, models[i])
+        if len(paths) == 1:
+             kwargs[paths[0]] = conditions[i]
+        elif len(paths) > 1:
+            for path in paths: 
+                repeat = True
+                while repeat:
+                    use = raw_input("Include the path "+path+" as a condition ([y]/n)? ")
+                    if string.lower(use) == 'y' or string.lower(use) == 'yes' or not use:
+                        kwargs[path] = conditions[i]
+                        repeat = False
+                    elif string.lower(use) == 'n' or string.lower(use) == 'no':
+                        repeat = False
+    return model.objects.filter(**kwargs)
