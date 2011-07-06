434211a913630b8f75b01d51c407e0f81130cf9e
Vishal Dugar <vishaldugar12@gmail.com>
2011-07-06 19:57:09 +0530

    Removed the old CourseSelect widget

--- esp/esp/customforms/forms.py
+++ esp/esp/customforms/forms.py
@@ -3,17 +3,6 @@ from django import forms
 from django.utils.datastructures import SortedDict
 from django.contrib.localflavor.us.forms import USStateField, USStateSelect
 
-choices=[(1,1), (2,2),]
-
-class CourseSelect(Select):
-	"""A select widget with courses related to a particular program as its choices"""
-	def __init__(self, program=None, attrs=None):
-		courses=program.classsubject_set.all()
-		choices=[]
-		for course in courses:
-			course_title=course.title()
-			choices.append((course_title, course_title))
-		super(CourseSelect, self).__init__(attrs, choices)
 		
 class NameWidget(forms.MultiWidget):
 	"""
