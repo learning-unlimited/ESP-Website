40a3f02fc3671d0082cd7cdcd63d013ac9e099cd
Andrew Geng <pteromys@gmail.com>
2011-07-16 07:09:26 -0400

    An attempt at getting rid of certain googlebot JS errors

--- esp/public/media/scripts/error_logger.js
+++ esp/public/media/scripts/error_logger.js
@@ -3,12 +3,32 @@ if (window.onerror) {
     window._orig_onerror_errorlog = window.onerror;
 }
 
+// An attempt at getting rid of certain googlebot JS errors which say
+//     "TypeError: JSON.stringify cannot serialize cyclic structures."
+// Will commit and push after seeing how effective it is.
+// If you have to clobber this, also restore crashkit-javascript.js
+// by running the following in /esp/esp/public/media/scripts:
+//     $ patch -p0 -R < /home/pteromys/crashkit-decircularize.diff
+// -ageng 2011-07-12
+function uniqueObjects() {
+    var seen = []
+    return function (key, val) {
+        if (typeof(val) == "object") {
+            for (var i in seen) {
+                if (val === seen[i]) { return '' + val; }
+            }
+            seen.push(val);
+        }
+        return val;
+    }
+}
+
 window.onerror = function(msg, url, line)
 {
     if (encodeURIComponent) {
 	var uri;
 	if (!url && !line && JSON) {
-	    uri = "/error_reporter?url=" + encodeURIComponent(url) + "&lineNum=" + line + "&msg=" + encodeURIComponent(JSON.stringify(msg));
+	    uri = "/error_reporter?url=" + encodeURIComponent(url) + "&lineNum=" + line + "&msg=" + encodeURIComponent(JSON.stringify(msg, uniqueObjects()));
 	} else {
 	    uri = "/error_reporter?url=" + encodeURIComponent(url) + "&lineNum=" + line + "&msg=" + encodeURIComponent(msg);
 	}
