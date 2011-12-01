7f8c09986888c37ddd3695fd7eb6a19495fa5ebc
Gurtej Kanwar <looterguf@71.244.112.67>
2011-12-01 00:39:27 -0500

    Limited adding the onsubmit to only forms with post. Any post forms without csrfmiddlewaretoken get it added.

--- esp/public/media/scripts/csrf_init.js
+++ esp/public/media/scripts/csrf_init.js
@@ -1,19 +1,31 @@
-function set_onsubmit()
-{ 
-  //Select all forms
-  $j("form").submit(function() { return check_csrf_cookie(this); });
-}
-
-$j.getScript("/media/scripts/csrf_check.js", set_onsubmit);
-
 var csrfmiddlewaretoken = $j.cookie("csrftoken");
 if (csrfmiddlewaretoken == "")
 {
-  //Block on this because it's a small view and not having the cookie
-  //set before the rest of the page loads would not resolve the issue
-  $j.ajax("/set_csrf_token", {asyc: false});
+    //Block on this because it's a small view and not having the cookie
+    //set before the rest of the page loads would not resolve the issue
+    $j.ajax("/set_csrf_token", {asyc: false});
 }
 
-function csrftoken() {
-  document.write("<input type='hidden' name='csrfmiddlewaretoken' value='" + csrfmiddlewaretoken + "' />");
+function csrftoken()
+{
+    document.write(csrftokenstring());
 }
+
+function csrftokenstring()
+{
+    return "<input type='hidden' name='csrfmiddlewaretoken' value='" + csrfmiddlewaretoken + "' />";
+}
+
+function set_onsubmit()
+{ 
+    //Select all forms with post methods
+    $j("form[method=post]")
+        //Set their onsubmit functions to be check_csrf_cookie
+        .submit(function() { return check_csrf_cookie(this); })
+        //Filter down to ones without csrfmiddlewaretokens
+        .filter(":not(:has(input[name=csrfmiddlewaretoken]))")
+        //Add the csrfmiddlewaretoken hidden input
+        .append(csrftokenstring())
+}
+
+$j.getScript("/media/scripts/csrf_check.js", set_onsubmit);
