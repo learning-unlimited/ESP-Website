7a3870dc06c187229da0e4c484da9ac9a7fcb294
Gurtej Kanwar <looterguf@71.244.112.67>
2011-12-08 02:24:19 -0500

    Added in the popup
    I added in the popup on submission of a form which has grade_min = 7
    and grade_max = 12. It is an OK/Cancel alert, so if they click OK, it
    submits the form, otherwise doesn't.

---
+++ esp/public/media/scripts/program/modules/teacherclassregmodule.js
@@ -0,0 +1,16 @@
+function check_grade_range(form)
+{
+    console.log("Checking!");
+    if ($j(form).find('#id_grade_min').val() == 7 && $j(form).find('#id_grade_max').val() == 12)
+    {
+        return confirm("Are you sure you want your class to have grade range 7-12? \
+Managing a class with grade range 7-12 can be more difficult due to the \
+difference in maturity levels between middle and high school students. \
+Click OK if you are sure this is what you want, or click Cancel to go back \
+and change the grade range.");
+    }
+    else
+    {
+        return true;
+    }
+}
