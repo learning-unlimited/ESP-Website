2a9cd670be5538e4329601e4a2834e5a066bd58b
Michael Price <price@kilentra.net>
2011-12-13 10:00:27 -0500

    Add missing inline_template attribute to TextMessageModule module_properties

--- esp/esp/program/modules/handlers/textmessagemodule.py
+++ esp/esp/program/modules/handlers/textmessagemodule.py
@@ -49,6 +49,7 @@ class TextMessageModule(ProgramModuleObj):
             "admin_title": 'Text Message Reminders',
             "link_title": "Text Message Reminders",
             "module_type": "learn",
+            "inline_template": "textmessage.html",
             "seq": 100
             }
 
