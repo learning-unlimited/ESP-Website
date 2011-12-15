31a931c2fc000da0f8483d3c343eb5aad450abd2
Jordan Moldow <jmoldow@mit.edu>
2011-12-15 03:22:48 -0500

    Switchback redirect to admin_morph, not dashboard
    Resolves github Issue #79.

--- esp/esp/program/modules/handlers/adminmorph.py
+++ esp/esp/program/modules/handlers/adminmorph.py
@@ -83,7 +83,7 @@ class AdminMorph(ProgramModuleObj):
 
         self.user.switch_to_user(request,
                                  user,
-                                 self.getCoreURL(tl),
+                                 '/manage/%s/%s/admin_morph/' % (one,two),
                                  'Managing %s' % self.program.niceName(),
                                  False)
 
