90473ffd05d033ca1591ba7946b75e044c998843
Gurtej Kanwar <looterguf@71.244.112.67>
2011-12-17 03:34:28 -0500

    Uses grade_min and grade_max and a Tag as a switch

--- esp/public/media/scripts/program/modules/teacherclassregmodule.js
+++ esp/public/media/scripts/program/modules/teacherclassregmodule.js
@@ -1,7 +1,7 @@
-function check_grade_range(form)
+function check_grade_range(form, grade_min, grade_max)
 {
     console.log("Checking!");
-    if ($j(form).find('#id_grade_min').val() == 7 && $j(form).find('#id_grade_max').val() == 12)
+    if ($j(form).find('#id_grade_min').val() == grade_min && $j(form).find('#id_grade_max').val() == grade_max)
     {
         return confirm("Are you sure you want your class to have grade range 7-12? \
 Managing a class with grade range 7-12 can be more difficult due to the \
