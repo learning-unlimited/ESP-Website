94b97bb35702b770b3cc18a51004c0e0d9b8f512
Michael Price <price@kilentra.net>
2011-12-18 18:29:00 -0500

    Make Varnish purging disabled by default; set via Tag

--- esp/esp/cache/varnish.py
+++ esp/esp/cache/varnish.py
@@ -1,7 +1,9 @@
 import httplib
 from sys import stdout
 from esp.settings import VARNISH_HOST, VARNISH_PORT
+from esp.tagdict.decorators import require_tag
 
+@require_tag('varnish_purge')
 def purge_page(url, host = VARNISH_HOST + ":" + str(VARNISH_PORT)):
     stdout.write("Purging: " + str(url) + "\n")
     conn = httplib.HTTPConnection(host)
