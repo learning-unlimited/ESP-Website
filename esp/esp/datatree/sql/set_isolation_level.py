e0375739fe2631ad7a9b3c49019495978502c2a1
Adam Seering <adam@seering.org>
2012-01-14 08:53:41 +0400

    Support setting the isolation level in SQLite.  Probably inaccurate, but conservative; for our (single-threaded) use case, an exclusive llock is probably cheaper anyway.

--- esp/esp/datatree/sql/set_isolation_level.py
+++ esp/esp/datatree/sql/set_isolation_level.py
@@ -2,11 +2,19 @@ from django.db import transaction
 
 DISABLE_TRANSACTIONS = False
 
+# This is somewhat arbitrary, and generally conservative
+ISOLATION_STRINGS_SQLITE = {0: 'DEFERRED', 1: 'IMMEDIATE', 2: 'EXCLUSIVE', 3: 'EXCLUSIVE'}
 
 def set_isolation_level(connection, n):
     if not DISABLE_TRANSACTIONS:
-        return connection.connection.set_isolation_level(n)
-
+        if hasattr(connection.connection, 'set_isolation_level'):
+            return connection.connection.set_isolation_level(n)
+        elif hasattr(connection.connection, 'isolation_level'):
+            connection.connection.isolation_level = ISOLATION_STRINGS_SQLITE[n]
+            return n
+        else:
+            assert False, "Don't know how to set isolation level for DB backend type '%s'" % (connection.connection)
+            
 def commit_manually(fn):
     decorated_fn = transaction.commit_manually(fn)
     def _inner(*args, **kwargs):
