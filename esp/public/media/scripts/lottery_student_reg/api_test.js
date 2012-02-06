ca82c3a3dfd7e7079796dfdbe6a55e45e14a5f13
Michael Price <price@kilentra.net>
2012-02-06 00:24:43 -0500

    Update jstest view in lottery module to use current JSON API

--- esp/public/media/scripts/lottery_student_reg/api_test.js
+++ esp/public/media/scripts/lottery_student_reg/api_test.js
@@ -1,90 +1,26 @@
 
-var data_fetch_status = Object();
-var data = Object();
-
-function check_json_status(components)
-{
-    if (components != null)
-    {
-        for (var i in components)
-        {
-            var key = components[i];
-            if (data_fetch_status[key] != "Ready")
-                return false;
-        }
-    }
-    else for (var key in data_fetch_status)
-    {
-        if (data_fetch_status[key] != "Ready")
-            return false;
-    }
-    return true;
-}
-
-function handle_json_response(new_data, text_status, jqxhr)
-{
-    console.log("Handling JSON response");
-    
-    //  Update data
-    for (var key in new_data)
-    {
-        if (data.hasOwnProperty(key))
-        {
-            //  Update existing dictionary
-            
-            console.log("Updated data dictionary " + key);
-        }
-        else
-        {
-            //  Create new dictionary
-            data[key] = {};
-            for (var index in new_data[key])
-            {
-                var item = new_data[key][index];
-                data[key][item.id] = item;
-            }
-            console.log("Created new data dictionary " + key);
-        }
-    }
-    
-    //  Set flags
-    console.log("JQXHR is: " + jqxhr);
-}
-
-function json_fetch(components)
-{
-    for (var i in components)
-    {
-        data_fetch_status[components[i]] = "Waiting";
-        $j.ajax({
-            url: program_base_url + components[i],
-            success: handle_json_response
-        });
-    }
-    console.log(data_fetch_status);
-    json_fetch_complete(components);
-}
-
-function json_fetch_complete(components)
-{
-    if (check_json_status(components))
-    {
-        console.log("All fetches complete");
-    }
-    else
-    {
-        setTimeout(function (){json_fetch_complete(components)}, 100);
-    }
-}
+var test_data;
 
 function init_jstest()
 {
+    var data = {};
+
+    //  Test fetching information needed for initial display of lottery
     data_components = [
-        'rooms_status',
-        'counts_status',
-        //  'lottery_preferences'
+        'timeslots',
+        'sections',
+        'lottery_preferences'
     ];
-    data = json_fetch(data_components);
+    json_fetch(data_components, function (data) {test_data = data; console.log("Completed initial data fetch"); jstest_continue();});
+}
+
+function jstest_continue()
+{
+    //  Test fetching catalog information for a particular class
+    update_components = [
+        'class_info?section_id=5322'
+    ];
+    json_fetch(update_components, function (data) {test_data = data; console.log("Completed catalog data fetch: " + test_data.classes[test_data.sections[5322].parent_class].class_info);}, test_data);
 }
 
 $j(document).ready(init_jstest);
