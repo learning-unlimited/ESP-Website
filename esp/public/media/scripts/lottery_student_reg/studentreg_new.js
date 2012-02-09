1777a0792d08c35bfce277c9c2ac35b63885912b
Gurtej Kanwar <looterguf@HelloThere.(none)>
2012-02-09 01:45:48 -0500

    Makes the lottery UI into a jQueryUI accordion

--- esp/public/media/scripts/lottery_student_reg/studentreg_new.js
+++ esp/public/media/scripts/lottery_student_reg/studentreg_new.js
@@ -4,6 +4,8 @@
 
 
 $j(document).ready(function() { 
+    $j("#lsr_content").accordion({ header: "a.header"});
+
     data_components = [
         'timeslots',
         'sections',
@@ -32,17 +34,20 @@ show_app = function(data){
     //adds timeslot links to page
     for(index in sorted_timeslots){
 	t = sorted_timeslots[index];
-	$j("#timeslots").append(get_timeslot_html(t));
+	$j("#timeslots_anchor").after(get_timeslot_html(t));
 	add_classes_to_timeslot(t, sections);
     }
 
     //adds preferences section
     updatePreferences(data);
+
+    //recreate the accordion now to update for the timeslots
+    $j("#lsr_content").accordion('destroy').accordion({ header: 'a.header'});
 };
 
 get_timeslot_html = function(timeslot_data)
 {
-    template = "<a onclick='show_timeslot(%TIMESLOT_ID%)'><b>%TIMESLOT_LABEL% </b></a> <div id='%TIMESLOT_DIV%' hidden=true></div><br>";
+    template = "<a href='#' class='header'><b>%TIMESLOT_LABEL% </b></a> <div id='%TIMESLOT_DIV%'></div><br>";
     template = template.replace(/%TIMESLOT_ID%/g, timeslot_data['id']).replace(/%TIMESLOT_DIV%/g, ts_div_from_id(timeslot_data['id'])).replace(/%TIMESLOT_LABEL%/g, timeslot_data['label']);
     return template;
 };
