5f131fd1598fa0a829b03999682b6a21777f98e5
Gurtej Kanwar <looterguf@HelloThere.(none)>
2012-03-20 01:27:58 -0400

    Fixes some formatting in customforms_responses.js

--- esp/public/media/scripts/customforms_response.js
+++ esp/public/media/scripts/customforms_response.js
@@ -1,56 +1,56 @@
 $(document).ready(function() {
-	//csrf stuff
-	$(document).ajaxSend(function(event, xhr, settings) {
-	    function getCookie(name) {
-	        var cookieValue = null;
-	        if (document.cookie && document.cookie != '') {
-	            var cookies = document.cookie.split(';');
-	            for (var i = 0; i < cookies.length; i++) {
-	                var cookie = jQuery.trim(cookies[i]);
-	                // Does this cookie string begin with the name we want?
-	                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-	                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-	                    break;
-	                }
+    //csrf stuff
+    $(document).ajaxSend(function(event, xhr, settings) {
+	function getCookie(name) {
+	    var cookieValue = null;
+	    if (document.cookie && document.cookie != '') {
+	        var cookies = document.cookie.split(';');
+	        for (var i = 0; i < cookies.length; i++) {
+	            var cookie = jQuery.trim(cookies[i]);
+	            // Does this cookie string begin with the name we want?
+	            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+	                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+	                break;
 	            }
 	        }
-	        return cookieValue;
 	    }
-	    function sameOrigin(url) {
-	        // url could be relative or scheme relative or absolute
-	        var host = document.location.host; // host + port
-	        var protocol = document.location.protocol;
-	        var sr_origin = '//' + host;
-	        var origin = protocol + sr_origin;
-	        // Allow absolute or scheme relative URLs to same origin
-	        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-	            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-	            // or any other URL that isn't scheme relative or absolute i.e relative.
-	            !(/^(\/\/|http:|https:).*/.test(url));
-	    }
-	    function safeMethod(method) {
-	        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-	    }
+	    return cookieValue;
+	}
+	function sameOrigin(url) {
+	    // url could be relative or scheme relative or absolute
+	    var host = document.location.host; // host + port
+	    var protocol = document.location.protocol;
+	    var sr_origin = '//' + host;
+	    var origin = protocol + sr_origin;
+	    // Allow absolute or scheme relative URLs to same origin
+	    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+	        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+	        // or any other URL that isn't scheme relative or absolute i.e relative.
+	        !(/^(\/\/|http:|https:).*/.test(url));
+	}
+	function safeMethod(method) {
+	    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
 
-	    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
-	        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-	    }
-	});
-	//end of csrf stuff
-	
-	//Grabbing the form-id
-	var form_id=$('#form_id').val();
-	//Getting response data
-        $.ajax({
-		url:'/customforms/getData/',
-		data:{'form_id':form_id},
-		type:'GET',
-		dataType:'json',
-		async:false,
-		success: function(form_data) {
-			createGrid(form_data);
-		}
-	});
+	if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+	    xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+	}
+    });
+    //end of csrf stuff
+    
+    //Grabbing the form-id
+    var form_id=$('#form_id').val();
+    //Getting response data
+    $.ajax({
+	url:'/customforms/getData/',
+	data:{'form_id':form_id},
+	type:'GET',
+	dataType:'json',
+	async:false,
+	success: function(form_data) {
+	    createGrid(form_data);
+	}
+    });
 });
 
 var createGrid=function(form_data){
@@ -80,11 +80,11 @@ var createGrid=function(form_data){
 };
 
 var copyObject=function(answers){
-	//Copies the 'answers' array into another array
-	ret_val=[];
-	$.each(answers, function(idx,el){
-		ret_val.push({});
-		$.extend(ret_val[idx], el);
-	});
-	//return ret_val;
+    //Copies the 'answers' array into another array
+    ret_val=[];
+    $.each(answers, function(idx,el){
+	ret_val.push({});
+	$.extend(ret_val[idx], el);
+    });
+    //return ret_val;
 }
