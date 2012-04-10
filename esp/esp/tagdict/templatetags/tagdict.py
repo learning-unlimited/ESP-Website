e43b61c27b2f23b82e5b29d0940287d5009b358f
Gurtej Kanwar <gurtej@mit.edu>
2012-04-10 02:33:45 -0400

    Adds a template tag for getProgramTag in the tagdict module

---
+++ esp/esp/tagdict/templatetags/tagdict.py
@@ -0,0 +1,49 @@
+from django import template
+from django.template.base import Node
+from esp.tagdict.models import Tag
+from esp.program.models import ClassSubject
+
+register = template.Library()
+
+class GetProgramTagNode(Node):
+    """ Gives access to the getProgramTag function """
+
+    def __init__(self, key, program, default):
+        self.key = key
+        self.program = program
+        self.default = default
+
+    @classmethod
+    def handle_token(cls, parser, token):
+        """ Class method to handle the tokens received """
+        tokens = token.contents.split()
+        if len(tokens) < 2:
+            raise template.TemplateSyntaxError("At least 2 arguments required")
+        key = parser.compile_filter(tokens[1])
+        program = None
+        default = None
+        if len(tokens) > 3:
+            program = parser.compile_filter(token[2])
+        if len(tokens) > 4:
+            default = parser.compile_filter(token[3])
+     
+        return cls(key, program, default)
+      
+
+    def render(self, context):
+        key = self.key.resolve(context)
+        if self.program:
+            program = self.program.resolve(context)
+        else:
+            program = None
+        if self.default:
+           u default = self.default.resolve(context)
+        else:
+            default = None
+        return str(Tag.getProgramTag(key, program, default))
+
+
+def doGetProgramTag(parser, token):
+    return GetProgramTagNode.handle_token(parser, token)
+
+register.tag('getProgramTag', doGetProgramTag)
