d18b4c370e686151adf468a048143d36f348ff21
Michael Price <price@kilentra.net>
2012-05-08 07:58:31 -0400

    Turn on teacher class import by default

--- esp/esp/program/modules/handlers/teacherclassregmodule.py
+++ esp/esp/program/modules/handlers/teacherclassregmodule.py
@@ -90,7 +90,7 @@ class TeacherClassRegModule(ProgramModuleObj, module_ext.ClassRegModuleInfo):
         context['teacherclsmodule'] = self # ...
         context['clslist'] = self.clslist(get_current_request().user)
         context['friendly_times_with_date'] = (Tag.getProgramTag(key='friendly_times_with_date',program=self.program,default=False) == "True")
-        context['allow_class_import'] = 'false' not in Tag.getTag('allow_class_import', default='false').lower()
+        context['allow_class_import'] = 'false' not in Tag.getTag('allow_class_import', default='true').lower()
         return context
 
 
@@ -643,7 +643,7 @@ class TeacherClassRegModule(ProgramModuleObj, module_ext.ClassRegModuleInfo):
         context = {}
         context['all_class_list'] = request.user.getTaughtClasses()
         context['noclasses'] = (len(context['all_class_list']) == 0)
-        context['allow_class_import'] = 'false' not in Tag.getTag('allow_class_import', default='false').lower()
+        context['allow_class_import'] = 'false' not in Tag.getTag('allow_class_import', default='true').lower()
         return render_to_response(self.baseDir()+'listcopyclasses.html', request, (prog, tl), context)
 
     @aux_call
