5d24c76ee024d221677c9daa58e993bd1db35809
Vikrant Varma <vikrant.varma94@gmail.com>
2012-05-25 18:26:41 +0530

    Replace admin_hidden, logged_in_hidden, not_logged_in_hidden et c with hidden.

--- esp/find_and_replace_hidden.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for file in $(grep -lir "_hidden" templates/*)
-do
-sed -e 's/[a-z]*_hidden/hidden/ig' $file > /tmp/tempfile.tmp
-mv /tmp/tempfile.tmp $file
-done
--- esp/public/media/scripts/content/user_classes.js
+++ esp/public/media/scripts/content/user_classes.js
@@ -1,29 +1,28 @@
 function update_user_classes() {
   if (esp_user.cur_admin == "1") {
-    $j(".admin").removeClass("admin_hidden");
+    $j(".admin").removeClass("hidden");
   }
   if (esp_user.cur_retTitle) {
-    $j(".unmorph").removeClass("unmorph_hidden");
+    $j(".unmorph").removeClass("hidden");
     document.getElementById("unmorph_text").innerHTML = "Click above to return to your administrator account - " + esp_user.cur_retTitle;
   }
   if (esp_user.cur_qsd_bits == "1") {
-    $j(".qsd_bits").removeClass("qsd_bits_hidden");
+    $j(".qsd_bits").removeClass("hidden");
   }
   if (esp_user.cur_username != null) {
-    $j(".not_logged_in").addClass("not_logged_in_hidden");
-    $j(".logged_in").removeClass("logged_in_hidden");
+    $j(".not_logged_in").addClass("hidden");
+    $j(".logged_in").removeClass("hidden");
   }
   else {
-    $j(".not_logged_in").removeClass("not_logged_in_hidden");
-    $j(".logged_in").addClass("logged_in_hidden");
+    $j(".not_logged_in").removeClass("hidden");
+    $j(".logged_in").addClass("hidden");
   }
 
   var type_name = '';
   var hidden_name = '';
   for (var i = 0; i < esp_user.cur_roles.length; i++) {
     type_name = "." + esp_user.cur_roles[i];
-    hidden_name = esp_user.cur_roles[i] + "_hidden";
-    $j(type_name).removeClass(hidden_name);
+    $j(type_name).removeClass("hidden");
   }
   
   //    Write user's name in the appropriate spot in the login box
