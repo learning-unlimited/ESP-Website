21d792f21f7d42abf794628092ed63fdc56f2ea4
Vikrant Varma <vikrant.varma94@gmail.com>
2012-06-14 03:50:45 +0530

    Add variables_template.less that the Django template engine uses to generate the variables.less file. Consequently, stop git from tracking variables.less.

--- esp/esp/theme_editor/views.py
+++ esp/esp/theme_editor/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.template import Context, Template
+from esp.settings import PROJECT_ROOT
+from os import path
 
 def editor(request):
+    # can we avoid hardcoding this?
+    less_dir = path.join(PROJECT_ROOT, 'public/media/theme_editor/less/') #directory containing modified less files
+    variables_template_less = path.join(less_dir, 'variables_template.less')
+    variables_less = path.join(less_dir, 'variables.less')
+    f = open(variables_template_less)
+    variables_template = Template(f.read())
+    f.close()
+    variables_context = {} # retrieve context from form input
+    w = variables_template.render(Context(variables_context))
+    f = open(variables_less, 'w')
+    f.write(w)
+    f.close()
     return render_to_response('theme_editor/editor.html', {}, context_instance=RequestContext(request))
 
     
