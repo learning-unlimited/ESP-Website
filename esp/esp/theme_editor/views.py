92833d8541ceb054f78531c5ac5bcd55f6ce9857
Vikrant Varma <vikrant.varma94@gmail.com>
2012-06-14 21:09:29 +0530

    Parse variables.less file to get current theme settings, and show them on the form.

--- esp/esp/theme_editor/views.py
+++ esp/esp/theme_editor/views.py
@@ -2,22 +2,37 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.template import Context, Template
 from esp.settings import PROJECT_ROOT
+from django.http import HttpResponse
 from os import path
+import re
 
+def parse_less(less_file):
+    f = open(less_file).read()
+    matches = re.findall("@(\w+):\s*(.+?);", f)
+    d = {}
+    for match in matches:
+        d[match[0]] = match[1]
+    return d
+    
 def editor(request):
     # can we avoid hardcoding this?
     less_dir = path.join(PROJECT_ROOT, 'public/media/theme_editor/less/') #directory containing modified less files
     variables_template_less = path.join(less_dir, 'variables_template.less')
     variables_less = path.join(less_dir, 'variables.less')
+    variables_settings = parse_less(variables_less)
     f = open(variables_template_less)
     variables_template = Template(f.read())
     f.close()
-    context = request.GET # retrieve context from form input, change to POST eventually
-    
-    w = variables_template.render(Context(context))
+    form_settings = dict(request.GET) # retrieve context from form input, change to POST eventually
+    for k,v in form_settings.items():
+        form_settings[k] = form_settings[k][0] # because QueryResponse objects store values as lists
+        if not form_settings[k]: # if a form element returns no value, don't
+            del form_settings[k];
+    variables_settings.update(form_settings)
+    w = variables_template.render(Context(variables_settings))
     f = open(variables_less, 'w')
     f.write(w)
     f.close()
-    return render_to_response('theme_editor/editor.html', {}, context_instance=RequestContext(request))
+    return render_to_response('theme_editor/editor.html', variables_settings, context_instance=RequestContext(request))
 
     
