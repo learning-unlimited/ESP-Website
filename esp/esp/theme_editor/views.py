2f0158cc3d1a625a84ae92d4ba4bc742d48f4e96
Vikrant Varma <vikrant.varma94@gmail.com>
2012-06-16 21:09:37 +0530

    Add Navbar section to Theme Editor with features Background, Highlight and Text, Link and Link Hover.

--- esp/esp/theme_editor/views.py
+++ esp/esp/theme_editor/views.py
@@ -11,12 +11,21 @@ less_dir = path.join(PROJECT_ROOT, 'public/media/theme_editor/less/') #directory
 variables_template_less = path.join(less_dir, 'variables_template.less')
 variables_less = path.join(less_dir, 'variables.less')
 
+color_list = ['black', 'grayDarker', 'grayDark', 'gray', 'grayLight', 'grayLighter', 'whiteblue', 'blueDark', 'green', 'red', 'yellow', 'orange', 'pink', 'purple']                
+
 def parse_less(less_file):
     f = open(less_file).read()
+    #this regex is supposed to match @(property) = (value);
+    #or @(property) = (function)(args) in match[0] and 
+    #match[1] respectively
     matches = re.findall("@(\w+):\s*([^,;\(]*)[,;\(]", f)
     d = {}
     for match in matches:
         d[match[0]] = match[1]
+        #in case color values like @white, @black are encountered, substitute
+        #that with the hex value
+        if match[1] and match[1][1:] in color_list:
+            d[match[0]] = d[match[1][1:]]
     return d
     
 def editor(request):
@@ -30,10 +39,12 @@ def apply(request):
     variables_template = Template(f.read())
     f.close()
     form_settings = dict(request.GET) # retrieve context from form input, change to POST eventually
+
     for k,v in form_settings.items():
         form_settings[k] = form_settings[k][0] # because QueryResponse objects store values as lists
         if not form_settings[k]: # if a form element returns no value, don't
             del form_settings[k];
+
     variables_settings.update(form_settings)
     w = variables_template.render(Context(variables_settings))
     f = open(variables_less, 'w')
