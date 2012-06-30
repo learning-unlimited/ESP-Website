71740c5fcaf48385f39f0aad23f43143358449f9
Vikrant Varma <vikrant.varma94@gmail.com>
2012-06-30 07:13:53 +0530

    Add feature: Theme palette: This lets the user define a set of colors that provide a more limited, and hopefully, less bewildering choice when setting colors for various elements. (courtesy Mallika Sobti)

--- esp/esp/theme_editor/views.py
+++ esp/esp/theme_editor/views.py
@@ -15,6 +15,8 @@ less_dir = path.join(PROJECT_ROOT, 'public/media/theme_editor/less/') #directory
 themes_dir = path.join(PROJECT_ROOT, 'public/media/theme_editor/themes/') #directory containing the themes
 variables_template_less = path.join(less_dir, 'variables_template.less')
 variables_less = path.join(less_dir, 'variables.less')
+# directory containing the javascript that shows the palette
+palette_dir = path.join(PROJECT_ROOT, 'public/media/theme_editor/')
 
 # and this...
 sans_serif_fonts = {"Impact":"Impact, Charcoal, sans-serif",
@@ -53,29 +55,19 @@ def parse_less(less_file_path):
         #that with the hex value
             if match[1] and match[1][1:] in d and d[match[1][1:]][0] == '#':
                 d[match[0]] = d[match[1][1:]]
-    #if theme_name is set, retrieve that
+        #if theme_name is set, retrieve that
         match = re.search(r"// Theme Name: (.+?)\n", f)
         if match:
             d.update({'theme_name':match.group(1)})
+        # retrieve the palette
+        match = re.findall(r"palette:([\#]?\w+);", f) 
+        palette = []
+        for m in match:
+            palette.append(m)
+        d.update({'palette':palette})
         return d
     except IOError:
         return {}
-
-@admin_required    
-def editor(request):
-    context = parse_less(path.join(less_dir, variables_less))
-    # load a list of available themes
-    available_themes_paths = glob.glob(path.join(themes_dir,'*.less'))
-    available_themes = []
-    for theme_path in available_themes_paths:
-        available_themes.append(re.search(r'theme_editor/themes/(.+)\.less',theme_path).group(1))
-    context.update({'available_themes':available_themes})
-    context.update({'last_used_settings':'variables_backup'})
-    context.update({'sans_fonts':sorted(sans_serif_fonts.iteritems())})
-#    for debugging, see context by uncommenting the next line
-#    return HttpResponse(str(context))
-
-    return render_to_response('theme_editor/editor.html', context, context_instance=RequestContext(request))
 
 def save(request, less_file):
     
@@ -101,7 +93,8 @@ def save(request, less_file):
     form_settings = dict(request.POST) # retrieve context from form input, change to POST eventually
 
     for k,v in form_settings.items():
-        form_settings[k] = form_settings[k][0] # because QueryResponse objects store values as lists
+        if len(form_settings[k]) == 1:
+            form_settings[k] = form_settings[k][0] # because QueryResponse objects store values as lists
         if not form_settings[k]: # if a form element returns no value, don't keep it in the context
             del form_settings[k];
 
@@ -110,7 +103,6 @@ def save(request, less_file):
     # if theme is only applied, but has some changes, just apply theme and set name as 'None'
     if 'apply' in request.POST:
         del variables_settings['theme_name']
-
 
     w = variables_template.render(Context(variables_settings))
     f = open(theme_file_path, 'w')
@@ -137,7 +129,6 @@ def apply_theme(less_file):
     except shutil.Error:
         pass
 
-
 def delete_theme(theme_name):
     remove(path.join(themes_dir, theme_name))
     if get_theme_name(variables_less) == theme_name:
@@ -151,7 +142,16 @@ def generate_default():
     f = open(path.join(themes_dir, 'Default.less'), 'w')
     f.write(w)
     f.close()
-        
+
+def update_palette(palette_list):
+    # is it worth making a new file just for this?
+    palette_template = 'var palette_list = [{% for color in palette %} "{{color}}"{% if not forloop.last %},{% endif %} {% endfor %}];'
+    template = Template(palette_template)
+    w = template.render(Context({'palette':palette_list}))
+    f = open(path.join(palette_dir, 'palette.js'), 'w')
+    f.write(w)
+    f.close()
+
 @admin_required
 def theme_submit(request):
     if 'save' in request.POST:
@@ -183,4 +183,28 @@ def theme_submit(request):
     # finally compile less to css
     subprocess.call(["lessc", "--compress", path.join(less_dir, 'bootstrap.less')], stdout=f)
     f.close()
+    # for debugging, uncomment the next line
+    #return HttpResponse(str(dict(request.POST)))
+
+
     return HttpResponseRedirect('/theme/')
+
+@admin_required    
+def editor(request):
+    context = parse_less(path.join(less_dir, variables_less))
+    # load a list of available themes
+    available_themes_paths = glob.glob(path.join(themes_dir,'*.less'))
+    available_themes = []
+    for theme_path in available_themes_paths:
+        available_themes.append(re.search(r'theme_editor/themes/(.+)\.less',theme_path).group(1))
+    context.update({'available_themes':available_themes})
+    context.update({'last_used_settings':'variables_backup'})
+    # load a bunch of preset fonts
+    context.update({'sans_fonts':sorted(sans_serif_fonts.iteritems())})
+    # load the theme's palette
+    update_palette(context['palette'])
+#    for debugging, see context by uncommenting the next line
+#    return HttpResponse(str(context))
+
+    return render_to_response('theme_editor/editor.html', context, context_instance=RequestContext(request))
+
--- esp/public/media/theme_editor/theme_editor.js
+++ esp/public/media/theme_editor/theme_editor.js
@@ -1,11 +1,54 @@
-$j(document).ready(function(){
+function showColor() {
     $j('.color').each(function(i){
 	$j(this).spectrum({
 	    showInput: true,
 	    showPalette: true,
 	    showInitial: true,
-	    showButtons: false
+	    showButtons: false,
+	    preferredFormat: "name",
+	    palette: [palette_list],
+	    showPaletteOnly: true
 	});
+    })
+	}
+
+function showPalette() {
+    $j('.palette').each(function(i){
+	$j(this).spectrum({
+	    showInput: true,
+	    showPalette: true,
+	    showInitial: true,
+	    showButtons: false,
+	    preferredFormat: "name",
+	    palette: [palette_list]
+	});
+    })
+	}
+		     
+
+$j(document).ready(function(){
+    showColor();
+    showPalette();
+    $j('#addToPalette').click(function(){
+	var newColor = $j('<input>');
+	newColor.addClass('palette');
+	newColor.attr({
+	    name: 'palette',
+	    type: 'text',
+	    value: ''
+	});
+	$j('#palette_div').append(newColor);
+	showPalette()
     });
-})
+
+    $j('#removeFromPalette').click(function(){
+	$j('#palette_div div.sp-replacer.sp-light:last').remove();
+	$j('#palette_div input:last').remove()
+    });    
+
+});
+
+
+
+
 
