1c05ef2d07a0c3a395d44b868438c5de8bd60386
Alan Yeo Jie Wei <yeojw10@gmail.com>
2012-07-16 06:01:26 +0800

    Fix bug in Class model that causes session times to not show minutes

--- esp/public/media/mobile/app/model/Class.js
+++ esp/public/media/mobile/app/model/Class.js
@@ -73,7 +73,7 @@ Ext.define('LU.model.Class', {
                 convert: function(value, record) {
                     if (value) {
                         var date = value.split(/[\-T:]/);
-                        return new Date(date[0], date[1]-1, date[2], date[3]);
+                        return new Date(date[0], date[1]-1, date[2], date[3], date[4]);
                     } else {
                         return new Date();
                     }
@@ -85,7 +85,7 @@ Ext.define('LU.model.Class', {
                 convert: function(value, record) {
                     if (value) {
                         var date = value.split(/[\-T:]/);
-                        return new Date(date[0], date[1]-1, date[2], date[3]);
+                        return new Date(date[0], date[1]-1, date[2], date[3], date[4]);
                     } else {
                         return new Date();
                     }
