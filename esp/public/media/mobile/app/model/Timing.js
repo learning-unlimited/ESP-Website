e0b3b8dad12c67faac2cc156afec08d020063748
Alan Yeo Jie Wei <yeojw10@gmail.com>
2012-07-16 06:24:14 +0800

    Fix bug in Timing model that causes session times to not show minutes

--- esp/public/media/mobile/app/model/Timing.js
+++ esp/public/media/mobile/app/model/Timing.js
@@ -10,7 +10,7 @@ Ext.define('LU.model.Timing', {
                 convert: function(value, record) {
                     if (value) {
                         var date = value.split(/[\-T:]/);
-                        return new Date(date[0], date[1]-1, date[2], date[3]);
+                        return new Date(date[0], date[1]-1, date[2], date[3], date[4]);
                     } else {
                         return new Date();
                     }
@@ -22,7 +22,7 @@ Ext.define('LU.model.Timing', {
                 convert: function(value, record) {
                     if (value) {
                         var date = value.split(/[\-T:]/);
-                        return new Date(date[0], date[1]-1, date[2], date[3]);
+                        return new Date(date[0], date[1]-1, date[2], date[3], date[4]);
                     } else {
                         return new Date();
                     }
