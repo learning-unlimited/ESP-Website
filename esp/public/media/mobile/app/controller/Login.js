dc4663de3ac2ce7445d3c0b7512cad849763ccd0
Alan Yeo Jie Wei <yeojw10@gmail.com>
2012-08-12 20:19:31 +0800

    Add form validation and error prompt for Login

--- esp/public/media/mobile/app/controller/Login.js
+++ esp/public/media/mobile/app/controller/Login.js
@@ -4,60 +4,69 @@ Ext.define('LU.controller.Login', {
     config: {
         refs: {
             loginForm: 'loginForm',
+            usernameField: 'loginForm textfield',
+            passwordField: 'loginForm passwordfield',
             main: 'main'
         },
         control: {
-            'loginForm passwordfield': {
+            passwordField: {
                 action: 'onLogin'
             }
         }
     },
 
     onLogin: function(textfield) {
-        this.getLoginForm().submit({
-            url: '/myesp/ajax_login/',
-            method: 'POST',
-            params: {
-                // tells the server that we are logging in using mobile site
-                'isMobile': true
-            },
+        if (this.getUsernameField().getValue() != '' && this.getPasswordField().getValue() != '') {
+            this.getLoginForm().submit({
+                url: '/myesp/ajax_login/',
+                method: 'POST',
+                params: {
+                    // tells the server that we are logging in using mobile site
+                    'isMobile': true
+                },
 
-            success: function(form, result) {
-                var program = Ext.widget('programList');
-                var role = '';
+                success: function(form, result) {
+                    var program = Ext.widget('programList');
+                    var role = '';
 
-                if (result.isOnsite === 'true') {
-                    role = 'onsite';
-                } else if (result.isStudent == 'true') {
-                    role = 'student';
-                } else {
-                    // display error message for unknown role
-                    Ext.Msg.alert('Unauthorized Role', 'You have to be either a student or onsite user to access the app.');
-                    return;
+                    if (result.isOnsite === 'true') {
+                        role = 'onsite';
+                    } else if (result.isStudent == 'true') {
+                        role = 'student';
+                    } else {
+                        // display error message for unknown role
+                        Ext.Msg.alert('Unauthorized Role', 'You have to be either a student or onsite user to access the app.');
+                        return;
+                    }
+
+                    // stores the user's role in localstorage
+                    // (we need this to determine which interface to show later)
+                    var user = Ext.getStore('User');
+                    user.removeAll();
+                    user.add({role: role});
+                    user.sync();
+
+                    Ext.Viewport.getActiveItem().destroy();
+                    Ext.Viewport.setActiveItem(program);
+                },
+
+                failure: function(form, result) {
+                    console.log(result);
+                    if (result.message) {
+                        Ext.Msg.alert('Login Error', result.message);
+                    } else if (result.statusText) {
+                        Ext.Msg.alert('Login Error', 'An error has occurred. (' + result.statusText + ')')
+                    } else {
+                        Ext.Msg.alert('Login Error', 'An unknown error has occurred. You may wish to try logging again later.');
+                    }
                 }
-
-                // stores the user's role in localstorage
-                // (we need this to determine which interface to show later)
-                var user = Ext.getStore('User');
-                user.removeAll();
-                user.add({role: role});
-                user.sync();
-
-                Ext.Viewport.getActiveItem().destroy();
-                Ext.Viewport.setActiveItem(program);
-            },
-
-            failure: function(form, result) {
-                console.log(result);
-                if (result.message) {
-                    Ext.Msg.alert('Login Error', result.message);
-                } else if (result.statusText) {
-                    Ext.Msg.alert('Login Error', 'An error has occurred. (' + result.statusText + ')')
-                } else {
-                    Ext.Msg.alert('Login Error', 'An unknown error has occurred. You may wish to try logging again later.');
-                }
-            }
-        });
+            });
+        } else {
+            var field_string = '';
+            if (this.getUsernameField().getValue() === '') field_string = 'username';
+            if (this.getPasswordField().getValue() === '') field_string += (field_string === '') ? 'password' : ' and password';
+            Ext.Msg.alert('Invalid Form', 'You did not provide your ' + field_string + '.');
+        }
     }
 });
 
