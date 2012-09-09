82f63ed0be9c442785782cf2fe7cf2fae9f75dc7
Anthony Lu <lua@mit.edu>
2012-09-09 02:20:05 -0400

    Add method FsStudentApp.get_responses that returns a list of (question, response) pairs for an app

--- esp/esp/application/models.py
+++ esp/esp/application/models.py
@@ -17,6 +17,21 @@ class FsStudentApp(models.Model):
         return "{}'s app for {}".format(self.user, self.program)
 
     def get_submitted_data(self):
+        """ Returns the raw submitted data from the API, as a JSON dict. """
+
         fsas = self.program.getModuleExtension('FormstackAppSettings')
         submission = fsas.formstack.submission(self.id)
         return submission
+
+    def get_responses(self):
+        """ Returns a list of (question, response) tuples from submitted data. """
+
+        data = self.get_submitted_data()
+        fsas = self.program.getModuleExtension('FormstackAppSettings')
+        info = fsas.get_field_info()
+        id_to_label = { field['id']: field['label'] for field in info }
+        result = []
+        for response in data['data']:
+            result.append((id_to_label[response['field']],
+                           response['value']))
+        return result
