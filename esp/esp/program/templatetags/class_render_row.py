61c8022b153bade0a9b6319c1fef96454f834c1e
Gurtej Kanwar <gurtej@esp.mit.edu>
2012-09-12 19:20:05 -0400

    Fixes email host in email addresses

--- esp/esp/program/templatetags/class_render_row.py
+++ esp/esp/program/templatetags/class_render_row.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from esp.web.util.template import cache_inclusion_tag
 from esp.cache.key_set import wildcard
 from esp.program.models import Program, ClassSubject, ClassSection
@@ -21,7 +22,9 @@ def render_class_teacher_list_row(klass, can_edit, friendly_times_with_date):
             'program': klass.parent_program,
             'teacherclsmodule': klass.parent_program.getModuleExtension('ClassRegModuleInfo'),
             'can_edit': can_edit,
-            'friendly_times_with_date': friendly_times_with_date}
+            'friendly_times_with_date': friendly_times_with_date,
+            'email_host': settings.EMAIL_HOST
+            }
 render_class_teacher_list_row.cached_function.depend_on_row(ClassSubject, lambda cls: {'klass': cls})
 render_class_teacher_list_row.cached_function.depend_on_row(ClassSection, lambda sec: {'klass': sec.parent_class})
 render_class_teacher_list_row.cached_function.depend_on_cache(ClassSubject.title, lambda self=wildcard, **kwargs: {'klass': self})
