7392bbc880bf7693fb4b213e9e0f2b0742bf18b7
Anthony Lu <lua@mit.edu>
2012-10-14 21:25:01 -0400

    Add FormstackMedliabModule

---
+++ esp/esp/program/modules/handlers/formstackmedliabmodule.py
@@ -0,0 +1,88 @@
+
+__author__    = "Individual contributors (see AUTHORS file)"
+__date__      = "$DATE$"
+__rev__       = "$REV$"
+__license__   = "AGPL v.3"
+__copyright__ = """
+This file is part of the ESP Web Site
+Copyright (c) 2007 by the individual contributors
+  (see AUTHORS file)
+
+The ESP Web Site is free software; you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+Contact information:
+MIT Educational Studies Program
+  84 Massachusetts Ave W20-467, Cambridge, MA 02139
+  Phone: 617-253-4882
+  Email: esp-webmasters@mit.edu
+Learning Unlimited, Inc.
+  527 Franklin St, Cambridge, MA 02139
+  Phone: 617-379-0178
+  Email: web-team@lists.learningu.org
+"""
+from esp.program.modules.base import ProgramModuleObj, needs_teacher, needs_student, needs_admin, usercheck_usetl, meets_deadline, main_call, aux_call
+from esp.datatree.models import *
+from esp.program.modules import module_ext
+from esp.web.util        import render_to_response
+from esp.middleware      import ESPError
+from esp.users.models    import ESPUser, UserBit, User
+from django.db.models.query       import Q
+from django.template.loader import get_template
+from esp.program.models  import FinancialAidRequest
+from esp.tagdict.models import Tag
+from django.conf import settings
+from esp.middleware.threadlocalrequest import get_current_request
+from django              import forms
+from urllib import urlencode
+
+
+# hackish solution for Splash 2012
+class FormstackMedliabModule(ProgramModuleObj):
+    """ Module for collecting medical information online via Formstack """
+
+    reg_verb = GetNode('V/Flags/Registration/FormstackMedliabDone')
+
+    @classmethod
+    def module_properties(cls):
+        return {
+            "admin_title": "Formstack Med-liab Module",
+            "link_title": "Medical and Emergency Contact Information",
+            "module_type": "learn",
+            "seq": 0,
+            "required": True
+            }
+
+    def isCompleted(self):
+        return UserBit.valid_objects().filter(user=get_current_request().user,
+                                              qsc=self.program.anchor,
+                                              verb=self.reg_verb).exists()
+
+    @main_call
+    @needs_student
+    def medliab(self, request, tl, one, two, module, extra, prog):
+        """
+        Landing page redirecting to med-liab form on Formstack.
+        """
+        # code for Junction admissions will have better handling of
+        # Formstack forms
+        # for now, just render a QSD page
+        context = {}
+        return render_to_response(self.baseDir()+'medliab.html',
+                                  request, (prog, tl), context)
+
+
+    class Meta:
+        abstract = True
+
