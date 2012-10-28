ff5df00a8990083d388b738d0bf0d12252a41d2d
Sweet Tea Dorminy <sweettea@mit.edu>
2012-10-28 19:13:00 -0400

    Added icon-based resource visualization to rooms and classes.

--- esp/esp/settings.py
+++ esp/esp/settings.py
@@ -34,7 +34,7 @@ Learning Unlimited, Inc.
 
 import os
 
-PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
+PROJECT_ROOT = "/home/sweettea/esp/esp/"
 
 # Configure Django to support ESP
 from django_settings import *
