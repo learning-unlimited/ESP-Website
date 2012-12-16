11ae882ec597028cb0450547741a059387733fdf
uakf.b <perennate@gmail.com>
2012-12-15 21:43:07 -0500

    Teacher events module management web interface.
    Altered the existing teacher events module to be able to add and remove teacher training and interview timeslots. The name and friendly name are set automatically with a nice date format.

---
+++ esp/esp/program/modules/forms/teacherevents.py
@@ -0,0 +1,26 @@
+from django import forms
+
+from datetime import timedelta
+
+from esp.resources.models import ResourceType, Resource, ResourceAssignment
+from esp.cal.models import EventType, Event
+from esp.program.models import Program
+from esp.utils.widgets import DateTimeWidget
+from esp.datatree.models import GetNode
+
+class TimeslotForm(forms.Form):
+    start = forms.DateTimeField(label='Start Time', help_text='Format: MM/DD/YYYY HH:MM:SS <br />Example: 10/14/2007 14:00:00', widget=DateTimeWidget)
+    hours = forms.IntegerField(widget=forms.TextInput(attrs={'size':'6'}))
+    minutes = forms.IntegerField(widget=forms.TextInput(attrs={'size':'6'}))
+        
+    def save_timeslot(self, program, slot, type, anchor):
+        slot.start = self.cleaned_data['start']
+        slot.end = slot.start + timedelta(hours=self.cleaned_data['hours'], minutes=self.cleaned_data['minutes'])
+        slot.event_type = EventType.objects.all()[0]    # default event type for now
+        slot.anchor = GetNode(anchor.uri + "/" + str(slot.start))
+        
+        slot.short_description = slot.start.strftime('%A, %B %d %Y %I:%M %p') + " to " + slot.end.strftime('%I:%M %p')
+        slot.description = slot.short_description
+        
+        slot.save()
+
