learning-unlimited/ESP-Website

commit f0f5c97cc218fcb19cce941060f87523e4c2168e

Moves admin vital init code to adminvitals.js

esp/public/media/scripts/program/modules/adminvitals.js
@@ -1,5 +1,20 @@
 statsFilled = false;
 
+// Initialize the json_data holder
+json_data = {};
+
+$j(document).ready(function() {
+    $j(".module_group_expandable").click(function() {
+	var $module_group_body = $j(this).next(".module_group_body").toggle();
+	if($module_group_body.is(":visible")) {
+            $j(this).children(".expand_collapse_text").text("(click to collapse)");
+	}
+	else {
+            $j(this).children(".expand_collapse_text").text("(click to expand)");
+	}
+    });
+});
+
 function fillStats(data)
 {
     // Pull out the stats data

esp/templates/program/modules/admincore/mainpage.html
@@ -20,23 +20,6 @@ function updateDocs(docs) {
 <script type="text/javascript" src="/media/scripts/jquery-ui.js"></script>
 <script type="text/javascript" src="/media/scripts/json_fetch.js"></script>
 <script type="text/javascript" src="/media/scripts/sorttable.js"></script>
-
-<script type="text/javascript">
-  // Initialize the json_data holder
-  json_data = {};
-
-  $j(document).ready(function() {
-    $j(".module_group_expandable").click(function() {
-      var $module_group_body = $j(this).next(".module_group_body").toggle();
-      if($module_group_body.is(":visible")) {
-        $j(this).children(".expand_collapse_text").text("(click to collapse)");
-      }
-      else {
-        $j(this).children(".expand_collapse_text").text("(click to expand)");
-      }
-    });
-  });
-</script>
 {% endblock %}
 
 {% block stylesheets %}

commit 52a65788e7b0b968140ed05ca160c203bf2c4e8c

Moves admin dashboard init code from adminvitals.js to admincore.js
Fixes bug introduced in commit f0f5c97cc2, which moved code from the
dashboard to adminvitals. This broke the rest of the dashboard
(i.e. adminclass and any future dashboard modules) if adminvitals
wasn't enabled.

esp/public/media/scripts/program/modules/admincore.js
@@ -1,0 +1,14 @@
+// Initialize the json_data holder
+json_data = {};
+
+$j(document).ready(function() {
+    $j(".module_group_expandable").click(function() {
+	var $module_group_body = $j(this).next(".module_group_body").toggle();
+	if($module_group_body.is(":visible")) {
+            $j(this).children(".expand_collapse_text").text("(click to collapse)");
+	}
+	else {
+            $j(this).children(".expand_collapse_text").text("(click to expand)");
+	}
+    });
+});

esp/public/media/scripts/program/modules/adminvitals.js
@@ -1,20 +1,5 @@
 statsFilled = false;
 
-// Initialize the json_data holder
-json_data = {};
-
-$j(document).ready(function() {
-    $j(".module_group_expandable").click(function() {
-	var $module_group_body = $j(this).next(".module_group_body").toggle();
-	if($module_group_body.is(":visible")) {
-            $j(this).children(".expand_collapse_text").text("(click to collapse)");
-	}
-	else {
-            $j(this).children(".expand_collapse_text").text("(click to expand)");
-	}
-    });
-});
-
 function fillStats(data)
 {
     // Pull out the stats data

esp/templates/program/modules/admincore/mainpage.html
@@ -20,6 +20,7 @@ function updateDocs(docs) {
 <script type="text/javascript" src="/media/scripts/jquery-ui.js"></script>
 <script type="text/javascript" src="/media/scripts/json_fetch.js"></script>
 <script type="text/javascript" src="/media/scripts/sorttable.js"></script>
+<script type="text/javascript" src="/media/scripts/program/modules/admincore.js"></script>
 {% endblock %}
 
 {% block stylesheets %}