98d1b1c94c257679713ec32d1ed0e0f666935731
Michael Price <price@kilentra.net>
2012-12-28 17:03:26 -0500

    Fix a bug in the LESS parsing in theme editor submission

--- esp/esp/themes/views.py
+++ esp/esp/themes/views.py
@@ -60,7 +60,7 @@ def parse_less(less_file_path):
     #this regex is supposed to match @(property) = (value);
     #or @(property) = (function)(args) in match[0] and 
     #match[1] respectively
-        matches = re.findall(r"@(\w+):\s*([^,;\(]*)[,;\(]", f)
+        matches = re.findall("@(\w+):\s*(.*?);", f)
         d = {}
         for match in matches:
             d[match[0]] = match[1]
