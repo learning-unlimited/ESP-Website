08c860f92d0f8d622b4a8dbb557c1290c907be0c
Michael Price <price@kilentra.net>
2012-12-30 17:06:41 -0500

    Add controllers for simpler accounting system
    The controllers are here to simplify code throughout the site that needs
    to manipulate accounting data.  Rather than accessing the accounting
    models directly, import one of these from esp.accounting.controllers:
    - ProgramAccountingController: handles setup, and fetches accounts and
      line item types, for a particular program
    - IndividualAccountingController: manipulates accounting data for an
      individual student at a fairly high level (get/set optional items,
      get/set financial aid results, handle payment, calculate amount due)

---
+++ esp/esp/accounting/controllers.py
@@ -0,0 +1,302 @@
+
+__author__    = "Individual contributors (see AUTHORS file)"
+__date__      = "$DATE$"
+__rev__       = "$REV$"
+__license__   = "AGPL v.3"
+__copyright__ = """
+This file is part of the ESP Web Site
+Copyright (c) 2012 by the individual contributors
+  (see AUTHORS file)
+
+The ESP Web Site is free software; you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+Contact information:
+MIT Educational Studies Program
+  84 Massachusetts Ave W20-467, Cambridge, MA 02139
+  Phone: 617-253-4882
+  Email: esp-webmasters@mit.edu
+Learning Unlimited, Inc.
+  527 Franklin St, Cambridge, MA 02139
+  Phone: 617-379-0178
+  Email: web-team@lists.learningu.org
+"""
+
+from esp.accounting.models import Transfer, Account, FinancialAidGrant, LineItemType
+from esp.program.models import FinancialAidRequest
+
+from django.db.models import Sum
+from django.template.defaultfilters import slugify
+
+from decimal import Decimal
+
+
+class BaseAccountingController(object):
+
+    def default_source_account(self):
+        return Account.objects.get(name='receivable')
+    
+    def default_finaid_account(self):
+        return Account.objects.get(name='grants')
+
+
+class GlobalAccountingController(BaseAccountingController):
+
+    def setup_accounts(self):
+        result = []
+        default_accounts = [
+            ('receivable', 'Accounts Receivable\nAll money coming from the outside is routed through here'),
+            ('payable', 'Accounts Payable\nAll money leaving the organization is routed through here'),
+            ('grants', 'Financial Aid Grants\nThis account is the source of financial aid transfers'),
+        ]
+        for account_info in default_accounts:
+            (account, created) = Account.objects.get_or_create(name=account_info[0], description=account_info[1])
+            result.append(account)
+        return result
+
+
+class ProgramAccountingController(BaseAccountingController):
+    def __init__(self, program, *args, **kwargs):
+        self.program = program
+
+    def setup_accounts(self):
+        #   For now, just create a single account for the program.  In the
+        #   future we may want finer grained accounting per program.
+        program = self.program
+        (account, created) = Account.objects.get_or_create(name=slugify(program.name), description='Main account for %s' % program.niceName(), program=program)
+        return account
+
+    def setup_lineitemtypes(self, base_cost, optional_items=None):
+        result = []
+        program = self.program
+
+        #   optional_items is list of 3-tuples: (item name, cost, max quantity)
+        if optional_items is None:
+            optional_items = ()
+            
+        (lit_required, created) = LineItemType.objects.get_or_create(
+            text='Program admission',
+            amount_dec=Decimal('%.2f' % base_cost),
+            required=True,
+            max_quantity=1,
+            program=program,
+            for_payments=False
+        )
+        result.append(lit_required)
+        
+        (lit_payments, created) = LineItemType.objects.get_or_create(
+            text='Student payment',
+            program=program,
+            for_payments=True
+        )
+        result.append(lit_payments)
+        
+        (lit_finaid, created) = LineItemType.objects.get_or_create(
+            text='Financial aid grant',
+            program=program,
+            for_finaid=True
+        )
+        result.append(lit_finaid)
+        
+        for item in optional_items:
+            (lit_optional, created) = LineItemType.objects.get_or_create(
+                text=item[0],
+                amount_dec=Decimal('%.2f' % item[1]),
+                required=False,
+                max_quantity=item[2],
+                program=program,
+                for_payments=False
+            )
+            result.append(lit_optional)
+        return result
+
+    def default_program_account(self):
+        return Account.objects.filter(program=self.program).order_by('id')[0]
+    
+    def default_payments_lineitemtype(self):
+        return LineItemType.objects.filter(program=self.program, for_payments=True).order_by('-id')[0]
+    
+    def default_finaid_lineitemtype(self):
+        return LineItemType.objects.filter(program=self.program, for_finaid=True).order_by('-id')[0]
+
+    def get_lineitemtypes(self, required_only=False, optional_only=False, payment_only=False):
+        if required_only:
+            qs = LineItemType.objects.filter(program=self.program, required=True, for_payments=False, for_finaid=False)
+        elif optional_only:
+            qs = LineItemType.objects.filter(program=self.program, required=False, for_payments=False, for_finaid=False)
+        elif payment_only:
+            qs = LineItemType.objects.filter(program=self.program, required=False, for_payments=True, for_finaid=False) 
+        else:
+            LineItemType.objects.filter(program=self.program) 
+            
+        return qs.order_by('text', '-id').distinct('text')
+
+    def execute_pending_transfers(self):
+        pass
+
+
+class IndividualAccountingController(ProgramAccountingController):
+    def __init__(self, program, user, *args, **kwargs):
+        super(IndividualAccountingController, self).__init__(program, *args, **kwargs)
+        self.user = user
+
+    def add_required_transfers(self):
+        """ Function to ensure there are transfers for this user corresponding
+            to required line item types, e.g. program admission """
+
+        result = []
+        program_account = self.default_program_account()
+        source_account = self.default_source_account()
+        line_items = self.get_lineitemtypes(required_only=True)
+
+        #   Clear existing transfers
+        Transfer.objects.filter(user=self.user, line_item__in=line_items, executed=False).delete()
+
+        #   Create transfers for required line item types
+        for lit in line_items:
+            result.append(Transfer.objects.create(source=source_account, destination=program_account, user=self.user, line_item=lit, amount_dec=lit.amount_dec))
+        return result
+
+    def apply_preferences(self, optional_items):
+        """ Function to ensure there are transfers for this user corresponding
+            to optional line item types, accoring to their preferences.
+            optional_items is a list of 2-tuples: (item name, quantity) """
+
+        result = []
+        program_account = self.default_program_account()
+        source_account = self.default_source_account()
+        line_items = self.get_lineitemtypes(optional_only=True)
+
+        #   Clear existing transfers
+        Transfer.objects.filter(user=self.user, line_item__in=line_items, executed=False).delete()
+
+        #   Create transfers for optional line item types
+        for item in optional_items:
+            matched = False
+            for lit in line_items:
+                if lit.text == item[0]:
+                    matched = True
+                    for i in range(item[1]):
+                        result.append(Transfer.objects.create(source=source_account, destination=program_account, user=self.user, line_item=lit, amount_dec=lit.amount_dec))
+                    break
+            if not matched:
+                raise Exception('Could not find a line item type matching "%s"' % item[0])
+
+        return result
+
+    def get_transfers(self, **kwargs):
+        program_account = self.default_program_account()
+        source_account = self.default_source_account()
+        line_items = self.get_lineitemtypes(**kwargs)
+        return Transfer.objects.filter(user=self.user, line_item__in=line_items)
+
+    def get_preferences(self):
+        #   Return a list of 2-tuples: (item name, quantity)
+        result = []
+        transfers = self.get_transfers(optional_only=True)
+        for transfer in transfers:
+            li_name = transfer.line_item.text
+            if li_name not in map(lambda x: x[0], result):
+                result.append([li_name, 0])
+            result_index = map(lambda x: x[0], result).index(li_name)
+            result[result_index][1] += 1
+        return result
+
+    def set_finaid_params(self, dollar_amount, discount_percent):
+        #   Get the user's financial aid request or create one if it doesn't exist
+        requests = FinancialAidRequest.objects.filter(user=self.user, program=self.program)
+        if requests.exists():
+            request = requests.order_by('-id')[0]
+        else:
+            request = FinancialAidRequest.objects.create(user=self.user, program=self.program, extra_explaination='Request automatically generated by accounting system')
+        
+        #   Create or modify a corresponding grant with the discount set to 100%
+        (grant, created) = FinancialAidGrant.objects.get_or_create(request=request)
+        if dollar_amount:
+            grant.amount_max_dec = Decimal('%.2f' % dollar_amount)
+        grant.percent = discount_percent
+        grant.save()
+
+    def grant_full_financial_aid(self):
+        self.set_finaid_params(None, 100)
+
+    def revoke_financial_aid(self):
+        FinancialAidGrant.objects.filter(request__user=self.user, request__program=self.program).delete()
+
+    def amount_requested(self):
+        self.add_required_transfers()
+        program_account = self.default_program_account()
+        
+        #   Compute sum of all transfers into program that are for this user
+        if Transfer.objects.filter(user=self.user, destination=program_account).exists():
+            amount_requested = Transfer.objects.filter(user=self.user, destination=program_account).aggregate(Sum('amount_dec'))['amount_dec__sum']
+        else:
+            amount_requested = Decimal('0')
+        
+        return amount_requested
+    
+    def amount_finaid(self, amount_requested=None):
+        if amount_requested is None:
+            amount_requested = self.amount_requested()
+
+        aid_amount = Decimal('0')
+        if FinancialAidGrant.objects.filter(request__user=self.user, request__program=self.program).exists():
+            latest_grant = FinancialAidGrant.objects.filter(request__user=self.user, request__program=self.program).order_by('-id')[0]
+            print 'Applying financial aid for %s at %s' % (self.user.name(), self.program.niceName())
+
+            if latest_grant.amount_max_dec is not None:
+                print '-- Aid ceiling: %s' % latest_grant.amount_max_dec
+                if amount_requested > latest_grant.amount_max_dec:
+                    aid_amount = latest_grant.amount_max_dec
+                    print '   Aid updated to %s' % aid_amount
+                else:
+                    aid_amount = amount_requested
+                    print '   Aid updated to %s (equal to amount due)'
+
+            if latest_grant.percent is not None:
+                print '-- Discount percentage: %s%%' % latest_grant.percent
+                discount_aid_amount = (Decimal('0.01') * latest_grant.percent) * (amount_requested - aid_amount)
+                aid_amount += discount_aid_amount
+                print '   Aid updated to %s' % aid_amount
+
+        return aid_amount
+    
+    def amount_paid(self):
+        #   Compute sum of all transfers from outside (e.g. credit card payments) that are for this user
+        if Transfer.objects.filter(user=self.user, line_item=self.default_payments_lineitemtype(), source__isnull=True).exists():
+            amount_paid = Transfer.objects.filter(user=self.user, line_item=self.default_payments_lineitemtype(), source__isnull=True).aggregate(Sum('amount_dec'))['amount_dec__sum']
+        else:
+            amount_paid = Decimal('0')
+        return amount_paid
+    
+    def amount_due(self):
+        amt_request = self.amount_requested()
+        return amt_request - self.amount_finaid(amt_request) - self.amount_paid()
+
+    def clear_payments(self):
+        #   Remove all payments listed for this user at this program
+        line_item_type = self.default_payments_lineitemtype()
+        target_account = self.default_source_account()
+        Transfer.objects.filter(source=None, destination=target_account, user=self.user, line_item=line_item_type).delete()
+
+    def submit_payment(self, amount):
+        #   Create a transfer representing a user's payment for this program
+        line_item_type = self.default_payments_lineitemtype()
+        target_account = self.default_source_account()
+        return Transfer.objects.create(source=None, destination=target_account, user=self.user, line_item=line_item_type, amount_dec=Decimal('%.2f' % amount))
+
+    def __unicode__(self):
+        return 'Accounting for %s at %s' % (self.user.name(), self.program.niceName())
+    
+
