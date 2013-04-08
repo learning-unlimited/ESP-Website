7e5099a8602adcc8bf892a50ea3487488219eb84
Ruthie Byers <ruthieatmit@gmail.com>
2013-04-08 15:26:04 -0400

    UI tests able to log in

--- esp/esp/program/modules/tests/ajaxschedulingmodule_ui.py
+++ esp/esp/program/modules/tests/ajaxschedulingmodule_ui.py
@@ -1,6 +1,8 @@
 from esp.program.modules.base import ProgramModule, ProgramModuleObj
 from esp.program.tests import ProgramFrameworkTest
+from esp.program.modules.tests import AJAXSchedulingModuleTest
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 class AJAXSchedulingModuleUITest(ProgramFrameworkTest):
     def setUp(self, *args, **kwargs):
@@ -16,8 +18,31 @@ class AJAXSchedulingModuleUITest(ProgramFrameworkTest):
     def tearDown(self):
         self.browser.quit()
 
-    def testAJAX(self):
-        self.browser.get(self.live_server_url + '/manage/' + self.program.get_url_base() + '/ajax_scheduling')
-        self.failUnless(True, "hi")
-
+    def loginAdmin(self, uname, pword):
+        uname_el_id = "user"
+        pword_el_id = "pass"
+        submit_el_id = "gologin"
+ 
+        # self.browser.find_element_by_id(uname_el_id).send_keys(uname)        
+        # self.browser.find_element_by_id(pword_el_id).send_keys(pword)        
+        # self.browser.find_element_by_id(submit_el_id).click()
+        self.browser.get(self.live_server_url)
         
+        e = WebDriverWait(self.browser, 10).until(
+             lambda driver: self.browser.find_element_by_id(uname_el_id))
+        e.send_keys(uname)
+        e = WebDriverWait(self.browser, 10).until(
+             lambda driver: self.browser.find_element_by_id(pword_el_id))
+        e.send_keys(pword)
+        e = WebDriverWait(self.browser, 10).until(
+             lambda driver:    self.browser.find_element_by_id(submit_el_id))
+        e.click()
+
+    def testGetChangeLog(self):
+        self.loginAdmin(self.admins[0].username, "password")
+        self.browser.get(self.live_server_url + '/manage/' + self.program.getUrlBase() + '/ajax_scheduling')
+        #schedule a class on the server
+        
+        #browser gets the change log
+        
+        #test that the class is actually scheduled
