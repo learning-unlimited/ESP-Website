cb8e86cdaf2164e5d3a105f5dfc5e773d598db3a
Ruthie Byers <ruthieatmit@gmail.com>
2013-04-09 18:30:14 -0400

    factored out ajaxschedulingmoduletestbase

--- esp/esp/program/modules/tests/ajaxschedulingmodule_ui.py
+++ esp/esp/program/modules/tests/ajaxschedulingmodule_ui.py
@@ -1,31 +1,22 @@
 from esp.program.modules.base import ProgramModule, ProgramModuleObj
 from esp.program.tests import ProgramFrameworkTest
-from esp.program.modules.tests import AJAXSchedulingModuleTest
+#from esp.program.modules.tests.ajaxschedulingmodule import AJAXSchedulingModuleTestBase
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-class AJAXSchedulingModuleUITest(ProgramFrameworkTest):
-    def setUp(self, *args, **kwargs):
-        # Set up the program -- we want to be sure of these parameters
-        kwargs.update({
-            'num_rooms': 4,
-            'num_timeslots': 4, 'timeslot_length': 50, 'timeslot_gap': 10,
-            'num_teachers': 3, 'classes_per_teacher': 2, 'sections_per_class': 1
-            })
-        super(AJAXSchedulingModuleUITest, self).setUp(*args, **kwargs)
-        self.browser = WebDriver()
+class AJAXSchedulingModuleUITest(ProgramFrameworkTest):#AJAXSchedulingModuleTestBase):
+    def setUp(self, *args, **kwargs): 
+         super(AJAXSchedulingModuleUITest, self).setUp(*args, **kwargs)
+         self.browser = WebDriver()
 
     def tearDown(self):
         self.browser.quit()
 
-    def loginAdmin(self, uname, pword):
+    def loginAdminBrowser(self, uname, pword):
         uname_el_id = "user"
         pword_el_id = "pass"
         submit_el_id = "gologin"
  
-        # self.browser.find_element_by_id(uname_el_id).send_keys(uname)        
-        # self.browser.find_element_by_id(pword_el_id).send_keys(pword)        
-        # self.browser.find_element_by_id(submit_el_id).click()
         self.browser.get(self.live_server_url)
         
         e = WebDriverWait(self.browser, 10).until(
@@ -34,15 +25,31 @@ class AJAXSchedulingModuleUITest(ProgramFrameworkTest):
         e = WebDriverWait(self.browser, 10).until(
              lambda driver: self.browser.find_element_by_id(pword_el_id))
         e.send_keys(pword)
-        e = WebDriverWait(self.browser, 10).until(
+        e = WebDriverWait(self.browser, 60).until(
              lambda driver:    self.browser.find_element_by_id(submit_el_id))
         e.click()
 
-    def testGetChangeLog(self):
-        self.loginAdmin(self.admins[0].username, "password")
-        self.browser.get(self.live_server_url + '/manage/' + self.program.getUrlBase() + '/ajax_scheduling')
-        #schedule a class on the server
+    #mostly exists as sanity on testing framework
+    def testAjaxLoads(self):
+        self.loadAjax()
+        self.failUnless(self.browser.title == "ESP Scheduling Application")
         
-        #browser gets the change log
-        
-        #test that the class is actually scheduled
+    def testUpdateScheduledClass(self):
+        self.loadAjax()
+
+        #     #schedule a class on the server
+        #     #self.loginAdmin()
+        #     print "loaded ajax"
+        #     self.loginAdmin()
+        #     (section, rooms, times) = self.scheduleClass()
+        #     print "scheduled a class"
+        #     print section
+        #     print rooms
+        #     print times
+         
+        #     #self.browser.implicitly_wait(30)
+        #     #section turns up in the browser after no more than 30 seconds
+        #     el = self.browser.get_element_by_css('CLS_id_'+str(section.id))
+        #     print el
+        #     #self.failUnless()
+
