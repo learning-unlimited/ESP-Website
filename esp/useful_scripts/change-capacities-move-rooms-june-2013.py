60333c5216d920f8f9b73254638a7ff1c0909d0c
Benjamin Kraft <benjaminjkraft@gmail.com>
2013-06-29 12:48:38 -0400

    Makes minor improvements to class capacities script.

--- esp/useful_scripts/change-capacities-move-rooms-june-2013.py
+++ esp/useful_scripts/change-capacities-move-rooms-june-2013.py
@@ -14,40 +14,45 @@ import csv
 from esp.program.models.class_ import ClassSection, ClassSubject
 from esp.resources.models import Resource
 
-filename = '/home/ben/Documents/esp-website/hssp-capacities.csv'
+def makeChanges(filename):
 
-secs = []
+    secs = []
 
-with open(filename) as f:
-    c = csv.reader(f)
-    for row in c:
-        emailcode, capacity, room = row
-        class_id,sec_id = map(int,emailcode[1:].split('s'))
-        cls = ClassSubject.objects.get(id=class_id)
-        for s in cls.sections.all():
-            if s.index()==sec_id:
-                sec=s
-        secs.append((cls,sec,capacity,room))
+    with open(filename) as f:
+        c = csv.reader(f)
+        for row in c:
+            emailcode, capacity, room = row
+            class_id,sec_id = map(int,emailcode[1:].split('s'))
+            cls = ClassSubject.objects.get(id=class_id)
+            for s in cls.sections.all():
+                if s.index()==sec_id:
+                    sec=s
+            try:
+                secs.append((cls,sec,capacity,room))
+            except NameError:
+                print "Section %s not found" % emailcode
+                raise
 
-for cls,sec,capacity,room in secs:
-    if room is not "":
-        print "Removing %s from its room" % sec.emailcode()
-        sec.classroomassignments().delete()
+    for cls,sec,capacity,room in secs:
+        if room is not "":
+            cas = sec.classroomassignments()
+            print "Removing %s from %s" % (sec.emailcode(), ', '.join([x.resource.name for x in cas]))
+            cas.delete()
 
-for cls,sec,capacity,room in secs:
-    if capacity is not "":
-        print "Changing class %s size to %s" % (cls.emailcode(),capacity)
-        cls.class_size_max = int(capacity)
-        cls.save()
-    if room is not "":
-        print "Moving section %s to room %s" % (sec.emailcode(), room)
-        # the following is stolen from the manage class page code.  I don't like it, but that page works so I am using it.
-        room_res = Resource.objects.filter(name=room)
-        if room_res.count() > 0:
-            print sec.assign_room(room_res[0])
-        else:
-            print "I don't like room %s" % room
-    print "Marking class %s accepted" % cls.emailcode()
-    cls.accept()
+    for cls,sec,capacity,room in secs:
+        if capacity is not "":
+            print "Changing class %s size to %s" % (cls.emailcode(),capacity)
+            cls.class_size_max = int(capacity)
+            cls.save()
+        if room is not "":
+            print "Moving section %s to room %s" % (sec.emailcode(), room)
+            # the following is stolen from the manage class page code.  I don't like it, but that page works so I am using it.
+            room_res = Resource.objects.filter(name=room)
+            if room_res.count() > 0:
+                print sec.assign_room(room_res[0])
+            else:
+                print "I don't like room %s" % room
+        print "Marking class %s accepted" % cls.emailcode()
+        cls.accept()
             
 
