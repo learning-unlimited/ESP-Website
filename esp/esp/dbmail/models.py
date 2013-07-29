fb9de51735f708dbfbca809ca986e2919a8acd21
Andrew Geng <pteromys@gmail.com>
2013-07-29 14:28:43 -0500

    Revert "Disable context processors when rendering e-mail"
    Django internally caches the list of functions specified in
    TEMPLATE_CONTEXT_PROCESSORS, to save on imports:
    
    https://github.com/django/django/blame/1.4/django/template/context.py#L139
    
    Side effect of the reverted commit: If a comm panel preview is the first
    request handled after a code reload, the website will run with no
    context processors.
    
    With a testing procedure of sitting on the preview page and refreshing,
    this gives the appearance of that fix having worked.
    
    (As MessageRequest.parseSmartText was only ever triggered by the dbmail
    cron script, half of this didn't actually need to be reverted for
    correctness.)
    
    This reverts commit d9263ef802fa4abc351a69e0425562fd059623e6.

--- esp/esp/dbmail/models.py
+++ esp/esp/dbmail/models.py
@@ -152,17 +152,7 @@ class MessageRequest(models.Model):
         newtext = ''
         template = Template(text)
 
-        #   Save the current context processors - we will disable them for rendering e-mail
-        old_context_processors = settings.TEMPLATE_CONTEXT_PROCESSORS
-        settings.TEMPLATE_CONTEXT_PROCESSORS = []
-
-        #   Render the e-mail using the unaltered context
-        result = template.render(context)
-
-        #   Restore context processors for future template rendering
-        settings.TEMPLATE_CONTEXT_PROCESSORS = old_context_processors
-
-        return result
+        return template.render(context)
 
     def process(self, processoverride = False):
         """ Process this request...if it's an email, create all the necessary email requests. """
