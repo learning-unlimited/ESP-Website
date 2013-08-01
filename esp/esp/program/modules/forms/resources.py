ec81c1e89aa9524b6a133cc07b932a96c9b36809
Michael Price <price@learningu.org>
2013-08-01 10:41:39 -0400

    Allow classrooms to be imported with complete availability
    The classroom import form used to match the timeslots from the previous
    program with the timeslots of the new program, and make the new classrooms
    available only at those timeslots.  Now you can still do that, but there
    is a checkbox on the classroom import form to simply create classrooms for
    all of the new program's timeslots.

--- esp/esp/program/modules/forms/resources.py
+++ esp/esp/program/modules/forms/resources.py
@@ -219,6 +219,7 @@ class ClassroomForm(forms.Form):
 
 class ClassroomImportForm(forms.Form):
     program = forms.ModelChoiceField(queryset=Program.objects.all())
+    complete_availability = forms.BooleanField(required=False, help_text='Check this box if you would like the new classrooms to be available at all times during the program, rather than attempting to replicate their availability from the previous program.')
     
 class TimeslotImportForm(forms.Form):
     program = forms.ModelChoiceField(queryset=Program.objects.all())
