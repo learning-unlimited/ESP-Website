5e82cf6bde8d993aa46d94b6e8a5a91677088bd8
Anthony Lu <lua@mit.edu>
2013-08-11 23:29:31 -0400

    Add sections model; fix dependencies between observables

--- esp/public/media/scripts/catalog-new.js
+++ esp/public/media/scripts/catalog-new.js
@@ -24,14 +24,27 @@ var ClassSubject = function (data, vm) {
     // teacher objs for the teacher ids
     self.teachers = ko.computed(function () {
         var teachersIndex = vm.teachers();
-        return ko.utils.arrayMap(self.teacher_ids(), function (teacher_id) {
-            return teachersIndex[teacher_id];
+        var ret = [];
+        ko.utils.arrayForEach(self.teacher_ids(), function (teacher_id) {
+            var teacher = teachersIndex[teacher_id];
+            if (teacher !== undefined) {
+                ret.push(teacher);
+            }
         });
+        return ret;
     });
 
-    // TODO: make a Sections model and a sections index and fill this in
+    // section objs for the section ids
     self.sections = ko.computed(function () {
-        return [];
+        var sectionsIndex = vm.sections();
+        var ret = [];
+        ko.utils.arrayForEach(self.section_ids(), function (section_id) {
+            var section = sectionsIndex[section_id];
+            if (section !== undefined) {
+                ret.push(section)
+            }
+        });
+        return ret;
     });
 
     // rename attributes: teachers -> teacher_ids, sections -> section_ids
@@ -54,11 +67,19 @@ var ClassSubject = function (data, vm) {
     });
 };
 
+// Section model constructor
+var ClassSection = function (data, vm) {
+    var self = this;
+    self.index = ko.observable("");
+
+    simpleFromJS(data, self);
+};
+
 // Teacher model constructor
 var Teacher = function (data, vm) {
     var self = this;
-    self.first_name = ko.observable();
-    self.last_name  = ko.observable();
+    self.first_name = ko.observable("");
+    self.last_name  = ko.observable("");
 
     self.name = ko.computed(function () {
         return self.first_name() + " " + self.last_name();
@@ -82,13 +103,18 @@ var CatalogViewModel = function () {
         }
         return ret;
     });
-    json_fetch(['class_subjects'], function (data) {
-	self.loading(false);
+    json_fetch(['class_subjects', 'sections'], function (data) {
+        self.loading(false);
         // update classes
         for (var key in data.classes) {
             data.classes[key] = new ClassSubject(data.classes[key], self);
         }
         self.classes(data.classes);
+        // update sections
+        for (var key in data.sections) {
+            data.sections[key] = new ClassSection(data.sections[key], self);
+        }
+        self.sections(data.sections);
         // update teachers
         for (var key in data.teachers) {
             data.teachers[key] = new Teacher(data.teachers[key], self);
