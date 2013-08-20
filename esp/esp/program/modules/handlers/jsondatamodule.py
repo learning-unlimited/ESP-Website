532b25542719a9068085665fd3b8ff4550e0cfcf
Michael Price <price@learningu.org>
2013-08-19 20:30:22 -0400

    Fix class titles in lottery

--- esp/esp/program/modules/handlers/jsondatamodule.py
+++ esp/esp/program/modules/handlers/jsondatamodule.py
@@ -194,6 +194,7 @@ class JSONDataModule(ProgramModuleObj, CoreModule):
                 'parent_class__category__symbol',
                 'parent_class__grade_max',
                 'parent_class__grade_min',
+                'parent_class__title',
                 'enrolled_students'))
         for section in sections:
             s = ClassSection.objects.get(id=section['id'])
