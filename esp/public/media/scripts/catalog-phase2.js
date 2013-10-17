168be58d30d496524e38385b758a30af00a4ce2d
Anthony Lu <lua@mit.edu>
2013-10-17 12:55:10 -0400

    Make phase 2 "save and exit" do a simple GET if non-dirty

--- esp/public/media/scripts/catalog-phase2.js
+++ esp/public/media/scripts/catalog-phase2.js
@@ -3,38 +3,43 @@ var dirty = false;
 $j(document).ready(function() {
     $j('#catalog-sticky .pri-select').select2();
     for (var i = 0; i < num_priorities; ++i) {
-	catalog_view_model.prioritySelection[i].subscribe(function() {
-	    dirty = true;
-	});
+        catalog_view_model.prioritySelection[i].subscribe(function() {
+            dirty = true;
+        });
     }
 });
 
 $j(window).on('beforeunload', function() {
     if (dirty) {
-	return ('Warning: You have unsaved changes. Please click save and ' +
-		'exit if you wish to preserve your changes.')
+        return ('Warning: You have unsaved changes. Please click save and ' +
+                'exit if you wish to preserve your changes.')
     }
 });
 
 var error_and_quit = function(err) {
     alert('Error: ' + err + '. Please report the issue to esp-web@mit.edu ' +
-	  'if it is recurring.');
+          'if it is recurring.');
     window.location = '/learn/' + program_core_url + 'studentreg_2';
 };
 
 build_json_data = function(form) {
+    var $form = $j(form);
+    var learn_url = program_base_url.replace(/^\/json/, '/learn');
     if (dirty) {
-	priorities = {}
-	$j('#catalog-sticky .pri-select').each(function() {
-	    priorities[$j(this).data('pri')] = $j(this).val();
-	});
-	response = {};
-	response[timeslot_id] = priorities;
-	json_data = JSON.stringify(response);
-	$input = $j('<input type="hidden"/>');
-	$input.attr('name', 'json_data');
-	$input.val(json_data);
-	console.log(form);
-	$j(form).append($input);
+        var priorities = {};
+        $j('#catalog-sticky .pri-select').each(function() {
+            priorities[$j(this).data('pri')] = $j(this).val();
+        });
+        var response = {};
+        response[timeslot_id] = priorities;
+        var json_data = JSON.stringify(response);
+        $form.find("input[name=json_data]").val(json_data);
+        $form.attr('action', learn_url + 'save_priorities');
+        $form.attr('method', 'post');
     }
+    else {
+        $form.attr('action', learn_url + 'studentreg');
+        $form.attr('method', 'get');
+    }
+    return true;
 };
