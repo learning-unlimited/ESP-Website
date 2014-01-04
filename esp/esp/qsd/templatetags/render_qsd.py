f7964995893ced71cca1257678e0547f93f3db21
Jordan Moldow <jmoldow@mit.edu>
2014-01-04 04:02:56 -0500

    Removes user from InlineQSDNode
    The InlineQSDNode class and the inline_qsd_block tag used to take a
    template.Variable parameter called user. On the rendered page, the
    inline QSD would be editable if and only if the resolved user had the
    required permissions.
    
    A while ago, we stopped doing this at the template level, and started
    doing this at the CSS/JS level. But we didn't remove the user parameter
    from InlineQSDNode / inline_qsd_block, even though it mostly doesn't do
    anything anymore.
    
    This commit removes the user parameter, and updates templates to remove
    the remaining user arguments.
    
    At the same time, this commit also simplifies some templates (by
    replacing trivial uses of inline_qsd_block with the render_inline_qsd
    tag), fixes a typo, makes some minor improvements to
    InlineQSDNode.render(), and removes the unused QSDStringNode class.
    
    NOTE: This commit changes the behavior of new inline QSDs. Previously, a
    QSD object was created and saved on the first load of a page, using the
    resolution of the user parameter as the author. Now, since InlineQSDNode
    no longer has a user parameter, this will not occur, and the QSD object
    will be created on first edit. This may or may not have caching
    implications. If it does, the old functionality can easily be restored,
    by getting request.user from the context parameter to
    InlineQSDNode.render(), and using that user as the author.

--- esp/esp/qsd/templatetags/render_qsd.py
+++ esp/esp/qsd/templatetags/render_qsd.py
@@ -1,7 +1,6 @@
 from django import template
 from django.shortcuts import render_to_response
 from django.core.cache import cache
-from esp.users.models import Permission
 from esp.web.util.template import cache_inclusion_tag, DISABLED
 from esp.qsd.models import QuasiStaticData
 from esp.qsd.models import qsd_cache_key, qsd_edit_id
@@ -34,69 +33,53 @@ def render_inline_program_qsd(program, name):
         return {'url': url, 'edit_id': qsd_edit_id(url)}
 
     return {'qsdrec': qsd_obj}
-render_inline_qsd.cached_function.depend_on_row(QuasiStaticData, lambda qsd: {'url':qsd.url})
+render_inline_program_qsd.cached_function.depend_on_row(QuasiStaticData, lambda qsd: {'url':qsd.url})
     
 
 class InlineQSDNode(template.Node):
-    def __init__(self, nodelist, url, user_variable, program_variable):
+    def __init__(self, nodelist, url, program_variable):
         self.nodelist = nodelist
         self.url = url
-        self.user_variable = template.Variable(user_variable) if user_variable is not None else None
         self.program_variable = template.Variable(program_variable) if program_variable is not None else None
         
     def render(self, context):
-        try:
-            user = self.user_variable.resolve(context) if self.user_variable is not None else None
-        except template.VariableDoesNotExist:
-            user = None
         try:
             program = self.program_variable.resolve(context) if self.program_variable is not None else None
         except template.VariableDoesNotExist:
             program = None
         #   Accept literal string url argument if it is quoted; otherwise expect a template variable.
-        if not self.url.startswith('"'):
-            url_resolved = template.Variable(self.url).resolve(context)
-        else:
-            url_resolved = self.url.strip('"')
+        #   template.Variable resolves string literals to themselves automatically
+        url_resolved = template.Variable(self.url).resolve(context)
         if program is not None:
             url = QuasiStaticData.prog_qsd_url(program,url_resolved)
         else:
             url = url_resolved
         #probably should have an error message if variable was not None and prog was
 
-        edit_bits = Permission.user_can_edit_qsd(user, url)
-
         qsd_obj = QuasiStaticData.objects.get_by_url(url)
-        if qsd_obj == None:
-            new_qsd = QuasiStaticData()
-            new_qsd.url = url
-            new_qsd.name = url.split('/')[0]
-            new_qsd.title = url
-            new_qsd.content = self.nodelist.render(context)
-            
-            if getattr(user, 'id', False):
-                new_qsd.author = user
-                new_qsd.save()
+        if qsd_obj is None:
+            title = self.url
+            if program is not None:
+                title += ' - ' + unicode(program)
+            qsd_obj = QuasiStaticData(url=url,
+                                      name='',
+                                      title=title,
+                                      content=self.nodelist.render(context))
 
-            qsd_obj = new_qsd
-
-        return render_to_response("inclusion/qsd/render_qsd_inline.html", {'qsdrec': qsd_obj, 'edit_bits': edit_bits}, context_instance=context).content
+        return render_to_response("inclusion/qsd/render_qsd_inline.html", {'qsdrec': qsd_obj}, context_instance=context).content
 
 @register.tag
 def inline_qsd_block(parser, token):
     tokens = token.split_contents()
     if len(tokens) == 2:
         iqb, url = tokens
-        user_variable = None
-    elif len(tokens) == 3:
-        iqb, url, user_variable = tokens
     else:
-        raise Exception("Wrong number of inputs for %s, either 1 or 2 expected" % (tokens))
+        raise Exception("Wrong number of inputs for %s, 1 expected" % (tokens))
     
     nodelist = parser.parse(("end_inline_qsd_block",))
     parser.delete_first_token()
     
-    return InlineQSDNode(nodelist, url, user_variable, None)
+    return InlineQSDNode(nodelist, url, None)
 
 
 @register.tag
@@ -104,32 +87,11 @@ def inline_program_qsd_block(parser, token):
     tokens = token.split_contents()
     if len(tokens) == 3:
         iqb, program, url = tokens
-        user_variable = None
-    elif len(tokens) == 4:
-        iqb, program, url, user_variable = tokens
     else:
-        raise Exception("Wrong number of inputs for %s, either 1 or 2 expected" % (tokens))
+        raise Exception("Wrong number of inputs for %s, 2 expected" % (tokens))
     
     nodelist = parser.parse(("end_inline_program_qsd_block",))
     parser.delete_first_token()
     
-    return InlineQSDNode(nodelist, url, user_variable, program)
+    return InlineQSDNode(nodelist, url, program)
 
-
-class QSDStringNode(template.Node):
-    def __init__(self, input_url):
-        self.url = url
-      
-    def render(self, context):
-        try:
-            qsd_obj = QuasiStaticData.objects.get_by_url(self.url)
-            result = qsd_obj.content
-        except AttributeError:
-            result = "QSD Does Not Exist"        
-        return result
-
-@register.tag
-def qsd_string(parser, token):
-    tag, url = token.split_contents()
-    return QSDStringNode(url)
-    
