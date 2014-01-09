learning-unlimited/ESP-Website

commit 9988b0b15227068260b1ce6bfc112e7102afe8d4

Remove UserBit from miniblog get_visible_announcements()

esp/esp/miniblog/views/__init__.py
@@ -43,9 +43,7 @@ from esp.miniblog.models import Entry, AnnouncementLink
 
 from esp.cache import cache_function
 from esp.web.util import render_to_response
-from esp.datatree.models import *
-from esp.users.models import UserBit, ESPUser
-
+from esp.users.models import ESPUser
 
 #	Function for previewing announcements  - Michael P
 #	Generates the block structure used by battle screens
@@ -60,37 +58,33 @@ def preview_miniblog(request, section = None):
     else:
         curUser = ESPUser(AnonymousUser())
 
-
     return curUser.getMiniBlogEntries()
     
 @cache_function
 def get_visible_announcements(user, limit, tl):
-    verb = GetNode('V/Subscribe')
-    
     models_to_search = [Entry, AnnouncementLink]
     results = []
     grand_total = 0
     overflowed = False
     for model in models_to_search:
-        result = UserBit.find_by_anchor_perms(model, user, verb).order_by('-timestamp').filter(Q(highlight_expire__gte = datetime.now()) | Q(highlight_expire__isnull = True)).filter(Q(highlight_begin__lte = datetime.now()) | Q(highlight_begin__isnull = True))
-	
+        result = model.objects.order_by('-timestamp').filter(Q(highlight_expire__gte = datetime.now()) | Q(highlight_expire__isnull = True)).filter(Q(highlight_begin__lte = datetime.now()) | Q(highlight_begin__isnull = True))
+
 	if tl:
 	    result = result.filter(section=tl)
-	
-        if limit:
-            overflowed = ((len(result) - limit) > 0)
-            total = len(result)
-            result = result[:limit]
-        else:
-            overflowed = False
-            total = len(result)
 
-        results += result
-        grand_total += total
+    if limit:
+        overflowed = ((len(result) - limit) > 0)
+        total = len(result)
+        result = result[:limit]
+    else:
+        overflowed = False
+        total = len(result)
+
+    results += result
+    grand_total += total
 
     return {'announcementList': results,
               'overflowed':       overflowed,
               'total':            grand_total}
 get_visible_announcements.depend_on_model(Entry)
 get_visible_announcements.depend_on_model(AnnouncementLink)
-# FIXME: Really should depend on the UserBit...

commit 1b88dba0133949fe140a6247e3a4d0d6d034d215

Restore Entry objects in announcement list.
An indentation change in 9988b0b15227068260b1ce6bfc112e7102afe8d4
caused only the last model scanned (AnnouncementLink, namely) to
make it into the results.

esp/esp/miniblog/views/__init__.py
@@ -71,16 +71,16 @@ def get_visible_announcements(user, limit, tl):
 	if tl:
 	    result = result.filter(section=tl)
 
-    if limit:
-        overflowed = ((len(result) - limit) > 0)
-        total = len(result)
-        result = result[:limit]
-    else:
-        overflowed = False
-        total = len(result)
+        if limit:
+            overflowed = ((len(result) - limit) > 0)
+            total = len(result)
+            result = result[:limit]
+        else:
+            overflowed = False
+            total = len(result)
 
-    results += result
-    grand_total += total
+        results += result
+        grand_total += total
 
     return {'announcementList': results,
               'overflowed':       overflowed,