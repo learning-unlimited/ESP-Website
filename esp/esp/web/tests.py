340ad4ae20afe3817eb22e8e0d973796d6047b64
Gurtej Kanwar <gurtej@mit.edu>
2014-01-14 20:56:18 -0500

    Fixes NoVaryOnCookie tests

--- esp/esp/web/tests.py
+++ esp/esp/web/tests.py
@@ -145,14 +145,14 @@ class NoVaryOnCookieTest(ProgramFrameworkTest):
         res = c.get(self.url + "index.html")
         
         self.assertEqual(res.status_code, 200)
-        self.assertNotIn('Cookie', res['Vary'])
+        self.assertTrue('Vary' not in res or 'Cookie' not in res['Vary'])
         logged_out_content = res.content
         
         c.login(username=self.admins[0], password='password')
         res = c.get(self.url + "index.html")
         
         self.assertEqual(res.status_code, 200)
-        self.assertNotIn('Cookie', res['Vary'])
+        self.assertTrue('Vary' not in res or 'Cookie' not in res['Vary'])
         logged_in_content = res.content
         
         self.assertEqual("\n".join(difflib.context_diff(logged_out_content.split("\n"), logged_in_content.split("\n"))), "")
@@ -162,14 +162,14 @@ class NoVaryOnCookieTest(ProgramFrameworkTest):
         res = c.get(self.url + "catalog")
         
         self.assertEqual(res.status_code, 200)
-        self.assertNotIn('Cookie', res['Vary'])
+        self.assertTrue('Vary' not in res or 'Cookie' not in res['Vary'])
         logged_out_content = res.content
         
         c.login(username=self.admins[0], password='password')
         res = c.get(self.url + "catalog")
         
         self.assertEqual(res.status_code, 200)
-        self.assertNotIn('Cookie', res['Vary'])
+        self.assertTrue('Vary' not in res or 'Cookie' not in res['Vary'])
         logged_in_content = res.content
         
         self.assertEqual("\n".join(difflib.context_diff(logged_out_content.split("\n"), logged_in_content.split("\n"))), "")
