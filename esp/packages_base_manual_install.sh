2876c08bd2facff6e9931076d84205340e241a47
Michael Price <price@learningu.org>
2014-01-17 18:20:15 -0500

    Move libc-ares2 and libev4 to packages_base.txt; shrink manual install code

--- esp/packages_base_manual_install.sh
+++ esp/packages_base_manual_install.sh
@@ -5,19 +5,12 @@
 
 wget http://mirrors.kernel.org/ubuntu/pool/universe/libv/libv8/libv8-3.8.9.20_3.8.9.20-2_amd64.deb -O libv8.deb
 sudo dpkg -i libv8.deb
-sudo apt-get install libc-ares2 libev4
 
-if [ -z $(which nodejs) ]
-then
-    wget http://mirrors.kernel.org/ubuntu/pool/universe/n/nodejs/nodejs_0.6.19~dfsg1-5ubuntu1_amd64.deb -O nodejs.deb
-    sudo dpkg -i nodejs.deb
-    rm nodejs.deb
-fi
+wget http://mirrors.kernel.org/ubuntu/pool/universe/n/nodejs/nodejs_0.6.19~dfsg1-5ubuntu1_amd64.deb -O nodejs.deb
+sudo dpkg -i nodejs.deb
 
-if [ -z $(which lessc) ]
-then
-    wget http://mirrors.kernel.org/ubuntu/pool/universe/l/less.js/node-less_1.3.1~20121105-1_all.deb -O node-less.deb
-    sudo dpkg -i node-less.deb
-    rm node-less.deb
-fi
+wget http://mirrors.kernel.org/ubuntu/pool/universe/l/less.js/node-less_1.3.1~20121105-1_all.deb -O node-less.deb
+sudo dpkg -i node-less.deb
+
+rm *.deb
 
