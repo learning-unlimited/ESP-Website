93b80c08c55f7f9bb226c535fcd9cfd9aa0382a5
Jordan Moldow <jmoldow@mit.edu>
2014-01-22 01:23:46 -0500

    Fixes bug with virtualenv pre-activation
    Travis uses its own virtualenv install, and has the activate script
    located differently than us, and activates it before running manage.py.
    This causes the check for our activate script to fail.
    
    Fixes this problem by catching IOErrors, and not raising if the
    VIRTUAL_ENV environmental variable has been defined, which would
    indicate that the virtualenv has already been activated.

--- esp/dbmail_cron.py
+++ esp/dbmail_cron.py
@@ -13,7 +13,16 @@ sys.path.insert(0, project)
 # activate virtualenv
 root = os.path.dirname(project)
 activate_this = os.path.join(root, 'env', 'bin', 'activate_this.py')
-execfile(activate_this, dict(__file__=activate_this))
+try:
+    execfile(activate_this, dict(__file__=activate_this))
+except IOError, e:
+    # Check if a virtualenv has been installed and activated from elsewhere.
+    # If this has happened, then the VIRTUAL_ENV environment variable should be
+    # defined, and we can ignore the IOError.
+    # If the variable isn't defined, then we really should be using our own
+    # virtualenv, so we re-raise the error.
+    if os.environ.get('VIRTUAL_ENV') is None:
+        raise e
 
 from esp import cache_loader
 from esp.dbmail.cronmail import process_messages, send_email_requests
