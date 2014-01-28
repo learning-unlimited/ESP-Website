a6dd509eda692c315986eb0ab10e620644d81db5
Michael Price <price@learningu.org>
2014-01-27 21:18:30 -0500

    Remove esp.twilltests app
    The code is 5+ years old and has not been used recently.  There are
    now other tools for testing against a live server, which should not
    be in our test suite, and we will have to come up with a better
    solution for testing against a dev server running "in parallel"
    with the test suite (like our Selenium tests).

--- esp/esp/web/tests.py
+++ esp/esp/web/tests.py
@@ -43,17 +43,6 @@ import re
 import os
 import tempfile
 
-# We don't want to do "from esp.twilltests.tests import AllFilesTest
-# because then UnitTest sees AllFilesTest twice and tries to run it
-# twice, which is unnecessary.
-# Just using plain 'import' thwarts UnitTest's introspection magic.
-import esp.twilltests.tests
-
-# Define a new AllFilesTest subclass
-class WebTest(esp.twilltests.tests.AllFilesTest):
-    # This class will test all .twill files in the 'web' Django app
-    test_module = 'esp/web'
-
 # Make sure that we can actually download the homepage
 class PageTest(TestCase):
     """ Validate common hard-coded flatpages """
