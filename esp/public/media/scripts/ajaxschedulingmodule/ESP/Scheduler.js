95884394c7c56c233a23bbe1dbad8e9b65a4c7f8
Ruthie Byers <ruthie@mit.edu>
2014-02-09 10:34:34 -0800

    Directory specs!

--- esp/public/media/scripts/ajaxschedulingmodule/ESP/Scheduler.js
+++ esp/public/media/scripts/ajaxschedulingmodule/ESP/Scheduler.js
@@ -1,10 +1,20 @@
-function Directory() {
+function Directory(sections, el) {
+    this.sections = sections
+    this.el = el
+    this.render = function(){
+	table = $("<table/>")[0]
+	$(sections).each(function(i, section){
+	    row = $("<tr><td>" + section.title + "</td></tr>")[0]
+	    table.appendChild(row)
+	})
+	this.el.appendChild(table)
+    }
 }
 
 function Matrix() {
 }
 
-function Scheduler() {
-    this.directory = new Directory()
+function Scheduler(data, directoryEl) {
+    this.directory = new Directory(data.sections, directoryEl)
     this.matrix = new Matrix()
 }
--- esp/public/media/scripts/ajaxschedulingmodule/spec/SchedulerSpec.js
+++ esp/public/media/scripts/ajaxschedulingmodule/spec/SchedulerSpec.js
@@ -1,7 +1,7 @@
 describe("Scheduler", function(){
     beforeEach(function(){
 	//TODO:  what data should be passed in?
-	s = new Scheduler([])
+	s = new Scheduler({}, $("<div/>")[0])
     })
 
     it("should have a directory", function(){
@@ -12,4 +12,65 @@ describe("Scheduler", function(){
 	expect(s.matrix).toBeDefined()
     })
 
+})
+
+var data = {sections: [
+	{
+	    status: 10, 
+	    category: 'S', 
+	    parent_class: 3188, 
+	    emailcode: 'S3188s1', 
+	    index: 1, 
+	    title: "Fascinating Science Phenomena", 
+	    category_id: 17, 
+	    class_size_max: 150, 
+	    length: 1.83, 
+	    grade_min: 7, 
+	    num_students: 42, 
+	    grade_max: 12, 
+	    id: 3329, 
+	    teachers: [6460]
+	}, 
+	{
+	    status: 10, 
+	    category: 'M', 
+	    parent_class: 3343, 
+	    emailcode: 'M3343s1', 
+	    index: 1, 
+	    title: "Become a LaTeX Guru", 
+	    category_id: 16, 
+	    class_size_max: 15, 
+	    length: 1.83, 
+	    grade_min: 7, 
+	    num_students: 14, 
+	    grade_max: 12, 
+	    id: 3538, 
+	    teachers: [45225]
+	}
+]}
+
+describe("Matrix", function(){
+
+    beforeEach(function(){
+	d = new Directory(data.sections, $("<div/>")[0])
+    })
+
+    it("should have a list of sections and an el", function(){
+	expect(d.sections).toBeArray()
+	expect(d.el).toBeHtmlNode()
+    })
+
+    describe("render", function(){
+	beforeEach(function(){
+	    d.render()
+	})
+
+	it("el", function (){
+	    expect(d.el.children.length).toEqual(1)
+	    table = d.el.children[0]
+	    expect(table.rows.length).toEqual(2)
+	    expect(table.rows[0].innerHTML).toMatch("Fascinating Science Phenomena")
+	    expect(table.rows[1].innerHTML).toMatch("Become a LaTeX Guru")
+	})
+    })
 })
