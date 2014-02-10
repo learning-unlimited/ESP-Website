learning-unlimited/ESP-Website

commit 335ec7933117b368b14630e830b15377e576b359

Gives admins implicit permissions
Since the admins field is no longer part of program creation, this makes
it easier for admins to be able to have deadline permissions on a
program: they will always have them. In the old schema, being an
administrator was the same as having recursive permissions, so this
change is a revert back to old behavior.

esp/esp/users/models/__init__.py
@@ -2110,6 +2110,8 @@ class Permission(ExpirableModel):
 
     @classmethod
     def user_has_perm(self, user, name, program=None, when=None):
+        if user.isAdministrator(program=program):
+            return True
         perms=[name]
         for k,v in self.implications.items():
             if name in v: perms.append(k)

commit fd398648d2d48c8c75a4f1ae6d182c9db968d62a

Prevents circular references.
Prevents circular references between isAdministrator and user_has_perm.
They were calling each other in an infinite loop, causing a RuntimeError
for exceeding the maximum recursion call depth.

Fixes a bug introduced in 335ec7933117b368b14630e830b15377e576b359.

Also adds the functionality that having Administer on None acts as if
you also have Administer on every individual program.

esp/esp/users/models/__init__.py
@@ -781,10 +781,11 @@ class ESPUser(User, AnonymousUser):
         if self.is_anonymous() or self.id is None: return False
         is_admin_role = self.groups.filter(name="Administrator").exists()
         if is_admin_role: return True
-        if program is None:
-            return Permission.user_has_perm(self, "Administer")
-
-        return Permission.user_has_perm(self, "Administer",program=program)
+        quser = Q(user=self) | Q(user=None, role__in=self.groups.all())
+        return Permission.objects.filter(
+                        quser & Permission.is_valid_qobject(),
+                        permission_type="Administer",
+                        program__in=[None, program]).exists()
     isAdmin = isAdministrator
 
     @cache_function