c29e5480088ab36f2832ff115d348e46233f7765
Ruthie Byers <ruthie@mit.edu>
2014-02-11 07:39:19 -0800

    App fits vertically within the window

--- esp/public/media/scripts/ajaxschedulingmodule/ESP/Main.js
+++ esp/public/media/scripts/ajaxschedulingmodule/ESP/Main.js
@@ -1,6 +1,10 @@
 var data = {}
 
 json_fetch(['sections', 'timeslots', 'rooms', 'schedule_assignments'], function(){
+    var window_height = window.innerHeight - 20
+    $j("#directory-div").height(window_height);
+    $j("#matrix-div").height(window_height);
+
     s = new Scheduler(data, $j("#directory-div")[0], $j("#matrix-div")[0])
     s.render()
 }, 
