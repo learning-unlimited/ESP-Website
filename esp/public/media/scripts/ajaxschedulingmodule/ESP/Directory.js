9dcf179299e86c4aaa100044926f4962c80eddbb
Ruthie Byers <ruthie@mit.edu>
2014-02-11 08:38:48 -0800

    Added email code to directory

--- esp/public/media/scripts/ajaxschedulingmodule/ESP/Directory.js
+++ esp/public/media/scripts/ajaxschedulingmodule/ESP/Directory.js
@@ -4,7 +4,7 @@ function Directory(sections, el) {
     this.render = function(){
 	table = $j("<table/>")[0]
 	$j.each(sections, function(id, section){
-	    row = $j("<tr><td>" + section.title + "</td></tr>")[0]
+	    row = $j("<tr><td>" + section.title + "</td><td>"+section.emailcode+"</td></tr>")[0]
 	    table.appendChild(row)
 	})
 	this.el.appendChild(table)
--- esp/public/media/scripts/ajaxschedulingmodule/spec/DirectorySpec.js
+++ esp/public/media/scripts/ajaxschedulingmodule/spec/DirectorySpec.js
@@ -13,12 +13,14 @@ describe("Directory", function(){
 	    d.render()
 	})
 
-	it("should present a list of classes", function (){
+	it("should present a list of classes with emailcodes", function (){
 	    expect(d.el.children.length).toEqual(1)
 	    table = d.el.children[0]
 	    expect(table.rows.length).toEqual(2)
 	    expect(table.rows[0].innerHTML).toMatch("Fascinating Science Phenomena")
+	    expect(table.rows[0].innerHTML).toMatch("S3188s1")
 	    expect(table.rows[1].innerHTML).toMatch("Become a LaTeX Guru")
+	    expect(table.rows[1].innerHTML).toMatch("M3343s1")
 	})
     })
 })
