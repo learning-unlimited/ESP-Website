daed187da710ceee9abad9b62231b2e15b4568a1
Michael Price <price@learningu.org>
2014-03-16 13:10:04 -0400

    Remove old code which was specific to First Data

--- esp/esp/program/modules/handlers/creditcardmodule_stripe.py
+++ esp/esp/program/modules/handlers/creditcardmodule_stripe.py
@@ -83,62 +83,6 @@ class CreditCardModule_Stripe(ProgramModuleObj, module_ext.StripeCreditCardSetti
     def studentDesc(self):
         return {'creditcard': """Students who have filled out the credit card form."""}
 
-    @aux_call
-    def payment_success(self, request, tl, one, two, module, extra, prog):
-        """ Receive payment from First Data Global Gateway """
-
-        if request.method == 'GET' or request.POST.get('status', '') != 'APPROVED':
-            return self.payment_failure(request, tl, one, two, module, extra, prog)
-
-        #   We should already know what user/program this is for, but it should also be stored.
-        iac = IndividualAccountingController(self.program, request.user)
-        post_locator = request.POST.get('ponumber', '')
-        assert(post_locator == iac.get_id())
-
-        post_identifier = request.POST.get('invoice_number', '')
-        #   Optional: The line of code below would check consistency of the user's
-        #   invoice items against the ones associated with the payment.
-        #   assert(post_identifier == iac.get_identifier())
-
-        post_amount = Decimal(request.POST.get('total', '0.0'))
-
-        #   Warn for possible duplicate payments
-        prev_payments = iac.get_transfers().filter(line_item=iac.default_payments_lineitemtype())
-        if prev_payments.count() > 0 and iac.amount_due() <= 0:
-            from django.conf import settings
-            recipient_list = [contact[1] for contact in settings.ADMINS]
-
-            subject = 'Possible Duplicate Postback/Payment'
-            refs = 'User: %s (%d); Program: %s (%d)' % (request.user.name(), request.user.id, self.program.niceName(), self.program.id)
-            refs += '\n\nPrevious payments\' Transfer IDs: ' + ( u', '.join([x.id for x in prev_payments]) )
-
-            # Send mail!
-            send_mail('[ ESP CC ] ' + subject + ' by ' + invoice.user.first_name + ' ' + invoice.user.last_name, \
-                  """%s Notification\n--------------------------------- \n\n%s\n\nUser: %s %s (%s)\n\nCardholder: %s\n\nRequest: %s\n\n""" % \
-                  (subject, refs, request.user.first_name, request.user.last_name, request.user.id, request.POST.get('bname', '--'), request) , \
-                  settings.SERVER_EMAIL, recipient_list, True)
-
-        #   Save the payment as a transfer in the database
-        iac.submit_payment(post_amount)
-
-        context = {}
-        context['postdata'] = request.POST.copy()
-        context['support_email'] = settings.DEFAULT_EMAIL_ADDRESSES['support']
-        context['prog'] = prog
-
-        #   Don't redirect to receipt just yet, in case they haven't finished all steps of registration
-        #   return HttpResponseRedirect("http://%s/learn/%s/%s/confirmreg" % (request.META['HTTP_HOST'], one, two))
-        return render_to_response(self.baseDir() + 'success.html', request, context)
-        
-    @aux_call
-    def payment_failure(self, request, tl, one, two, module, extra, prog):
-        context = {}
-        if request.method == 'POST':
-            context['postdata'] = request.POST.copy()
-        context['prog'] = prog
-        context['support_email'] = settings.DEFAULT_EMAIL_ADDRESSES['support']
-        return render_to_response(self.baseDir() + 'failure.html', request, context)
-
     def check_setup(self):
         """ Validate the keys specified in the StripeCreditCardSettings object.
             If something is wrong, provide an error message which will hopefully
