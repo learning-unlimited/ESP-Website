9bee2236c8c62058d5e77f4cfabc8f591bea5ce6
Favyen Bastani <fbastani@perennate.com>
2014-04-01 15:01:52 -0400

    Add file lock for dbmail_cron.

--- esp/dbmail_cron.py
+++ esp/dbmail_cron.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+import tempfile
+import fcntl
 os.environ['DJANGO_SETTINGS_MODULE'] = 'esp.settings'
 
 import os.path
@@ -23,6 +25,15 @@ except IOError, e:
     # virtualenv, so we re-raise the error.
     if os.environ.get('VIRTUAL_ENV') is None:
         raise e
+
+# lock to ensure only one cron instance runs at a time
+lock_file_path = os.path.join(tempfile.gettempdir(), 'espweb.dbmailcron.lock')
+lock_file_handle = open(lock_file_path, 'w')
+try:
+    fcntl.lockf(lock_file_handle, fcntl.LOCK_EX | fcntl.LOCK_NB)
+except IOError:
+    # another instance has the lock
+    sys.exit(0)
 
 from esp import cache_loader
 from esp.dbmail.cronmail import process_messages, send_email_requests
