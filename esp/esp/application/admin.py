8cfe57ee260edd5c5e60ba6b8c89ec494087d197
Shulin Ye <nightshadequeen@gmail.com>
2014-07-13 02:01:42 -0400

    Moves formstack stuff to default_search_fields

--- esp/esp/application/admin.py
+++ esp/esp/application/admin.py
@@ -41,6 +41,8 @@ from esp.program.models import ClassSubject
 from esp.formstack.objects import get_forms_for_api_key, get_form_by_id
 from esp.application.models import FormstackAppSettings, FormstackStudentProgramApp, FormstackStudentClassApp
 
+from esp.utils.admin_user_search import default_search_fields
+
 class FormstackAppSettingsAdmin(admin.ModelAdmin):
     fields = ['module', 'api_key', 'forms_for_api_key',
               'form_id', 'form_fields',
@@ -108,7 +110,7 @@ class FormstackStudentProgramAppAdmin(admin.ModelAdmin):
                     'choices_pretty', 'admissions_pretty']
     list_editable = ['admin_status']
     list_filter = ['admin_status', 'program']
-    search_fields = ['user__username', 'user__first_name', 'user__last_name', '=user__email']
+    search_fields = default_search_fields()
 
     def choices_pretty(self, app):
         lines = []
@@ -163,7 +165,7 @@ class FormstackStudentClassAppAdmin(admin.ModelAdmin):
                     'admissions_pretty']
     list_display_links = ['user']
     list_filter = ['app__admin_status', 'subject', 'subject__parent_program']
-    search_fields = ['app__user__username', 'app__user__first_name', 'app__user__last_name', '=app__user__email', 'subject__title', 'subject__id']
+    search_fields = default_search_fields('app__user') + ['subject__title', 'subject__id']
 
     def user(self, classapp):
         return classapp.app.user
