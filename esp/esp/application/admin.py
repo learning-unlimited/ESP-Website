3717f085d74eb7d465699520bcd1d5153f4d1a17
Shulin Ye <nightshadequeen@gmail.com>
2014-07-12 23:59:07 -0400

    Adds email searching as well

--- esp/esp/application/admin.py
+++ esp/esp/application/admin.py
@@ -108,7 +108,7 @@ class FormstackStudentProgramAppAdmin(admin.ModelAdmin):
                     'choices_pretty', 'admissions_pretty']
     list_editable = ['admin_status']
     list_filter = ['admin_status', 'program']
-    search_fields = ['user__username', 'user__first_name', 'user__last_name']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'user__email']
 
     def choices_pretty(self, app):
         lines = []
@@ -164,7 +164,7 @@ class FormstackStudentClassAppAdmin(admin.ModelAdmin):
     list_display_links = ['user']
     list_filter = ['app__admin_status']
     list_filter = ['subject', 'subject__parent_program']
-    search_fields = ['app__user__username', 'app__user__first_name', 'app__user__last_name', 'subject__title', 'subject__id']
+    search_fields = ['app__user__username', 'app__user__first_name', 'app__user__last_name', 'app__user__email', 'subject__title', 'subject__id']
 
     def user(self, classapp):
         return classapp.app.user
