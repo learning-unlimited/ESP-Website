0f8552e86e23f4cf622f77f25e1ad1498c64475b
Anthony Lu <lua@mit.edu>
2014-07-16 23:50:07 -0400

    Revert "Fix nodejs link in packages_base_manual_install"
    This reverts commit f575cd6ca1cc118ea2be9ae1a87d346d9f53f21b.

--- esp/packages_base_manual_install.sh
+++ esp/packages_base_manual_install.sh
@@ -5,11 +5,11 @@
 
 if [ $(uname -m) == 'x86_64' ]; then
   wget http://mirrors.kernel.org/ubuntu/pool/universe/libv/libv8/libv8-3.8.9.20_3.8.9.20-2_amd64.deb -O libv8.deb
-  wget http://mirrors.kernel.org/ubuntu/pool/universe/n/nodejs/nodejs_0.6.12~dfsg1-1ubuntu1_amd64.deb -O nodejs.deb
+  wget http://mirrors.kernel.org/ubuntu/pool/universe/n/nodejs/nodejs_0.6.19~dfsg1-5ubuntu1_amd64.deb -O nodejs.deb
   wget http://mirrors.kernel.org/ubuntu/pool/universe/l/less.js/node-less_1.3.1~20121105-1_all.deb -O node-less.deb
 else
   wget http://mirrors.kernel.org/ubuntu/pool/universe/libv/libv8/libv8-3.8.9.20_3.8.9.20-2_i386.deb -O libv8.deb
-  wget http://mirrors.kernel.org/ubuntu/pool/universe/n/nodejs/nodejs_0.6.12~dfsg1-1ubuntu1_i386.deb -O nodejs.deb
+  wget http://mirrors.kernel.org/ubuntu/pool/universe/n/nodejs/nodejs_0.6.19~dfsg1-5ubuntu1_i386.deb -O nodejs.deb
   wget http://mirrors.kernel.org/ubuntu/pool/universe/l/less.js/node-less_1.3.1~20121105-1_all.deb -O node-less.deb
 fi
 
