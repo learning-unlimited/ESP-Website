3a62ddc5fa0c2fad7a1471804203cb1f7b7d0f92
Anthony Lu <lua@mit.edu>
2014-07-17 00:40:07 -0400

    Try using the nodejs package instead

--- esp/packages_base_manual_install.sh
+++ esp/packages_base_manual_install.sh
@@ -3,5 +3,5 @@
 # This script will install the package dependencies for this website install
 # that cannot be installed via apt-get.
 
-sudo apt-get install npm
+sudo apt-get install nodejs
 sudo npm install -g less@1.3.1
