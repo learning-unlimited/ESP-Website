21045b2abd6d4612e06ae33184d8cb23eb9a8355
Anthony Lu <lua@mit.edu>
2014-07-17 00:46:17 -0400

    Try using the nodejs from the PPA

--- esp/packages_base_manual_install.sh
+++ esp/packages_base_manual_install.sh
@@ -3,5 +3,8 @@
 # This script will install the package dependencies for this website install
 # that cannot be installed via apt-get.
 
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
 sudo apt-get install nodejs
 sudo npm install -g less@1.3.1
