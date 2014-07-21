d23a0b49c577724c51ede9ab74ac53c8fe5a3442
Miriam Gershenson <mgershe@gmail.com>
2014-07-20 21:50:30 -0700

    Merge pull request #1269 from learning-unlimited/lua-testing-travis
    Fix nodejs install(cherry picked from commit 50ee7fcc4a386ce0fed816169739484039376e81)
    
    Conflicts:
    	esp/packages_base_manual_install.sh

--- esp/packages_base_manual_install.sh
+++ esp/packages_base_manual_install.sh
@@ -3,14 +3,8 @@
 # This script will install the package dependencies for this website install
 # that cannot be installed via apt-get.
 
-wget http://mirrors.kernel.org/ubuntu/pool/universe/libv/libv8/libv8-3.8.9.20_3.8.9.20-2_amd64.deb -O libv8.deb
-sudo dpkg -i libv8.deb
-
-wget http://mirrors.kernel.org/ubuntu/pool/universe/n/nodejs/nodejs_0.6.19~dfsg1-5ubuntu1_amd64.deb -O nodejs.deb
-sudo dpkg -i nodejs.deb
-
-wget http://mirrors.kernel.org/ubuntu/pool/universe/l/less.js/node-less_1.3.1~20121105-1_all.deb -O node-less.deb
-sudo dpkg -i node-less.deb
-
-rm *.deb
-
+sudo apt-get install python-software-properties
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+sudo npm install -g less@1.3.1
