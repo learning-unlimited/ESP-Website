f8057def734b98b70a841f4d4bc0fdaadbae94a9
Ben Kraft <benjaminjkraft@gmail.com>
2014-07-21 23:31:48 -0700

    Fix the previous fix, and make mailman not send remove notifications
    They might really just be having their email changed, and that would be confusing.

--- esp/esp/mailman/__init__.py
+++ esp/esp/mailman/__init__.py
@@ -161,7 +161,7 @@ def remove_list_member(list, member):
     if isinstance(member, unicode):
         member = member.encode('iso-8859-1', 'replace')
 
-    return Popen([MM_PATH + "remove_members", "--file=-", list], stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate(member)
+    return Popen([MM_PATH + "remove_members", "--nouserack", "--noadminack", "--file=-", list], stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate(member)
 
 @enable_with_setting(settings.USE_MAILMAN)
 def list_contents(lst):
@@ -218,5 +218,6 @@ def lists_containing(user):
     #
     # We only want the lists; grab those:
 
-    lists = [x.strip() for x in data if len(x) > 0 and x[0] in " \t"]
+    lists = [x.strip() for x in data]
+    lists = [x for x in lists if x]
     return lists
