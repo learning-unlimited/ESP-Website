5d2434e815ff3c8f3477619a6e9381bcc2ee3501
Anthony Lu <lua@mit.edu>
2014-08-27 21:37:28 -0400

    Kill cache_by_uid

--- esp/esp/cache/argcache.py
+++ esp/esp/cache/argcache.py
@@ -48,7 +48,7 @@ from esp.cache.marinade import args_to_key, normalize_args
 from esp.cache.function import describe_func, get_uid
 from esp.cache.token import Token, SingleEntryToken
 from esp.cache.key_set import is_wildcard, specifies_key, token_list_for
-from esp.cache.registry import cache_by_uid, register_cache, all_caches
+from esp.cache.registry import register_cache, all_caches
 from esp.cache.sad_face import warn_if_loaded
 
 __all__ = ['ArgCache', 'ArgCacheDecorator', 'cache_function']
