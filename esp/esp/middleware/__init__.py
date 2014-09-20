5e020e8a57300feff4959376cd110f2fe4ec79ca
Jordan Moldow <jmoldow@learningu.org>
2014-09-20 15:41:17 -0700

    Revert "IDebug and ServerNameRedirect middleware developed by Pdox"
    This reverts commit d4a5ef2ddf4ecbb7955d972d97a7789cf8f22498.
    
    Conflicts:
    	esp/esp/middleware/__init__.py
    	esp/esp/middleware/servernameredirect.py
    	esp/esp/utils/memcached_multikey.py
    	esp/esp/web/util/idebug.py
    	esp/esp/web/util/main.py

--- esp/esp/middleware/__init__.py
+++ esp/esp/middleware/__init__.py
@@ -38,6 +38,3 @@ from esp.middleware.whitespacemiddleware import *
 from esp.middleware.fixiemiddleware import *
 from esp.middleware.espcachemiddleware import *
 from esp.middleware.prettyerroremail import *
-#from esp.middleware.espauthmiddleware import *
-from esp.middleware.servernameredirect import *
-from esp.middleware.idebug import *
