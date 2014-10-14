learning-unlimited/ESP-Website

commit 833b201b53b5e8696c7064d71c500ceeb9b5afa0

Refactors teacher check-in
This will make it easier to add more class info to the teacher check-in page,
and maybe improve performance.  (The performance still isn't great until we
improve resources and user profiles; so it goes.)

esp/esp/program/modules/handlers/teachercheckinmodule.py
@@ -147,7 +147,13 @@ class TeacherCheckinModule(ProgramModuleObj):
                 else:
                     json_data['message'] = self.checkIn(teachers[0], prog, when)
         return HttpResponse(json.dumps(json_data), mimetype='text/json')
-    
+
+    @staticmethod
+    def get_phone(user):
+        """Get the phone number to display for a user."""
+        contact_info = user.getLastProfile().contact_user
+        return contact_info.phone_cell or contact_info.phone_day
+
     def getMissingTeachers(self, prog, date=None, starttime=None, when=None):
         """Return a list of class sections with missing teachers as of 'when'.
 
@@ -179,14 +185,14 @@ class TeacherCheckinModule(ProgramModuleObj):
         and is used to filter the set of checked-in Records, so that the view
         shows who is not checked in yet for the day.
 
-        Returns the 2-tuple (class_arr, teacher_dict):
-          class_arr (list of ClassSection): A list of all sections starting at
-                                            'starttime' (if given), otherwise
-                                            all sections on 'date'.
-          teacher_dict (dict):              The keys are the id numbers of all
-                                            the ESPUsers teaching classes in
-                                            'class_arr'. The values are dicts
-                                            of teacher information.
+        Returns the 2-tuple (sections, arrived):
+            sections: A list of all sections starting at the given time or on
+                      the given date which do not have all teachers checked in,
+                      with those with no teachers checked in first.  If a date
+                      was given, includes only the first section of each class
+                      on that date.
+            arrived:  A dict of id -> teacher for all teachers who have already
+                      checked in.
         """
         if when is None:
             when = datetime.now()
@@ -200,74 +206,67 @@ class TeacherCheckinModule(ProgramModuleObj):
                                        meeting_times__start__day   = date.day)
         if starttime is not None:
             sections = sections.filter(begin_time=starttime.start)
+        sections = sections.select_related(
+            'parent_class',
+            'parent_class__category',
+        ).prefetch_related(
+            'parent_class__teachers',
+        )
         sections = sections.distinct()
-        teachers = ESPUser.objects.filter(classsubject__sections__in=sections)
 
         # A teacher is considered to have arrived if:
         # - They have a "teacher_checked_in" Record for the program
         # - which is from before 'when' (since we are considering the state of
         #   check-in at this time).
         # - which is from the same date as 'when'.
-        arrived = teachers.filter(record__program=prog,
-                                  record__event='teacher_checked_in',
-                                  record__time__lte   = when,
-                                  record__time__year  = when.year,
-                                  record__time__month = when.month,
-                                  record__time__day   = when.day).distinct()
+        teachers = ESPUser.objects.filter(
+            classsubject__sections__in=sections,
+            record__program=prog,
+            record__event='teacher_checked_in',
+            record__time__lte=when,
+            record__time__year=when.year,
+            record__time__month=when.month,
+            record__time__day=when.day).distinct()
 
-        missing = teachers.exclude(id__in=arrived)
-        missing_sections = sections.filter(parent_class__teachers__in=missing,)
-        teacher_tuples = ESPUser.objects.filter(classsubject__sections__in=missing_sections) \
-                                          .order_by('classsubject__sections__meeting_times__start',
-                                                    'last_name', 'first_name') \
-                                          .values_list('id', 'classsubject__id', 'classsubject__title') \
-                                          .distinct()
-        
-        teacher_dict = {}
-        for teacher in list(arrived) + list(missing):
-            contact = teacher.getLastProfile().contact_user
-            if contact is None:
-                contact = ContactInfo(phone_cell='N/A')
-            teacher_dict[teacher.id] = {'username': teacher.username,
-                                        'name': teacher.name(),
-                                        'last_name': teacher.last_name,
-                                        'phone': contact.phone_cell or contact.phone_day,
-                                        'arrived': True}
-        for teacher in missing:
-            teacher_dict[teacher.id]['arrived'] = False
-        
-        class_dict = {}
-        class_arr = []
-        for teacher_id, class_id, class_name in teacher_tuples:
-            if class_id not in class_dict:
-                class_dict[class_id] = {'id': ClassSubject.objects.get(id=class_id).emailcode(),
-                                        'name': class_name,
-                                        'teachers': {},
-                                        'any_arrived': False}
-                class_arr.append(class_dict[class_id])
-            if teacher_id not in class_dict[class_id]['teachers']:
-                class_dict[class_id]['teachers'][teacher_id] = teacher_dict[teacher_id]
-        
-        for sec in missing_sections:
-            if sec.parent_class.id in class_dict:
-                class_ = class_dict[sec.parent_class.id]
-                class_['room'] = (sec.prettyrooms() or [None])[0]
-                if 'time' in class_:
-                    class_['time'] = min(class_['time'], sec.begin_time)
-                else:
-                    class_['time'] = sec.begin_time
-        
-        #Move sections where at least one teacher showed up to end of list
-        for sec in class_arr:
-            for teacher in sec['teachers'].itervalues():
-                if teacher['arrived']:
-                    sec['any_arrived'] = True
-                    break
-        class_arr = [sec for sec in class_arr if sec['any_arrived'] == False] + \
-                    [sec for sec in class_arr if sec['any_arrived'] == True]
-        
-        return class_arr, teacher_dict
-    
+        # To save multiple calls to getLastProfile, precompute the teacher
+        # phones.
+        teacher_phones = {}
+        for section in sections:
+            for teacher in section.teachers:
+                if teacher.id not in teacher_phones:
+                    teacher_phones[teacher.id] = self.get_phone(teacher)
+
+        arrived = {}
+        for teacher in teachers:
+            if teacher.id not in teacher_phones:
+                teacher_phones[teacher.id] = self.get_phone(teacher)
+            teacher.phone = teacher_phones[teacher.id]
+            arrived[teacher.id] = teacher
+
+        sections_by_class = {}
+        for section in sections:
+            if not all(teacher.id in arrived for teacher in section.teachers):
+                # Put the first section of each class into sections_by_class
+                if (section.parent_class.id not in sections_by_class
+                        or sections_by_class[section.parent_class_id].begin_time > section.begin_time):
+                    # Precompute some things and pack them on the section.
+                    section.any_arrived = any(teacher.id in arrived
+                                              for teacher in section.teachers)
+                    section.room = (section.prettyrooms() or [None])[0]
+                    for teacher in section.teachers:
+                        teacher.phone = teacher_phones[teacher.id]
+                    sections_by_class[section.parent_class_id] = section
+
+        sections = [
+            section for section in sections_by_class.values()
+            if not section.any_arrived
+        ] + [
+            section for section in sections_by_class.values()
+            if section.any_arrived
+        ]
+
+        return sections, arrived
+
     @aux_call
     @needs_onsite
     def missingteachers(self, request, tl, one, two, module, extra, prog):
@@ -300,10 +299,11 @@ class TeacherCheckinModule(ProgramModuleObj):
         else:
             when = None
         context['date'] = date
-        context['sections'], teachers = self.getMissingTeachers(prog, date, starttime, when)
-        context['arrived'] = [teacher for teacher in teachers.values() if teacher['arrived']]
+        context['sections'], context['arrived'] = self.getMissingTeachers(
+            prog, date, starttime, when)
         context['start_time'] = starttime
-        return render_to_response(self.baseDir()+'missingteachers.html', request, context)
-    
+        return render_to_response(self.baseDir()+'missingteachers.html',
+                                  request, context)
+
     class Meta:
         abstract = True

esp/templates/program/modules/teachercheckinmodule/missingteachers.html
@@ -97,17 +97,17 @@
 {% for section in sections %}
 <tr>
     <td rowspan="{{ section.teachers|length }}">
-        {{ section.id }}: {{ section.name }}
+        {{ section.parent_class.emailcode }}: {{ section.name }}
     </td>
     <td class="room" rowspan="{{ section.teachers|length }}">
         {% if start_time %}
             {{ section.room }}
         {% else %}
-            {{ section.time }}
+            {{ section.begin_time }}
         {% endif %}
     </td>
-    {% for teacher in section.teachers.itervalues %}
-        {% if teacher.arrived %}
+    {% for teacher in section.teachers %}
+        {% if teacher.id in arrived %}
         <td class="checked-in">
         {% else %}
         <td class="not-checked-in">
@@ -115,7 +115,7 @@
             <a href="/manage/userview?username={{ teacher.username }}" target="_blank">{{ teacher.name }}</a>
         </td>
         <td class="checkin-column">
-            {% if not teacher.arrived %}
+            {% if teacher.id not in arrived %}
             <input id="checkin_{{ teacher.username }}" name="{{ teacher.last_name }}" 
                    class="button checkin" type="button" value="Check In" {% if when %}disabled="disabled"{% endif %} /><span />
             {% else %}
@@ -152,7 +152,7 @@
     </td>
 </tr>    
 {% endif %}
-{% for teacher in arrived %}
+{% for teacher in arrived.values %}
 <tr>
     <td class="checked-in">
         <a href="/manage/userview?username={{ teacher.username }}" target="_blank">{{ teacher.name }}</a>

commit 9e6fdd8c068fabfb9cf91d6f6e391ba492035091

Adds phone numbers back to teacher checkin page
833b201b introduced a regression on the teacher checkin page,
accidentally removing the phone numbers of missing teachers. This is
because phone numbers were being set by iterating over
ClassSection.teachers and setting a field on those ESPUser objects. This
doesn't work because `teachers` in this context is a property, so the
assignments of phone numbers were quickly lost.

This is fixed by creating a list of teachers and then modifying that. It
is this list which is now iterated over in the template.

This commit also:
- Adds test_phone_numbers_on_checkin_page() to catch this regression.
- To support this test, adds ProgramFrameworkTest.add_user_profiles() to
  create teacher profiles with phone numbers.
- Provides default text for when the teacher has no contact info, or for
  when the teacher has no provided phone number.

esp/esp/program/modules/handlers/teachercheckinmodule.py
@@ -153,7 +153,10 @@ class TeacherCheckinModule(ProgramModuleObj):
     def get_phone(user):
         """Get the phone number to display for a user."""
         contact_info = user.getLastProfile().contact_user
-        return contact_info.phone_cell or contact_info.phone_day
+        default = '(missing contact info)'
+        if contact_info:
+            return contact_info.phone_cell or contact_info.phone_day or default
+        return default
 
     def getMissingTeachers(self, prog, date=None, starttime=None, when=None,
                            show_flags=True):
@@ -263,7 +266,11 @@ class TeacherCheckinModule(ProgramModuleObj):
                     section.any_arrived = any(teacher.id in arrived
                                               for teacher in section.teachers)
                     section.room = (section.prettyrooms() or [None])[0]
-                    for teacher in section.teachers:
+                    # section.teachers is a property, so we can't add extra
+                    # data to the ESPUser objects and have them stick. We must
+                    # make a new list and then modify that.
+                    section.teachers_list = list(section.teachers)
+                    for teacher in section.teachers_list:
                         teacher.phone = teacher_phones[teacher.id]
                     sections_by_class[section.parent_class_id] = section
 

esp/esp/program/tests.py
@@ -39,7 +39,7 @@ from esp.cal.models import EventType, Event
 from esp.program.models import Program, ClassSection, RegistrationProfile, ScheduleMap, ProgramModule, StudentRegistration, RegistrationType, ClassCategories, ClassSubject, BooleanExpression, ScheduleConstraint, ScheduleTestOccupied, ScheduleTestCategory, ScheduleTestSectionList
 from esp.qsd.models import QuasiStaticData
 from esp.resources.models import Resource, ResourceType
-from esp.users.models import ESPUser, ContactInfo, StudentInfo, Permission
+from esp.users.models import ESPUser, ContactInfo, StudentInfo, TeacherInfo, Permission
 from esp.web.models import NavBarCategory
 
 from django.contrib.auth.models import Group
@@ -655,15 +655,42 @@ class ProgramFrameworkTest(TestCase):
                         sec.assign_room(random.choice(vr))
                         #   print '%s -> %s at %s' % (sec, sec.start_time().short_time(), sec.initial_rooms()[0].name)
 
-    #   Helper function to give each student a profile so they can sign up for classes.
-    #   Does not get called by default, but subclasses can call it.
-    def add_student_profiles(self):
+    def add_user_profiles(self):
+        """Helper function to give each user a profile so they can register.
+
+        Does not get called by default, but subclasses can call it.
+        """
         for student in self.students:
             student_studentinfo = StudentInfo(user=student, graduation_year=ESPUser.current_schoolyear(self.program)+2)
             student_studentinfo.save()
             student_regprofile = RegistrationProfile(user=student, program=self.program, student_info=student_studentinfo, most_recent_profile=True)
             student_regprofile.save()
-            
+        for teacher in self.teachers:
+            teacher_teacherinfo = TeacherInfo(user=teacher)
+            teacher_teacherinfo.save()
+            digit = teacher.id % 10
+            phone = (u'%d' % digit) * 10
+            teacher_contactinfo = ContactInfo(
+                user=teacher,
+                first_name=teacher.first_name,
+                last_name=teacher.last_name,
+                e_mail=teacher.email,
+                phone_day=phone,
+                phone_cell=phone,
+            )
+            teacher_contactinfo.save()
+            teacher_regprofile = RegistrationProfile(
+                user=teacher,
+                program=self.program,
+                teacher_info=teacher_teacherinfo,
+                contact_user=teacher_contactinfo,
+                most_recent_profile=True,
+            )
+            teacher_regprofile.save()
+
+    # For backwards compatability.
+    add_student_profiles = add_user_profiles
+
     #   Helper function to put the students in classes.
     #   Does not get called by default, but subclasses can call it.
     def classreg_students(self):

esp/templates/program/modules/teachercheckinmodule/missingteachers.html
@@ -127,7 +127,7 @@
             {{ section.begin_time }}
         {% endif %}
     </td>
-    {% for teacher in section.teachers %}
+    {% for teacher in section.teachers_list %}
         {% if teacher.id in arrived %}
         <td class="checked-in">
         {% else %}

esp/esp/program/modules/tests/teachercheckinmodule.py
@@ -43,6 +43,7 @@ class TeacherCheckinModuleTest(ProgramFrameworkTest):
 
     def setUp(self, *args, **kwargs):
         super(TeacherCheckinModuleTest, self).setUp(*args, **kwargs)
+        self.add_user_profiles()
         self.schedule_randomly() # only scheduled classes used in module
         self.ccc      = ClassCreationController(self.program)
         pm            = ProgramModule.objects.get(handler='TeacherCheckinModule')
@@ -50,6 +51,7 @@ class TeacherCheckinModuleTest(ProgramFrameworkTest):
         self.now      = self.settings['start_time']
         self.past     = datetime.datetime(1970, 1, 1)
         self.future   = datetime.datetime.max
+        self.admin    = self.admins[0]
         self.teacher  = self.teachers[0]
         self.cls      = self.teacher.getTaughtClasses()[0]
         self.event    = 'teacher_checked_in'
@@ -91,10 +93,7 @@ class TeacherCheckinModuleTest(ProgramFrameworkTest):
 
     # End aliases.
 
-    def runTest(self):
-        self.runCheckInTest()
-
-    def runCheckInTest(self):
+    def test_checkIn(self):
         """Run tests for checkIn() and undoCheckIn()."""
 
         # Test that calling checkIn() works, and
@@ -137,3 +136,8 @@ class TeacherCheckinModuleTest(ProgramFrameworkTest):
         self.assertIn('was not checked in for',
                       self.undoCheckIn(teacher=self.teacher2))
 
+    def test_phone_numbers_on_checkin_page(self):
+        self.failUnless(self.client.login(username=self.admin.username, password='password'), "Couldn't log in as admin %s" % self.admin.username)
+        response = self.client.get(u'%smissingteachers' % self.program.get_onsite_url())
+        phone = self.teacher.getLastProfile().contact_user.phone_cell
+        self.assertIn(phone, response.content.decode('utf-8'))