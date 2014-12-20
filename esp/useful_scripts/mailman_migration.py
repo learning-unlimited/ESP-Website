ffcc205229392498faa2c55fb2adc5b5efb0250a
Ben Kraft <benjaminjkraft@gmail.com>
2014-12-20 15:34:30 -0500

    More fixes to mailman migration
    (cherry picked from commit cb292e99550c15c67566959ad2be9a5fb21e0290)

--- esp/useful_scripts/mailman_migration.py
+++ esp/useful_scripts/mailman_migration.py
@@ -12,7 +12,7 @@ corresponds to an active user on the website, and all associated user accounts
 are active, we add it to mailman.
 """
 
-from setup_script import *
+from script_setup import *
 from esp import mailman
 from esp.users.models import ESPUser
 
@@ -30,7 +30,7 @@ def update_membership(base_users, list_name, add=False, dry_run=DRY_RUN):
     users_not_to_remove = ESPUser.objects.filter(is_active=True)
     users_to_remove = users_to_maybe_remove.exclude(
         email__in=users_not_to_remove.values('email'))
-    members_to_remove = current_members - set(
+    members_to_remove = current_members & set(
         users_to_remove.values_list('email', flat=True))
     print "Removing %s users..." % len(members_to_remove),
     if not dry_run:
@@ -47,7 +47,7 @@ def update_membership(base_users, list_name, add=False, dry_run=DRY_RUN):
             mailman.add_list_members(list_name, users_to_add)
         print "added."
     else:
-        print "User adding disabled"
+        print "User adding disabled."
 
     new_members = mailman.list_contents(list_name)
     print """List "%s" now contains %s users.""" % (
@@ -55,23 +55,23 @@ def update_membership(base_users, list_name, add=False, dry_run=DRY_RUN):
     print
 
 
-def update_lists(dry_run=DRY_RUN):
+def update_lists(add=False, dry_run=DRY_RUN):
     students = ESPUser.objects.filter(
         groups=Group.objects.get(name='Student'),
         studentinfo__graduation_year__gte=2015,
         studentinfo__graduation_year__lte=2030,
         id__gte=50000)
     parents = ESPUser.objects.filter(
-        groups=Group.objects.get('Guardian'),
+        groups=Group.objects.get(name='Guardian'),
         id__gte=50000)
     educators = ESPUser.objects.filter(
-        groups=Group.objects.get('Educator'),
+        groups=Group.objects.get(name='Educator'),
         id__gte=50000)
-    update_membership(students | parents | educators, "announcements", dry_run)
+    update_membership(students | parents | educators, "announcements", add, dry_run)
     teachers = ESPUser.objects.filter(
-        groups=Group.objects.get('Teachers'),
+        groups=Group.objects.get(name='Teacher'),
         id__gte=50000)
-    update_membership(teachers, "teachers", dry_run)
+    update_membership(teachers, "teachers", add, dry_run)
 
 if __name__ == '__main__':
     update_lists()
