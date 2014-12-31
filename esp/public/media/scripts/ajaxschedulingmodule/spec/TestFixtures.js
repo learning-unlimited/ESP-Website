6ab47a5de6ed1d436f4b76ee7710a5de3bd8d5cd
Corinn Herrick <herrickc@mit.edu>
2014-12-31 06:00:26 -0500

    Made fixtures match the data format

--- esp/public/media/scripts/ajaxschedulingmodule/spec/TestFixtures.js
+++ esp/public/media/scripts/ajaxschedulingmodule/spec/TestFixtures.js
@@ -1,113 +1,138 @@
+/**
+ * Here's the fake schedule:
+ *
+ * Legend:
+ *  xxxxx: disabled cell
+ *  top row: timeslot ids (num_students)
+ *  left column: room ids
+ *  ||: boundary between days
+ *  numbers in cells: IDs of sections
+ *
+ *             |  3  |  5  ||  7  |  11  |  13  |
+ * ----------------------------------------------
+ * room-1 (30) |S11s1|     ||     |xxxxxx|xxxxxx|
+ * ----------------------------------------------
+ * room-2 (10) |     |     ||xxxxx|     A22     |
+ * ----------------------------------------------
+ * room-3 (100)|     |xxxxx||xxxxx|      |      |
+ * ----------------------------------------------
+ *
+ * Classes:
+ * --------------------
+ *   code    length   availability       teachers
+ * - S11s1   1        3, 5               1, 2          
+ * - S11s2   1        3, 5               1, 2
+ * - A22s1   2        11, 13             4        
+ * - S33s1   2        3, 5, 7, 11        1
+ * - A44s1   1        7, 11, 13          3
+ * - M55S1   1        7, 11              1, 3
+ */
+
 function teacher_fixture() {
     return {
         1: {
+            id: 1,
             first_name: "Alyssa P.",
             last_name: "Hacker",
-            availability: [1],
-            sections: 3329, 
+            username: "aphacker",
+            availability: [3, 5, 7, 11],
+            sections: [1, 2, 4, 6], 
         },
         2: {
+            id: 2,
             first_name: "Ben",
             last_name: "Bitdiddle",
-            availability: [1, 2],
-            sections: 3538,
+            username: "bitdiddle",
+            availability: [3, 5],
+            sections: [1, 2],
+        },
+        3: {
+            id: 3,
+            first_name: "Edward S",
+            last_name: "Pembroke",
+            username: "esp",
+            availability: [7, 11, 13],
+            sections: [5, 6],
+        },
+        4: {
+            id: 4,
+            first_name: "Tim",
+            last_name: "Beaver",
+            username: "tbeaver",
+            availability: [11, 13],
+            sections: [3],            
         },
     };
 };
 
 function time_fixture() {
     return {
-	1: {
-	    label:  "first timeslot",
-	    start:  [2010, 4, 17, 10, 0, 0],
-	    end:  [2010, 4, 17, 10, 50, 0],
-	    id: 1
-	},
-	2: {
-	    label:  "second timeslot",
-	    start:  [2010, 4, 17, 11, 0, 0],
-	    end:  [2010, 4, 17, 11, 50, 0],
-	    id: 2
-	}
-    };
-};
-
-function time_fixture_with_order(){
-    return {
-	1: {
-	    label:  "first timeslot",
-	    start:  [2010, 4, 17, 10, 0, 0],
-	    end:  [2010, 4, 17, 10, 50, 0],
-	    order: 0,
-	    id: 1
-	},
-	2: {
-	    label:  "second timeslot",
-	    start:  [2010, 4, 17, 11, 0, 0],
-	    end:  [2010, 4, 17, 11, 50, 0],
-	    order: 1,
-	    id: 2
-	}
-    };
-};
-
-function time_fixture_out_of_order() {
-    return {
-	2: {
-	    label:  "first timeslot",
-	    start:  [2010, 4, 17, 10, 0, 0],
-	    end:  [2010, 4, 17, 10, 50, 0],
-	    id: 2
-	},
-	1: {
-	    label:  "second timeslot",
-	    start:  [2010, 4, 17, 11, 0, 0],
-	    end:  [2010, 4, 17, 11, 50, 0],
-	    id: 1
-	}
+        7: {
+            label:  "third timeslot",
+            short_description: "slot 1",
+            start:  [2010, 4, 18, 10, 5, 0],
+            end:  [2010, 4, 18, 10, 55, 0],
+            id: 1,
+            order: 3,
+        },
+        5: {
+            label:  "second timeslot",
+            short_description: "slot 2",
+            start:  [2010, 4, 17, 11, 5, 0],
+            end:  [2010, 4, 17, 11, 55, 0],
+            id: 2,
+            order: 2,
+        },
+        3: {
+            label:  "first timeslot",
+            short_description: "slot 1",
+            start:  [2010, 4, 17, 10, 5, 0],
+            end:  [2010, 4, 17, 10, 55, 0],
+            id: 2,
+            order: 1,
+        },
+        11: {
+            label:  "fourth timeslot",
+            short_description: "slot 4",
+            start:  [2010, 4, 18, 11, 5, 0],
+            end:  [2010, 4, 18, 11, 55, 0],
+            id: 2,
+            order: 4,
+        },
+        13: {
+            label:  "fifth timeslot",
+            short_description: "slot 5",
+            start:  [2010, 4, 18, 12, 5, 0],
+            end:  [2010, 4, 18, 12, 55, 0],
+            id: 2,
+            order: 5,
+        },
     };
 };
 
 function room_fixture() {
     return {
-	"room-1": {
-	    availability: [1, 2],
-        num_students: 23,
-        id: "room-1"
-	},
-	"room-2": {
-	    availability: [1, 2],
-        num_students: 7,
-        id: "room-2"
-	}
-    };
-};
-
-function schedule_assignments_fixture() {
-    return {
-	3329: {
-	    room_name: "room-1",
-	    id: 3329,
-	    timeslots: [1, 2]
-	},
-	3538: {
-	    room_name: null,
-	    id: 3538,
-	    timeslots: []
-	}
-    };
-};
-
-function empty_schedule_assignments_fixture() {
-    return {
-	3329: {
-	    id: 3329,
-	    timeslots: []
-	},
-	3538: {
-	    id: 3538,
-	    timeslots: []
-	}
+        "room-1": {
+            availability: [3, 5, 7],
+            num_students: 23,
+            id: "room-1",
+            text: "room-1",
+            uid: "room-1",
+        },
+        "room-2": {
+            availability: [3, 5, 11, 13],
+            num_students: 7,
+            id: "room-2",
+            text: "room-2",
+            uid: "room-2",
+        },
+        "room-3": {
+            availability: [3, 11, 13],
+            num_students: 7,
+            id: "room-3",
+            text: "room-3",
+            uid: "room-3",
+        },
     };
 };
 
@@ -115,59 +140,158 @@ function section_1() {
     return {
 	    status: 10, 
 	    category: 'S', 
-	    parent_class: 3188, 
-	    emailcode: 'S3188s1', 
+	    parent_class: 11, 
+	    emailcode: 'S11s1', 
 	    index: 1, 
 	    title: "Fascinating Science Phenomena", 
-	    category_id: 17, 
+	    category_id: 1, 
 	    class_size_max: 150, 
-	    length: 1, 
-	    grade_min: 7, 
-	    num_students: 42, 
+	    length: .83, 
+	    grade_min: 9, 
+	    num_students: 0, 
 	    grade_max: 12, 
-	    id: 3329, 
+	    id: 1, 
 	    teachers: [1, 2]
 	};
+};
+
+function section_1b() {
+	status: 10, 
+	category: 'S', 
+	parent_class: 11, 
+	emailcode: 'S11s2', 
+	index: 1, 
+	title: "Fascinating Science Phenomena", 
+	category_id: 1, 
+	class_size_max: 150, 
+	length: .83, 
+	grade_min: 9, 
+	num_students: 0, 
+	grade_max: 12, 
+	id: 2, 
+	teachers: [1, 2]
 };
 
 function section_2() {
     return {
 	    status: 10, 
-	    category: 'M', 
-	    parent_class: 3343, 
-	    emailcode: 'M3343s1', 
-	    index: 1, 
-	    title: "Become a LaTeX Guru", 
-	    category_id: 16, 
-	    class_size_max: 15, 
+	    category: 'A', 
+	    parent_class: 22, 
+	    emailcode: 'A22s1', 
+	    index: 2, 
+	    title: "Art History", 
+	    category_id: 2, 
+	    class_size_max: 75, 
 	    length: 1.83, 
-	    grade_min: 7, 
-	    num_students: 14, 
+	    grade_min: 10, 
+	    num_students: 0, 
+	    grade_max: 11, 
+	    id: 3, 
+	    teachers: [4]
+	};
+};
+
+function section_3() {
+    return {
+	    status: 10, 
+	    category: 'S', 
+	    parent_class: 33, 
+	    emailcode: 'S33s1', 
+	    index: 4,
+	    title: "Hands on Science", 
+	    category_id: 1, 
+	    class_size_max: 3, 
+	    length: 1.83, 
+	    grade_min: 9, 
+	    num_students: 0, 
+	    grade_max: 10, 
+	    id: 4, 
+	    teachers: [1]
+	};
+};
+
+function section_4() {
+    return {
+	    status: 10, 
+	    category: 'A',
+	    parent_class: 44, 
+	    emailcode: 'A44s1', 
+	    index: 5, 
+	    title: "Drawing 101", 
+	    category_id: 2, 
+	    class_size_max: 50, 
+	    length: 1.83, 
+	    grade_min: 9, 
+	    num_students: 0, 
 	    grade_max: 12, 
-	    id: 3538, 
-	    teachers: [2]
+	    id: 5, 
+	    teachers: [3]
+	};
+};
+
+function section_5() {
+    return {
+	    status: 10, 
+	    category: 'M', 
+	    parent_class: 55, 
+	    emailcode: 'M55s1', 
+	    index: 6, 
+	    title: "Representation Theory", 
+	    category_id: 3, 
+	    class_size_max: 25, 
+	    length: 1.83, 
+	    grade_min: 10, 
+	    num_students: 0, 
+	    grade_max: 12, 
+	    id: 6, 
+	    teachers: [1, 3]
 	};
 };
 
 function sections_fixture() {
     return {
-	3329: section_1(), 
-	3538: section_2()
+        1: section_1(), 
+        2: section_1b(),
+        3: section_2(),
+        4: section_3(),
+        5: section_4(),
+        6: section_5(),
     };
 };
 
-function matrix_fixture() {
-    var modified_room_fixture = room_fixture();
-	modified_room_fixture["room-2"].availability = [2];
-    var mp = new MessagePanel($j("<div>"), "Welcome to the Ajax Scheduler!");
-    var sections = new Sections(sections_fixture(), teacher_fixture(), 
-                                schedule_assignments_fixture(), new FakeApiClient());
-    var sip = new SectionInfoPanel($j("<div>"), teacher_fixture(), sections, mp);
-    return new Matrix(
-	    new Timeslots(time_fixture()),
-	    modified_room_fixture,
-        sections,
-	    $j("<div/>"),
-        mp, 
-        sip);
+
+function schedule_assignments_fixture() {
+    return {
+        1: {
+            room_name: "room-1",
+            id: 1,
+            timeslots: [3]
+        },
+        2: {
+            room_name: null,
+            id: 2,
+            timeslots: []
+        },
+        3: {
+            room_name: "room-2",
+            id: 3,
+            timeslots: [11, 13]
+        },
+        4: {
+            room_name: null,
+            id: 4,
+            timeslots: []
+        },
+        5: {
+            room_name: null,
+            id: 5,
+            timeslots: []
+        },
+        6: {
+            room_name: null,
+            id: 6,
+            timeslots: []
+        }
+    };
 };
+
