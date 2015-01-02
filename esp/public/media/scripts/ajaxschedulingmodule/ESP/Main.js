8cc1306e0f103cc847a94841d12c6ffb0fca8edf
Corinn Herrick <herrickc@mit.edu>
2015-01-02 13:36:54 -0500

    When browser window resizes, elements do as well.

--- esp/public/media/scripts/ajaxschedulingmodule/ESP/Main.js
+++ esp/public/media/scripts/ajaxschedulingmodule/ESP/Main.js
@@ -4,34 +4,40 @@
 // For the changelog
 var last_applied_index = 0;
 $j.getJSON('ajax_schedule_last_changed', function(data, status) {
-    if (status == "success") {
+        if (status == "success") {
         last_applied_index = data['latest_index'];
-    }
-});
+        }
+        });
+
+// Set the width and height of the matrix and directory
+var resizeElements = function() {
+        var window_height = window.innerHeight - 20;
+        var window_width = window.innerWidth - 20;
+        $j("#directory-wrapper-div").height(window_height)
+        .width(window_width/4);
+        $j("#matrix-div").height(window_height)
+        .width(window_width*3/4);
+};
+
+// Resize window handler
+window.onresize = resizeElements;
 
 // Fetch the data from the server
 var data = {};
 json_fetch(['sections', 'timeslots', 'rooms', 'schedule_assignments'], function(){
-    console.log(data)
-
-    // Set the width and height of the matrix and directory
-    var window_height = window.innerHeight - 20;
-    var window_width = window.innerWidth - 20;
-    $j("#directory-wrapper-div").height(window_height)
-        .width(window_width/4);
-    $j("#matrix-div").height(window_height)
-        .width(window_width*3/4);
-
-    // Create a new Scheduler which does the rest
-    var s = new Scheduler(
-	    data,
-	    $j("#directory-div"),
-	    $j("#matrix-div"),
-        $j("#message-div"),
-        $j("#section-info-div"),
-	    last_applied_index,
-	    5000
-    );
-    s.render();
+        console.log(data)
+        resizeElements();
+        
+        // Create a new Scheduler which does the rest
+        var s = new Scheduler(
+            data,
+            $j("#directory-div"),
+            $j("#matrix-div"),
+            $j("#message-div"),
+            $j("#section-info-div"),
+            last_applied_index,
+            5000
+            );
+        s.render();
 }, 
-data);
+    data);
