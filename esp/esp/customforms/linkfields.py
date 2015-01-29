00b51f8050f7d5cba9db23d59d15009a79a5a6de
Jordan Moldow <jmoldow@learningu.org>
2015-01-29 01:52:46 -0500

    Add UKPhoneNumberField, replace USPhoneNumberField
    (cherry picked from commit 50d1e26d13d908f456176504b71166d6cfa4fdea)
    
    Conflicts:
    	esp/esp/program/modules/forms/volunteer.py

--- esp/esp/customforms/linkfields.py
+++ esp/esp/customforms/linkfields.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms.models import fields_for_model
 from django.apps import apps
 from localflavor.us.forms import USStateField, USPhoneNumberField, USStateSelect
+from esp.utils.forms import UKPhoneNumberField
 from esp.customforms.forms import NameField, AddressField
 from esp.utils.forms import DummyField
 
@@ -18,7 +19,7 @@ generic_fields = {
     'date': {'typeMap': forms.DateField,'attrs': {'widget': forms.DateInput,}, 'widget_attrs': {'class': 'ddate ', 'format': '%m-%d-%Y'},},
     'time': {'typeMap': forms.TimeField, 'attrs': {'widget': forms.TimeInput,}, 'widget_attrs': {'class': 'time '},},
     'file': {'typeMap': forms.FileField, 'attrs': {'widget': forms.ClearableFileInput,}, 'widget_attrs': {'class': 'file'},},
-    'phone': {'typeMap': USPhoneNumberField, 'attrs': {'widget': forms.TextInput,}, 'widget_attrs': {'class': 'USPhone '}},
+    'phone': {'typeMap': UKPhoneNumberField, 'attrs': {'widget': forms.TextInput,}, 'widget_attrs': {'class': 'USPhone '}},
     'email': {'typeMap': forms.EmailField, 'attrs': {'max_length': 30, 'widget': forms.TextInput,}, 'widget_attrs': {'class': 'email '}},
     'state': {'typeMap': USStateField, 'attrs': {'widget': USStateSelect}, 'widget_attrs': {'class': ''}},
     'gender': {'typeMap': forms.ChoiceField, 'attrs': {'widget': forms.RadioSelect, 'choices': [('F', 'Female'), ('M', 'Male')]}, 'widget_attrs': {'class': 'gender '}, },
