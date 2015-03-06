learning-unlimited/ESP-Website

commit 3faa63e0999dd6bafc34a38ffdc5aebbae54772c

Add publicly viewable onsite class list
Should be revisited in the future, as this is a simple hack to show a
public version of the class list already displayed by the onsite
interface.

Partially addresses #167.

Cherry picked from portions of 88ec6d6.

esp/esp/program/modules/handlers/studentclassregmodule.py
@@ -47,6 +47,7 @@ from django.views.decorators.vary import vary_on_cookie
 from django.core.cache import cache
 
 from esp.program.modules.base import ProgramModuleObj, needs_teacher, needs_student, needs_admin, usercheck_usetl, meets_deadline, meets_any_deadline, main_call, aux_call
+from esp.program.modules.handlers.onsiteclasslist import OnSiteClassList
 from esp.datatree.models import *
 from esp.program.models  import ClassSubject, ClassSection, ClassCategories, RegistrationProfile, ClassImplication, StudentRegistration
 from esp.program.modules import module_ext
@@ -754,6 +755,20 @@ class StudentClassRegModule(ProgramModuleObj, module_ext.StudentClassRegModuleIn
         
         else:
             return []
+
+    @aux_call
+    def openclasses(self, request, tl, one, two, module, extra, prog):
+        """ A publicly viewable version of the onsite class list. 
+            Should be revisited in the future, as this was a temporary
+            hack created for Stanford Splash in fall 2013. """
+
+        modules = prog.getModules()
+        for module in modules:
+            if isinstance(module, OnSiteClassList):
+                return module.classList_base(request, tl, one, two, module, 'by_time', prog, 'allclass_fragment.html')
+        
+        #  Otherwise this will be a 404
+        return None
 
     class Meta:
         abstract = True

esp/esp/program/modules/handlers/onsiteclasslist.py
@@ -42,7 +42,7 @@ from django.db.models.query import Q
 from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 
-from esp.program.modules.base import ProgramModuleObj, needs_onsite, main_call, aux_call
+from esp.program.modules.base import ProgramModuleObj, needs_onsite, needs_student, main_call, aux_call
 from esp.program.models import ClassSubject, ClassSection, StudentRegistration, ScheduleMap
 from esp.web.util import render_to_response
 from esp.cal.models import Event
@@ -378,6 +378,14 @@ LIMIT 1
     @aux_call
     @needs_onsite
     def classList(self, request, tl, one, two, module, extra, prog):
+        return self.classList_base(request, tl, one, two, module, extra, prog, 'classlist.html')
+
+    @aux_call
+    @needs_student
+    def classlist_public(self, request, tl, one, two, module, extra, prog):
+        return self.classList_base(request, tl, one, two, module, extra, prog, 'allclass_fragment.html')
+
+    def classList_base(self, request, tl, one, two, module, extra, prog, template_name=None):
         """ Display a list of all classes that still have space in them """
         context = {}
         defaults = {'refresh': 120, 'scrollspeed': 1}

esp/templates/program/modules/onsiteclasslist/allclass_fragment.html
@@ -1,12 +1,16 @@
-
-  <table cellpadding="2" cellspacing="0" width="100%">
+<table cellpadding="2" cellspacing="0" width="100%" style="table-layout: fixed;"> 
+            <tr>
+            <td width="15%">&nbsp;</td>
+            <td width="75%">&nbsp;</td>
+            <td width="10%">&nbsp;</td>
+            </tr>
     <tr>
-        <th>All Open Classes</th>
+        <th colspan="4">All Open Classes - Currently {{ current_time }}</th>
     </tr>
-    <tr><td>
     {% for class in classes %}
+        {% if class.category.category != "Lunch" %}
         {% if not class.isFull %}
-        {% if not class.already_passed %}
+         {% if use_categories %}
             {% ifchanged %}
                 </td></tr>
                <tr><th class="start_time">{{ class.begin_time }}</th></tr>
@@ -17,8 +21,13 @@
                <tr><th class="category">{{ class.category.category }}</th></tr>
                <tr><td>
             {% endifchanged %}
-            <div class="classitem"><b>{{ class.emailcode }}</b> ({% for ft in class.friendly_times %}{{ ft }}{% if not forloop.last %}, {% endif %}{% endfor %}): <br /> {{ class.title }} (Grades {{class.parent_class.grade_min}}-{{class.parent_class.grade_max}})<br /></div>
-        {% endif %}
+         {% endif %}
+            <tr>
+            <td width="15%"><div class="hideextra"><span class="classitem"style="color: #00CC00;">{% for ft in class.friendly_times %}{{ ft }}{% if not forloop.last %}, {% endif %}{% endfor %}</span></div></td>
+            <td width="75%"><div class="hideextra"><span class="classitem"><b>{{ class.emailcode }}</b>: {{ class.title }}</span></div></td>
+            <td width="10%"><div class="hideextra"><span class="classitem"style="color: #CC0000;">Grades {{ class.parent_class.grade_min }}--{{ class.parent_class.grade_max }}</span></div> </td>
+            </tr>
         {% endif %} 
+        {% endif %}
     {% endfor %}
-    </table>
+</table>

esp/templates/program/modules/onsiteclasslist/classlist.html
@@ -22,7 +22,7 @@ function getElementStyleById(id, propertyName) {
     return el.style[propertyName];
 }
 
-function scroll() 
+function scroll()
 {
     scroll_offset -= {{ scrollspeed }};
     elt1 = document.getElementById("scroller_top");
@@ -32,7 +32,7 @@ function scroll()
     }
     setElementStyleById("scroller_top", "top", scroll_offset + "px");
     setElementStyleById("scroller_bottom", "top", scroll_offset + elt1.offsetHeight + "px");
-    
+
     if(start_times[next_start_time]){
         if($(start_times[next_start_time]).position().top < -scroll_offset + 24){
             $(".fixed_header1").css("top", $(start_times[next_start_time]).position().top + scroll_offset);
@@ -54,7 +54,7 @@ function scroll()
             next_category++;
         }
     }
-    
+
     sessionStorage.onsiteClassListOffset = scroll_offset;
 }
 
@@ -120,6 +120,21 @@ font-size: 1.7em;
     width: 49%;
     z-index: 10;
 }
+
+.classlist_emailcode {
+   font-size: 0.7em;
+   line-height: 0.6em;
+   font-weight: bold;
+}
+
+.classlist_title {
+    font-size: 0.7em;
+}
+
+table .openclasslist td {
+    height: 16px !important;
+}
+
 </style>
 
 <meta http-equiv="refresh" content="{{ refresh }}" />
@@ -138,21 +153,23 @@ font-size: 1.7em;
 <table width="100%">
 <tr>
 <td width="50%" valign="top">
-  <table cellpadding="2" cellspacing="0" width="100%">
+  <table class="openclasslist" cellpadding="2" cellspacing="0" width="100%">
     <tr>
-        <th style="font-size: 2.0em" colspan="2">{{ prog.niceName }}: Open classes for the upcoming hour: starting at {{ current_time.start|date:"D P" }}</th>
+        <th style="font-size: 1.0em" colspan="2">Open classes starting at {{ current_time.start|date:"D P" }}</th>
     </tr>
+{% comment %}
     <tr>
-        <th style="font-size: 1.7em" class="materials">Code</th>
-        <th style="font-size: 1.7em" class="materials">Class Title</th>
+        <th style="font-size: 1.3em" class="materials">Code</th>
+        <th style="font-size: 1.3em" class="materials">Class Title</th>
     </tr>
+{% endcomment %}
     {% for class in classes %}
         {% ifequal class.start_time current_time %}
         {% if not class.isFull %}
         {% if not class.already_passed %}
     <tr>
-        <td>{{class.emailcode}}</td>
-        <td>{{class.title}} ({% for ft in class.friendly_times %}{{ ft }}{% if not forloop.last %}, {% endif %}{% endfor %}; Grades {{class.parent_class.grade_min}}-{{class.parent_class.grade_max}})</td>
+        <td><div class="classlist_emailcode">{{class.emailcode}}</div></td>
+        <td><div class="classlist_title">{{class.title}} ({% for ft in class.friendly_times %}{{ ft }}{% if not forloop.last %}, {% endif %}{% endfor %}; {{class.parent_class.grade_min}}-{{class.parent_class.grade_max}})</div></td>
     </tr>
         {% endif %}
         {% endif %}

commit 5718359829ae64cb1520e56af8ace28685c40888

Fix the openclasses view.
a33412c made some changes to the onsite class list view; unfortunately
it also changed the method signature, which broke the public open
class list introduced in 3faa63e.

esp/esp/program/modules/handlers/studentclassregmodule.py
@@ -776,7 +776,7 @@ class StudentClassRegModule(ProgramModuleObj):
 
         module = prog.getModule('OnSiteClassList')
         if module:
-            return module.classList_base(request, tl, one, two, module, 'by_time', prog, 'allclass_fragment.html')
+            return module.classList_base(request, tl, one, two, module, 'by_time', prog, options={}, template_name='allclass_fragment.html')
         
         #  Otherwise this will be a 404
         return None