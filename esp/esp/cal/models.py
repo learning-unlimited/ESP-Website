45960df3152157981dc547d9949c6cb99e73f0b1
Ben Kraft <benjaminjkraft@gmail.com>
2015-03-27 13:34:37 -0400

    Add `Event.available_locations`

--- esp/esp/cal/models.py
+++ esp/esp/cal/models.py
@@ -66,6 +66,7 @@ class Event(models.Model):
     program = models.ForeignKey('program.Program',blank=True, null=True)
     event_type = models.ForeignKey(EventType) # The type of event.  This implies, though does not require, the types of data that are keyed to this event.
     priority = models.IntegerField(blank=True, null=True) # Priority of this event
+    available_locations = models.ManyToManyField('resources.Location',blank=True)
 
     def title(self):
         return self.name
