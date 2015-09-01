ca87a2104e48adf2af6c5b15a55b3fe6f9bac4bd
Jordan Moldow <jmoldow@learningu.org>
2015-09-01 02:49:59 -0400

    merge conflicts

--- esp/esp/web/templatetags/navbar.py
+++ esp/esp/web/templatetags/navbar.py
@@ -1,6 +1,9 @@
 from django import template
 from django.utils.http import urlquote as urlencode
 from esp.utils.cache_inclusion_tag import cache_inclusion_tag
+from esp.web.models import NavBarCategory, NavBarEntry
+from esp.web.views.navBar import makeNavBar
+
 register = template.Library()
 
 @cache_inclusion_tag(register,'inclusion/web/navbar_left.html')
@@ -10,3 +13,17 @@ def navbar_gen(request_path, user, navbar_list):
             'request_path': request_path,
             'user': user,
             }
+
+# So I'm cheating here because I couldn't figure out a neat way to
+# do what Chicago wanted using their existing infrastructure.
+# Maybe I'll eventually move this to template overrides.
+# -ageng 2013-08-12
+@cache_inclusion_tag(register,'inclusion/web/navbar_entries.html')
+def navbar_by_category(cat):
+    try:
+        cat = NavBarCategory.objects.get(name=cat)
+    except (NavBarCategory.DoesNotExist, NavBarCategory.MultipleObjectsReturned):
+        cat = None
+    navbar_list = makeNavBar('', cat)
+    return {'navbar_list': navbar_list}
+navbar_by_category.cached_function.depend_on_row(NavBarEntry, lambda entry: {'cat': entry.category.name})
