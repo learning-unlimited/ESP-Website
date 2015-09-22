680a458902e21f3f0c75b4061596eca893640f9c
Ben Kraft <benjaminjkraft@gmail.com>
2015-09-21 22:41:21 -0700

    Use a 404 instead of a 410 for no redirect

--- esp/esp/middleware/patchedredirect.py
+++ esp/esp/middleware/patchedredirect.py
@@ -1,7 +1,8 @@
 from django.contrib.redirects import RedirectFallbackMiddleware
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 
 class PatchedRedirectFallbackMiddleware(RedirectFallbackMiddleware):
     """Uses a 302 instead of Django's default 301."""
     response_redirect_class = HttpResponseRedirect
+    response_gone_class = HttpResponseNotFound
