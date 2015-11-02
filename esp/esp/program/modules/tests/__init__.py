796d54171013ae59938f7cdae5de74612fc3549a
Ben Kraft <benjaminjkraft@gmail.com>
2015-11-01 17:05:56 -0800

    Remove ajax scheduling selenium tests
    They don't work (even after changes to get them to run at all), they appear to
    be outdated, and we have Javascript tests now.

--- esp/esp/program/modules/tests/__init__.py
+++ esp/esp/program/modules/tests/__init__.py
@@ -47,5 +47,4 @@ from esp.program.modules.tests.commpanel import CommunicationsPanelTest
 from esp.program.modules.tests.resourcemodule import ResourceModuleTest
 from esp.program.modules.tests.admincore import RegistrationTypeManagementTest
 from esp.program.modules.tests.adminclass import CancelClassTest
-from esp.program.modules.tests.ajaxschedulingmoduleui import AJAXSchedulingModuleUITest
 from esp.program.modules.tests.classsearchmodule import ClassSearchModuleTest
