ab53b61304bb3173677bc6d7a21e2f5e980ad005
Ben Kraft <benjaminjkraft@gmail.com>
2015-11-24 13:24:08 -0500

    Remove esp.web.templatetags.last
    Two of the tags were unused, and `last` is in django now.

--- esp/esp/utils/globaltags.py
+++ esp/esp/utils/globaltags.py
@@ -2,4 +2,3 @@ from django import template
 
 # TODO: private API, please fix!
 template.base.add_to_builtins('esp.web.templatetags.latex')
-template.base.add_to_builtins('esp.web.templatetags.last')
