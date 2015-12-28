fff6942e8472c1843409871d114bca6f6d14a7f5
btidor <btidor@mit.edu>
2015-12-28 00:10:28 -0500

    Admin: set list_select_related on Transfer
    This causes Django to collapse all its database queries into one, which
    significantly speeds up the load time of the Transfer list view (2.9 sec to 0.5
    sec on my dev server).

--- esp/esp/accounting/admin.py
+++ esp/esp/accounting/admin.py
@@ -53,8 +53,9 @@ class TransferAdmin(admin.ModelAdmin):
         else:
             return u'--'
     list_display = ['id', 'line_item', 'user', 'timestamp', 'source', 'destination', 'amount_dec', 'option_description']
-    search_fields = default_user_search() +['source__name', 'destination__name', 'line_item__text', '=transaction_id']
     list_filter = ['source', 'destination', 'line_item__program']
+    list_select_related = ['source', 'destination', 'line_item', 'option', 'user', 'line_item__program']
+    search_fields = default_user_search() +['source__name', 'destination__name', 'line_item__text', '=transaction_id']
     raw_id_fields = ['paid_in']  # it's too expensive to iterate over all Transfers to create the dropdown menu
 admin_site.register(Transfer, TransferAdmin)
 
