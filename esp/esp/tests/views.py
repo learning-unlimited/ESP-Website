e974eb42dd2e2130960933cff58704fb7220535c
Anthony Lu <lua@mit.edu>
2016-02-12 16:54:43 -0500

    Fix for move-web-util

--- esp/esp/tests/views.py
+++ esp/esp/tests/views.py
@@ -1,4 +1,4 @@
-from esp.web.util import render_to_response
+from esp.utils.web import render_to_response
 
 def javascript_tests(request):
     return render_to_response('SpecRunner.html', request, {})
