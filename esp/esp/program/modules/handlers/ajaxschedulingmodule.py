learning-unlimited/ESP-Website

commit 4fbf15658af690857efe131e48dc473042cf4a3e

refactored ajax_schedule_class to make it easier to test

esp/esp/program/modules/handlers/ajaxschedulingmodule.py
@@ -296,6 +296,55 @@ class AJAXSchedulingModule(ProgramModuleObj):
 
         return HttpResponse('\n'.join([','.join(['"%s"' % v for v in x]) for x in lst]), mimetype='text/csv')
 
+    #helper functions for ajax_schedule_class
+    #seperated out here to make code more readeable and enable testing
+    def makeret(self, prog, **kwargs):
+        last_changed = self.ajax_schedule_last_changed_cached(prog).raw_value
+        kwargs['val'] = last_changed['val']
+        response = HttpResponse(content_type="application/json")
+        simplejson.dump(kwargs, response)
+        return response            
+
+    def ajax_schedule_deletereg(self, prog, cls):
+        cls.clearRooms()
+        cls.clear_meeting_times()
+        return self.makeret(prog, ret=True, msg="Schedule removed for Class Section '%s'" % cls.emailcode())
+
+    def ajax_schedule_assignreg(self, prog, cls, blockrooms, times, classrooms):
+        if len(times) < 1:
+            return self.makeret(prog, ret=False, msg="No times specified!, can't assign to a timeblock")
+
+        if len(classrooms) < 1:
+            return self.makeret(prog, ret=False, msg="No classrooms specified!, can't assign to a timeblock")
+
+        basic_cls = classrooms[0]
+        for c in classrooms:
+            if c != basic_cls:
+                return self.makeret(prog, ret=False, msg="Assigning one section to multiple rooms.  This interface doesn't support this feature currently; assign it to one room for now and poke a Webmin to do this for you manually.")
+            
+            times = Event.objects.filter(id__in=times).order_by('start')
+            if len(times) < 1:
+                return self.makeret(prog, ret=False, msg="Specified Events not found in the database")
+
+            classrooms = Resource.objects.filter(name=basic_cls, res_type__name="Classroom")
+            if len(classrooms) < 1:
+                return self.makeret(prog, ret=False, msg="Specified Classrooms not found in the database")
+
+            classroom = classrooms[0]
+
+            cannot_schedule = cls.cannotSchedule(times, ignore_classes=False)
+            if cannot_schedule:
+                return self.makeret(prog, ret=False, msg=cannot_schedule)
+            
+            cls.assign_meeting_times(times)
+            status, errors = cls.assign_room(classroom)
+
+            if not status: # If we failed any of the scheduling-constraints checks in assign_room()
+                cls.clear_meeting_times()
+                return self.makeret(prog, ret=False, msg=" | ".join(errors))
+            
+            return self.makeret(prog, ret=True, msg="Class Section '%s' successfully scheduled" % cls.emailcode())
+
     @aux_call
     @needs_admin
     def ajax_schedule_class(self, request, tl, one, two, module, extra, prog):
@@ -307,21 +356,9 @@ class AJAXSchedulingModule(ProgramModuleObj):
         # Pull relevant data out of the JSON structure
         cls = ClassSection.objects.get(id=request.POST['cls'])
         action = request.POST['action']
-
-        def makeret(**kwargs):
-            last_changed = self.ajax_schedule_last_changed_cached(prog).raw_value
-            kwargs['val'] = last_changed['val']
-            response = HttpResponse(content_type="application/json")
-            simplejson.dump(kwargs, response)
-            return response            
         
         if action == 'deletereg':
-            # raise ESPError(), "Tried to delete a class via AJAX-Scheduling!"
-            
-            cls.clearRooms()
-            cls.clear_meeting_times()
-
-            return makeret(ret=True, msg="Schedule removed for Class Section '%s'" % cls.emailcode())
+            return self.ajax_schedule_deletereg(prog, cls)
 
         elif action == 'assignreg':
             blockrooms = request.POST['block_room_assignments'].split("\n")
@@ -331,41 +368,9 @@ class AJAXSchedulingModule(ProgramModuleObj):
             times = [br['time_id'] for br in blockrooms]
             classrooms = [br['room_id'] for br in blockrooms]
 
-            if len(times) < 1:
-                return makeret(ret=False, msg="No times specified!, can't assign to a timeblock")
-
-            if len(classrooms) < 1:
-                return makeret(ret=False, msg="No classrooms specified!, can't assign to a timeblock")
-
-            basic_cls = classrooms[0]
-            for c in classrooms:
-                if c != basic_cls:
-                    return makeret(ret=False, msg="Assigning one section to multiple rooms.  This interface doesn't support this feature currently; assign it to one room for now and poke a Webmin to do this for you manually.")
-                
-            times = Event.objects.filter(id__in=times).order_by('start')
-            if len(times) < 1:
-                return makeret(ret=False, msg="Specified Events not found in the database")
-
-            classrooms = Resource.objects.filter(name=basic_cls, res_type__name="Classroom")
-            if len(classrooms) < 1:
-                return makeret(ret=False, msg="Specified Classrooms not found in the database")
-
-            classroom = classrooms[0]
-
-            cannot_schedule = cls.cannotSchedule(times, ignore_classes=False)
-            if cannot_schedule:
-                return makeret(ret=False, msg=cannot_schedule)
-            
-            cls.assign_meeting_times(times)
-            status, errors = cls.assign_room(classroom)
-
-            if not status: # If we failed any of the scheduling-constraints checks in assign_room()
-                cls.clear_meeting_times()
-                return makeret(ret=False, msg=" | ".join(errors))
-            
-            return makeret(ret=True, msg="Class Section '%s' successfully scheduled" % cls.emailcode())
+            return self.ajax_schedule_assignreg(prog, cls, blockrooms, times, classrooms)
         else:
-            return makeret(ret=False, msg="Unrecognized command: '%s'" % action)
+            return self.makeret(prog, ret=False, msg="Unrecognized command: '%s'" % action)
     
     @aux_call
     @needs_admin

commit c83193813da7969328c0e9d9032e57a609c69183

Fix indentation bug introduced in 4fbf15658af69085
If you look at the diff, the body of ajax_schedule_assignreg was copied
over from the old ajax_schedule_class, but an extra level of indentation
was accidentally introduced after the for-loop after classrooms.

This was mostly okay because the body of the for-loop would only ever
run once (because it returns at the end), with the first classroom in
classrooms, which is what the intended code does anyway as long as all
the classrooms are the same, and all that the original for-loop did was
check that all the classrooms are the same anyway.

esp/esp/program/modules/handlers/ajaxschedulingmodule.py
@@ -124,38 +124,36 @@ class AJAXSchedulingModule(ProgramModuleObj):
         if len(classrooms) < 1:
             return self.makeret(prog, ret=False, msg="No classrooms specified!, can't assign to a timeblock")
 
-        #TODO:  this loom modifies classrooms from within the loop.  That seems like a bad idea.
-        #we should figure out why
         basic_cls = classrooms[0]
         for c in classrooms:
             if c != basic_cls:
                 return self.makeret(prog, ret=False, msg="Assigning one section to multiple rooms.  This interface doesn't support this feature currently; assign it to one room for now and poke a Webmin to do this for you manually.")
 
-            times = Event.objects.filter(id__in=times).order_by('start')
-            if len(times) < 1:
-                return self.makeret(prog, ret=False, msg="Specified Events not found in the database")
+        times = Event.objects.filter(id__in=times).order_by('start')
+        if len(times) < 1:
+            return self.makeret(prog, ret=False, msg="Specified Events not found in the database")
 
-            classrooms = Resource.objects.filter(name=basic_cls, res_type__name="Classroom")
-            if len(classrooms) < 1:
-                return self.makeret(prog, ret=False, msg="Specified Classrooms not found in the database")
+        classrooms = Resource.objects.filter(name=basic_cls, res_type__name="Classroom")
+        if len(classrooms) < 1:
+            return self.makeret(prog, ret=False, msg="Specified Classrooms not found in the database")
 
-            classroom = classrooms[0]
+        classroom = classrooms[0]
 
-            cannot_schedule = cls.cannotSchedule(times, ignore_classes=False)
-            if cannot_schedule:
-                return self.makeret(prog, ret=False, msg=cannot_schedule)
+        cannot_schedule = cls.cannotSchedule(times, ignore_classes=False)
+        if cannot_schedule:
+            return self.makeret(prog, ret=False, msg=cannot_schedule)
 
-            cls.assign_meeting_times(times)
-            status, errors = cls.assign_room(classroom, clear_others=True)
+        cls.assign_meeting_times(times)
+        status, errors = cls.assign_room(classroom, clear_others=True)
 
-            if not status: # If we failed any of the scheduling-constraints checks in assign_room()
-                cls.clear_meeting_times()
-                return self.makeret(prog, ret=False, msg=" | ".join(errors))
+        if not status: # If we failed any of the scheduling-constraints checks in assign_room()
+            cls.clear_meeting_times()
+            return self.makeret(prog, ret=False, msg=" | ".join(errors))
 
-            #add things to the change log here
-            self.get_change_log(prog).appendScheduling([int(t.id) for t in times], classroom_names[0], int(cls.id), user)
+        #add things to the change log here
+        self.get_change_log(prog).appendScheduling([int(t.id) for t in times], classroom_names[0], int(cls.id), user)
 
-            return self.makeret(prog, ret=True, msg="Class Section '%s' successfully scheduled" % cls.emailcode())
+        return self.makeret(prog, ret=True, msg="Class Section '%s' successfully scheduled" % cls.emailcode())
 
     @aux_call
     @needs_admin