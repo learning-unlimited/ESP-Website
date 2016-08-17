a2eb65094ec61188b02112dc1b419fc78459c92e
hanoi137 <samuel.banks@merton.ox.ac.uk>
2016-08-17 14:42:18 +0100

    Minor changes to resolve issues
    Django toolbar updated and changes made to users/migrations

---
+++ esp/esp/users/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0007_auto_20160709_2038'),
+        ('users', '0003_british_address_localization'),
+    ]
+
+    operations = [
+    ]
