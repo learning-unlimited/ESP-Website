4ea6f85ecd1894f541264ea062a2bcc6a9d17a32
Miles Calabresi <mcalabresi1@gmail.com>
2016-10-02 20:08:59 -0400

    Add change to popup for teachers having trouble submitting classes

--- esp/public/media/scripts/program/modules/teacherclassregmodule.js
+++ esp/public/media/scripts/program/modules/teacherclassregmodule.js
@@ -1,15 +1,29 @@
 function check_grade_range(form, grade_min, grade_max)
 {
     console.log("Checking!");
-    if ($j(form).find('#id_grade_min').val() == grade_min && $j(form).find('#id_grade_max').val() == grade_max)
+// Local changes (chapter request) made by milescalabresi... apparently
+// one can't use template overrides to tweak scripts like this one.
+
+//    if ($j(form).find('#id_grade_min').val() == grade_min && $j(form).find('#id_grade_max').val() == grade_max)
+//    {
+//        return confirm("Are you sure you want your class to have grade range "+grade_min+"-"+grade_max+"? \
+//Managing a class with grade range "+grade_min+"-"+grade_max+" can be more difficult due to the \
+//difference in maturity levels between middle and high school students. \
+//Click OK if you are sure this is what you want, or click Cancel to go back \
+//and change the grade range.");
+//    }
+    if ($j(form).find('#id_grade_min').val() > 8)
     {
-        return confirm("Are you sure you want your class to have grade range "+grade_min+"-"+grade_max+"? \
-Managing a class with grade range "+grade_min+"-"+grade_max+" can be more difficult due to the \
-difference in maturity levels between middle and high school students. \
-Click OK if you are sure this is what you want, or click Cancel to go back \
-and change the grade range.");
-    }
-    else
+        return confirm("Hey there! We really need some classes open to middle"
+                       +" school students. Would you consider helping us out by"
+                       +" opening your class to them? Click Cancel to go back"
+                       +" and change your minimum grade, or click OK to"
+                       +" proceed with your original selection. Thanks for"
+                       +" your consideration :) and if you hit OK but still"
+		       +" can't submit, check your ad-blocker or other add-ons."
+		       +" (If you still can't submit after that, email us at"
+		       +" yalesplash@gmail.com and we'll help you sort it out.");
+    }  else
     {
         return true;
     }
