fe6898cc3343109f4ce4d621f8f0edcf78c7a3dc
Ben Kraft <ben@benkraft.org>
2016-10-14 22:19:12 -0400

    Don't fetch all the TextOfEmails at once
    It's unnecessary, and is causing us to run out of memory.  Let's just not.  In
    addition, even iterating over the queryset seems to cause us to suddenly use
    600M of memory and run out of memory.  Using `.iterator()`, which explicitly
    doesn't cache results client-side, seems to be allowing us to make progress, so
    we'll use that!

--- esp/esp/dbmail/cronmail.py
+++ esp/esp/dbmail/cronmail.py
@@ -101,7 +101,6 @@ def send_email_requests(debug=False):
                                           created_at__gte=one_week_ago,
                                           sent__isnull=True,
                                           tries__lte=retries)
-    mailtxts_list = list(mailtxts)
 
     wait = getattr(settings, 'EMAILTIMEOUT', None)
     if wait is None:
@@ -110,7 +109,7 @@ def send_email_requests(debug=False):
     num_sent = 0
     errors = [] # if any messages failed to deliver
 
-    for mailtxt in mailtxts_list:
+    for mailtxt in mailtxts.iterator():
         exception = mailtxt.send(debug=debug)
         if exception is not None:
             errors.append({'email': mailtxt, 'exception': str(exception)})
