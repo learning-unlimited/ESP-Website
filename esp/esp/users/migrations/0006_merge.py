dd17644bc1b3fe2844a475bd7e21ff8c451ed226
Miriam Gershenson <mgersh@alum.mit.edu>
2016-10-17 00:39:38 -0400

    Add merge migration for oxford

---
+++ esp/esp/users/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0005_auto_20151226_2257'),
+        ('users', '0003_british_address_localization'),
+    ]
+
+    operations = [
+    ]
