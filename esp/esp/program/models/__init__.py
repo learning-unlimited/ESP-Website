377a2feaafeab0963550f90d76a151f0c4cc5347
willgearty@gmail.com <willgearty@gmail.com>
2016-10-24 13:09:17 -0700

    Minor tweaks

--- esp/esp/program/models/__init__.py
+++ esp/esp/program/models/__init__.py
@@ -1882,7 +1882,7 @@ class RegistrationType(models.Model):
 
 class PhaseZeroRecord(models.Model):
     def __unicode__(self):
-        return self.lottery_number
+        return str(self.lottery_number)
 
     user = AjaxForeignKey(ESPUser)
     program = models.ForeignKey(Program, blank=True)
