04c81eb24ecf0c06384dc04fa0fe5c0f71d9e494
Enze Chen <enze.chen1@gmail.com>
2016-10-26 14:06:14 -0700

    Fixes to algorithm

--- esp/esp/program/modules/handlers/studentregphasezeromanage.py
+++ esp/esp/program/modules/handlers/studentregphasezeromanage.py
@@ -35,7 +35,7 @@ Learning Unlimited, Inc.
 from esp.utils.web import render_to_response
 from esp.middleware.threadlocalrequest import get_current_request
 from esp.program.modules.base import ProgramModuleObj, main_call, aux_call, meets_deadline, needs_student, meets_grade, meets_cap, no_auth, needs_admin
-from esp.users.models import Record, ESPUser
+from esp.users.models import Record, ESPUser, Permission
 from esp.program.models import PhaseZeroRecord
 from esp.program.modules.forms.phasezero import LotteryNumberForm, SubmitForm
 
@@ -65,12 +65,12 @@ class StudentRegPhaseZeroManage(ProgramModuleObj):
             group_number = entry.lottery_number
             user = entry.user
             user_grade = entry.user.getGrade(prog)
-            user_id = entry.id
+            lottery_id = entry.id
 
             if group_number not in sibgroups.keys():
-                sibgroups[group_number] = [(user, user_grade, user_id)]
+                sibgroups[group_number] = [(user, user_grade, lottery_id)]
             else:
-                sibgroups[group_number].append((user, user_grade, user_id))
+                sibgroups[group_number].append((user, user_grade, lottery_id))
 
         for number in sibgroups:
             if len(sibgroups[number]) > 4:
@@ -111,7 +111,7 @@ class StudentRegPhaseZeroManage(ProgramModuleObj):
         Permission(permission_type='Student/All', role=winners, start_date=datetime.datetime.now(), program=prog)
         
         # Add tag to indicate student lottery has been run
-        Tag().setTag('student_lottery_run', prog, True)
+        Tag.setTag('student_lottery_run', prog, True)
  
         return
 
