aa63fa47d9f542d21eaf960c48df68666a89687c
Ben Kraft <ben@benkraft.org>
2016-11-23 22:51:39 -0500

    Add berkeley theme config form
    Also had to rename the theme to an underscore to make things import right.  The
    widget is a mess and I made it a little messier instead of fixing it and I'm
    sorry.

--- esp/esp/themes/theme_data/berkeley_alpha/config_form.py
+++ esp/esp/themes/theme_data/berkeley_alpha/config_form.py
@@ -1,8 +1,8 @@
 from django import forms
 
 from esp.themes.forms import ThemeConfigurationForm
-from esp.utils.widgets import NavStructureWidget
+from esp.utils.widgets import NavStructureWidgetWithIcons
 
 class ConfigForm(ThemeConfigurationForm):
     # TODO: modify to support choosing icons
-    nav_structure = forms.Field(widget=NavStructureWidget)
+    nav_structure = forms.Field(widget=NavStructureWidgetWithIcons)
