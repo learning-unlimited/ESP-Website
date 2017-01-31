60768b7d94f239cf4409afcae827e990c0f0d8c0
Jerry Wu <jerrywu9876@gmail.com>
2017-01-31 18:12:18 -0500

    More work on Constraints and fleshing out Models
     - Added CompositeConstraint
     - constraints now extend BaseConstraint
     - Model distinguishes between Timeslots and Events
     - Miscellaneous bugfixes

--- esp/esp/program/controllers/autoscheduler/constraints.py
+++ esp/esp/program/controllers/autoscheduler/constraints.py
@@ -3,6 +3,7 @@
 
 class BaseConstraint:
     """Abstract class for constraints."""
+    default_on = True
 
     def check_schedule(self, schedule):
         """Returns False if an AS_Schedule violates the constraint,
@@ -22,64 +23,91 @@ class BaseConstraint:
         True otherwise."""
         return self.check_schedule(schedule)
 
-    def check_swap_section(self, section1, section2, schedule):
+    def check_swap_sections(self, section1, section2, schedule):
         """Assuming that we start with a valid schedule, returns False
         if swapping two sections will violate the constraint,
         True otherwise."""
         return self.check_schedule(schedule)
 
 
-class ConsistencyConstraint:
+class CompositeConstraint(BaseConstraint):
+    """A constraint which checks all the constraints you actually want."""
+    def __init__(self, constraint_names):
+        """Takes in a list of constraint names, as strings."""
+        self.constraints = []
+        available_constraints = globals()
+        for constraint in constraint_names:
+            self.constraints.append(available_constraints[constraint]())
+
+    def check_schedule(self, schedule):
+        return all(map(lambda c: c.check_schedule(schedule), self.constraints))
+
+    def check_add_section(self, section, room, start_time, schedule):
+        return all(map(
+            lambda c: c.check_add_section(section, room, start_time, schedule),
+            self.constraints))
+
+    def check_remove_section(self, section, schedule):
+        return all(map(lambda c: c.check_remove_section(section, schedule),
+                       self.constraints))
+
+    def check_swap_sections(self, section1, section2, schedule):
+        return all(map(
+            lambda c: c.check_swap_sections(section1, section2, schedule),
+            self.constraints))
+
+
+class ConsistencyConstraint(BaseConstraint):
     """Different parts of the schedule should be consistent.
     For example, if a section thinks it is scheduled in a room,
     the room should think a section is scheduled in it."""
     pass  # TODO
 
 
-class ContiguousConstraint:
+class ContiguousConstraint(BaseConstraint):
     """Multi-hour sections may only be scheduled across
     contiguous timeblocks."""
     pass  # TODO
 
 
-class LunchConstraint:
+class LunchConstraint(BaseConstraint):
     """Multi-hour sections can't be scheduled over both blocks of lunch."""
     pass  # TODO
 
 
-class ResourceConstraint:
+class ResourceConstraint(BaseConstraint):
     """If a section demands an unprovided required resource."""
     pass  # TODO
 
 
-class RestrictedRoomConstraint:
+class RestrictedRoomConstraint(BaseConstraint):
     """If a room demands only sections of a particular type
     (e.g. if only computer classes may be scheduled in computer labs)"""
     pass  # TODO
 
 
-class RoomAvailabilityConstraint:
+class RoomAvailabilityConstraint(BaseConstraint):
     """Sections can only be in rooms which are available."""
     pass  # TODO
 
 
-class RoomConcurrencyConstraint:
+class RoomConcurrencyConstraint(BaseConstraint):
     """Rooms can't be double-booked."""
     pass  # TODO
 
 
-class SectionLengthConstraint:
+class SectionLengthConstraint(BaseConstraint):
     """Sections must be scheduled for exactly their length.
     This also accounts for not scheduling a section twice,
     in conjunction with consistency constraints."""
     pass  # TODO
 
 
-class TeacherAvailabilityConstraint:
+class TeacherAvailabilityConstraint(BaseConstraint):
     """Teachers can only teach during times they are available."""
     pass  # TODO
 
 
-class TeacherConcurrencyConstraint:
+class TeacherConcurrencyConstraint(BaseConstraint):
     """Teachers can't teach two classes at once."""
     pass  # TODO
--- esp/esp/program/controllers/autoscheduler/models.py
+++ esp/esp/program/controllers/autoscheduler/models.py
@@ -15,6 +15,12 @@ class AS_Schedule:
 
         self.program = program
 
+        self.timeslots = \
+            sorted(AS_Timeslot.batch_convert(program.getTimeSlots(), program))
+
+        # Maps from start and end times to a timeslot.
+        self.timeslot_dict = {(t.start, t.end): t for t in self.timeslots}
+
         # List of class sections and dict of teachers by id
         self.class_sections, self.teachers = \
             self.load_sections_and_teachers(exclude_lunch, exclude_walkins,
@@ -22,7 +28,7 @@ class AS_Schedule:
 
         # List of classrooms
         self.classrooms = AS_Classroom.batch_convert(
-                program.groupedClassrooms(), self.program)
+                program.groupedClassrooms(), self.program, self.timeslot_dict)
 
     def load_sections_and_teachers(
             self, exclude_lunch, exclude_walkins, exclude_scheduled):
@@ -51,12 +57,12 @@ class AS_Schedule:
 
         # Return!
         return AS_ClassSection.batch_convert(
-                sections, self.program, teachers), \
+                sections, self.program, teachers, self.timeslot_dict), \
             teachers
 
 
 class AS_ClassSection:
-    def __init__(self, section, program, teachers_dict):
+    def __init__(self, section, program, teachers_dict, timeslot_dict):
         """Create a AS_ClassSection from a ClassSection and Program"""
         assert section.parent_class.parent_program == program
         self.id = section.id
@@ -64,61 +70,68 @@ class AS_ClassSection:
         self.teachers = []
         for teacher in section.teachers:
             if teacher.id not in teachers_dict:
-                teachers_dict[teacher.id] = AS_Teacher(teacher, program)
+                teachers_dict[teacher.id] = AS_Teacher(
+                        teacher, program, timeslot_dict)
             self.teachers.append(teachers_dict[teacher.id])
         self.capacity = section.capacity
         self.resource_requests = AS_ResourceType.batch_convert(
-                section.getResourceRequests(), program)
-        self.viable_times = \
-            AS_Timeblock.batch_convert(section.viable_times(), program)
+                section.getResourceRequests())
+        assert len(section.meeting_times.all()) == 0, "Already-scheduled sections \
+            aren't supported yet"
+        self.assigned_event = None
+        # self.viable_times = \
+        #     AS_Timeslot.batch_convert(section.viable_times(), program)
 
     @staticmethod
-    def batch_convert(sections, program, teachers_dict):
+    def batch_convert(sections, program, teachers_dict, timeslot_dict):
         return map(lambda s: AS_ClassSection(
-            s, program, teachers_dict), sections)
+            s, program, teachers_dict, timeslot_dict), sections)
 
 
 class AS_Teacher:
-    def __init__(self, teacher, program):
+    def __init__(self, teacher, program, timeslot_dict):
         """Create a AS_Teacher from an ESPUser"""
         assert teacher.isTeacher()
         self.id = teacher.id
-        self.availability = AS_Timeblock.batch_convert(
-            teacher.getAvailableTimes(program, ignore_classes=False), program)
+        self.availability = AS_Timeslot.batch_find(
+            teacher.getAvailableTimes(program, ignore_classes=False),
+            timeslot_dict)
         self.is_admin = teacher.isAdministrator()
 
     @staticmethod
-    def batch_convert(teachers, program):
-        return map(lambda t: AS_Teacher(t, program), teachers)
+    def batch_convert(teachers, program, timeslot_dict):
+        return map(lambda t: AS_Teacher(t, program, timeslot_dict), teachers)
 
 
 class AS_Classroom:
-    def __init__(self, classroom, program):
+    def __init__(self, classroom, program, timeslot_dict):
         """Create a AS_Classroom from a grouped Classroom (see
         Program.groupedClassrooms()) and Program"""
         assert classroom.res_type == ResourceType.get_or_create("Classroom")
         self.id = classroom.id
         self.room = classroom.name
         self.availability = \
-            AS_Timeblock.batch_convert(classroom.timeslots, program)
-        self.furnishings = AS_ResourceType.batch_convert(
-               classroom.furnishings, program)
+            AS_Event.batch_convert(classroom.timeslots, self, timeslot_dict)
+        self.furnishings = AS_ResourceType.batch_convert(classroom.furnishings)
 
     @staticmethod
-    def batch_convert(classrooms, program):
-        return map(lambda c: AS_Classroom(c, program), classrooms)
+    def batch_convert(classrooms, program, timeslot_dict):
+        return map(lambda c: AS_Classroom(
+            c, program, timeslot_dict), classrooms)
 
 
 # Ordered by start time, then by end time.
 @total_ordering
-class AS_Timeblock:
+class AS_Timeslot:
+    """A timeslot, not specific to any teacher or class or room."""
     def __init__(self, event, program):
-        """Create an AS_Timeblock from an Event."""
+        """Create an AS_Timeslot from an Event."""
         assert event.parent_program() == program, \
             "Event parent program doesn't match"
         self.start = event.start
         self.end = event.end
-        assert self.start < self.end, "Timeblock doesn't end after start time"
+        assert self.start < self.end, "Timeslot doesn't end after start time"
+        self.associated_events = []  # AS_Events during this timeslot
 
     def __eq__(self, other):
         if type(other) is type(self):
@@ -130,11 +143,12 @@ class AS_Timeblock:
         return (self.start, self.end) < (other.start, other.end)
 
     @staticmethod
-    def overlaps(block1, block2):
-        return (block1.start < block2.end) and (block2.start < block1.end)
+    def overlaps(timeslot1, timeslot2):
+        return (timeslot1.start < timeslot2.end) \
+                and (timeslot2.start < timeslot1.end)
 
     @staticmethod
-    def contiguous(block1, block2):
+    def contiguous(timeslot1, timeslot2):
         """ Returns true if the second argument is less than 20 minutes apart
         from the first one.
 
@@ -142,14 +156,35 @@ class AS_Timeblock:
         """
         tol = timedelta(minutes=20)
 
-        if (block2.start - block2.end) < tol:
+        if (timeslot2.start - timeslot2.end) < tol:
             return True
         else:
             return False
 
     @staticmethod
     def batch_convert(events, program):
-        return map(lambda e: AS_Timeblock(e, program), events)
+        return map(lambda e: AS_Timeslot(e, program), events)
+
+    @staticmethod
+    def batch_find(events, timeslot_dict):
+        """Finds the timeslots in the dict matching the events. Ignores if it
+        doesn't exit."""
+        return [timeslot_dict[(e.start, e.end)] for e in events
+                if (e.start, e.end) in timeslot_dict]
+
+
+class AS_Event:
+    """A specific timeslot where a specific room is available."""
+    def __init__(self, timeslot, room):
+        self.timeslot = timeslot
+        timeslot.associated_events.append(self)
+        self.room = room
+        self.assigned_section = None
+
+    @staticmethod
+    def batch_convert(events, room, timeslot_dict):
+        return map(lambda e: AS_Event(
+            timeslot_dict[(e.start, e.end)], room), events)
 
 
 class AS_ResourceType:
@@ -160,5 +195,6 @@ class AS_ResourceType:
         self.description = restype.description
 
     @staticmethod
-    def batch_convert(restypes, program):
-        return map(lambda r: AS_ResourceType(r, program), restypes)
+    def batch_convert(res):
+        """Converts from ResourceRequests or Resources."""
+        return map(lambda r: AS_ResourceType(r.res_type), res)
