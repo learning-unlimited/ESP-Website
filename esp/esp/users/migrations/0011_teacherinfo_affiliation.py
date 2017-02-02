63978282781bc27331a01f5fcad730714a38ace5
Theodore Hwa <ted@learningu.org>
2017-02-01 22:52:50 -0800

    fix migrations

--- esp/esp/users/migrations/0011_teacherinfo_affiliation.py
+++ esp/esp/users/migrations/0011_teacherinfo_affiliation.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0007_auto_20160709_2038'),
+        ('users', '0010_auto_20170131_0056'),
     ]
 
     operations = [
