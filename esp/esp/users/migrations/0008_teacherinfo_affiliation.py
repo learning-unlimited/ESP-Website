45610df6c90c74eef04d2e0e95250af7ce4fcdb8
Theodore Hwa <ted@learningu.org>
2017-02-01 21:39:08 -0800

    fix migrations

--- esp/esp/users/migrations/0008_teacherinfo_affiliation.py
+++ esp/esp/users/migrations/0008_teacherinfo_affiliation.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='teacherinfo',
             name='affiliation',
-            field=models.CharField(max_length=100, null=True, blank=True),
+            field=models.CharField(max_length=100, blank=True),
         ),
     ]
