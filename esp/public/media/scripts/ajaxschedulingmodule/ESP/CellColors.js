6c55f3f093d68addfd4603d408db560210453006
Brian Chen <brian.chxn@gmail.com>
2017-02-19 16:07:23 -0500

    AJAX scheduler: Add gradient to classes with special scheduling needs
    Add a gradient background to the corners of classes with special
    scheduling needs, as determined by the tags.

--- esp/public/media/scripts/ajaxschedulingmodule/ESP/CellColors.js
+++ esp/public/media/scripts/ajaxschedulingmodule/ESP/CellColors.js
@@ -6,16 +6,32 @@
  * @method textColor(section)
  */
 function CellColors() {
+    this.specialColor = "rgb(255, 153, 0)";
     /**
-     * Returns the background color of the section.
+     * Returns the primary background color of the section, as an array of red,
+     * green, and blue components.
      *
      * @param section: The section to compute the background color of
      */
-    this.color = function(section){
-        return "#" +
-            section.emailcode[2] + "0" +
-            section.emailcode[3] + "0" +
-            section.emailcode [4] + "0";
+    this.color = function(section) {
+        return [17*(+section.emailcode[2]),
+                17*(+section.emailcode[3]),
+                17*(+section.emailcode[4])];
+    };
+    this.background = function(section) {
+        var color = this.color(section);
+        var rgb = ("rgb(" +
+          color[0] + "," +
+          color[1] + "," +
+          color[2] + ")");
+        if (section.flags.indexOf("Special scheduling needs") !== -1) {
+          return ("linear-gradient(to bottom right, " +
+            this.specialColor + " 0%," +
+            rgb + " 50%," +
+            this.specialColor + " 100%)");
+        } else {
+          return rgb;
+        }
     };
 
     /**
@@ -24,7 +40,7 @@ function CellColors() {
      * @param section: The section to compute the background color of
      */
     this.textColor = function(section) {
-        var color = helpers_hex_string_to_color(this.color(section));
+        var color = this.color(section);
 
         // The relative luminance is a measure of how bright the color is.
         // Green counts more because human eyes are more sensitive to it.
