58396cde3fdf8b605a932c6f621da75a669b9641
willgearty@gmail.com <willgearty@gmail.com>
2017-03-03 11:18:18 -0800

    Okay, I really think this will work

--- esp/esp/program/migrations/0011_auto_20170109_1715.py
+++ esp/esp/program/migrations/0011_auto_20170109_1715.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0007_auto_20160709_2038'),
+        ('users', '0009_auto_20170205_2131'),
         ('program', '0010_phasezerorecord'),
     ]
 
