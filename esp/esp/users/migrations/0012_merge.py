8ff16cc7b1924395e49fc8b580d93fcab99cc2d6
Will Gearty <willgearty@gmail.com>
2017-11-24 13:24:42 -0800

    Teacher Big Board (#2396)
    This is my first stab at creating a teacher big board (#1587).
    As of right now, it shows a graph of the following stats:
    
    - Number of registered classes
    - Number of approved classes
    - Number of teachers teaching
    - Number of registered hours of classes (class-hours and class-student-hours) (on separate y-axes)
    
    It also displays the following static stats:
    
    - Number of teachers registering in the last 10 minutes
    - Number of teachers teaching a class
    - Number of classes registered
    - Number of classes approved
    - Number of registered hours of classes (class-hours and class-student-hours)
    - Number of teachers checked in today

---
+++ esp/esp/users/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0011_auto_20161017_1848'),
+        ('users', '0010_auto_20170709_1347'),
+    ]
+
+    operations = [
+    ]
