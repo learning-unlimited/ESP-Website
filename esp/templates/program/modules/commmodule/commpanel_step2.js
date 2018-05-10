06a548b40131982aca1c3421903f9f659afcf0fd
Miles Calabresi <mcalabresi1@gmail.com>
2018-05-09 21:47:05 -0400

    add explanation for 'your email' and move JS to separate script file

---
+++ esp/templates/program/modules/commmodule/commpanel_step2.js
@@ -0,0 +1,38 @@
+
+function showEmailExplanation() {
+    document.getElementById("from-help").style.display = '';
+}
+
+function hideEmailExplanation() {
+    document.getElementById("from-help").style.display = 'none';
+    document.getElementById("from").focus();
+}
+
+
+function validateMsgLang() {
+  var msgTypes = document.getElementsByName("msglang");
+  var containsTag = /<(\/?((.)|(br ?\/?)))>|(<img)/i.test(
+    document.getElementById("emailbody").value);
+  var containsHTag = /<\/?html>/i.test(
+    document.getElementById("emailbody").value);
+
+  if(containsHTag) {
+      return confirm("Didn't we say not to include <html> tags?!? "
+                + "If you're sure you know what you're doing, "
+                + "click 'OK' to continue.");
+  } else if(msgTypes[1].checked && !containsTag) {
+      return confirm('You selected "HTML" but included no HTML tags. '
+                + 'Continuing might squash your formatting. '
+                + 'Would you still like to proceed?');
+  } else if(msgTypes[0].checked && containsTag) {
+      return confirm('You selected "Plain Text" but have HTML tags '
+                + '(such as <p>) in your message. '
+                + 'Continuing will leave your tags in your message '
+                + '(so you might see "<b>hello</b>" instead of '
+                + 'a bold "hello"). '
+                + 'Would you still like to proceed?');
+  }
+  else {
+      return true;
+  }
+}
