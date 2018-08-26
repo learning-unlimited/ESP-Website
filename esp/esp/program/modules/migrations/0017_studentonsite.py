f392f1ae886558b241870044d73a84cce7e09777
willgearty@gmail.com <willgearty@gmail.com>
2018-08-25 22:17:54 -0700

    fixes for travis

--- esp/esp/program/modules/migrations/0017_studentonsite.py
+++ esp/esp/program/modules/migrations/0017_studentonsite.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
- from django.db import migrations, models
+
+from django.db import migrations, models
 import esp.program.modules.base
- class Migration(migrations.Migration):
+
+class Migration(migrations.Migration):
      dependencies = [
         ('modules', '0016_merge'),
     ]
