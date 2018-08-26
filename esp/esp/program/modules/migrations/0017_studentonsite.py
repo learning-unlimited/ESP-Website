34a8b6b9c1cbc2aedf09c5f26cc214e4248471a6
willgearty@gmail.com <willgearty@gmail.com>
2018-08-25 22:11:49 -0700

    meh, they aren't that bad

---
+++ esp/esp/program/modules/migrations/0017_studentonsite.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+ from django.db import migrations, models
+import esp.program.modules.base
+ class Migration(migrations.Migration):
+     dependencies = [
+        ('modules', '0016_merge'),
+    ]
+     operations = [
+       migrations.CreateModel(
+            name='StudentOnsite',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('modules.programmoduleobj', esp.program.modules.base.CoreModule),
+        ),
+    ]
