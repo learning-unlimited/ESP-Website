0e68b237ab27a66dce7a8a437d1cadf4291e846e
willgearty@gmail.com <willgearty@gmail.com>
2018-08-29 12:58:51 -0700

    fix stuff that just broke

--- esp/public/media/scripts/program/modules/resources.js
+++ esp/public/media/scripts/program/modules/resources.js
@@ -19,10 +19,12 @@ $j('.furnishing-formset').formset({
     formCssClass: 'furnishing-dynamic-form',
 });
 
+prog_url = $j('#resourcesscript').data('prog_url');
+
 //Get choices for a furnishing via POST
 function getChoices(furnishing, callback){
     var data = {furnishing: furnishing, csrfmiddlewaretoken: csrf_token()};
-    $j.post('/manage/{{ prog.url }}/ajaxfurnishingchoices', data, "json").success(callback);
+    $j.post('/manage/' + prog_url + '/ajaxfurnishingchoices', data, "json").success(callback);
 }
 
 //Update the option field to reflect whether there are specific choices (dropdown) or not (open response). If a value is supplied, set that value as selected afterwards.
@@ -49,24 +51,23 @@ function update_choices(obj, value) {
 
 //Upon page load and dynamic creation of new selects
 $j.initialize("select", function() {
-    <!--Selects start with (option) or (furnishing) by default, but we want to hide these as options in the dropdown.-->
+    //Selects start with (option) or (furnishing) by default, but we want to hide these as options in the dropdown.
     var opt = $j(this).find("option")[0];
     $j(opt).hide();
     $j(opt).css("display", "none");
     opt.disabled = true;
-    <!--Convert option field to dropdown if necessary-->
+    //Convert option field to dropdown if necessary
     if ($j(this).attr('id').split("-")[2] == "furnishing") {
         var num = $j(this).attr('id').split("-")[1];
-        <!--Make sure to preserve selected value-->
+        //Make sure to preserve selected value
         var value = $j('#id_furnishings-' + num + '-choice').val();
         if (value) {
             update_choices($j(this), value);
         }
     }
-}, { target: $j("#furnishing_formset")[0] });
+}, { target: document.getElementById('furnishing_formset') });
 
 //Whenever a new furnishing is selected, update the respective option field
 $j('.furnishing-dynamic-form select').change(function() {
     update_choices($j(this));
 });
-</script>
