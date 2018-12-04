033c48ae195e460e2d9b3122016223877259d283
Miles Calabresi <mcalabresi1@gmail.com>
2018-12-03 22:07:53 -0500

    fix renewal script location

--- deploy/server_setup/new_site.sh
+++ deploy/server_setup/new_site.sh
@@ -433,7 +433,7 @@ WSGIDaemonProcess $SITENAME processes=2 threads=1 maximum-requests=500 display-n
 EOF
     service apache2 graceful
     # TODO(benkraft): put the renewal script in git too.
-    /lu/certbot/renew_lu.py
+    /lu/scripts/certbot/renew_lu.py
     echo "Added VirtualHost to Apache configuration $APACHE_CONF_FILE"
 
     echo "Apache has been set up, and a new SSL cert has been requested."
