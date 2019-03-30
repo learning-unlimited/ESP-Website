fbc85430ee08ca8d595775a17e5e510b99c6f652
Will Gearty <willgearty@gmail.com>
2019-03-30 12:31:08 -0700

    Fix classesbyFOO and sectionsbyFOO, change options/defaults

--- esp/esp/program/modules/handlers/programprintables.py
+++ esp/esp/program/modules/handlers/programprintables.py
@@ -312,34 +312,38 @@ class ProgramPrintables(ProgramModuleObj):
 
     @needs_admin
     def classesbyFOO(self, request, tl, one, two, module, extra, prog, sort_exp = lambda x,y: cmp(x,y), filt_exp = lambda x: True, split_teachers = False):
-        classes_raw = ClassSubject.objects.filter(parent_program = self.program)
+        classes = ClassSubject.objects.filter(parent_program = self.program)
+        
+        if 'clsids' in request.GET:
+            clsids = [int(clsid) for clsid in request.GET['clsids'].split(",")]
+            classes = [cls for cls in classes if cls.id in clsids]
 
-        classes_filt = [cls for cls in classes_raw if cls.isAccepted()]
+        if 'grade_min' in request.GET:
+            classes = [cls for cls in classes if cls.grade_max >= int(request.GET['grade_min'])]
 
-        if split_teachers:
-            classes = []
-            for cls in classes_filt:
-                for teacher in cls.get_teachers():
-                    cls_split = copy.copy(cls)
-                    cls_split.split_teacher = teacher
-                    classes.append(cls_split)
-        else:
-            classes = classes_filt
+        if 'grade_max' in request.GET:
+            classes = [cls for cls in classes if cls.grade_min <= int(request.GET['grade_max'])]
+
+        if 'accepted' in request.GET:
+            classes = [cls for cls in classes if cls.status > 0]
+        elif 'cancelled' in request.GET:
+            classes = [cls for cls in classes if cls.isCancelled()]
+        elif 'all' not in request.GET:
+            classes = [cls for cls in classes if cls.status >= 0]
+
+        if 'scheduled' in request.GET:
+            classes = [cls for cls in classes if cls.all_meeting_times.count() > 0]
 
         classes = filter(filt_exp, classes)
 
-        if 'grade_min' in request.GET:
-            classes = filter(lambda x: x.grade_max > int(request.GET['grade_min']), classes)
-
-        if 'grade_max' in request.GET:
-            classes = filter(lambda x: x.grade_min < int(request.GET['grade_max']), classes)
-
-        if 'clsids' in request.GET:
-            clsids = request.GET['clsids'].split(',')
-            cls_dict = {}
+        if split_teachers:
+            classes_temp = []
             for cls in classes:
-                cls_dict[str(cls.id)] = cls
-            classes = [cls_dict[clsid] for clsid in clsids]
+                for teacher in cls.get_teachers():
+                    cls_split = copy.copy(cls)
+                    cls_split.split_teacher = teacher
+                    classes_temp.append(cls_split)
+            classes = classes_temp
 
         classes.sort(sort_exp)
 
@@ -354,24 +358,31 @@ class ProgramPrintables(ProgramModuleObj):
         if extra == 'csv':
             template_file = 'sections_list.csv'
 
-        if 'cancelled' in request.GET or (extra and 'cancelled' in extra):
-            sections = filter(lambda z: z.isCancelled(), sections)
-        else:
-            sections = filter(lambda z: (z.isAccepted() and z.meeting_times.count() > 0), sections)
-        sections = filter(filt_exp, sections)
+        if 'secids' in request.GET:
+            secids = [int(secid) for secid in request.GET['secids'].split(",")]
+            sections = [sec for sec in sections if sec.id in secids]
+
+        if 'clsids' in request.GET:
+            clsids = [int(clsid) for clsid in request.GET['clsids'].split(",")]
+            sections = [sec for sec in sections if sec.parent_class.id in clsids]
 
         if 'grade_min' in request.GET:
-            sections = filter(lambda x: (x.parent_class.grade_max > int(request.GET['grade_min'])), sections)
+            sections = [sec for sec in sections if sec.parent_class.grade_max >= int(request.GET['grade_min'])]
 
         if 'grade_max' in request.GET:
-            sections = filter(lambda x: (x.parent_class.grade_min < int(request.GET['grade_max'])), sections)
+            sections = [sec for sec in sections if sec.parent_class.grade_min <= int(request.GET['grade_max'])]
 
-        if 'secids' in request.GET:
-            clsids = request.GET['secids'].split(',')
-            cls_dict = {}
-            for cls in sections:
-                cls_dict[str(cls.id)] = cls
-            sections = [cls_dict[clsid] for clsid in clsids]
+        if 'accepted' in request.GET:
+            sections = [sec for sec in sections if sec.status > 0]
+        elif 'cancelled' in request.GET or (extra and 'cancelled' in extra):
+            sections = [sec for sec in sections if sec.isCancelled()]
+        elif 'all' not in request.GET:
+            sections = [sec for sec in sections if sec.status >= 0]
+
+        if 'scheduled' in request.GET:
+            sections = [sec for sec in sections if sec.meeting_times.count() > 0]
+
+        sections = filter(filt_exp, sections)
 
         sections.sort(sort_exp)
 
