a0e62f195e91c50dbeaa1220f54893708e3c46e1
kkbrum <katherine.brumberg@yale.edu>
2019-04-07 21:53:36 -0400

    Fixes login error when clicking the login button (#2681)
    * No redirect message when user initiates login
    
    * Updates the rest of the themes
    
    * Using GET field to know whether login initiated
    
    * revert old changes
    
    * user and pw error messages
    
    * fixing typo
    
    * Check for POST before checking for username

--- esp/esp/users/views/__init__.py
+++ esp/esp/users/views/__init__.py
@@ -41,6 +41,16 @@ def login_checked(request, *args, **kwargs):
 
     reply = login(request, *args, **kwargs)
 
+    if hasattr(reply, 'context_data'):
+        if not request.GET:
+            reply.context_data['initiated_login'] = True
+        if request.POST and request.POST['username']:
+            #if a user was entered and it's not in the database, the pw must be wrong
+            if ESPUser.objects.filter(username=request.POST['username']).exists():
+                reply.context_data['wrong_pw'] = True
+            else:
+                reply.context_data['wrong_user'] = True
+
     # Check for user forwarders
     if request.user.is_authenticated():
         old_username = request.user.username
