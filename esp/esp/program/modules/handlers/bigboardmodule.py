6fe04ebfbe6ab512ef5347bdb52187a77ccdfff2
Will Gearty <willgearty@gmail.com>
2019-04-13 10:05:45 -0700

    Stats for registered and approved classes (#2641)
    * Calculate hours without cancelled/rejected classes
    
    * Separate registered and approved stats
    
    * Fixes for when there are no [approved] classes
    
    * Fix axis assignment
    
    * Make big board chart more general, reduce code duplication
    
    * Reduce code duplication, remove lunch from class counts, lint fix

--- esp/esp/program/modules/handlers/bigboardmodule.py
+++ esp/esp/program/modules/handlers/bigboardmodule.py
@@ -65,7 +65,11 @@ class BigBoardModule(ProgramModuleObj):
             ("signed up for classes", [(1, time) for time in self.times_classes(prog)]),
         ]
 
-        left_axis_data, start = self.make_graph_data(timess, 10, 10, 25)
+        timess_data, start = self.make_graph_data(timess, 10, 10, 25)
+
+        left_axis_data = [
+            {"axis_name": "#", "series_data": timess_data},
+        ]
 
         context = {
             "type": "Student",
--- esp/esp/program/modules/handlers/teacherbigboardmodule.py
+++ esp/esp/program/modules/handlers/teacherbigboardmodule.py
@@ -7,7 +7,7 @@ from django.db.models.query import Q
 from django.db.models import Count
 
 from argcache import cache_function_for
-from esp.program.models import ClassSubject
+from esp.program.models import ClassSubject, ClassSection
 from esp.program.modules.base import ProgramModuleObj, needs_admin, main_call
 from esp.users.models import Record
 from esp.utils.decorators import cached_module_view
@@ -42,19 +42,26 @@ class TeacherBigBoardModule(ProgramModuleObj):
         # Most of the numbers on the page are rendered from this list, which
         # should consist of pairs (description, number)
         hours_stats = self.static_hours(prog)
+        approved_hours_stats = self.static_hours(prog, True)
         numbers = [
             ("teachers registering in the last 10 minutes",
              self.num_active_users(prog)),
-            ("teachers teaching a class",
+            ("teachers with a class registered",
              self.num_teachers_teaching(prog)),
+            ("teachers with a class approved",
+             self.num_teachers_teaching(prog, True)),
             ("classes registered",
              self.num_class_reg(prog)),
             ("classes approved",
-             self.num_class_app(prog)),
+             self.num_class_reg(prog, True)),
             ("class-hours registered",
              hours_stats[0]),
+            ("class-hours approved",
+             approved_hours_stats[0]),
             ("class-student-hours registered",
              hours_stats[1]),
+            ("class-student-hours approved",
+             approved_hours_stats[1]),
             ("teachers checked in today",
              self.num_checked_in_teachers(prog)),
         ]
@@ -63,20 +70,36 @@ class TeacherBigBoardModule(ProgramModuleObj):
 
         timess = [
             ("number of registered classes", [(1, time) for time in self.reg_classes(prog)]),
-            ("number of approved classes", [(1, time) for time in self.approved_classes(prog)]),
-            ("number of teachers teaching", [(1, time) for time in self.teach_times(prog)]),
+            ("number of approved classes", [(1, time) for time in self.reg_classes(prog, True)]),
+            ("number of teachers registered", [(1, time) for time in self.teach_times(prog)]),
+            ("number of teachers approved", [(1, time) for time in self.teach_times(prog, True)]),
         ]
 
-        left_axis_data, start = BigBoardModule.make_graph_data(timess)
+        timess_data, start = BigBoardModule.make_graph_data(timess)
+
+        left_axis_data = [
+            {"axis_name": "#", "series_data": timess_data},
+        ]
 
         class_hours, student_hours = self.get_hours(prog)
+        class_hours_approved, student_hours_approved = self.get_hours(prog, approved = True)
 
-        hourss = [
-            ("number of class-hours", class_hours),
-            ("number of class-student-hours", student_hours),
+        class_hourss = [
+            ("number of registered class-hours", class_hours),
+            ("number of approved class-hours", class_hours_approved),
         ]
+        class_hourss_data, _ = BigBoardModule.make_graph_data(class_hourss)
 
-        right_axis_data, _ = BigBoardModule.make_graph_data(hourss)
+        student_hourss = [
+            ("number of registered class-student-hours", student_hours),
+            ("number of approved class-student-hours", student_hours_approved),
+        ]
+        student_hourss_data, _ = BigBoardModule.make_graph_data(student_hourss)
+
+        right_axis_data = [
+            {"axis_name": "class-hours", "series_data": class_hourss_data},
+            {"axis_name": "class-student-hours", "series_data": student_hourss_data},
+        ]
 
         context = {
             "type": "Teacher",
@@ -95,10 +118,10 @@ class TeacherBigBoardModule(ProgramModuleObj):
     # time the page refreshes for the same admin, they will get new numbers
 
     @cache_function_for(105)
-    def num_teachers_teaching(self, prog):
+    def num_teachers_teaching(self, prog, approved = False):
         # Querying for SRs and then extracting the users saves us joining the
         # users table.
-        return ClassSubject.objects.filter(parent_program=prog
+        return ClassSubject.objects.filter(self.get_filter(prog, approved)
         ).exclude(category__category__iexact="Lunch"
         ).values_list('teachers').distinct().count()
 
@@ -118,42 +141,27 @@ class TeacherBigBoardModule(ProgramModuleObj):
             time__day=now.day).count()
 
     @cache_function_for(105)
-    def num_class_reg(self, prog):
-        return ClassSubject.objects.filter(parent_program=prog
-        ).exclude(category__category__iexact="Lunch"
-        ).values_list('id').count()
+    def num_class_reg(self, prog, approved = False):
+        return ClassSubject.objects.filter(self.get_filter(prog, approved)
+        ).exclude(category__category__iexact="Lunch").distinct().count()
 
     @cache_function_for(105)
-    def num_class_app(self, prog):
-        return ClassSubject.objects.filter(parent_program=prog, status__gt=0, sections__status__gt=0
+    def reg_classes(self, prog, approved = False):
+        class_times = ClassSubject.objects.filter(self.get_filter(prog, approved)
         ).exclude(category__category__iexact="Lunch"
-        ).distinct().count()
-
-    @cache_function_for(105)
-    def reg_classes(self, prog):
-        class_times = ClassSubject.objects.filter(parent_program=prog
-        ).exclude(category__category__iexact="Lunch"
-        ).values_list('timestamp', flat=True)
+        ).distinct().values_list('timestamp', flat=True)
         return sorted(class_times)
 
     @cache_function_for(105)
-    def approved_classes(self, prog):
-        #all ClassSubjects that are approved (and have an approved section)
-        class_times = ClassSubject.objects.filter(parent_program=prog, status__gt=0, sections__status__gt=0
+    def teach_times(self, prog, approved = True):
+        teacher_times = dict(ClassSubject.objects.filter(self.get_filter(prog, approved)
         ).exclude(category__category__iexact="Lunch"
-        ).values_list('timestamp', flat=True).distinct()
-        return sorted(class_times)
-
-    @cache_function_for(105)
-    def teach_times(self, prog):
-        teacher_times = dict(ClassSubject.objects.filter(parent_program=prog
-        ).exclude(category__category__iexact="Lunch"
-        ).values_list('teachers').annotate(Min('timestamp')))
+        ).distinct().values_list('teachers').annotate(Min('timestamp')))
         return sorted(teacher_times.itervalues())
 
     @cache_function_for(105)
-    def get_hours(self, prog):
-        hours = ClassSubject.objects.filter(parent_program=prog
+    def get_hours(self, prog, approved = False):
+        hours = ClassSubject.objects.filter(self.get_filter(prog, approved)
         ).annotate(num_sections=Count('sections')).filter(num_sections__gt=0
         ).exclude(category__category__iexact="Lunch"
         ).values_list('timestamp','class_size_max'
@@ -164,9 +172,19 @@ class TeacherBigBoardModule(ProgramModuleObj):
         return class_hours, student_hours
 
     @cache_function_for(105)
-    def static_hours(self, prog):
-        hours = self.get_hours(prog)
-        return [sum(zip(*j)[0]) for j in hours]
+    def static_hours(self, prog, approved = False):
+        hours = self.get_hours(prog, approved)
+        if hours[0]:
+            return [sum(zip(*j)[0]) for j in hours]
+        else:
+            return [0,0]
+
+    @staticmethod
+    def get_filter(prog, approved):
+        filt = Q(parent_program=prog)
+        if approved:
+            filt &= Q(status__gt=0, sections__status__gt=0)
+        return filt
 
     # runs in 9ms, so don't bother caching
     def load_averages(self):
