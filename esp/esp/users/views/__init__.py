f28677de0314afd1c84e748cefa558316e37f7f9
Will Gearty <willgearty@gmail.com>
2019-09-06 17:03:57 -0500

    Switch order of redirect options

--- esp/esp/users/views/__init__.py
+++ esp/esp/users/views/__init__.py
@@ -76,10 +76,10 @@ def login_checked(request, *args, **kwargs):
         # We're getting redirected to somewhere undesirable.
         # Let's try to do something smarter.
         admin_home_url = Tag.getTag('admin_home_page')
-        if request.user.isTeacher():
-            reply = HttpMetaRedirect("/teach/index.html")
-        elif request.user.isAdmin() and admin_home_url:
+        if request.user.isAdmin() and admin_home_url:
             reply = HttpMetaRedirect(admin_home_url)
+        elif request.user.isTeacher():
+            reply = HttpMetaRedirect("/teach/index.html")
         else:
             reply = HttpMetaRedirect("/learn/index.html")
     elif reply.status_code == 302:
