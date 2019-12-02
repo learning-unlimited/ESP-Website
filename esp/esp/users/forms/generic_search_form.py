785a1c86d545f9def737ee977a4c84886ca6a77d
Will Gearty <willgearty@gmail.com>
2019-12-02 11:41:14 -0600

    Add student and teacher search forms

--- esp/esp/users/forms/generic_search_form.py
+++ esp/esp/users/forms/generic_search_form.py
@@ -5,3 +5,11 @@ from esp.users.models import ESPUser
 class GenericSearchForm(forms.Form):
     target_user = AjaxForeignKeyNewformField(key_type=ESPUser, field_name='target_user', label='Target User',
         help_text='Select a user.')
+
+class TeacherSearchForm(forms.Form):
+    target_user = AjaxForeignKeyNewformField(key_type=ESPUser, field_name='target_user', label='Target Teacher',
+        help_text='Select a teacher.', ajax_func='ajax_autocomplete_teacher')
+
+class StudentSearchForm(forms.Form):
+    target_user = AjaxForeignKeyNewformField(key_type=ESPUser, field_name='target_user', label='Target Student',
+        help_text='Select a student.', ajax_func='ajax_autocomplete_student')
