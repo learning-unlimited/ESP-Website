bcf604dde429c8673e95a55daf9ae0bb40a84039
Will Gearty <willgearty@gmail.com>
2019-12-10 19:15:42 -0600

    Fix template tag

--- esp/esp/users/templatetags/users.py
+++ esp/esp/users/templatetags/users.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.filter
 def getGradeForProg(user, prog_id):
     """Return the grade of the user for the specified program id."""
-    if program is None:
+    if prog_id is None:
         return user.getGrade()
     else:
         return user.getGrade(Program.objects.get(id=int(prog_id)))
