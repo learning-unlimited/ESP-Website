5115c753b439f3f08a1e72589ea905dcf2bcac7b
Katherine <katherine.brumberg@yale.edu>
2020-01-17 16:45:29 -0500

    Student acknowledgement module (#2896)
    * Added student acknowledgement module
    
    * Add Yale's code of conduct and move checkbox
    
    * Add table alignment and make acknowledgement required by default

--- esp/esp/program/modules/handlers/teacheracknowledgementmodule.py
+++ esp/esp/program/modules/handlers/teacheracknowledgementmodule.py
@@ -26,7 +26,7 @@ class TeacherAcknowledgementModule(ProgramModuleObj):
             "admin_title": "Teacher Acknowledgement",
             "link_title": "Teacher Acknowledgement",
             "module_type": "teach",
-            "required": False,
+            "required": True,
         }
 
     def isCompleted(self):
