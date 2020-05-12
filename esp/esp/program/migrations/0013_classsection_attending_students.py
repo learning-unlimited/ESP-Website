23e3dc2e55fca3663575e30fd076999ab368580a
Katherine <katherine.brumberg@yale.edu>
2020-05-12 10:05:04 -0400

    Change dependency and name of new migration

--- esp/esp/program/migrations/0013_classsection_attending_students.py
+++ esp/esp/program/migrations/0013_classsection_attending_students.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('program', '0013_auto_20200502_1312'),
+        ('program', '0012_remove_phasezerorecord_lottery_number'),
     ]
 
     operations = [
