bec84381bd087552e9262e466f1e3ea3b9dc875d
Miles Calabresi <mcalabresi1@gmail.com>
2020-06-07 13:58:46 -0400

    put import back in the right migration

--- esp/esp/program/migrations/0013_auto_20200217_2130.py
+++ esp/esp/program/migrations/0013_auto_20200217_2130.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations
+from django.db import migrations, models
 import django.core.validators
 
 def set_my_defaults(apps, schema_editor):
