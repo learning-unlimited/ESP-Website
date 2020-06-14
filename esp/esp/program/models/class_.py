c36ef5794f902a62a35a1d5506303f6c52f58255
Will Gearty <willgearty@gmail.com>
2020-06-14 11:22:58 -0500

    Fix and unify section capacity calculation (#2998)
    * Use all classrooms to calculate capacity
    
    * Use capacity property for onsite class changes
    
    * Fix room capacity calculation

--- esp/esp/program/models/class_.py
+++ esp/esp/program/models/class_.py
@@ -389,12 +389,12 @@ class ClassSection(models.Model):
     category = property(_get_category)
 
     def _get_room_capacity(self, rooms = None):
+        # rooms should be a queryset
         if rooms == None:
-            rooms = self.initial_rooms()
+            rooms = self.classrooms()
 
-        rc = 0
-        for r in rooms:
-            rc += r.num_students
+        # Take the summed classroom capacity for each timeblock, then take the minimum of those sums
+        rc = min(d.get('capacity', 0) for d in rooms.values('event').order_by('event').annotate(capacity=Sum('num_students')))
 
         options = self.parent_program.studentclassregmoduleinfo
         if options.apply_multiplier_to_room_cap:
@@ -405,7 +405,7 @@ class ClassSection(models.Model):
     @cache_function
     def _get_capacity(self, ignore_changes=False):
         ans = None
-        rooms = self.initial_rooms()
+        rooms = self.classrooms()
         if self.max_class_capacity is not None:
             ans = self.max_class_capacity
         else:
