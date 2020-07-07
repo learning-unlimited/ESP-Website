d22e28ff646d1f1b79b4247bd4ad8353e3453245
Will Gearty <willgearty@gmail.com>
2020-07-06 21:01:33 -0500

    Only apply to existing program modules

--- esp/esp/program/modules/migrations/0022_change_teacheronsite_seq.py
+++ esp/esp/program/modules/migrations/0022_change_teacheronsite_seq.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import migrations
 
 def set_my_defaults(apps, schema_editor):
     ProgramModule = apps.get_model('program', 'ProgramModule')
-    pm = ProgramModule.objects.get_or_create(handler="TeacherOnsite")[0]
-    pm.seq = 9999
-    pm.save()
-    ProgramModuleObj = apps.get_model('modules', 'ProgramModuleObj')
-    for pmo in ProgramModuleObj.objects.filter(module=pm):
-        pmo.seq = 9999
-        pmo.save()
+    for pm in ProgramModule.objects.filter(handler="TeacherOnsite"):
+        pm.seq = 9999
+        pm.save()
+        ProgramModuleObj = apps.get_model('modules', 'ProgramModuleObj')
+        for pmo in ProgramModuleObj.objects.filter(module=pm):
+            pmo.seq = 9999
+            pmo.save()
 
 def reverse_func(apps, schema_editor):
     ProgramModule = apps.get_model('program', 'ProgramModule')
-    pm = ProgramModule.objects.get_or_create(handler="TeacherOnsite")[0]
-    pm.seq = -9999
-    pm.save()
-    ProgramModuleObj = apps.get_model('modules', 'ProgramModuleObj')
-    for pmo in ProgramModuleObj.objects.filter(module=pm):
-        pmo.seq = -9999
-        pmo.save()
+    for pm in ProgramModule.objects.filter(handler="TeacherOnsite"):
+        pm.seq = -9999
+        pm.save()
+        ProgramModuleObj = apps.get_model('modules', 'ProgramModuleObj')
+        for pmo in ProgramModuleObj.objects.filter(module=pm):
+            pmo.seq = -9999
+            pmo.save()
 
 class Migration(migrations.Migration):
 
