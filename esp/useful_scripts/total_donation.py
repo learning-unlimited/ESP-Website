fae3fef1b73d259255e4b14033a3ffb28ba8ef8d
Miles Calabresi <mcalabresi1@gmail.com>
2020-07-13 21:59:48 -0400

    lint fixes

--- esp/useful_scripts/total_donation.py
+++ esp/useful_scripts/total_donation.py
@@ -1,15 +1,12 @@
-
 from esp.accounting.models import Transfer
-
 from esp.accounting.controllers import ProgramAccountingController, IndividualAccountingController
-
 from esp.program.models import Program
 
 program = Program.objects.get(url='Splash/2017_Fall')
 pac = ProgramAccountingController(program)
 transfers = pac.all_transfers()
 
-total = 0 
+total = 0
 for t in transfers:
     if t.line_item_id==342:
         total=total+t.amount
