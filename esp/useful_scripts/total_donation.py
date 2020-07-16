9905224bf66da8ea933cce5260808a4d504d7612
Miles Calabresi <mcalabresi1@gmail.com>
2020-07-15 22:54:42 -0400

    add some flexibility

--- esp/useful_scripts/total_donation.py
+++ esp/useful_scripts/total_donation.py
@@ -1,13 +1,18 @@
 from esp.accounting.controllers import ProgramAccountingController
 from esp.program.models import Program
 
-program = Program.objects.get(url='Splash/2017_Fall')
-pac = ProgramAccountingController(program)
-transfers = pac.all_transfers()
 
-total = 0
-for t in transfers:
-    if t.line_item_id==342:
-        total=total+t.amount
+def get_donation_total(prog_name):
+    prog = Program.objects.get(url=prog_name)
+    pac = ProgramAccountingController(prog)
+    transfers = pac.all_transfers()
+    total = 0
+    for t in transfers:
+        if 'donation' in str(t.line_item).lower():
+            total += t.amount
+    return total
 
-print total
+if __name__ == '__main__':
+    name = raw_input("Enter program URL (e.g. 'Splash/2020_Fall' without quotes): ")
+    print('Your total is ${0:.2f}.'.format(get_donation_total(name.strip())))
+
