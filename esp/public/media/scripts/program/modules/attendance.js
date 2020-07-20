418fe0521b5ac21121d1856212835b959e99f93c
Will Gearty <willgearty@gmail.com>
2020-07-20 10:17:53 -0500

    Update checked in status and sortable custom keys

--- esp/public/media/scripts/program/modules/attendance.js
+++ esp/public/media/scripts/program/modules/attendance.js
@@ -29,7 +29,7 @@ Quagga.onDetected(function(result) {
 prog_url = $j('#attendancescript').data('prog_url');
 
 $j(function(){
-    function markAttendance(username, secid, callback, undo = false, errorCallback){
+    function markAttendance(username, secid, undo = false, callback, errorCallback){
         refresh_csrf_cookie();
         var data = {student: username, secid: secid, undo: undo, csrfmiddlewaretoken: csrf_token()};
         $j.post('/teach/' + prog_url + '/ajaxstudentattendance', data, "json").success(callback)
@@ -46,16 +46,18 @@ $j(function(){
         var $me = $j(this);
         var username = $me.data("username");
         var secid = $me.data("secid");
-        var $msg = $me.parents("td").children(".msg");
-        var $checkedin = $me.parents("tr").children("[name=checkedin]");
+        var $msg = $me.closest("td").find(".msg");
+        var $checkedin = $me.closest("tr").find("[name=checkedin]");
         $me.prop('disabled', true);
-        markAttendance(username, secid, function(response) {
+        markAttendance(username, secid, !checked, function(response) {
+            $me.closest("td").attr("sorttable_customkey", (checked) ? 2 : 1);
+            if (response.checkedin) {
+                $checkedin.prop("checked", true);
+                $checkedin.closest("td").attr("sorttable_customkey", 2);
+            }
             $msg.text(response.message);
             $me.prop('disabled', false);
-            console.log(response.checkedin);
-            if (response.checkedin)
-                $checkedin.prop("checked", true);
-        }, !checked);
+        });
         $msg.text('Updating attendance...');
     });
 });
