b047932b61f4609ef576c13eb53e76a20ba35de7
Will Gearty <willgearty@gmail.com>
2020-07-31 12:35:49 -0500

    travis fix

--- esp/esp/program/modules/migrations/0023_auto_20200730_2006.py
+++ esp/esp/program/modules/migrations/0023_auto_20200730_2006.py
@@ -6,26 +6,31 @@ from django.db import migrations
 def set_my_defaults(apps, schema_editor):
     ProgramModule = apps.get_model('program', 'ProgramModule')
     old_pms = ProgramModule.objects.filter(handler="SurveyModule")
-    for pm in old_pms:
-        if pm.module_type == "teach":
-            pm.handler = "TeacherSurveyModule"
-            pm.link_title = "Teacher Surveys"
-        else:
-            pm.handler = "StudentSurveyModule"
-            pm.link_title = "Student Surveys"
-        pm.seq = 9999
-        pm.save()
-    new_pm1 = ProgramModule.objects.get(handler="StudentSurveyModule")
-    new_pm2 = ProgramModule.objects.get(handler="TeacherSurveyModule")
-    ProgramModuleObj = apps.get_model('modules', 'ProgramModuleObj')
-    for pmo in ProgramModuleObj.objects.filter(module__in=[new_pm1, new_pm2]):
-        pmo.seq = 9999
-        pmo.save()
+    if old_pms.exists():
+        for pm in old_pms:
+            if pm.module_type == "teach":
+                pm.handler = "TeacherSurveyModule"
+                pm.link_title = "Teacher Surveys"
+            else:
+                pm.handler = "StudentSurveyModule"
+                pm.link_title = "Student Surveys"
+            pm.seq = 9999
+            pm.save()
+        new_pm1 = ProgramModule.objects.get(handler="StudentSurveyModule")
+        new_pm2 = ProgramModule.objects.get(handler="TeacherSurveyModule")
+        ProgramModuleObj = apps.get_model('modules', 'ProgramModuleObj')
+        for pmo in ProgramModuleObj.objects.filter(module__in=[new_pm1, new_pm2]):
+            pmo.seq = 9999
+            pmo.save()
 
 def reverse_func(apps, schema_editor):
     ProgramModule = apps.get_model('program', 'ProgramModule')
-    new_pm1 = ProgramModule.objects.get(handler="StudentSurveyModule")
-    new_pm2 = ProgramModule.objects.get(handler="TeacherSurveyModule")
+    pms = ProgramModule.objects.filter(handler="StudentSurveyModule")
+    if pms.count() == 1:
+        new_pm1 = pms[0] if pms.count() == 1 else None
+    pms = ProgramModule.objects.filter(handler="TeacherSurveyModule")
+    if pms.count() == 1:
+        new_pm2 = pms[0] if pms.count() == 1 else None
     for pm in [new_pm1, new_pm2]:
         pm.handler = "SurveyModule"
         pm.link_title = "Surveys"
