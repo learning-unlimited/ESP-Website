1f85606fcd01a518a7aa3af4e7b9061fed531574
Will Gearty <willgearty@gmail.com>
2020-08-28 08:52:04 -0500

    lint fix

--- esp/esp/middleware/esperrormiddleware.py
+++ esp/esp/middleware/esperrormiddleware.py
@@ -189,20 +189,20 @@ class ESPErrorMiddleware(object):
         exc_info = sys.exc_info()
         logger.log(log_level, exc_info[1], exc_info=exc_info)
         context = {'error': exc_info[1]}
-        try:	
-            # attempt to set up variables the template needs	
-            # - actually, some things will fail to be set up due to our	
-            #   silly render_to_response hack, but hopefully that will all	
-            #   just silently fail...	
-            # - alternatively, we could, I dunno, NOT GET RID OF THE SAFE	
-            #   TEMPLATE in main?	
+        try:
+            # attempt to set up variables the template needs
+            # - actually, some things will fail to be set up due to our
+            #   silly render_to_response hack, but hopefully that will all
+            #   just silently fail...
+            # - alternatively, we could, I dunno, NOT GET RID OF THE SAFE
+            #   TEMPLATE in main?
             context = RequestContext(request, context).flatten()
-        except:	
-            # well, we couldn't, but at least display something	
-            # (actually it will immediately fail on main because someone	
-            # removed the safe version of the template and	
-            # miniblog_for_user doesn't silently fail but best not to put	
-            # in ugly hacks and make random variables just happen to work.)	
+        except:
+            # well, we couldn't, but at least display something
+            # (actually it will immediately fail on main because someone
+            # removed the safe version of the template and
+            # miniblog_for_user doesn't silently fail but best not to put
+            # in ugly hacks and make random variables just happen to work.)
             pass
         # TODO(benkraft): merge our various error templates (403, 500, error).
         # They're all slightly different, but should probably be more similar
