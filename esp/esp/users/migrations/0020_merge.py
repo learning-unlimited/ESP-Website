f540861ef3f5c35219882b5bd45ddef9355bde09
Jeffery Yu <jeffery@mit.edu>
2020-10-10 21:50:32 -0400

    Merging prod and stable migrations

---
+++ esp/esp/users/migrations/0020_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0018_auto_20200531_1357'),
+        ('users', '0019_auto_20200705_2349'),
+    ]
+
+    operations = [
+    ]
