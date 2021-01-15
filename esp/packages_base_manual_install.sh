f54262bec4dfea49afeb158ea2f6aff0328e9a84
Will Gearty <willgearty@gmail.com>
2021-01-15 09:28:29 -0600

    try nodejs legacy?

--- esp/packages_base_manual_install.sh
+++ esp/packages_base_manual_install.sh
@@ -3,9 +3,10 @@
 # This script will install the package dependencies for this website install
 # that cannot be installed via apt-get.
 
-sudo apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#sudo apt-get install -y curl
+#curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+sudo apt-get install nodejs-legacy
 
 if [[ ":$PATH:" == *":/usr/bin:"* ]]
 then
