7ee78d51fec55e3f677d072ba3b3f0d733596500
Will Gearty <willgearty@gmail.com>
2021-01-15 10:23:12 -0600

    even newer version of less?

--- esp/packages_base_manual_install.sh
+++ esp/packages_base_manual_install.sh
@@ -4,8 +4,6 @@
 # that cannot be installed via apt-get.
 
 sudo apt-get install -y curl
-node -v
-which node
 curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 node -v
@@ -14,8 +12,11 @@ which node
 if [[ ":$PATH:" == *":/usr/bin:"* ]]
 then
     # explicitly pass --prefix /usr to npm
-    sudo -H npm install --prefix /usr less@1.4 -g
+    sudo -H npm install --prefix /usr less@2 -g
 else
     # no /usr/bin? hopefully this doesn't happen, let npm guess
     sudo -H npm install less@1.3.1 -g
 fi
+
+lessc -v
+which lessc
