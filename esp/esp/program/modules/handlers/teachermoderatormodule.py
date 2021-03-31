e759864f6af9f3d56affb440b155b00915ffe628
Will Gearty <willgearty@gmail.com>
2021-03-31 12:18:23 -0500

    Add missing import

--- esp/esp/program/modules/handlers/teachermoderatormodule.py
+++ esp/esp/program/modules/handlers/teachermoderatormodule.py
@@ -1,6 +1,7 @@
 from esp.program.models import ModeratorRecord
 from esp.program.modules.base import ProgramModuleObj, needs_teacher, main_call, meets_deadline
 from esp.program.modules.forms.moderate import ModeratorForm
+from esp.users.models import ESPUser
 from esp.utils.web import render_to_response
 from django.db.models.query import Q
 from esp.middleware.threadlocalrequest import get_current_request
