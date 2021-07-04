325f5017e958a3506ef4554632affde9faf5607a
Will Gearty <willgearty@gmail.com>
2021-07-04 19:12:54 -0400

    fix comm panel

--- esp/esp/program/modules/handlers/commmodule.py
+++ esp/esp/program/modules/handlers/commmodule.py
@@ -112,16 +112,9 @@ class CommModule(ProgramModuleObj):
         if '<html>' not in body:
             body = '<html>' + body + '</html>'
 
-<<<<<<< HEAD
-        htmlbody = unicode(loader.get_template('email/default_email_html.txt').render({'msgbdy': body,
-                     'user': ActionHandler(firstuser, firstuser),
-                     'program': ActionHandler(self.program, firstuser)}))
-=======
         htmlbody = unicode(loader.get_template('email/default_email_html.txt').render(DjangoContext({'msgbdy': body,
                      'user': ActionHandler(firstuser, firstuser),
                      'program': ActionHandler(self.program, firstuser)})))
->>>>>>> ae9356e31... resolve Yale customization merge conflicts
-
         contextdict = {'user'   : ActionHandler(firstuser, firstuser),
                        'program': ActionHandler(self.program, firstuser),
                        'request': ActionHandler(MessageRequest(), firstuser)}
@@ -199,10 +192,10 @@ class CommModule(ProgramModuleObj):
                                                       sender     = fromemail,
                                                       creator    = request.user,
                                                       public = public_view,
-                                                      msgtext = unicode(loader.get_template('email/default_email_html.txt').render(DjangoContext(
+                                                      msgtext = unicode(loader.get_template('email/default_email_html.txt').render(
                                                                    {'msgbdy': body,
                                                                     'user': request.user,
-                                                                    'program': self.program }))),
+                                                                    'program': self.program })),
                                                       special_headers_dict
                                                                  = { 'Reply-To': replytoemail, }, )
 
