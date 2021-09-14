1dbdff7a1692fdd32e5e31712e97b6104db95c6c
willgearty@gmail.com <willgearty@gmail.com>
2021-09-14 13:52:59 -0500

    wrap dict.keys() with list()

--- esp/esp/themes/controllers.py
+++ esp/esp/themes/controllers.py
@@ -513,7 +513,7 @@ class ThemeController(object):
             palette = self.get_palette()['custom']
 
         vars_orig = self.find_less_variables(theme_name, flat=True)
-        keys = copy.copy(vars.keys())
+        keys = copy.copy(list(vars.keys()))
         for key in keys:
             if key not in vars_orig:
                 del vars[key]
