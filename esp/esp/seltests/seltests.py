c928c4cdb1064577c17943bc6ad9cf0a8bae0395
Will Gearty <willgearty@gmail.com>
2021-11-30 19:54:04 -0600

    Fix csrf selenium test

--- esp/esp/seltests/seltests.py
+++ esp/esp/seltests/seltests.py
@@ -1,9 +1,10 @@
-from django_selenium.testcases import SeleniumTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
 from esp.seltests.util import try_normal_login, logout
 from esp.users.models import ESPUser
 from esp.utils.models import TemplateOverride
 
-class CsrfTestCase(SeleniumTestCase):
+class CsrfTestCase(StaticLiveServerTestCase):
     def setUp(self):
         super(CsrfTestCase, self).setUp()
         user, created = ESPUser.objects.get_or_create(username='student', first_name='Student', last_name='Student')
@@ -16,8 +17,13 @@ class CsrfTestCase(SeleniumTestCase):
         else:
              to = TemplateOverride.objects.filter(name='index.html')[0]
              self.good_version = to.next_version() - 1
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        self.selenium = webdriver.Firefox(firefox_options=options)
+        self.selenium.implicitly_wait(10)
 
     def tearDown(self):
+        self.selenium.quit()
         super(CsrfTestCase, self).tearDown()
         if (self.good_version > 1):
             # Tear down the template override for consistent behavior
@@ -44,16 +50,18 @@ class CsrfTestCase(SeleniumTestCase):
     def test_csrf_delete(self):
         # Now set up and test normal login
         self.setUpNormalLogin()
-        self.open_url("/") # Load index
+        self.selenium.get('%s%s' % (self.live_server_url, '/')) # Load index
 
-        try_normal_login(self, "student", "student")
-        self.assertTrue(self.is_text_present('Student Student'))
-        logout(self)
+        try_normal_login(self.selenium, self.live_server_url, "student", "student")
+        self.assertTrue(self.selenium.find_element_by_class_name('logged_in').is_displayed())
+        self.assertTrue(self.selenium.find_element_by_id('user_first_name').text == "Student")
+        self.assertTrue(self.selenium.find_element_by_id('user_last_name').text == "Student")
+        logout(self.selenium, self.live_server_url)
 
-        self.delete_cookie("esp_csrftoken")
+        self.selenium.delete_cookie("esp_csrftoken")
 
-        try_normal_login(self, "student", "student")
-        self.assertTrue(self.is_text_present('Student Student'))
-        logout(self)
-
-        self.close()
+        try_normal_login(self.selenium, self.live_server_url, "student", "student")
+        self.assertTrue(self.selenium.find_element_by_class_name('logged_in').is_displayed())
+        self.assertTrue(self.selenium.find_element_by_id('user_first_name').text == "Student")
+        self.assertTrue(self.selenium.find_element_by_id('user_last_name').text == "Student")
+        logout(self.selenium, self.live_server_url)
