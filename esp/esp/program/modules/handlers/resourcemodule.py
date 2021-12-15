40400eadf979ef4fcd3beaeeab6ff82f83edc82e
Will Gearty <willgearty@gmail.com>
2021-12-14 20:09:05 -0600

    Move resources step to top of the checklist

--- esp/esp/program/modules/handlers/resourcemodule.py
+++ esp/esp/program/modules/handlers/resourcemodule.py
@@ -68,7 +68,7 @@ class ResourceModule(ProgramModuleObj):
             "admin_title": "Resource Management",
             "link_title": "Manage Times and Rooms",
             "module_type": "manage",
-            "seq": 10,
+            "seq": -99999,
             "choosable": 1,
             }
 
