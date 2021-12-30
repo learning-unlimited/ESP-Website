c7d13dd34e42916a7693cfc8e8c757ea3ae1a7ee
Will Gearty <willgearty@gmail.com>
2021-12-30 11:47:10 -0500

    Prevent non-admins from accessing manage QSD pages

--- esp/esp/qsd/views.py
+++ esp/esp/qsd/views.py
@@ -79,6 +79,8 @@ def qsd(request, url):
 
     # Detect edit authorizations
     have_read = True
+    if url_parts[0] == 'manage' and not request.user.isAdministrator():
+        have_read = False
 
     if not have_read and action == 'read':
         raise Http403, "You do not have permission to access this page."
@@ -122,8 +124,6 @@ def qsd(request, url):
 
     # Detect the standard read verb
     if action == 'read':
-        if not have_read:
-            raise Http403, 'You do not have permission to read this page.'
 
         # Render response
         response = render_to_response('qsd/qsd.html', request, {
