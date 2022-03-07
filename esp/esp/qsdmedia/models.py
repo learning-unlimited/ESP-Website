12710a3a4e8174facc3904c29e3f27946e613c51
Will Gearty <willgearty@gmail.com>
2022-03-07 10:49:03 -0600

    Fix handout download links

--- esp/esp/qsdmedia/models.py
+++ esp/esp/qsdmedia/models.py
@@ -96,6 +96,11 @@ class Media(models.Model):
 
         self.target_file.save(self.hashed_name, file)
 
+    # returns a download path for this file
+    def get_download_path(self):
+        return "/download/" + self.hashed_name + "/" + self.file_name
+    download_path = property(get_download_path)
+
     # returns an absolute path to this file
     def get_uploaded_filename(self):
         return os.path.join(settings.MEDIA_ROOT, "..", self.target_file.url.lstrip('/'))
