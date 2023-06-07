c42786b7351fb54223eecdd911cfc4e8be847743
William Gearty <willgearty@gmail.com>
2023-06-07 11:10:05 +1000

    Remove deleteclass for teachers (#3651)
    * Remove deleteclass for teachers
    
    * remove test call

--- esp/esp/users/models/__init__.py
+++ esp/esp/users/models/__init__.py
@@ -2434,7 +2434,6 @@ class Permission(ExpirableModel):
             ("Teacher/Quiz", "Teacher quiz"),
             ("Teacher/Catalog", "Catalog"),
             ("Teacher/Classes/All", "All classes deadlines"),
-            ("Teacher/Classes", "Classes"),
             ("Teacher/Classes/View", "View registered classes"),
             ("Teacher/Classes/Edit", "Edit registered classes"),
             ("Teacher/Classes/CancelReq", "Request class cancellation"),
