a031f61a896a4df13c7f7c37198bbbd0b67d6e14
Miles Calabresi <mcalabresi1@gmail.com>
2023-10-05 21:54:40 -0400

    fix partition name in one more spot

--- fabfile.py
+++ fabfile.py
@@ -105,7 +105,7 @@ def setup():
     # Name of the encrypted volume group in the Vagrant VM based on which VM is loaded
     ubuntu_version = run("lsb_release -r | awk '{print $2}'")
     try:
-        env.encvg = {"22.04": "ubuntu--vg-vgvagrant--keep_1",
+        env.encvg = {"22.04": "ubuntu--vg-keep_1",
                      "20.04": "vgvagrant-keep_1",
                      "12.04": "ubuntu--12--vg-keep_1"}[ubuntu_version]
     except KeyError:
