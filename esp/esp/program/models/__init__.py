4b5cfd34ae0d36e1f82886a41e16d7db2b37a98c
Katherine <katherine.kkbrum@gmail.com>
2023-11-30 21:32:05 -0500

    Fix cats and flags links on program settings page (#3691)
    * Fix cats and flags links on program settings page
    
    * Put extra-s back in
    
    ---------
    
    Co-authored-by: William Gearty <willgearty@gmail.com>

--- esp/esp/program/models/__init__.py
+++ esp/esp/program/models/__init__.py
@@ -260,11 +260,11 @@ class Program(models.Model, CustomFormsLinkModel):
                          'the documentation</a> for details.')
     class_categories = models.ManyToManyField('ClassCategories',
                                               blank=True,
-                                              help_text='You can add new categories or modify existing ones <a href="/manage/categoriesandflags/categories">here</a>.')
+                                              help_text='You can add new categories or modify existing ones <a href="/manage/catsflagsrecs/categories">here</a>.')
 
     flag_types = models.ManyToManyField('ClassFlagType',
                     blank=True,
-                    help_text='The set of flags that can be used to tag classes for this program. You can add and modify flag types <a href="/manage/categoriesandflags/flagtypes">here</a>.')
+                    help_text='The set of flags that can be used to tag classes for this program. You can add and modify flag types <a href="/manage/catsflagsrecs/flagtypes">here</a>.')
 
     documents = GenericRelation(Media, content_type_field='owner_type', object_id_field='owner_id')
 
