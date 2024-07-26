d432d247f4243ed6180a4e6d17785252f89cf654
Miles Calabresi <mcalabresi1@gmail.com>
2024-07-25 21:13:18 -0400

    use copyreg to serialize memoryview objects

--- esp/esp/application/apps.py
+++ esp/esp/application/apps.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import
 from django.apps import AppConfig
+import copyreg
 
 class ApplicationConfig(AppConfig):
     name = 'esp.application'
 
     def ready(self):
         import esp.application.signals
+        # h/t https://github.com/noripyt/django-cachalot/issues/125
+        # https://docs.python.org/3/library/copyreg.html#copyreg.pickle
+        copyreg.pickle(memoryview, lambda val: (memoryview, (bytes(val),)))
