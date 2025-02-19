f26414f9badc9b54b9d7555933eccf6a336bf00e
William Gearty <willgearty@gmail.com>
2025-02-18 21:57:16 -0500

    Various fixes for styling and mobile; add logo

--- esp/esp/themes/theme_data/droplets/config_form.py
+++ esp/esp/themes/theme_data/droplets/config_form.py
@@ -7,7 +7,8 @@ from esp.utils.widgets import NavStructureWidgetWithIcons, ContactFieldsWidget
 class ConfigForm(ThemeConfigurationForm):
     titlebar_prefix = forms.CharField()
     full_group_name = forms.CharField()
-    show_group_name = forms.BooleanField(required = False, help_text='Should the full group name be shown in the page header?')
+    show_logo = forms.BooleanField(initial = True, required = False, help_text='Should the logo be shown in the page header?')
+    show_group_name = forms.BooleanField(initial = True, required = False, help_text='Should the full group name be shown in the page header?')
     show_email = forms.BooleanField(required = False, help_text='Should the group email address be shown in the page header?')
     contact_info = forms.CharField(required = False, widget=forms.Textarea,
                                    help_text='Generic text to include in the page header. Leave blank to omit this field in the header.')
