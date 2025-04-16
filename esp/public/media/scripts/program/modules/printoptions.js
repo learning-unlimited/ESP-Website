9716d87f4fdf820a364f0328a53a94f3ca5b7566
William Gearty <willgearty@gmail.com>
2025-04-15 21:57:38 -0400

    Condense buttons for teacher lists and line items

---
+++ esp/public/media/scripts/program/modules/printoptions.js
@@ -0,0 +1,41 @@
+$j(function () {
+    $j("#class_list_options :input").on("change", function (){
+        // Get user-selected options
+        var fields = $j("#class_list_options :input");
+        var get = "";
+        // Create GET string
+        fields.each(function (index, element) {
+            var val = $j(this).val();
+            if (val != "") {
+                if (get != "" & get.charAt(get.length - 1) != "&") {
+                    get += "&";
+                }
+                get += val;
+            }
+        });
+        // Add GET string to all links
+        $j("#class_lists a").each(function (){
+            var base_url = $j(this).attr('href').split('?')[0];
+            $j(this).attr('href', base_url + "?" + get)
+        });
+    });
+    
+    $j("#teacher_list_options :input").on("change", function (){
+        // Get user-selected options
+        var url = "./" + $j("#user_type").val() + "/";
+        url += $j("#day_type").val();
+        url += $j("#output_type").val();
+        url += "?" + $j("#user_type option:selected").data("get");
+        
+        // Update href
+        $j("#teacher_list a").attr('href', url);
+    });
+    
+    $j("#item_type").on("change", function (){
+        // Get user-selected options
+        var url = "./selectList?recipient_type=Student&base_list=extracosts_" + $j("#item_type").val();
+        
+        // Update href
+        $j("#line_item_list a").attr('href', url);
+    });
+});
