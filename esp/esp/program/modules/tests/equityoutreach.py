203634547a6efc035cc3edd7c8f71d2428b050c6
aryan7081 <haryan458@gmail.com>
2026-02-22 01:03:16 +0530

    Refactor Equity Outreach to expose cohorts as user lists

--- esp/esp/program/modules/tests/equityoutreach.py
+++ esp/esp/program/modules/tests/equityoutreach.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
-from esp.dbmail.models import MessageRequest
-from esp.program.models import EquityOutreachCampaign, EquityOutreachRecipient, FinancialAidRequest, ProgramModule, RegistrationProfile
+from esp.program.models import FinancialAidRequest, ProgramModule, RegistrationProfile
 from esp.program.modules.handlers.equityoutreach import EquityOutreachCohorts
 from esp.program.tests import ProgramFrameworkTest
 from esp.users.models import StudentInfo
@@ -35,32 +34,15 @@ class EquityOutreachModuleTest(ProgramFrameworkTest):
         self.assertIn(self.student, list(transportation_users))
         self.assertIn(self.student, list(finaid_users))
 
-    def test_email_campaign_creates_message_request_and_snapshot(self):
-        self._add_student_profile(self.student, "Bus")
-        FinancialAidRequest.objects.create(program=self.program, user=self.student, done=False)
+    def test_equity_lists_in_program_lists(self):
+        """Equity cohorts are exposed as student lists for Communications Panel / User Records."""
+        pm = self.program.getModule("EquityOutreachModule")
+        lists = pm.students(QObject=False)
+        descs = pm.studentDesc()
 
-        self.assertTrue(self.client.login(username=self.admin.username, password="password"))
-        response = self.client.post(
-            "/manage/%s/equityoutreach_send" % self.program.getUrlBase(),
-            {
-                "cohort_key": EquityOutreachCohorts.COHORT_INCOMPLETE_FINAID,
-                "channel": "email",
-                "subject": "Need help finishing aid",
-                "body": "Please complete your financial aid form.",
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-
-        campaign = EquityOutreachCampaign.objects.get(program=self.program, channel=EquityOutreachCampaign.CHANNEL_EMAIL)
-        self.assertEqual(campaign.recipient_count, 1)
-        self.assertEqual(campaign.success_count, 1)
-        self.assertEqual(campaign.failure_count, 0)
-        self.assertIsNotNone(campaign.message_request)
-
-        msg_request = MessageRequest.objects.get(id=campaign.message_request_id)
-        self.assertEqual(msg_request.subject, "Need help finishing aid")
-
-        recipients = EquityOutreachRecipient.objects.filter(campaign=campaign)
-        self.assertEqual(recipients.count(), 1)
-        self.assertEqual(recipients[0].status, EquityOutreachRecipient.STATUS_QUEUED)
+        for key in EquityOutreachCohorts.all_cohort_keys():
+            list_name = "equity_" + key
+            self.assertIn(list_name, lists)
+            self.assertIn(list_name, descs)
+            self.assertEqual(descs[list_name], EquityOutreachCohorts.cohort_label(key))
 
