6122d95d0fdee8ffed962e3fad9d499c5c4a830b
Arnav Kumar <140815511+Arnav1709@users.noreply.github.com>
2026-02-22 01:24:42 +0530

    refactor: remove unnecessary __future__ imports from useful_scripts (#4250)
    These imports are all default in Python 3 and have no effect.

--- esp/useful_scripts/single_query.py
+++ esp/useful_scripts/single_query.py
@@ -5,8 +5,6 @@
 #            $0 /lu/sites/smith "$(cat script.py)"
 # Intended primarily to be used by run_queries.sh.
 
-from __future__ import absolute_import
-from __future__ import print_function
 import sys
 sys.path.insert(0, sys.argv[1]+'/esp/useful_scripts')
 
