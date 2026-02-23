9f304af0e19240cd3809122f05cf052c2acdf7e9
Suyash Jain <suyashjain1707@gmail.com>
2026-02-24 00:59:29 +0530

    Guard install() call in utils post-migrate hook

--- esp/esp/utils/apps.py
+++ esp/esp/utils/apps.py
@@ -1,8 +1,19 @@
 from django.apps import AppConfig
 from django.db.models import signals
 
+
 def run_install(sender, **kwargs):
-    sender.models_module.install()
+    """
+    Post-migrate hook used by apps that define an install() helper on their
+    models module to populate initial data.
+
+    Some apps (like esp.utils) use InstallConfig but do not need an install()
+    function; in that case we should simply do nothing rather than error.
+    """
+    models_module = sender.models_module
+    if hasattr(models_module, "install"):
+        models_module.install()
+
 
 class InstallConfig(AppConfig):
     """
