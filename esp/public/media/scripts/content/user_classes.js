f02c0f57389de752551d648ececc8af168916589
Cizelle <rawatipshita07@gmail.com>
2026-02-26 22:27:52 +0530

    Fix: handled undefined last name and added guest fallback

--- esp/public/media/scripts/content/user_classes.js
+++ esp/public/media/scripts/content/user_classes.js
@@ -1,40 +1,23 @@
 function update_user_classes() {
+    // safety check - if esp_user does not exist
+    if(!window.esp_user) return ;
+
+    // login / logout visibility
+    const isLoggedIn = !!esp_user.cur_username;
+    $j(".logged_in").toggleClass("hidden", !isLoggedIn);
+    $j(".not_logged_in").toggleClass("hidden", isLoggedIn);
+
+    // update name display - the 'undefined undefined' issue
+    $j("#user_first_name").text(esp_user.cur_first_name || "Guest");
+    $j("#user_last_name").text(esp_user.cur_last_name || "");
+
+    // handle admin roles
     if (esp_user.cur_admin == "1" || esp_user.cur_retTitle) {
-        $j(".admin").removeClass("hidden");
-        $j(".onsite").removeClass("hidden");
+        $j(".admin, .onsite").removeClass("hidden");
         $j(".hide-if-admin").addClass("hidden");
     }
-    if (esp_user.cur_retTitle) {
-        $j(".unmorph").removeClass("hidden");
-        $j("#unmorph_text").html("Click above to return to your administrator account - " + esp_user.cur_retTitle);
-    }
-    if (esp_user.cur_qsd_bits == "1") {
-        $j(".qsd_bits").removeClass("hidden");
-    }
-    if (esp_user.cur_username != null) {
-        $j(".not_logged_in").addClass("hidden");
-        $j(".logged_in").removeClass("hidden");
-    } else {
-        $j(".not_logged_in").removeClass("hidden");
-        $j(".logged_in").addClass("hidden");
-    }
 
-    var type_name = '';
-    var hidden_name = '';
-    for (var i = 0; i < esp_user.cur_roles.length; i++) {
-        type_name = "." + esp_user.cur_roles[i];
-        try {
-            // This is wrapped in a try/catch block, because custom user
-            // role names might not be in a format that jQuery accepts.
-            $j(type_name).removeClass("hidden");
-        } catch (e) {}
-    }
-    
-    //    Write user's name in the appropriate spot in the login box
-    $j("#user_first_name").text(esp_user.cur_first_name);
-    $j("#user_last_name").text(esp_user.cur_last_name);
-    $j("#user_username").text(esp_user.cur_username);
-    $j("#user_userid").text(esp_user.cur_userid);
 }
+
 
 $j(document).ready(update_user_classes)
