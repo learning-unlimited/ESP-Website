041ebfdb0d7849f9c73c9d4479d17ce7c92988a8
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-27 11:37:09 +0530

    Strengthen test assertions and add add_user_profiles to setUp

--- esp/esp/users/controllers/tests/test_usersearch.py
+++ esp/esp/users/controllers/tests/test_usersearch.py
@@ -1,15 +1,14 @@
 from __future__ import absolute_import
-from django.db.models import Q
-
 from esp.program.tests import ProgramFrameworkTest
 from esp.users.models import ESPUser
-
 from esp.users.controllers.usersearch import UserSearchController
 
 
 class TestUserSearchController(ProgramFrameworkTest):
+
     def setUp(self):
         super(TestUserSearchController, self).setUp()
+        self.add_user_profiles()
         self.controller = UserSearchController()
 
     def _get_combination_post_data(self, list_a, list_b):
@@ -23,7 +22,7 @@ class TestUserSearchController(ProgramFrameworkTest):
             'gradyear_max': '',
             'userid': '',
             'zipcode': '',
-            'combo_base_list': '%s:%s'%(list_a, list_b),
+            'combo_base_list': '%s:%s' % (list_a, list_b),
             'email': '',
             'states': '',
             'zipdistance': '',
@@ -35,43 +34,22 @@ class TestUserSearchController(ProgramFrameworkTest):
             'zipdistance_exclude': '',
         }
 
-
     def test_student_confirmed(self):
         post_data = self._get_combination_post_data('Student', 'confirmed')
-        qobject = self.controller.filter_from_postdata(self.program, post_data).getList(ESPUser)
-        self.assertEqual(qobject.model, ESPUser)
-
-
-    def test_teacher_classroom_tables(self):
-        post_data = self._get_combination_post_data('Teacher', 'all')
-        qobject = self.controller.filter_from_postdata(self.program, post_data).getList(ESPUser)
-        self.assertEqual(qobject.model, ESPUser)
-
-    def test_teacher_classroom_tables_query_from_post(self):
-        post_data = {'username': '',
-                     'zipdistance_exclude': '',
-                     'first_name': '',
-                     'last_name': '',
-                     'use_checklist': '0',
-                     'gradyear_max': '',
-                     'userid': '',
-                     'school': '',
-                     'combo_base_list': 'Teacher:all',
-                     'zipcode': '',
-                     'states': '',
-                     'student_sendto_self': '1',
-                     'checkbox_and_interview': '',
-                     'grade_min': '',
-                     'gradyear_min': '',
-                     'zipdistance': '',
-                     'grade_max': '',
-                     'email': ''}
-
-        query =  self.controller.query_from_postdata(self.program, post_data)
-        self.assertIsInstance(query, Q)
-        self.assertEqual(ESPUser.objects.filter(query).model, ESPUser)
+        query_result = self.controller.filter_from_postdata(self.program, post_data).getList(ESPUser)
+        self.assertEqual(query_result.model, ESPUser)
+        self.assertGreaterEqual(query_result.count(), 0)
 
     def test_teacher_interview(self):
         post_data = self._get_combination_post_data('Teacher', 'interview')
-        qobject = self.controller.filter_from_postdata(self.program, post_data).getList(ESPUser)
-        self.assertEqual(qobject.model, ESPUser)
+        query_result = self.controller.filter_from_postdata(self.program, post_data).getList(ESPUser)
+        self.assertEqual(query_result.model, ESPUser)
+        self.assertGreaterEqual(query_result.count(), 0)
+
+    def test_teacher_classroom_tables_query_from_post(self):
+        post_data = self._get_combination_post_data('Teacher', 'all')
+        query = self.controller.query_from_postdata(self.program, post_data)
+        self.assertIsNotNone(query)
+        result = query.getList(ESPUser)
+        self.assertEqual(result.model, ESPUser)
+        self.assertGreater(result.count(), 0)
