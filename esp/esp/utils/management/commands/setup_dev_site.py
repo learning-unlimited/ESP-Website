8fbc4a112d0e6b0e6f67b58302f181bcceaf4c28
Supriyo Chowdhury <supriyochowdhury760@gmail.com>
2026-02-27 12:31:29 +0530

    Apply Copilot review for setup_dev_site

--- esp/esp/utils/management/commands/setup_dev_site.py
+++ esp/esp/utils/management/commands/setup_dev_site.py
@@ -1,3 +1,38 @@
+
+__author__    = "Individual contributors (see AUTHORS file)"
+__date__      = "$DATE$"
+__rev__       = "$REV$"
+__license__   = "AGPL v.3"
+__copyright__ = """
+This file is part of the ESP Web Site
+Copyright (c) 2025 by the individual contributors
+  (see AUTHORS file)
+
+The ESP Web Site is free software; you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+Contact information:
+MIT Educational Studies Program
+  84 Massachusetts Ave W20-467, Cambridge, MA 02139
+  Phone: 617-253-4882
+  Email: esp-webmasters@mit.edu
+Learning Unlimited, Inc.
+  527 Franklin St, Cambridge, MA 02139
+  Phone: 617-379-0178
+  Email: web-team@learningu.org
+"""
+
 from django.core.management.base import BaseCommand
 from django.contrib.sites.models import Site
 
@@ -5,10 +40,8 @@ class Command(BaseCommand):
     help = "Sets the Django Site domain and name to 'localhost' for local development."
 
     def handle(self, *args, **kwargs):
-        count = Site.objects.all().count()
-        if count == 0:
-            self.stdout.write(self.style.WARNING('No Site objects found. Creating a default localhost Site.'))
+        if not Site.objects.filter(id=1).exists():
+            self.stdout.write(self.style.WARNING('No Site with id=1 found. Creating a default localhost Site with id=1.'))
             Site.objects.create(id=1, domain='localhost', name='localhost')
-        else:
-            Site.objects.update(domain='localhost', name='localhost')
+        Site.objects.filter(id=1).update(domain='localhost', name='localhost')
         self.stdout.write(self.style.SUCCESS("Successfully updated Site domain and name to 'localhost'."))
