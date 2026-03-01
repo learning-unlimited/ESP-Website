7d0753195625947b61090da1e0e7becc47373c6a
rafiya618 <rafiyam007@gmail.com>
2026-03-01 20:48:00 +0500

    Updated the URL regex in your urls.py

--- esp/esp/users/urls.py
+++ esp/esp/users/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     url(r'^makeadmin/?$', views.make_admin),
     url(r'^loginhelp', views.LoginHelpView.as_view(), name='Login Help'),
     url(r'^morph/?$', views.morph_into_user),
-    url(r'^unsubscribe/(?P<username>[\w.@+-]+)/(?P<token>[\w.:\-_=]+)/$', views.unsubscribe, name="unsubscribe"),
-    url(r'^unsubscribe_oneclick/(?P<username>[\w.@+-]+)/(?P<token>[\w.:\-_=]+)/$', views.unsubscribe_oneclick, name="unsubscribe_oneclick"),
+    url(r'^unsubscribe/(?P<username>[\w.@+\-:]+)/(?P<token>[\w.:\-_=]+)/$', views.unsubscribe, name="unsubscribe"),
+    url(r'^unsubscribe_oneclick/(?P<username>[\w.@+\-:]+)/(?P<token>[\w.:\-_=]+)/$', views.unsubscribe_oneclick, name="unsubscribe_oneclick"),
 
     # AJAX endpoints for real-time validation
     url(r'^ajax/check_email/?$', views.ajax_check_email_availability, name='ajax_check_email'),
