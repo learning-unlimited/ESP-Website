fc07be4e415789c05c0afb4976da9d12cde697b8
William Gearty <willgearty@gmail.com>
2026-03-03 21:49:10 -0500

    Apply suggestion from @willgearty

--- esp/esp/web/apps.py
+++ esp/esp/web/apps.py
@@ -3,9 +3,3 @@ from esp.utils.apps import InstallConfig
 
 class WebConfig(InstallConfig):
     name = 'esp.web'
-
-    def ready(self):
-        super(WebConfig, self).ready()
-        # Monkey patches removed: File extension lowercasing is handled by
-        # LowercaseExtensionStorage backend (see esp.web.storage
-        # and django_settings.py DEFAULT_FILE_STORAGE)
