53e35e97ee96c1833c7fb47cfdc91aec7878963a
DarshanCode2005 <darshanthakaregsoc2023@gmail.com>
2026-03-05 18:43:12 +0530

    Pass qsd_url in ajax_qsd_preview POST data

--- esp/public/media/scripts/qsd.js
+++ esp/public/media/scripts/qsd.js
@@ -1,15 +1,12 @@
 
 //  Straight forward functions for Ajax QSD editing
 
-function post_encode(data)
-{
+function post_encode(data) {
     //  Convert a Javascript object to x-www-form-urlencoded format.
     var result = '';
     var i = 0;
-    for (key in data)
-    {
-        if (key != 'toJSON')
-        {
+    for (key in data) {
+        if (key != 'toJSON') {
             if (i != 0) result += '&';
             result += key + '=' + encodeURIComponent(data[key]);
             i++;
@@ -18,8 +15,7 @@ function post_encode(data)
     return result
 }
 
-function qsd_inline_edit(qsd_url, edit_id)
-{
+function qsd_inline_edit(qsd_url, edit_id) {
     //  Switch the visibility of the edit and view areas.
     document.getElementById("inline_edit_" + edit_id).className = "qsd_edit_visible";
     $j("#inline_edit_msg_" + edit_id).hide();
@@ -27,42 +23,35 @@ function qsd_inline_edit(qsd_url, edit_id)
     document.getElementById("inline_qsd_" + edit_id).className = "hidden";
 }
 
-function qsd_send_command(qsd_url, edit_id, postdata)
-{
+function qsd_send_command(qsd_url, edit_id, postdata) {
     //Refresh the csrf token if needed
     refresh_csrf_cookie();
     postdata.csrfmiddlewaretoken = csrf_token();
 
-    $j.post("/admin/ajax_qsd", post_encode(postdata), function(data, status)
-    {
-        if (status == "success")
-        {
-            if (data)
-            {
+    $j.post("/admin/ajax_qsd", post_encode(postdata), function (data, status) {
+        if (status == "success") {
+            if (data) {
                 qsd_inline_update(qsd_url, edit_id, data,
                     "Saved! Now reloading...", 'green', 'glyphicon-ok');
                 window.location.reload(true); // bust the cache
             }
         }
-        else
-        {
+        else {
             alert("Abnormal Status: " + status + "\nData: " + data);
         }
-    }).fail(function(request, jquery_status, http_status)
-    {
+    }).fail(function (request, jquery_status, http_status) {
         alert(jquery_status + ": " + http_status + "\n" + request.responseText);
     });
 
-    $j.post("/varnish/purge_page", { page: $j(location).attr('pathname'), csrfmiddlewaretoken: csrf_token()});
+    $j.post("/varnish/purge_page", { page: $j(location).attr('pathname'), csrfmiddlewaretoken: csrf_token() });
 }
 
-function qsd_send_preview(qsd_url, edit_id, postdata)
-{
+function qsd_send_preview(qsd_url, edit_id, postdata) {
     //Refresh the csrf token if needed
     refresh_csrf_cookie();
     postdata.csrfmiddlewaretoken = csrf_token();
 
-    $j.post("/admin/ajax_qsd_preview", post_encode(postdata), function(data, status) {
+    $j.post("/admin/ajax_qsd_preview", post_encode(postdata), function (data, status) {
         if (status === "success") {
             if (data) {
                 qsd_inline_update(qsd_url, edit_id, data,
@@ -72,13 +61,12 @@ function qsd_send_preview(qsd_url, edit_id, postdata)
         } else {
             alert("Abnormal Status: " + status + "\nData: " + data);
         }
-    }).fail(function(request, jquery_status, http_status) {
+    }).fail(function (request, jquery_status, http_status) {
         alert(jquery_status + ": " + http_status + "\n" + request.responseText);
     });
 }
 
-function qsd_inline_finish(qsd_url, edit_id, action)
-{
+function qsd_inline_finish(qsd_url, edit_id, action) {
     //  Switch the visibility of the edit and view areas.
     document.getElementById("inline_edit_" + edit_id).className = "hidden";
     $j("#inline_edit_msg_" + edit_id).show();
@@ -87,10 +75,10 @@ function qsd_inline_finish(qsd_url, edit_id, action)
     if (action) {
         var content = document.getElementById("qsd_content_" + edit_id).value;
         if (action === 'save') {
-            var postdata = {cmd: "update", url: qsd_url, data: content};
+            var postdata = { cmd: "update", url: qsd_url, data: content };
             qsd_send_command(qsd_url, edit_id, postdata);
         } else {
-            qsd_send_preview(qsd_url, edit_id, {data: content});
+            qsd_send_preview(qsd_url, edit_id, { url: qsd_url, data: content });
         }
     }
 }
