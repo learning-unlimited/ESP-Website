4c15eace642a5ac31144f0f409e365576ce81bdb
ISHAN9876 <ishankumra13579@gmail.com>
2026-03-07 17:57:56 +0530

    test: add happy path test for dbmail_cron.main()

--- esp/test_dbmail_cron.py
+++ esp/test_dbmail_cron.py
@@ -1,10 +1,21 @@
 from unittest import mock
 import sys
 import fcntl
+import unittest
 
-# We must mock django and cronmail BEFORE importing dbmail_cron
-# because dbmail_cron runs django.setup() at module level
 sys.modules['django'] = mock.MagicMock()
 sys.modules['esp.dbmail.cronmail'] = mock.MagicMock()
 
 import dbmail_cron
+
+class TestDbmailCron(unittest.TestCase):
+
+    @mock.patch('dbmail_cron.send_email_requests')
+    @mock.patch('dbmail_cron.process_messages')
+    @mock.patch('dbmail_cron.fcntl.lockf')
+    @mock.patch('builtins.open', mock.mock_open())
+    def test_happy_path(self, mock_lockf, mock_process, mock_send):
+        dbmail_cron.main()
+
+        mock_process.assert_called_once()
+        mock_send.assert_called_once()
