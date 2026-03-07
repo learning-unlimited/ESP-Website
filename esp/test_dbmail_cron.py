3bcf06265ea882605c61ca596279d60595b63b57
ISHAN9876 <ishankumra13579@gmail.com>
2026-03-07 18:02:10 +0530

    test: verify lock is released when process_messages raises exception

--- esp/test_dbmail_cron.py
+++ esp/test_dbmail_cron.py
@@ -18,6 +18,16 @@ class TestDbmailCron(unittest.TestCase):
     @mock.patch('dbmail_cron.process_messages')
     @mock.patch('dbmail_cron.fcntl.lockf', side_effect=IOError)
     @mock.patch('builtins.open', mock.mock_open())
+    @mock.patch('dbmail_cron.send_email_requests')
+    @mock.patch('dbmail_cron.process_messages', side_effect=Exception('DB error'))
+    @mock.patch('dbmail_cron.fcntl.lockf')
+    @mock.patch('builtins.open', mock.mock_open())
+    def test_process_messages_exception_releases_lock(self, mock_lockf, mock_process, mock_send):
+        dbmail_cron.main()
+
+        mock_send.assert_not_called()
+        unlock_call = mock_lockf.call_args_list[-1]
+        assert unlock_call[0][1] == fcntl.LOCK_UN
     def test_lock_contention(self, mock_lockf, mock_process, mock_exit):
         dbmail_cron.main()
 
