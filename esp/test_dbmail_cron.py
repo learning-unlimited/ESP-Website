4cd04f798ce809c6e2070d6d89739ec04d2a4ca4
ISHAN9876 <ishankumra13579@gmail.com>
2026-03-07 18:01:19 +0530

    test: add lock contention test for dbmail_cron.main()

--- esp/test_dbmail_cron.py
+++ esp/test_dbmail_cron.py
@@ -14,6 +14,15 @@ class TestDbmailCron(unittest.TestCase):
     @mock.patch('dbmail_cron.process_messages')
     @mock.patch('dbmail_cron.fcntl.lockf')
     @mock.patch('builtins.open', mock.mock_open())
+    @mock.patch('sys.exit')
+    @mock.patch('dbmail_cron.process_messages')
+    @mock.patch('dbmail_cron.fcntl.lockf', side_effect=IOError)
+    @mock.patch('builtins.open', mock.mock_open())
+    def test_lock_contention(self, mock_lockf, mock_process, mock_exit):
+        dbmail_cron.main()
+
+        mock_exit.assert_called_once_with(0)
+        mock_process.assert_not_called()
     def test_happy_path(self, mock_lockf, mock_process, mock_send):
         dbmail_cron.main()
 
