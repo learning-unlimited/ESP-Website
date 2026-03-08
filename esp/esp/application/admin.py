a751487a4a9d2962a82644e10d8fb54cd8203123
Theodore Hwa <ted@learningu.org>
2026-03-07 22:22:57 -0800

    commit with lint failures

--- esp/esp/application/admin.py
+++ esp/esp/application/admin.py
@@ -7,10 +7,10 @@ This file is part of the ESP Web Site
 Copyright (c) 2008 by the individual contributors
   (see AUTHORS file)
 
-The ESP Web Site is free software; you can redistribute it and/or
-modify it under the terms of the GNU Affero General Public License
-as published by the Free Software Foundation; either version 3
-of the License, or (at your option) any later version.
+The ESP Web Site is free software; you can redistribute it and/or    
+modify it under the terms of the GNU Affero General Public License     
+as published by the Free Software Foundation; either version 3   
+of the License, or (at your option) any later version.    
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
