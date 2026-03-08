b5d3fe3d5fb157849f6ed57c312bdc7e99f2be7c
Abhinav Kumar Jha <abhinavjha0239@gmail.com>
2026-03-09 03:14:44 +0530

    Add merge migration for conflicting program 0032 migrations
    Resolves migration conflict between 0032_programmodule_null_out_defaults
    (this branch) and 0032_auto_20260302_0348 (from main).

---
+++ esp/esp/program/migrations/0033_merge_20260307_0400.py
@@ -0,0 +1,14 @@
+# Generated manually to merge conflicting migrations
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('program', '0032_auto_20260302_0348'),
+        ('program', '0032_programmodule_null_out_defaults'),
+    ]
+
+    operations = [
+    ]
