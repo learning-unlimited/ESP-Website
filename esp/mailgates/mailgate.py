f468c3daeeae01c7e7ec70dd4d8bb6af660fbea7
Prem <premsingh10193@gmail.com>
2026-03-17 23:45:30 +0530

    Replaced SHA1-based Message-ID generation with email.utils.make_msgid to ensure standard-compliant and more robust IDs

--- esp/mailgates/mailgate.py
+++ esp/mailgates/mailgate.py
@@ -7,6 +7,8 @@ from __future__ import absolute_import
 from __future__ import print_function
 import sys, os, email, hashlib, re, smtplib, socket, random
 from io import open
+from email.utils import make_msgid
+
 new_path = '/'.join(sys.path[0].split('/')[:-1])
 sys.path += [new_path]
 sys.path.insert(0, "/usr/sbin/")
@@ -105,7 +107,8 @@ try:
         del message['Message-ID']
 
         # get a new message id
-        message['Message-ID'] = '<%s@%s>' % (hashlib.sha1(str(random.random()).encode()).hexdigest(), host)
+        message['Message-ID'] = make_msgid(domain=host)
+
 
         if handler.cc_all:
             # send one mass-email
