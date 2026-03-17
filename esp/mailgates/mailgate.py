74ba6e209c839c6319c4db38448b85b1c365a4c4
Prem <premsingh10193@gmail.com>
2026-03-17 23:31:05 +0530

    fix(mailgate): use make_msgid for Message-ID and clean up imports
    - Replace SHA1-based Message-ID generation with email.utils.make_msgid
    - Split combined import statement into individual imports for readability

--- esp/mailgates/mailgate.py
+++ esp/mailgates/mailgate.py
@@ -5,7 +5,15 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
-import sys, os, email, hashlib, re, smtplib, socket, random
+import sys
+import os
+import email
+import hashlib
+import re
+import smtplib
+import socket
+import random
+from email.utils import make_msgid
 from io import open
 new_path = '/'.join(sys.path[0].split('/')[:-1])
 sys.path += [new_path]
@@ -105,7 +113,7 @@ try:
         del message['Message-ID']
 
         # get a new message id
-        message['Message-ID'] = '<%s@%s>' % (hashlib.sha1(str(random.random()).encode()).hexdigest(), host)
+        message['Message-ID'] = make_msgid(domain=host)
 
         if handler.cc_all:
             # send one mass-email
