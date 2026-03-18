bca055813ccb48fbc8d4dd0636b7b97efbbae14c
Imran Ahmed <imran.techvoyager@gmail.com>
2026-03-18 17:45:16 +0530

    Merge upstream/main and resolve conflicts: use re_path with name= args

--- esp/esp/themes/urls.py
+++ esp/esp/themes/urls.py
@@ -4,7 +4,7 @@ from esp.themes.views import editor, selector, configure, confirm_overwrite, lan
 from django.urls import re_path
 
 urlpatterns = [
-    re_path(r'^/?$', landing, name='themes_landing'),
+    re_path(r'^/$', landing, name='themes_landing'),
     re_path(r'^/select', selector, name='themes_select'),
     re_path(r'^/setup', configure, name='themes_configure'),
     re_path(r'^/confirm_overwrite', confirm_overwrite, name='themes_confirm_overwrite'),
