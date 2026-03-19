c402b0240122efbea1009b487c465e035495182c
Harshal Raut <rharshal398@gmail.com>
2026-03-19 10:56:55 +0530

    Fix base_url using getUrlBase() #5188

--- esp/esp/program/modules/tests/volunteermanage.py
+++ esp/esp/program/modules/tests/volunteermanage.py
@@ -45,9 +45,8 @@ class VolunteerManageTest(ProgramFrameworkTest):
         pm = ProgramModule.objects.get(handler='VolunteerManage')
         self.module = ProgramModuleObj.getFromProgModule(self.program, pm)
         self.admin = self.admins[0]
-        self.base_url = '/manage/%s/%s/volunteering' % (
-            self.program.anchor.name,
-            self.program.url
+        self.base_url = '/manage/%s/volunteering' % (
+            self.program.getUrlBase()
         )
 
     def test_volunteering_page_loads(self):
