6d604dd2215ae8f46d2a01af859e062b82b22227
Rahul-R79 <rahul.devworks@gmail.com>
2026-03-21 10:26:44 +0530

    Migrate remaining datetime.now() usages to timezone.now() for full timezone awareness

--- esp/esp/resources/tests.py
+++ esp/esp/resources/tests.py
@@ -50,7 +50,7 @@ class FloatingResourceAvailabilityTest(TestCase):
 
     def setUp(self):
         super(FloatingResourceAvailabilityTest, self).setUp()
-        now = datetime.now().replace(minute=0, second=0, microsecond=0)
+        now = timezone.now().replace(minute=0, second=0, microsecond=0)
         event_type = EventType.objects.all()[0]
         self.program = Program.objects.create(grade_min=7, grade_max=12)
 
