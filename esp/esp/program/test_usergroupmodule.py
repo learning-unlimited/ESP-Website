11c287cbe381dad9183a3eed7adc27e9d4f8d885
Singh <singh@192.168.1.3>
2026-03-22 13:34:29 +0530

    fix: address review feedback for UserGroupModule tests

--- esp/esp/program/test_usergroupmodule.py
+++ esp/esp/program/test_usergroupmodule.py
@@ -1,100 +1,91 @@
-import pytest
 from django.contrib.auth.models import Group
+from django.test import TestCase
 from esp.program.modules.handlers.usergroupmodule import UserGroupModule
 from esp.users.models import ESPUser
 
 
-@pytest.mark.django_db
-def test_create_new_group_and_add_users():
-    user1 = ESPUser.objects.create(username="user1")
-    user2 = ESPUser.objects.create(username="user2")
+class UserGroupModuleTests(TestCase):
 
-    class MockFilter:
-        def getList(self, model):
-            return ESPUser.objects.filter(id__in=[user1.id, user2.id])
+    def test_create_new_group_and_add_users(self):
+        user1 = ESPUser.objects.create(username="user1")
+        user2 = ESPUser.objects.create(username="user2")
 
-    message = UserGroupModule.updateGroup(MockFilter(), "new_group")
+        class MockFilter:
+            def getList(self, model):
+                return ESPUser.objects.filter(id__in=[user1.id, user2.id])
 
-    group = Group.objects.get(name="new_group")
+        message = UserGroupModule.updateGroup(MockFilter(), "new_group")
+        group = Group.objects.get(name="new_group")
 
-    assert group.user_set.count() == 2
-    assert "has been created" in message
-    assert "2 new users have been added" in message
+        self.assertEqual(group.user_set.count(), 2)
+        self.assertIn("new_group", message)
+        self.assertIn("added", message)
 
+    def test_add_users_to_existing_group(self):
+        user = ESPUser.objects.create(username="user3")
+        group = Group.objects.create(name="existing_group")
 
-@pytest.mark.django_db
-def test_add_users_to_existing_group():
-    user = ESPUser.objects.create(username="user3")
-    group = Group.objects.create(name="existing_group")
+        class MockFilter:
+            def getList(self, model):
+                return ESPUser.objects.filter(id=user.id)
 
-    class MockFilter:
-        def getList(self, model):
-            return ESPUser.objects.filter(id=user.id)
+        message = UserGroupModule.updateGroup(MockFilter(), "existing_group")
 
-    message = UserGroupModule.updateGroup(MockFilter(), "existing_group")
+        self.assertIn(user, group.user_set.all())
+        self.assertIn("existing_group", message)
+        self.assertIn("added", message)
 
-    assert user in group.user_set.all()
-    assert "1 new users have been added" in message
+    def test_clean_group_replaces_users(self):
+        user1 = ESPUser.objects.create(username="user4")
+        user2 = ESPUser.objects.create(username="user5")
 
+        group = Group.objects.create(name="clean_group")
+        group.user_set.add(user1)
 
-@pytest.mark.django_db
-def test_clean_group_replaces_users():
-    user1 = ESPUser.objects.create(username="user4")
-    user2 = ESPUser.objects.create(username="user5")
+        class MockFilter:
+            def getList(self, model):
+                return ESPUser.objects.filter(id=user2.id)
 
-    group = Group.objects.create(name="clean_group")
-    group.user_set.add(user1)
+        message = UserGroupModule.updateGroup(MockFilter(), "clean_group", clean=True)
 
-    class MockFilter:
-        def getList(self, model):
-            return ESPUser.objects.filter(id=user2.id)
+        users = group.user_set.all()
 
-    message = UserGroupModule.updateGroup(MockFilter(), "clean_group", clean=True)
+        self.assertNotIn(user1, users)
+        self.assertIn(user2, users)
+        self.assertIn("removed", message)
 
-    users = group.user_set.all()
+    def test_no_users_raises_error(self):
+        class MockFilter:
+            def getList(self, model):
+                return ESPUser.objects.none()
 
-    assert user1 not in users
-    assert user2 in users
-    assert "removed from user group" in message
+        with self.assertRaises(Exception) as context:
+            UserGroupModule.updateGroup(MockFilter(), "test_group")
 
+        self.assertIn("did not match any users", str(context.exception))
 
-@pytest.mark.django_db
-def test_no_users_raises_error():
-    class MockFilter:
-        def getList(self, model):
-            return ESPUser.objects.none()
+    def test_no_duplicate_addition(self):
+        user = ESPUser.objects.create(username="user6")
+        group = Group.objects.create(name="dup_group")
+        group.user_set.add(user)
 
-    with pytest.raises(Exception) as exc:
-        UserGroupModule.updateGroup(MockFilter(), "test_group")
+        class MockFilter:
+            def getList(self, model):
+                return ESPUser.objects.filter(id=user.id)
 
-    assert "did not match any users" in str(exc.value)
+        UserGroupModule.updateGroup(MockFilter(), "dup_group")
 
+        self.assertEqual(group.user_set.count(), 1)
 
-@pytest.mark.django_db
-def test_no_duplicate_addition():
-    user = ESPUser.objects.create(username="user6")
-    group = Group.objects.create(name="dup_group")
-    group.user_set.add(user)
+    def test_log_message_output(self):
+        user = ESPUser.objects.create(username="user7")
 
-    class MockFilter:
-        def getList(self, model):
-            return ESPUser.objects.filter(id=user.id)
+        class MockFilter:
+            def getList(self, model):
+                return ESPUser.objects.filter(id=user.id)
 
-    UserGroupModule.updateGroup(MockFilter(), "dup_group")
+        message = UserGroupModule.updateGroup(MockFilter(), "log_group")
 
-    # Ensure no duplicate
-    assert group.user_set.count() == 1
-
-
-@pytest.mark.django_db
-def test_log_message_output():
-    user = ESPUser.objects.create(username="user7")
-
-    class MockFilter:
-        def getList(self, model):
-            return ESPUser.objects.filter(id=user.id)
-
-    message = UserGroupModule.updateGroup(MockFilter(), "log_group")
-
-    assert "added to user group" in message
-    assert "now has" in message
+        self.assertIn("log_group", message)
+        self.assertIn("added", message)
+        self.assertIn("now has", message)
