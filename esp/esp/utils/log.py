af1a6c8618ad1d0281bad49ab75b67283810cd7e
Guni <gunibhatnagar1@gmail.com>
2026-03-27 17:14:17 +0530

    Remove unnecessary blank lines in log.py

--- esp/esp/utils/log.py
+++ esp/esp/utils/log.py
@@ -2,11 +2,9 @@ import logging
 
 from django.conf import settings
 
-
 class RequireInScript(logging.Filter):
     def filter(self, record):
         return settings.IS_IN_SCRIPT
-
 
 class RequireNotInScript(logging.Filter):
     def filter(self, record):
