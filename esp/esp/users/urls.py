f039e3dfcd2536c7dbb114a14b944c8bac8f6ba7
William Gearty <willgearty@gmail.com>
2026-03-27 17:11:55 -0400

    Switch to accountmanage and cover /myesp too

--- esp/esp/users/urls.py
+++ esp/esp/users/urls.py
@@ -42,7 +42,7 @@ urlpatterns += [
 ]
 
 urlpatterns += [
-    re_path(r'^$', RedirectView.as_view(url='profile/', permanent=False)),
+    re_path(r'^$', RedirectView.as_view(url='accountmanage/', permanent=False)),
     re_path(r'^switchback/?$', myesp.myesp_switchback, name='myesp_switchback'),
     re_path(r'^stop_testing/?$', myesp.myesp_stop_testing, name='myesp_stop_testing'),
     re_path(r'^onsite/?$', myesp.myesp_onsite, name='myesp_onsite'),
