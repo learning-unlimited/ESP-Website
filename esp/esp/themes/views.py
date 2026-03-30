13a02c8fceb56d7057b2cae2d9782e58c07ec671
Rishika Bothra <rshkbothra@gmail.com>
2026-03-30 17:23:59 +0530

    fix: prevent path traversal in theme file uploads and improve search input event handling

--- esp/esp/themes/views.py
+++ esp/esp/themes/views.py
@@ -157,6 +157,13 @@ def _safe_backup_path(filename, backups_dir):
         raise ESPError("Invalid file selection.", log=True)
     return resolved
 
+
+def _safe_backup_path(filename, backups_dir):
+    resolved = os.path.realpath(os.path.join(backups_dir, filename))
+    if not resolved.startswith(os.path.realpath(backups_dir) + os.sep):
+        raise ESPError("Invalid file selection.", log=True)
+    return resolved
+
 THEME_ERROR_STRING = "Your site's theme is not in the generic templates system. " + \
                      "If you want to switch to one of the standard themes, " + \
                      "please contact the web team."
@@ -252,22 +259,20 @@ def logos(request):
             _generate_favicon_variants(settings.MEDIA_ROOT + 'images/favicon.ico', settings.MEDIA_ROOT + 'images')
         elif 'logo_select' in request.POST:
             # Overwrite existing logo file
-            backups_dir = settings.MEDIA_ROOT + 'images/backups'
-            src = _safe_backup_path(request.POST['logo_select'], backups_dir)
-            shutil.copyfile(src, settings.MEDIA_ROOT + 'images/theme/logo.png')
+            safe_path = _safe_backup_path(request.POST['logo_select'], settings.MEDIA_ROOT + 'images/backups')
+            shutil.copyfile(safe_path, settings.MEDIA_ROOT + 'images/theme/logo.png')
             # Update logo version
             Tag.setTag("current_logo_version", value = hex(random.getrandbits(16)))
         elif 'header_select' in request.POST:
             # Overwrite existing header file
-            backups_dir = settings.MEDIA_ROOT + 'images/backups'
-            src = _safe_backup_path(request.POST['header_select'], backups_dir)
-            shutil.copyfile(src, settings.MEDIA_ROOT + 'images/theme/header.png')
+            safe_path = _safe_backup_path(request.POST['header_select'], settings.MEDIA_ROOT + 'images/backups')
+            shutil.copyfile(safe_path, settings.MEDIA_ROOT + 'images/theme/header.png')
             # Update header version
             Tag.setTag("current_header_version", value = hex(random.getrandbits(16)))
         elif 'favicon_select' in request.POST:
-            backups_dir = settings.MEDIA_ROOT + 'images/backups'
-            src = _safe_backup_path(request.POST['favicon_select'], backups_dir)
-            shutil.copyfile(src, settings.MEDIA_ROOT + 'images/favicon.ico')
+            # Update favicon version
+            safe_path = _safe_backup_path(request.POST['favicon_select'], settings.MEDIA_ROOT + 'images/backups')
+            shutil.copyfile(safe_path, settings.MEDIA_ROOT + 'images/favicon.ico')
             # Update favicon version
             Tag.setTag("current_favicon_version", value = hex(random.getrandbits(16)))
             _generate_favicon_variants(settings.MEDIA_ROOT + 'images/favicon.ico', settings.MEDIA_ROOT + 'images')
