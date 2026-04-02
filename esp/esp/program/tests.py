068854582f6d2339ef01c508a8a1612426e42baf
Vipul Pandey <vipulpandey7917@gmail.com>
2026-04-02 19:45:03 +0530

    Fix testStats weight calculation to match controller formula

--- esp/esp/program/tests.py
+++ esp/esp/program/tests.py
@@ -1307,8 +1307,9 @@ class LSRAssignmentTest(ProgramFrameworkTest):
             hours_priority = numpy.sum([len(sec.get_meeting_times()) for sec in sections_priority_and_enrolled])
             student_utility = (hours_interested + 1.5 * hours_priority) ** 0.5
 
-            # Weight is total hours of all registered sections (not just enrolled), matching student_utility_weights
-            weight_hours_interested = numpy.sum([len(sec.get_meeting_times()) for sec in set(sections_interested) - set(sections_priority)])
+            # Weight is total hours of registered sections, matching student_utility_weights
+            # (interest and priority are summed independently, mirroring the controller's matrix multiply)
+            weight_hours_interested = numpy.sum([len(sec.get_meeting_times()) for sec in sections_interested])
             weight_hours_priority = numpy.sum([len(sec.get_meeting_times()) for sec in sections_priority])
             student_weight = (weight_hours_interested + weight_hours_priority) ** 0.5
             student_screwed_val = (1.0 + student_utility) / (1.0 + student_weight)
