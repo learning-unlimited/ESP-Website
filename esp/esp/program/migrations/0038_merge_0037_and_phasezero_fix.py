d6e9cb1cc4e6c6473d003d7ed7e8718e83bdd74a
v0idheaven <dahiyavarun2007@gmail.com>
2026-04-03 20:09:05 +0530

    Fix migration graph conflict for PhaseZeroRecord branch

---
+++ esp/esp/program/migrations/0038_merge_0037_and_phasezero_fix.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('program', '0037_auto_20260402_2109'),
+        ('program', '0034_merge_0033_migrations'),
+    ]
+
+    operations = [
+    ]
