b6d0a72050539c12acdc5478be470becb6cac18d
suryaparua-official <surya.exce@gmail.com>
2026-04-08 18:05:00 +0530

    Fix admin UI overflow when Tag value is too long

--- esp/esp/tagdict/admin.py
+++ esp/esp/tagdict/admin.py
@@ -1,8 +1,35 @@
 from esp.tagdict.models import Tag
 from django.contrib import admin
 from esp.admin import admin_site
+from django.utils.html import format_html
+from django import forms
+
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('key', 'value', 'target', )
-    list_filter = ('key', 'object_id', 'content_type__model', )
+    list_display = ('key', 'short_value', 'target',)
+    list_filter = ('key', 'object_id', 'content_type__model',)
+
+    def short_value(self, obj):
+        val = obj.value or ""
+        short = val[:50] + "..." if len(val) > 50 else val
+
+        return format_html(
+            '<div style="max-width:300px; word-break:break-word;">{}</div>',
+            short
+        )
+
+    short_value.short_description = "Value"
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        field = super().formfield_for_dbfield(db_field, **kwargs)
+
+        if db_field.name == 'value':
+            field.widget = forms.Textarea(attrs={
+                'style': 'width:100%; max-width:600px; word-break:break-word;',
+                'rows': 4
+            })
+
+        return field
+
+
 admin_site.register(Tag, TagAdmin)
--- esp/esp/tagdict/models.py
+++ esp/esp/tagdict/models.py
@@ -35,7 +35,13 @@ class Tag(models.Model):
         # TODO:  Write this custom SQL for backends other than PostgreSQL.
 
     def __str__(self):
-        return f"{self.key}: {self.value} ({self.target})"
+        key = self.key or ""
+        val = self.value or ""
+
+        if len(val) > 50:
+            val = val[:50] + "..."
+
+        return f"{key}: {val}"
 
     EMPTY_TAG = " "
 
