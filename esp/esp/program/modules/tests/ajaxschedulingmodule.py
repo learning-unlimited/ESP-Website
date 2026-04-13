077ee57ad27c67dab30f72217c835c9b3dd77795
Darshan <darshan.kushal321@gmail.com>
2026-04-13 13:18:26 +0530

    Improve AJAX unschedule changelog test coverage

--- esp/esp/program/modules/tests/ajaxschedulingmodule.py
+++ esp/esp/program/modules/tests/ajaxschedulingmodule.py
@@ -189,7 +189,11 @@ class AJAXSchedulingModuleTest(AJAXSchedulingModuleTestBase):
         changelog_response = self.client.get(self.changelog_url, {'last_fetched_index': 1 })
         changelog = json.loads(changelog_response.content)["changelog"]
         self.assertTrue(len(changelog) == 1, "Change log did not contain the unscheduled class: " + str(changelog))
-        #TODO:  more detailed testing here
+        entry = changelog[0]
+        self.assertEqual(entry['id'], section.id, "Changelog entry has wrong class section ID")
+        self.assertTrue(entry['is_scheduling'], "Changelog entry should be a scheduling entry")
+        self.assertEqual(entry['timeslots'], [], "Timeslots should be empty for an unscheduled class")
+        self.assertEqual(entry['room_name'], "", "Room name should be empty for an unscheduled class")
 
     def testChangeLogFailedScheduling(self):
         #change log should not include failed scheduling of classes
