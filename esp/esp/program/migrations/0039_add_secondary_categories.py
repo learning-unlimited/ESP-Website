8a11314033efc7c4c429abe1f1f7e40e07860af6
Guni <gunibhatnagar1@gmail.com>
2026-04-28 22:51:08 +0530

    Add missing migration for secondary_categories field

---
+++ esp/esp/program/migrations/0039_add_secondary_categories.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('program', '0038_add_secondary_categories'),
+    ]
+
+    operations = [
+    ]
