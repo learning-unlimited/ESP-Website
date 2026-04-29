6f247ef982bdba1a6a25f9dc7f909c52574266d6
Guni <gunibhatnagar1@gmail.com>
2026-04-29 14:20:22 +0530

    Fix full migration graph conflicts and lint issues

--- esp/esp/program/migrations/0040_merge_conflicts.py
+++ esp/esp/program/migrations/0040_merge_conflicts.py
@@ -3,7 +3,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('program', '0037_auto_20260402_2109'),
         ('program', '0035_auto_20260319_1454'),
+        ('program', '0035_auto_20260326_1003'),
         ('program', '0039_add_secondary_categories'),
     ]
 
