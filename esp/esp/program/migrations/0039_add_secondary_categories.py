42952d3e697a8902b8c41eb99c45f30010636c53
Guni <gunibhatnagar1@gmail.com>
2026-04-29 14:08:33 +0530

    Fix migration graph and lint newline issues

--- esp/esp/program/migrations/0039_add_secondary_categories.py
+++ esp/esp/program/migrations/0039_add_secondary_categories.py
@@ -3,7 +3,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('program', '0040_merge_conflicts'),
+        ('program', '0037_auto_20260402_2109'),
     ]
 
     operations = [
--- esp/esp/program/migrations/0040_merge_conflicts.py
+++ esp/esp/program/migrations/0040_merge_conflicts.py
@@ -3,9 +3,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('program', '0037_auto_20260402_2109'),   # latest from one branch
-        ('program', '0035_auto_20260326_1003'),   # from another branch
-        ('program', '0034_auto_20260319_1454'),   # include all leaf nodes
+        ('program', '0035_auto_20260319_1454'),
+        ('program', '0039_add_secondary_categories'),
     ]
 
     operations = []
