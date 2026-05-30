332bf53f34ffd3113e157bac06f3e88175afc712
Suyash Jain <suyashjain1707@gmail.com>
2026-05-31 00:52:03 +0530

    Add merge migration to resolve users 0044 branch conflict

---
+++ esp/esp/users/migrations/0045_merge_0044_alter_contactinfo_0044_k12school.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0044_alter_contactinfo_address_country'),
+        ('users', '0044_merge_0043_auto_20260327_1528_0043_k12school_state_city'),
+    ]
+
+    operations = [
+    ]
