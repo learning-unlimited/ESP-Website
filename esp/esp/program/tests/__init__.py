c572ac8090e511fd0bc855555b151ccb93ab3ba8
sai udayagirii <saiudayagirii@sais-MacBook-Air.local>
2026-06-21 22:57:14 +0530

    updated file locations and added init.py

--- esp/esp/program/tests/__init__.py
+++ esp/esp/program/tests/__init__.py
@@ -1,1 +1,1 @@
-
+from esp.program.tests.test_program import ProgramFrameworkTest  # noqa: F401
---
+++ esp/esp/seltests/tests/test_csrf.py
@@ -0,0 +1,1 @@
+from esp.seltests.tests.test_selenium import CsrfTestCase  # Run Selenium tests with regular tests
