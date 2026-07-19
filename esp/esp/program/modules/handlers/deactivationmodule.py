84324aa2cd55b5b8e7435b5c1105ed8714e23002
hemant <nagar.hemant003@gmail.com>
2026-07-19 21:15:36 +0530

    fix deactivationmodule.py

--- esp/esp/program/modules/handlers/deactivationmodule.py
+++ esp/esp/program/modules/handlers/deactivationmodule.py
@@ -64,7 +64,10 @@ class DeactivationModule(ProgramModuleObj):
             raise ESPError()('You must confirm that you want to deactivate these users.')
 
         # get the filter to use and text message to send from the request; this is set in grouptextpanel form
-        filterObj = PersistentQueryFilter.objects.get(id=request.GET['filterid'])
+        try:
+            filterObj = PersistentQueryFilter.objects.get(id=request.GET['filterid'])
+        except (PersistentQueryFilter.DoesNotExist, ValueError):
+            raise ESPError()('The selected filter no longer exists or is invalid. Please restart the deactivation process.')
         users = filterObj.getList(ESPUser)
 
         if not users:
