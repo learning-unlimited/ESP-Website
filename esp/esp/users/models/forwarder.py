e3ad51bcc18c9b9e1513afc7ca1a54d70eb04204
Yashika Malhotra <malhotrayashika125@gmail.com>
2026-07-22 01:16:39 +0530

    Fix fields.W340 and W342 Django system-check warnings
    - Remove null=True from ClassSubject.allowable_class_size_ranges (ManyToManyField)
    - Change UserForwarder.source from ForeignKey(unique=True) to OneToOneField
    - Add corresponding migrations
    
    Fixes #5821

--- esp/esp/users/models/forwarder.py
+++ esp/esp/users/models/forwarder.py
@@ -12,7 +12,7 @@ class UserForwarder(models.Model):
     Links source user to target user, to make all login sessions under target.
 
     """
-    source = AjaxForeignKey(ESPUser, related_name='forwarders_out', unique=True, on_delete=models.CASCADE)
+    source = AjaxForeignKey(ESPUser, related_name='forwarders_out', on_delete=models.CASCADE)
     target = AjaxForeignKey(ESPUser, related_name='forwarders_in', on_delete=models.CASCADE)
 
     # Django tries to figure out the correct app label by going one level up.
