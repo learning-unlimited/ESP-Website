153772e5cce3416bfbb72b6ad26110a23134ce35
rafiya618 <rafiyam007@gmail.com>
2026-07-27 18:10:35 +0500

    fix spec to match the current toast-only behavior: no text log entries

--- esp/public/media/scripts/ajaxschedulingmodule/spec/MessagePanelSpec.js
+++ esp/public/media/scripts/ajaxschedulingmodule/spec/MessagePanelSpec.js
@@ -10,19 +10,28 @@ describe("MessagePanel", function() {
 
     it("should be able to be initialized with an initial message", function() {
         mp2 = new MessagePanel($j("<div>"), "Hello, World!");
-        expect(mp2.el[0].innerHTML).toEqual("<p>Hello, World!</p>");
+        expect(mp2.el[0].innerHTML).toEqual("");
     });
 
     describe("addMessage", function() {
-        it("should add a new message", function() {
-            mp.addMessage("new message");
-            expect(mp.el[0].innerHTML).toEqual("<p>new message</p>");
-        });
-
         it("should show error messages as a toast", function() {
             mp.addMessage("new error", "red");
             expect($j("body .scheduler-toast").length).toEqual(1);
             expect($j("body .scheduler-toast").text()).toEqual("new error");
+            expect(mp.el[0].innerHTML).toEqual("");
+        });
+
+        it("should show success messages as a green toast", function() {
+            mp.addMessage("new success", "green");
+            expect($j("body .scheduler-toast").length).toEqual(1);
+            expect($j("body .scheduler-toast").text()).toEqual("new success");
+            expect($j("body .scheduler-toast").hasClass("scheduler-toast-success")).toBeTrue;
+            expect(mp.el[0].innerHTML).toEqual("");
+        });
+
+        it("should not add a new text message", function() {
+            mp.addMessage("new message");
+            expect(mp.el[0].innerHTML).toEqual("");
         });
     });
 
