d623906bb53a3cf118821ea844efae2c4cc3c146
0xOval <anuragsingh7051@gmail.com>
2026-08-03 23:51:58 +0530

    Add unit tests for issue #5196
    Add unit tests for TeacherOnsite
    
    Closes #5196

---
+++ esp/esp/program/modules/tests/teacheronsite.py
@@ -0,0 +1,90 @@
+"""
+Unit tests for TeacherOnsite (teacheronsite.py).
+
+Refs: #5196
+"""
+from esp.program.modules.handlers.teacheronsite import TeacherOnsite
+from esp.program.modules.tests.support import ModuleHandlerTestMixin
+from esp.program.tests import ProgramFrameworkTest
+from esp.users.models import Record, RecordType
+
+
+class TeacherOnsiteTest(ModuleHandlerTestMixin, ProgramFrameworkTest):
+
+    def setUp(self, *args, **kwargs):
+        kwargs.update({
+            'num_students': 1,
+            'num_teachers': 2,
+            'num_admins': 1,
+            'num_timeslots': 3,
+            'num_rooms': 3,
+        })
+        super().setUp(*args, **kwargs)
+        self.add_user_profiles()
+        self.schedule_randomly()
+        self.module = self.get_module_obj('TeacherOnsite')
+        RecordType.objects.get_or_create(name='teacher_checked_in')
+
+    def test_teacheronsite_landing_page(self):
+        self.login_as('teacher')
+        response = self.assert_view_ok(self.get_module_url('teach', 'teacheronsite'))
+        self.assertEqual(response.context['webapp_page'], 'schedule')
+        self.assertIn('classes', response.context)
+        self.assertIn('checked_in', response.context)
+
+    def test_student_cannot_access_teacheronsite(self):
+        self.login_as('student')
+        response = self.client.get(self.get_module_url('teach', 'teacheronsite'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'errors/program/notateacher.html')
+
+    def test_onsitemap_page(self):
+        self.login_as('teacher')
+        response = self.assert_view_ok(self.get_module_url('teach', 'onsitemap'))
+        self.assertEqual(response.context['webapp_page'], 'map')
+
+    def test_onsitedetails_page(self):
+        self.login_as('teacher')
+        response = self.assert_view_ok(self.get_module_url('teach', 'onsitedetails'))
+        self.assertEqual(response.context['webapp_page'], 'details')
+        self.assertEqual(response.context['section_page'], 'info')
+        self.assertIn('sections', response.context)
+
+    def test_onsiteroster_page(self):
+        self.login_as('teacher')
+        response = self.assert_view_ok(self.get_module_url('teach', 'onsiteroster'))
+        self.assertEqual(response.context['webapp_page'], 'details')
+        self.assertEqual(response.context['section_page'], 'roster')
+
+    def test_onsitesurvey_page(self):
+        self.login_as('teacher')
+        response = self.assert_view_ok(self.get_module_url('teach', 'onsitesurvey'))
+        self.assertEqual(response.context['webapp_page'], 'survey')
+
+    def test_get_admin_search_entry_main_view(self):
+        entry = TeacherOnsite.get_admin_search_entry(
+            self.program, 'teach', 'teacheronsite', self.module
+        )
+        self.assertIsNotNone(entry)
+        self.assertEqual(entry.title, 'Teacher Onsite')
+        self.assertIn('teacher', entry.keywords)
+
+    def test_get_admin_search_entry_unknown_view(self):
+        entry = TeacherOnsite.get_admin_search_entry(
+            self.program, 'teach', 'not_a_real_view', self.module
+        )
+        self.assertIsNone(entry)
+
+    def test_get_admin_search_entry_roster(self):
+        entry = TeacherOnsite.get_admin_search_entry(
+            self.program, 'teach', 'onsiteroster', self.module
+        )
+        self.assertIsNotNone(entry)
+        self.assertIn('roster', entry.title.lower())
+
+    def test_onsitecontext_sets_program_and_user(self):
+        self.login_as('teacher')
+        # Hit landing page which uses onsitecontext
+        response = self.client.get(self.get_module_url('teach', 'teacheronsite'))
+        self.assertEqual(response.context['program'], self.program)
+        self.assertEqual(response.context['user'], self.teachers[0])
