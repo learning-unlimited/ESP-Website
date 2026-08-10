8e8f8fb0092d3cdf6554667934bd79700cb1b4b1
Supriyo Chowdhury <161750539+Supriyo760@users.noreply.github.com>
2026-08-11 02:56:54 +0530

    Fix lint whitespace errors in migration 0055

--- esp/esp/program/modules/migrations/0055_split_regprofilemodule.py
+++ esp/esp/program/modules/migrations/0055_split_regprofilemodule.py
@@ -2,31 +2,34 @@
 
 from django.db import migrations
 
+
 def split_regprofilemodule(apps, schema_editor):
     ProgramModule = apps.get_model('program', 'ProgramModule')
-    
+
     # Split learn modules
     ProgramModule.objects.filter(
-        handler='RegProfileModule', 
+        handler='RegProfileModule',
         module_type='learn'
     ).update(handler='StudentRegProfileModule')
-    
+
     # Split teach modules
     ProgramModule.objects.filter(
-        handler='RegProfileModule', 
+        handler='RegProfileModule',
         module_type='teach'
     ).update(handler='TeacherRegProfileModule')
 
+
 def reverse_split(apps, schema_editor):
     ProgramModule = apps.get_model('program', 'ProgramModule')
-    
+
     ProgramModule.objects.filter(
         handler='StudentRegProfileModule'
     ).update(handler='RegProfileModule')
-    
+
     ProgramModule.objects.filter(
         handler='TeacherRegProfileModule'
     ).update(handler='RegProfileModule')
+
 
 class Migration(migrations.Migration):
 
