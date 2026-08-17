d33755110ba989cfb84da9defc100b999ad15bb4
0xOval <anuragsingh7051@gmail.com>
2026-08-17 21:56:37 +0530

    Import SurveyManagementTest so Django test runner collects it

--- esp/esp/program/modules/tests/__init__.py
+++ esp/esp/program/modules/tests/__init__.py
@@ -95,3 +95,4 @@ from esp.program.modules.tests.studentregtwophase import StudentRegTwoPhaseTest
 from esp.program.modules.tests.test_onsiteattendance import TestTimesAttendingClass, TestTimesCheckedIn
 from esp.program.modules.tests.test_addclass_concurrency import AddClassConcurrencyRegressionTest
 from esp.program.modules.tests.test_usergroupmodule import UserGroupModuleTests
+from esp.program.modules.tests.surveymanagement import SurveyManagementTest
