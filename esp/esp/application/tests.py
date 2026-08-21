203b07ff5100c47a5c5b81914ccf413750f8bed1
Krishna Chaudhary <krishna5379chaudhary@gmail.com>
2026-08-21 22:12:28 +0530

    Remove unrelated formatting changes from tests

--- esp/esp/application/tests.py
+++ esp/esp/application/tests.py
@@ -2,9 +2,8 @@
 Tests for esp.application.models
 Source: esp/esp/application/models.py
 
-Tests StudentProgramApp and StudentClassApp models.
+Tests StudentProgramApp, StudentClassApp models and their status methods.
 """
-
 from django.contrib.auth.models import Group
 
 from esp.application.models import StudentClassApp, StudentProgramApp
@@ -15,14 +14,7 @@ from esp.users.models import ESPUser
 
 
 def _setup_roles():
-    for name in [
-        "Student",
-        "Teacher",
-        "Educator",
-        "Guardian",
-        "Volunteer",
-        "Administrator",
-    ]:
+    for name in ['Student', 'Teacher', 'Educator', 'Guardian', 'Volunteer', 'Administrator']:
         Group.objects.get_or_create(name=name)
 
 
@@ -30,79 +22,60 @@ class StudentProgramAppTest(TestCase):
     def setUp(self):
         super().setUp()
         _setup_roles()
-
-        self.program = Program.objects.create(
-            grade_min=7,
-            grade_max=12,
-        )
-
+        self.program = Program.objects.create(grade_min=7, grade_max=12)
         self.student = ESPUser.objects.create_user(
-            username="appstudent",
-            password="password",
+            username='appstudent', password='password',
         )
-
         self.app = StudentProgramApp.objects.create(
             user=self.student,
             program=self.program,
         )
 
     def test_str(self):
-        self.assertIn("appstudent", str(self.app))
+        result = str(self.app)
+        self.assertIn('appstudent', result)
 
     def test_default_status_unreviewed(self):
-        self.assertEqual(
-            self.app.admin_status,
-            StudentProgramApp.UNREVIEWED,
-        )
+        self.assertEqual(self.app.admin_status, StudentProgramApp.UNREVIEWED)
 
     def test_choices_empty(self):
-        self.assertEqual(self.app.choices(), {})
+        choices = self.app.choices()
+        self.assertEqual(len(choices), 0)
 
     def test_admitted_to_class_none(self):
         self.assertIsNone(self.app.admitted_to_class())
 
     def test_waitlisted_to_class_empty(self):
-        self.assertEqual(self.app.waitlisted_to_class(), [])
+        result = self.app.waitlisted_to_class()
+        self.assertEqual(len(result), 0)
 
 
 class StudentClassAppTest(TestCase):
     def setUp(self):
         super().setUp()
         _setup_roles()
-
-        self.program = Program.objects.create(
-            grade_min=7,
-            grade_max=12,
-        )
-
+        self.program = Program.objects.create(grade_min=7, grade_max=12)
         self.student = ESPUser.objects.create_user(
-            username="classstudent",
-            password="password",
+            username='classstudent', password='password',
         )
-
         self.teacher = ESPUser.objects.create_user(
-            username="classteacher",
-            password="password",
+            username='classteacher', password='password',
         )
-
         self.category = ClassCategories.objects.create(
-            category="Test Category",
-            symbol="T",
+            category='Test Category',
+            symbol='T',
             seq=0,
         )
-
         self.subject = ClassSubject.objects.create(
             parent_program=self.program,
             category=self.category,
             grade_min=7,
             grade_max=12,
         )
-
         self.program_app = StudentProgramApp.objects.create(
             user=self.student,
             program=self.program,
         )
-
         self.class_app = StudentClassApp.objects.create(
             app=self.program_app,
             subject=self.subject,
@@ -111,42 +84,27 @@ class StudentClassAppTest(TestCase):
 
     def test_str(self):
         result = str(self.class_app)
-        self.assertIn("classstudent", result)
-        self.assertIn("app for", result)
+        self.assertIn('classstudent', result)
+        self.assertIn('app for', result)
 
     def test_default_status(self):
-        self.assertEqual(
-            self.class_app.admission_status,
-            StudentClassApp.UNASSIGNED,
-        )
+        self.assertEqual(self.class_app.admission_status, StudentClassApp.UNASSIGNED)
 
     def test_admit(self):
         self.class_app.admit()
         self.class_app.refresh_from_db()
-
-        self.assertEqual(
-            self.class_app.admission_status,
-            StudentClassApp.ADMITTED,
-        )
+        self.assertEqual(self.class_app.admission_status, StudentClassApp.ADMITTED)
 
     def test_unadmit(self):
         self.class_app.admit()
         self.class_app.unadmit()
         self.class_app.refresh_from_db()
-
-        self.assertEqual(
-            self.class_app.admission_status,
-            StudentClassApp.UNASSIGNED,
-        )
+        self.assertEqual(self.class_app.admission_status, StudentClassApp.UNASSIGNED)
 
     def test_waitlist(self):
         self.class_app.waitlist()
         self.class_app.refresh_from_db()
-
-        self.assertEqual(
-            self.class_app.admission_status,
-            StudentClassApp.WAITLIST,
-        )
+        self.assertEqual(self.class_app.admission_status, StudentClassApp.WAITLIST)
 
     def test_admit_idempotency(self):
         self.class_app.admit()
