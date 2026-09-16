cad53003c880881a67aac193c3d62f0a0af1881d
William Gearty <willgearty@gmail.com>
2026-09-16 17:29:55 -0400

    Refactor; cleanup; tests; expand filtered categories and hide empty categories

--- esp/public/media/scripts/utils/search_filter.js
+++ esp/public/media/scripts/utils/search_filter.js
@@ -1,14 +1,21 @@
 /**
  * Reusable search filter utility for ESP Website.
  * filters elements in a listContainer based on the value of a search input.
+ *
+ * Elements may carry a data-search-text attribute to be matched on something
+ * other than their rendered text (e.g. an identifier that is not displayed).
+ *
+ * options (optional):
+ *   onFilter: function(query, $matched, $elements), run after each filter pass.
  */
 
-window.initSearchFilter = function(inputId, listContainerId, elementSelector) {
+window.initSearchFilter = function(inputId, listContainerId, elementSelector, options) {
     var $j = window.jQuery || window.$j;
     if (!$j) {
         console.error("jQuery not found for search filter");
         return;
     }
+    options = options || {};
 
     $j(document).ready(function() {
         var $input = $j("#" + inputId);
@@ -18,14 +25,20 @@ window.initSearchFilter = function(inputId, listContainerId, elementSelector) {
             return;
         }
 
-        $input.on("input", _.debounce(function() {
-            var query = $input.val().toLowerCase();
+        function searchText($el) {
+            var text = $el.attr("data-search-text");
+            return (text === undefined ? $el.text() : text).toLowerCase();
+        }
+
+        function filter() {
+            var query = ($input.val() || "").trim().toLowerCase();
             var $elements = $container.find(elementSelector);
+            var matched = [];
 
             $elements.each(function() {
                 var $el = $j(this);
-                var text = $el.text().toLowerCase();
-                if (text.indexOf(query) > -1) {
+                if (searchText($el).indexOf(query) > -1) {
+                    matched.push(this);
                     $el.show();
                     // If it's a list item, we might want to ensure the parent (like a fieldset row) is visible
                     $el.closest("tr").show();
@@ -35,6 +48,13 @@ window.initSearchFilter = function(inputId, listContainerId, elementSelector) {
                     // But for simple lists, just hiding the element is fine.
                 }
             });
-        }, 200));
+
+            if (options.onFilter) {
+                options.onFilter(query, $j(matched), $elements);
+            }
+        }
+
+        // "input" rather than "keyup" so paste, autofill, and IME input also filter.
+        $input.on("input", _.debounce(filter, 200));
     });
 };
